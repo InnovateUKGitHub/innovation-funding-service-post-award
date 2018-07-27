7d3de5dce81cc9aba3abf466c88f7bf564f81fb7
vladimir.pavlichenko <vladimirs.pavlicenko@bjss.com>
2018-07-27 13:41:20 +0100

    modified unit tests to use Jest

--- app/tests/src/server/features/contacts/getAllQuery.tests.ts
+++ app/tests/src/server/features/contacts/getAllQuery.tests.ts
@@ -1,19 +1,37 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
+// import { GetAllQuery } from '../../../../../src/server/features/contacts/getAllQuery';
+// import {TestContext} from '../../testContextProvider';
+
+// describe('getAllQuery', () => {
+//     it('when valid then returns all', async () => {
+//         let context = new TestContext();
+//         let testData = context.testData;
+
+//         let data = testData.range(10, i => testData.createContact());
+
+//         let query = new GetAllQuery();
+        
+//         let result = await context.runQuery(query);
+
+//         expect(result).to.not.be.null;
+//         expect(result.map(x => x.id)).to.eql(data.map(x => x.Id));
+//         expect(result.map(x => x.id)).to.be.equal(data.map(x => x.Id))
+//     })
+// });
+
 import { GetAllQuery } from '../../../../../src/server/features/contacts/getAllQuery';
 import {TestContext} from '../../testContextProvider';
 
-describe('getAllQuery', () => {
-    it('when valid then returns all', async () => {
-        let context = new TestContext();
-        let testData = context.testData;
+test('when valid then returns all', async () => {
+    let context = new TestContext();
+    let testData = context.testData;
 
-        let data = testData.range(10, i => testData.createContact());
+    let data = testData.range(10, i => testData.createContact());
 
-        let query = new GetAllQuery();
-        
-        let result = await context.runQuery(query);
+    let query = new GetAllQuery();
+    
+    let result = await context.runQuery(query);
 
-        expect(result).to.not.be.null;
-        expect(result.map(x => x.id)).to.eql(data.map(x => x.Id));
-    })
-});
+    expect(result).not.toBe(null)
+    expect(result.map(x => x.id)).toMatchObject(data.map(x => x.Id));
+})
--- app/tests/src/server/features/contacts/getByIdQuery.tests.ts
+++ app/tests/src/server/features/contacts/getByIdQuery.tests.ts
@@ -1,4 +1,27 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
+// import { GetByIdQuery } from '../../../../../src/server/features/contacts/getByIdQuery';
+// import {TestContext} from '../../testContextProvider';
+
+// describe('getByIdQuery', () => {
+//     it('when valid id then correct object', async () => {
+//         let context = new TestContext();
+//         let testData = context.testData;
+
+//         let data = testData.range(10, i => testData.createContact());
+//         let expected = data[5];
+
+//         let query = new GetByIdQuery(expected.Id);
+        
+//         let result = await context.runQuery(query);
+
+//         expect(result).to.not.be.null;
+//         expect(result.id).to.equal(expected.Id);
+//         expect(result.firstName).to.equal(expected.FirstName);
+//         expect(result.lastName).to.equal(expected.LastName);
+//         expect(result.lastName).to.be.equal(expected.LastName)
+//     })
+// });
+
 import { GetByIdQuery } from '../../../../../src/server/features/contacts/getByIdQuery';
 import {TestContext} from '../../testContextProvider';
 
@@ -14,9 +37,9 @@ describe('getByIdQuery', () => {
         
         let result = await context.runQuery(query);
 
-        expect(result).to.not.be.null;
-        expect(result.id).to.equal(expected.Id);
-        expect(result.firstName).to.equal(expected.FirstName);
-        expect(result.lastName).to.equal(expected.LastName);
+        expect(result).not.toBe(null);
+        expect(result.id).toBe(expected.Id);
+        expect(result.firstName).toBe(expected.FirstName);
+        expect(result.lastName).toBe(expected.LastName);
     })
 });
