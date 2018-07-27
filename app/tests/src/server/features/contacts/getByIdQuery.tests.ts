552e2b3a8071831b784f3f7108ed5708eb54bdf2
vladimir.pavlichenko <vladimirs.pavlicenko@bjss.com>
2018-07-27 15:49:45 +0100

    removed mocha and chai

--- app/tests/src/server/features/contacts/getByIdQuery.tests.ts
+++ app/tests/src/server/features/contacts/getByIdQuery.tests.ts
@@ -1,27 +1,3 @@
-// import { expect } from 'chai';
-// import { GetByIdQuery } from '../../../../../src/server/features/contacts/getByIdQuery';
-// import {TestContext} from '../../testContextProvider';
-
-// describe('getByIdQuery', () => {
-//     it('when valid id then correct object', async () => {
-//         let context = new TestContext();
-//         let testData = context.testData;
-
-//         let data = testData.range(10, i => testData.createContact());
-//         let expected = data[5];
-
-//         let query = new GetByIdQuery(expected.Id);
-        
-//         let result = await context.runQuery(query);
-
-//         expect(result).to.not.be.null;
-//         expect(result.id).to.equal(expected.Id);
-//         expect(result.firstName).to.equal(expected.FirstName);
-//         expect(result.lastName).to.equal(expected.LastName);
-//         expect(result.lastName).to.be.equal(expected.LastName)
-//     })
-// });
-
 import { GetByIdQuery } from '../../../../../src/server/features/contacts/getByIdQuery';
 import {TestContext} from '../../testContextProvider';
 
