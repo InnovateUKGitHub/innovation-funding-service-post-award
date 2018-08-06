c8bb444a3be733367be515ba3148921289f4040e
vladimir.pavlichenko <vladimirs.pavlicenko@bjss.com>
2018-08-06 11:47:39 +0100

    added tslint html report script

---
+++ app/tslint-report.js
@@ -0,0 +1,12 @@
+const tslintHtmlReport = require('tslint-html-report');
+
+tslintHtmlReport({
+    tslint: 'tslint.json', // path to tslint.json
+    srcFiles: 'src/**/*.ts{,x}', // files to lint
+    outDir: 'reports/tslint-html-report', // output folder to write the report to
+    html: 'tslint-report.html', // name of the html report generated
+    exclude: [''], // Files/patterns to exclude
+    breakOnError: false, // Should it throw an error in tslint errors are found
+    typeCheck: true, // enable type checking. requires tsconfig.json
+    tsconfig: 'tsconfig.json' // path to tsconfig.json
+});
