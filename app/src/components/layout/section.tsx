e90dc8a5cd1ed0fa9c4002ef5bafb8bd751f93e7
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-08-20 19:02:54 +0100

    Adds section around backlink

--- app/src/components/layout/section.tsx
+++ app/src/components/layout/section.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
 export interface Props {
-    title: React.ReactNode;
+    title?: React.ReactNode;
 }
 
 export const Section: React.SFC<Props> = (props) => {
     return (
         <div className="govuk-!-margin-bottom-9">
-            <h2 className="govuk-heading-m govuk-!-margin-bottom-9">{props.title}</h2>
+            {props.title ? <h2 className="govuk-heading-m govuk-!-margin-bottom-9">{props.title}</h2> : null}
             {props.children}
         </div>
     );
