c8b3d6f2aeee3f0f57c8a10b24fc74321255fee9
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-08-20 17:16:12 +0100

    Fix bug when navigating

--- app/src/redux/reducers/common.ts
+++ app/src/redux/reducers/common.ts
@@ -25,19 +25,19 @@ export const dataStoreReducer = <TData extends {}, TKey>(key: (key: TKey) => str
     result[action.payload.id] = pending;
     return result;
   }
-  if ((action as any).type == "@@router5/TRANSITION_START") {
-    let result = Object.assign({}, state);
-    Object.keys(result).forEach(store => Object.keys(result[store]).forEach(key => {
-      let pending = result[key];
+
+  if ((action as any).type === "@@router5/TRANSITION_START") {
+    const result = Object.assign({}, state);
+    Object.keys(result).forEach(itemKey => {
+      const pending = result[itemKey];
       if (pending.status === "LOADED" || pending.status === "ERROR") {
-        console.log("making stores stale", store, key, pending);
-        result[key] = { status: "STALE", data: pending.data, error: pending.error };
+        result[itemKey] = { status: "STALE", data: pending.data, error: pending.error };
       }
-    }));
+    });
     return result;
   }
   return state;
-}
+};
 
 export const dataReducer = combineReducers({
   contacts: dataStoreReducer<Dtos.IContact[], string>(x => x, "contacts"),
@@ -46,16 +46,3 @@ export const dataReducer = combineReducers({
   project: dataStoreReducer<Dtos.ProjectDto, string>(x => x, "project"),
   projectContacts: dataStoreReducer<Dtos.ProjectContactDto[], string>(x => x, "projectContacts")
 });
-
-  // if(action.type =="@@router5/TRANSITION_START") {
-  //     let result = Object.assign({}, state);
-  //     Object.keys(result).forEach(store => Object.keys(result[store]).forEach(key => {
-  //         let pending = result[store][key];
-  //         if(pending.status && pending.status !== "STALE"){
-  //             console.log("making stores stale", store, key, pending);
-  //             result[store][key] = { status: "STALE", data: pending.data, error: pending.error};
-  //         }
-  //     }))
-  //     console.log("making stores stale", state, result);
-  //     return result;
-  // }
