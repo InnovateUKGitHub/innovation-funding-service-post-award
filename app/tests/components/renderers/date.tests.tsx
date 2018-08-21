006b3c396b61fb184b9e7ad1a3a327288e26b6ce
JHutchBJSS <joe.hutchinson@bjss.com>
2018-08-21 15:29:23 +0100

    Addressed PR comment

--- app/tests/components/renderers/date.tests.tsx
+++ app/tests/components/renderers/date.tests.tsx
@@ -7,22 +7,20 @@ import Enzyme, { shallow } from "enzyme";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Date", () => {
-    let wrapper;
-
     it("should render the date as 7th January 1992", () => {
         const date = new Date("1992/01/07");
-        wrapper = shallow(<ACCDate.FullDate value={date} />);
+        const wrapper = shallow(<ACCDate.FullDate value={date} />);
         const expectedDate = <span>7 January 1992</span>;
         expect(wrapper.equals(expectedDate));
     });
 
     it("should return null when a null prop is passed in", () => {
-        wrapper = shallow(<ACCDate.FullDate value={null} />);
+        const wrapper = shallow(<ACCDate.FullDate value={null} />);
         expect(wrapper.equals(null));
     });
 
     it("should return null when an invalid string is passed in", () => {
-        wrapper = shallow(<ACCDate.FullDate value="someString" />);
+        const wrapper = shallow(<ACCDate.FullDate value="someString" />);
         expect(wrapper.equals(null));
     });
 });
