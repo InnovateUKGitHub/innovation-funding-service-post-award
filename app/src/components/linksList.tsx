2f5d601cf6703f95c62f878000f5355024eafc37
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-08-28 10:21:49 +0100

    tidy up link list props mapping

--- app/src/components/linksList.tsx
+++ app/src/components/linksList.tsx
@@ -15,7 +15,7 @@ export const LinksList: React.SFC<Props> = (props) => {
     return (
         <React.Fragment>
             {
-                !Array.isArray(links) ? null : links.filter(x => !!x.url).map(x => (Object.assign({...x}, {url: x.url!}))).map((x, i) => (
+                !Array.isArray(links) ? null : links.filter(x => !!x.url).map(x => ({text: x.text, url: x.url!})).map((x, i) => (
                     <div className="govuk-!-padding-bottom-4" key={`link-${i}`}>
                         <a href={x.url} className="govuk-link govuk-!-font-size-19">{x.text}</a>
                     </div>
