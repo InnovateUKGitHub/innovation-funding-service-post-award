e459cbc9e424d036028b2d36cd25ed5352d1ed7d
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-09-06 17:23:34 +0100

    Adds two column details view

---
+++ app/src/componentsGuide/detailsGuide.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Details } from "../components/details";
+import { render } from "enzyme";
+
+export const detailsGuide: IGuide = {
+    name: "Details",
+    options:[
+        {
+            name: "Simple",
+            comments: "A simple way of laying out details of an item",
+            example: (
+                "const ItemDetails = Details.forData(data);\n"+
+                "return (\n"+
+                "    <ItemDetails.Details>\n"+
+                "        <ItemDetails.String label=\"Id\" value={x => x.id}/>\n"+
+                "        <ItemDetails.String label=\"Name\" value={x => x.name}/>\n"+
+                "        <ItemDetails.Date label=\"Started\" value={x => x.created}/>\n"+
+                "    </ItemDetails.Details>\n"+
+                ");"
+            ),
+            render: () => {
+                const ItemDetails = Details.forData({ id: "Example 1", name: "The Example", created: new Date() });
+                return (
+                    <ItemDetails.Details>
+                        <ItemDetails.String label="Id" value={x => x.id}/>
+                        <ItemDetails.String label="Name" value={x => x.name}/>
+                        <ItemDetails.DateTime label="Started" value={x => x.created}/>
+                    </ItemDetails.Details>
+                );
+            }
+        },
+        {
+            name: "Double layout",
+            comments: "Laying out two columns of details",
+            example: (
+                "const ItemDetails = Details.forData(data);\n"+
+                "return (\n"+
+                "    <ItemDetails.Details>\n"+
+                "        <ItemDetails.String label=\"Id\" value={x => x.id}/>\n"+
+                "        <ItemDetails.String label=\"Name\" value={x => x.name}/>\n"+
+                "        <ItemDetails.Date label=\"Started\" value={x => x.created}/>\n"+
+                "    </ItemDetails.Details>\n"+
+                ");"
+            ),
+            render: () => {
+                const ItemDetails = Details.forData({ id: "Example 1", name: "The Example", created: new Date() });
+                return (
+                    <ItemDetails.Details layout="Double">
+                        <ItemDetails.String label="Id" value={x => x.id}/>
+                        <ItemDetails.String label="Name" value={x => x.name}/>
+                        <ItemDetails.DateTime label="Started" value={x => x.created}/>
+                    </ItemDetails.Details>
+                );
+            }
+        }
+    ]
+};
--- app/src/ui/components/details.tsx
+++ app/src/ui/components/details.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import { FullDate } from "./renderers/date";
+import * as Renderers from "./renderers";
+import { copyFileSync } from "fs";
 
-interface DetailsProps {}
+interface DetailsProps {
+    layout?: "Single" | "Double";
+}
 
 interface ItemProps<T, TField> {
     label: React.ReactNode;
@@ -9,16 +12,59 @@ interface ItemProps<T, TField> {
 }
 
 const DetailsComponent = <T extends {}>(data: T): React.SFC<DetailsProps> => (props) => {
+    let noCols = props.layout === "Double" ? 2 : 1;
+
+    let cols: React.ReactNode[][] = props.layout === "Double" ? [[], []] : [[]];
+
+    //distribute children accross array adding props
+    React.Children.toArray(props.children).forEach((field, index) => {
+        let newProps = {
+            data: data,
+            labelClass: "govuk-grid-column-one-quarter",
+            valueClass: noCols === 2 ? "govuk-grid-column-one-quarter" : "govuk-grid-column-three-quarters",
+        }
+        let cloned = React.cloneElement(field as React.ReactElement<any>, newProps);
+        cols[index % noCols].push(cloned);
+    });
+
     return (
         <React.Fragment>
             {
-                React.Children.map(props.children, (x,i) =>
+                cols.map((x, i) =>
                     <div className="govuk-grid-row govuk-!-margin-top-4" key={`details-row-${i}`}>{x}</div>
                 )
             }
         </React.Fragment>
     );
 };
+
+interface InternalFieldProps<T> {
+    label: React.ReactNode;
+    render: (item: T) => React.ReactNode;
+    labelClass?: string;
+    valueClass?: string;
+    data?: T;
+}
+
+interface ExternalFieldProps<T, TValue> {
+    label: React.ReactNode;
+    value: (item: T) => TValue;
+}
+
+class FieldComponent<T> extends React.Component<InternalFieldProps<T>, {}> {
+    render() {
+        return (
+            <React.Fragment>
+                <div className={this.props.labelClass}>
+                    <h4 className="govuk-heading-s">{this.props.label}</h4>
+                </div>
+                <div className={this.props.valueClass}>
+                    {this.props.render(this.props.data!)}
+                </div>
+            </React.Fragment>
+        );
+    }
+}
 
 const renderField = <T extends {}>(label: React.ReactNode, value: T, render: (item: T) => React.ReactNode) => {
     return (
@@ -33,23 +79,41 @@ const renderField = <T extends {}>(label: React.ReactNode, value: T, render: (it
     );
 };
 
-const DetailsString = <T extends {}>(data: T): React.SFC<ItemProps<T, string>> => (props) => {
-    return renderField(props.label, props.value(data), (value) => <p className="govuk-body">{value}</p>);
+const CustomField = <T extends {}>(): React.SFC<ExternalFieldProps<T, React.ReactNode>> => {
+    const TypedField = FieldComponent as { new(): FieldComponent<T> }
+    return (props) =>  <TypedField {...props} render={(item) => props.value(item)}/>;
 };
 
-const DetailsMultilineString = <T extends {}>(data: T): React.SFC<ItemProps<T, string>> => (props) => {
-    return renderField(props.label, props.value(data), (value) => (value || "").split("\n").filter(x => !!x).map((line, i) => <p className="govuk-body" key={`multiline-string-${i}`}>{line}</p>));
+const StringField = <T extends {}>(): React.SFC<ExternalFieldProps<T, string>> => {
+    const TypedField = FieldComponent as { new(): FieldComponent<T> }
+    return (props) =>  <TypedField {...props} render={(item) => <p className="govuk-body">{props.value(item)}</p>}/>;
 };
 
-const DetailsDate = <T extends {}>(data: T): React.SFC<ItemProps<T, Date>> => (props) => {
-    return renderField(props.label, props.value(data), (value) => <p className="govuk-body"><FullDate value={value}/> </p>);
+const MultilineStringField = <T extends {}>(): React.SFC<ExternalFieldProps<T, string>> => {
+    const TypedField = FieldComponent as { new(): FieldComponent<T> }
+    const splitString = (v: string) => {
+        return (v||"").split("\n").filter(x => !!x);
+    }
+    return (props) =>  <TypedField {...props} render={(item) => splitString(props.value(item)).map((line, index) => <p className="govuk-body" key={`multiline-string-${index}`}>{line}</p>)}/>;
+};
+
+const DateField = <T extends {}>(): React.SFC<ExternalFieldProps<T, Date>> => {
+    const TypedField = FieldComponent as { new(): FieldComponent<T> }
+    return (props) => <TypedField {...props} render={(item) => <p className="govuk-body"><Renderers.FullDate value={props.value(item)} /> </p>}/>;
+};
+
+const DateTimeField = <T extends {}>(): React.SFC<ExternalFieldProps<T, Date>> =>  {
+    const TypedField = FieldComponent as { new(): FieldComponent<T> }
+    return (props) => <TypedField {...props} render={(item) => <p className="govuk-body"><Renderers.FullDateTime value={props.value(item)} /> </p>}/>;
 };
 
 export const Details = {
     forData: <T extends {}>(data: T) => ({
         Details: DetailsComponent(data),
-        String: DetailsString(data),
-        MulilineString: DetailsMultilineString(data),
-        Date: DetailsDate(data),
+        String: StringField<T>(),
+        MulilineString: MultilineStringField<T>(),
+        Date: DateField<T>(),
+        DateTime: DateTimeField<T>(),
+        Custom: CustomField<T>(),
     })
 };
