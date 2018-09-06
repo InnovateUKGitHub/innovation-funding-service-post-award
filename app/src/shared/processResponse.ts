b09158f3496a8aef9d9e6d87d3d7a09bc0686da3
Curtis.Harding <Curtis.Harding@bjss.com>
2018-09-06 15:04:24 +0100

    fix for date regex matching any string containing a match

--- app/src/shared/processResponse.ts
+++ app/src/shared/processResponse.ts
@@ -1,4 +1,4 @@
-const dateRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
+const dateRegex = /^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)$/;
 
 export const processResponse = (response: Response) => response.json().then(processDto);
 
