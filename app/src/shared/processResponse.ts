11167f8c4f5ae0ece272d931976cc4cf1f479f77
Curtis.Harding <Curtis.Harding@bjss.com>
2018-09-06 13:05:21 +0100

    consolidating LoadingStatus & DataStoreStatus, wrapping ajax functions and fixing ajax error handling

---
+++ app/src/shared/processResponse.ts
@@ -0,0 +1,25 @@
+const dateRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
+
+export const processResponse = (response: Response) => response.json().then(processDto);
+
+export const processDto: any = (data: any) => {
+  if(Array.isArray(data)) {
+    return data.map(x => processDto(x));
+  }
+
+  if(data && data.constructor.prototype === Object.prototype) {
+    const newObj: {[key: string]: any} = {};
+
+    Object.keys(data).forEach(key => {
+      newObj[key] = processDto(data[key]);
+    });
+
+    return newObj;
+  }
+
+  if (typeof(data) === "string" && dateRegex.test(data)) {
+    return new Date(data);
+  }
+
+  return data;
+};
