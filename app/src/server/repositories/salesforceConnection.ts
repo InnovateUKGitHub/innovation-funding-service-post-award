9dbb08f66848f6238cbb6d02a8a1abea2ef02cb4
JHutchBJSS <joe.hutchinson@bjss.com>
2018-09-28 12:05:48 +0100

    Putting back salesforce connection

--- app/src/server/repositories/salesforceConnection.ts
+++ app/src/server/repositories/salesforceConnection.ts
@@ -3,14 +3,13 @@ import jsforce from "jsforce";
 // This will need revisting once SSO with Salesforce has been resolved
 
 // user we have got working for now
-// const username = "etlsalesforce@innovateuk.gov.uk.bjsspoc2";
-// const password = "pr0dETL2016";
-// const token    = "OWkKR7QYdHLgqz1s5HheCe86r";
-
+// let username = "oliver.armfelt@bjss.com.bjsspoc";
+// let password = "BjssBristol1";
+// let token    = "Wc2j67MQY6x0cXxYDKO2kddD";
 // user given to us but auth token required
 // const username2 = "bjss.integration@innovateuk.gov.uk.bjss";
 // const password2 = "86S@lhJVSXRtsX81";
-//
+
 const username = "etlsalesforce@innovateuk.gov.uk.bjsspoc";
 const password = "pr0dETL2016";
 const token = "OWkKR7QYdHLgqz1s5HheCe86r";
