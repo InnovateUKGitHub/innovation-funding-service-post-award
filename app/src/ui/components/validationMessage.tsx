682d440d7464385f121c3b56e4342cf9e3b49c1c
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2018-10-01 14:37:26 +0100

    changed to red

--- app/src/ui/components/validationMessage.tsx
+++ app/src/ui/components/validationMessage.tsx
@@ -11,19 +11,24 @@ export const ValidationMessage: React.SFC<Props> = ({ message, key }) => {
         return null;
     }
 
+    const warningColour = "#b10e1e";
     const backgroundStyle = {
         "padding": "2% 4% 2% 1%",
         "background": "rgb(248, 248, 248, 1)",
-        "border-left": "5px solid black",
+        "border-left": `5px solid ${warningColour}`,
     };
     const textStyle = {
         margin: 0,
+    };
+    const iconStyle = {
+        background: warningColour,
+        border: warningColour,
     };
 
     return (
         <div className="govuk-warning-text-background" style={backgroundStyle}>
             <div className="govuk-warning-text" key={key} style={textStyle}>
-                <span className="govuk-warning-text__icon" aria-hidden="true" >!</span>
+                <span className="govuk-warning-text__icon" aria-hidden="true" style={iconStyle} >!</span>
                 <strong className="govuk-warning-text__text">
                     <span className="govuk-warning-text__assistive">Warning</span> {message.errorMessage}
                 </strong>
