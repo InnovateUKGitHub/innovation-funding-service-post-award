a3d048ee3fc3a8b03eee5fe95c14e1992798ad67
Curtis.Harding <Curtis.Harding@bjss.com>
2018-10-15 14:26:34 +0100

    removing faker from forecastDetails api and updating tests

--- app/src/server/repositories/profileDetailsRepository.ts
+++ app/src/server/repositories/profileDetailsRepository.ts
@@ -33,26 +33,12 @@ export class ProfileDetailsRepository extends SalesforceBase<ISalesforceProfileD
   }
 
   public async getAllByPartnerWithPeriodGt(partnerId: string, periodId: number): Promise<ISalesforceProfileDetails[]> {
-    // const filter = `Acc_ProjectParticipant__c = '${partnerId}' AND RecordType.Name = '${this.recordType}' AND Acc_ProjectPeriodNumber__c >= ${periodId}`;
-    // return await super.whereString(filter);
-
-    // TODO - remove faker
-    return this.createFake(partnerId, periodId);
+    const filter = `Acc_ProjectParticipant__c = '${partnerId}' AND RecordType.Name = '${this.recordType}' AND Acc_ProjectPeriodNumber__c >= ${periodId}`;
+    return await super.whereString(filter);
   }
 
   public async getById(partnerId: string, periodId: number, costCategoryId: string): Promise<ISalesforceProfileDetails> {
     const filter = `Acc_ProjectParticipant__c = '${partnerId}' AND RecordType.Name = '${this.recordType}' AND Acc_ProjectPeriodNumber__c >= ${periodId} AND Acc_CostCategory__c = '${costCategoryId}'`;
     return await super.whereString(filter).then((res) => res && res[0] || null);
-  }
-
-  private createFake(partnerId: string, periodId: number): ISalesforceProfileDetails[] {
-    return [{
-      Acc_CostCategory__c: "a071X000000HHZtQAO",
-      Acc_LatestForecastCost__c: 10000,
-      Acc_ProjectParticipant__c: partnerId,
-      Acc_ProjectPeriodNumber__c: periodId,
-      Acc_ProjectPeriodStartDate__c: "2018-01-01",
-      Acc_ProjectPeriodEndDate__c: "2018-03-30",
-    }];
   }
 }
