ea3cfa0f668b5310de236ecb0c652323c7523e15
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-10-26 17:05:29 +0100

    Add ui tests for review claims page

--- app/src/ui/containers/claims/review.tsx
+++ app/src/ui/containers/claims/review.tsx
@@ -86,7 +86,7 @@ class ReviewComponent extends ContainerBase<Params, Data, Callbacks> {
           {/* TODO: Fix error display */}
           {data.editor.error ? <ACC.ValidationMessage messageType="error" message={data.editor.error.details || data.editor.error} /> : null}
           <ACC.Claims.ClaimTable {...data} validation={data.editor.validator.claimDetails.results} getLink={costCategoryId => ReviewClaimLineItemsRoute.getLink({ partnerId: this.props.partnerId, projectId: this.props.projectId, periodId: this.props.periodId, costCategoryId })} />
-          <Form.Form data={data.editor.data} onSubmit={() => this.props.onSave(this.props.projectId, this.props.partnerId, this.props.periodId, data.editor.data, data.claimDetails, data.costCategories)} onChange={(dto) => this.props.onChange(this.props.partnerId, this.props.periodId, dto, data.claimDetails, data.costCategories)}>
+          <Form.Form qa="review-form" data={data.editor.data} onSubmit={() => this.props.onSave(this.props.projectId, this.props.partnerId, this.props.periodId, data.editor.data, data.claimDetails, data.costCategories)} onChange={(dto) => this.props.onChange(this.props.partnerId, this.props.periodId, dto, data.claimDetails, data.costCategories)}>
             <Form.Fieldset heading={() => "How do you want to proceed with this claim?"}>
               <Form.Radio label="" name="status" options={options} value={(dto) => options.find(x => x.id === dto.status)} update={(dto, val) => this.updateStatus(dto, val)} validation={data.editor.validator.status}/>
               {showButton ? <Form.Submit>{data.editor.data.status === "MO Queried" ? "Send query" : "Submit"}</Form.Submit> : null}
