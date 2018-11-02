d5c03310c2d67cbca6af0a7001b90e81fed13a35
vladimir.pavlichenko <vladimirs.pavlicenko@bjss.com>
2018-11-02 13:58:38 +0000

    ACC-1063 added success message component

--- app/src/ui/componentsGuide/validationMessageGuide.tsx
+++ app/src/ui/componentsGuide/validationMessageGuide.tsx
@@ -15,6 +15,12 @@ export const validationMessageGuide: IGuide = {
         comments: "Renders an error message and a caption if present",
         example: "<ValidationMessage message={\"If you are unsure what evidence to provide, speak to your Monitoring Officer. They will use these documents when reviewing your claim.\"} messageType={\"error\"}/>",
         render: () => <ValidationMessage message={"You can be fined up to £5,000 if you don’t register."} messageType={"error"}/>
+      },
+      {
+        name: "Success",
+        comments: "Renders an success message and a caption if present",
+        example: "<ValidationMessage message={\"Your file has been uploaded.\"} messageType={\"success\"}/>",
+        render: () => <ValidationMessage message={"Your file has been uploaded."} messageType={"success"} />
       }
     ]
 };
