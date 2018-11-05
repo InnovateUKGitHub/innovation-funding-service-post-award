b8f2156c13aa979c2da4876d9b46594751c5803b
Seb Zapata <sebastian.zapata@bjss.com>
2018-11-05 18:54:48 +0000

    no longer in bold

--- app/src/ui/containers/claims/editClaimLineItems.tsx
+++ app/src/ui/containers/claims/editClaimLineItems.tsx
@@ -77,7 +77,7 @@ export class EditClaimLineItemsComponent extends ContainerBase<Params, Data, Cal
         </ACC.Section>
         <ACC.Section title="Supporting documents" subtitle={documents.length > 0 ? "(Documents open in a new window)" : ""}>
           <ValidationMessage message={"If you are unsure what evidence to provide, speak to your Monitoring Officer. They will use these documents when reviewing your claim."} messageType={"info"}/>
-          {documents.length > 0 ? <DocumentList documents={documents} qa="supporting-documents" /> : <h2 className="govuk-heading-s govuk-!-margin-bottom-0 govuk-!-margin-right-2">No documents attached</h2> }
+          {documents.length > 0 ? <DocumentList documents={documents} qa="supporting-documents" /> : <p className="govuk-body-m govuk-!-margin-bottom-0 govuk-!-margin-right-2">No documents attached</p> }
         </ACC.Section>
       </ACC.Page>
     );
