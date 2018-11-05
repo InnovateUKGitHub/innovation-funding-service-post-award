87fd0a062a4c02ba9584be5b8b974ea331df7dfe
Seb Zapata <sebastian.zapata@bjss.com>
2018-11-05 14:25:48 +0000

    new test added

---
+++ app/tests/server/features/documents/uploadClaimDetailDocument.tests.ts
@@ -0,0 +1,27 @@
+import {TestContext} from "../../testContextProvider";
+import {UploadClaimDetailDocumentCommand} from "../../../../src/server/features/documents/uploadClaimDetailDocument";
+
+describe("UploadClaimDetailDocumentCommand", () => {
+  it("should", async () => {
+    const context = new TestContext();
+    const costCat = context.testData.createCostCategory();
+    const partner = context.testData.createPartner();
+    const claimDetail = context.testData.createClaimDetail(costCat, partner);
+
+    const claimDetailKey = {
+      partnerId: claimDetail.Acc_ProjectParticipant__c,
+      periodId: claimDetail.Acc_ProjectPeriodNumber__c,
+      costCategoryId: claimDetail.Acc_CostCategory__c,
+    };
+    const content = "Some content";
+    const fileName = "fileName.txt";
+
+    const command = new UploadClaimDetailDocumentCommand(claimDetailKey, content, fileName);
+    await context.runCommand(command);
+
+    expect(context.repositories.contentVersions.Items[0].VersionData).toEqual(content);
+    expect(context.repositories.contentVersions.Items[0].PathOnClient).toEqual(fileName);
+    expect(context.repositories.contentDocumentLinks.Items[0].LinkedEntityId).toEqual(`${claimDetailKey.partnerId}_${claimDetailKey.periodId}_${claimDetailKey.costCategoryId}`);
+
+  });
+});
