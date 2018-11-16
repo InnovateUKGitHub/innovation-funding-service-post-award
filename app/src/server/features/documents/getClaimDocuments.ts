ee8c282f2c276ab25d8a46b182dfcf847ee33ffa
Vasilia Themis <vasilia.themis@bjss.com>
2018-11-16 16:45:51 +0000

    Pr feedback

--- app/src/server/features/documents/getClaimDocuments.ts
+++ app/src/server/features/documents/getClaimDocuments.ts
@@ -2,11 +2,11 @@ import {IContext, IQuery} from "../common/context";
 import {GetDocumentsLinkedToRecordQuery} from "./getAllForRecord";
 
 export class GetClaimDocumentsQuery implements IQuery<DocumentSummaryDto[]> {
-  constructor(private claimKey: ClaimKey, private description: string) {
+  constructor(private claimKey: ClaimKey, public filter?: DocumentFilter) {
   }
 
   public async Run(context: IContext) {
     const claim = await context.repositories.claims.get(this.claimKey.partnerId, this.claimKey.periodId);
-    return context.runQuery(new GetDocumentsLinkedToRecordQuery(claim.Id, this.description));
+    return context.runQuery(new GetDocumentsLinkedToRecordQuery(claim.Id, this.filter));
   }
 }
