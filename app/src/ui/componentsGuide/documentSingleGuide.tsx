e765f73c5ee81766a17e3b2242608d19f4bb390b
Seb Zapata <sebastian.zapata@bjss.com>
2018-11-21 12:36:55 +0000

    PR comments

--- app/src/ui/componentsGuide/documentSingleGuide.tsx
+++ app/src/ui/componentsGuide/documentSingleGuide.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { DocumentSingle } from "../components";
+import {ClaimDto} from "../../types";
+import * as ACC from "../components";
 
 const document = { link: "https://www.google.com/", fileName: "LABOUR_COSTS_Q3_2017-11-05.pdf", id: "1"};
+
+const Form = ACC.TypedForm<ClaimDto>();
+const button = <Form.Button name="return" onClick={()=>{}}>Remove</Form.Button>
 
 export const documentSingleGuide: IGuide = {
     name: "DocumentSingle",
@@ -10,7 +15,7 @@ export const documentSingleGuide: IGuide = {
             name: "Simple",
             comments: "Renders a DocumentSingle component",
             example: "<DocumentSingle message={\"test\"} document={document} qa={\"qa\"}/>",
-            render: () => <DocumentSingle message={"An IAR has been added to this claim"} document={document} qa={"qa"} onClick={() => console.log("onClick")}/>
+            render: () => <DocumentSingle message={"An IAR has been added to this claim"} document={document} qa={"qa"} renderRemove={() => button}/>
         }
     ]
 };
