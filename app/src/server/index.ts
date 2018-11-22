d1cb7d8042d37ab1484557fd4e0a7cead97c6969
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-11-22 14:17:16 +0000

    Adds auth handlers to enable shibboleth integration

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -4,13 +4,11 @@ DotEnv.config();
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import cookieSession from "cookie-session";
 import "isomorphic-fetch";
 import "isomorphic-form-data";
 
+import { router as authRouter } from "./auth";
 import { router } from "./router";
-
-const defaultSalesforceEmail = process.env.SALESFORCEUSERNAME || "iuk.accproject@bjss.com.bjsspoc2";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -19,34 +17,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cookieSession({
-  name: "chocolate-chip",
-  keys: ["thekey", "thesecret"]
-}));
-
 // serve the public folder contents
 app.use(express.static("public"));
 
-// until sso is configured this user is created and stored in the session
-// this user email is used to connect to salesforce.
-app.use((req, resp, next) => {
-  if (!req.session || !req.session.user) {
-    const newSession: { user: IUser } = {
-      user: {
-        email: defaultSalesforceEmail,
-        name: "Salesforce service account"
-      }
-    };
-
-    if (req.session) {
-      req.session.user = newSession.user;
-    }
-    else {
-      req.session = newSession;
-    }
-  }
-  next();
-});
+// auth handler
+app.use(authRouter);
 
 // all our defined routes
 app.use(router);
