10b381c70ced8146dfe50acd1deff0a293b16ee4
Joe Hutchinson <joe.hutchinson@bjss.com>
2018-11-22 16:09:36 +0000

    Configuring repo for SonarQube analysis

---
+++ sonarqube/scripts/addCredentialsAndRunSonarScan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SONARQUBE_PROPERTIES_FILE="sonar-project.properties"
+
+sed -i "s@sonarqubeServerUrl@$1@g" $SONARQUBE_PROPERTIES_FILE 
+sed -i "s/sonarqubePassword/$2/g" $SONARQUBE_PROPERTIES_FILE
+
+/tmp/sonar-scanner-3.2.0.1227-linux/bin/sonar-scanner
