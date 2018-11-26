3d51cdfe9901041dc2e7a9f2630cfa229a3a9682
Vladimir Pavlicenko <vladimirs.pavlicenko@bjss.com>
2018-11-26 11:36:56 +0000

    ACC-1658 fix validationMessage unit tests

--- app/tests/components/validationMessage.tests.tsx
+++ app/tests/components/validationMessage.tests.tsx
@@ -9,7 +9,7 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("ValidationMessage", () => {
     it("when valid should render an error message", () => {
         const wrapper = shallow(<ValidationMessage message={"Test message"} messageType="error" />).html();
-        expect(wrapper).toContain(`<strong class=\"govuk-warning-text__text\"><span class=\"govuk-warning-text__assistive\">Error</span>Test message</strong>`);
+        expect(wrapper).toContain(`<strong class=\"govuk-warning-text__text\"><span class=\"govuk-warning-text__assistive\">Error</span><span>Test message</span></strong>` );
     });
 
     it("when message is empty should render null", () => {
@@ -19,6 +19,6 @@ describe("ValidationMessage", () => {
 
     it("when valid should render an info message", () => {
         const wrapper = shallow(<ValidationMessage message={"Test message"} messageType="info" />).html();
-        expect(wrapper).toContain(`<p class=\"govuk-warning-text__text\"><span class=\"govuk-warning-text__assistive\">Info</span>Test message</p>`);
+        expect(wrapper).toContain(`<p class=\"govuk-warning-text__text\"><span class=\"govuk-warning-text__assistive\">Info</span><span>Test message</span></p>`);
     });
 });
