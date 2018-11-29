1bee44ace099220b1d1deaf3d53157c8c4cd7379
Seb Zapata <sebastian.zapata@bjss.com>
2018-11-29 11:24:35 +0000

    Files renamed to accordion and accordionItem

--- app/src/ui/components/accordionContainer.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export const AccordionContainer: React.SFC = (props) => {
-  return (
-    <div className="acc-accordion">
-      {props.children}
-    </div>
-  );
-}
