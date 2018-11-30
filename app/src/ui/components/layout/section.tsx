793907ba96c2818650c04d25bc510c69d8da6db4
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-11-30 11:01:58 +0000

    Adds claim window component to current claim on claim dashboard

--- app/src/ui/components/layout/section.tsx
+++ app/src/ui/components/layout/section.tsx
@@ -1,37 +1,29 @@
 import * as React from "react";
+import classNames from "classnames";
+import { SimpleString } from "../renderers";
 
 interface Props {
   title?: React.ReactNode;
   subtitle?: React.ReactNode;
   qa?: string;
+  badge?: React.ReactNode;
 }
 
-const renderTitleOnly = (title: React.ReactNode) => <h2 className="govuk-heading-m govuk-!-margin-bottom-9">{title}</h2>;
-
-const renderTitleAndSubtitle = (title: React.ReactNode, subtitle: React.ReactNode) => {
-  return (
-    <React.Fragment>
-      <h2 className="govuk-heading-m govuk-!-margin-bottom-2">{title}</h2>
-      <p className="govuk-body govuk-!-margin-bottom-9">{subtitle}</p>
-    </React.Fragment>
-  );
-};
-
-const renderHeader = ({title, subtitle}: Props) => {
-  if (title && subtitle) {
-    return renderTitleAndSubtitle(title, subtitle);
-  }
-  if (title) {
-    return renderTitleOnly(title);
-  }
-  return null;
-};
-
 export const Section: React.SFC<Props> = (props) => {
+  const { title, subtitle, badge } = props;
   return (
-    <div className="govuk-!-margin-bottom-9" data-qa={props.qa}>
-      {renderHeader(props)}
-      {props.children}
+    <div className="govuk-grid-row govuk-!-margin-bottom-9" data-qa={props.qa}>
+      <div className={classNames({ "govuk-grid-column-full": !badge, "govuk-grid-column-three-quarters": !!badge }, "govuk-!-margin-bottom-9")}>
+        {!!title ? <h2 className={classNames("govuk-heading-m", { "govuk-!-margin-bottom-2": !!subtitle })}>{title}</h2> : null}
+        {!!subtitle ? <SimpleString>{subtitle}</SimpleString> : null}
+      </div>
+      {
+        !!badge ? <div className={classNames("govuk-grid-column-one-quarter", "govuk-!-margin-bottom-9")}>{badge}</div> : null
+      }
+      <div className="govuk-grid-column-full">
+        {props.children}
+      </div>
     </div>
   );
 };
+ 
