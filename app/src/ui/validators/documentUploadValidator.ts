b4836ae5c9d9a9cea40d722a057f2cebc8d5280a
Vladimir Pavlicenko <vladimirs.pavlicenko@bjss.com>
2018-11-30 10:59:59 +0000

    ACC-1845 added error message to cost category claim page

--- app/src/ui/validators/documentUploadValidator.ts
+++ app/src/ui/validators/documentUploadValidator.ts
@@ -2,5 +2,5 @@ import * as Validation from "./common";
 import { Results } from "../validation/results";
 
 export class DocumentUploadValidator extends Results<DocumentUploadDto> {
-  public readonly file = Validation.required(this, this.model.file, "A file is required");
+  public readonly file = Validation.required(this, this.model.file, "Choose a file to upload");
 }
