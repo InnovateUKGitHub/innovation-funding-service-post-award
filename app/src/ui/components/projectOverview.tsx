65fd5cc9b3138e10adaccaf02fec1321d4612bd3
Seb Zapata <sebastian.zapata@bjss.com>
2018-12-04 15:53:31 +0000

    UI updated

--- app/src/ui/components/projectOverview.tsx
+++ app/src/ui/components/projectOverview.tsx
@@ -16,7 +16,7 @@ interface Props {
 export const ProjectOverviewPage: React.SFC<Props> = ({ project, selectedTab, children, partnerId, partners, validationMessage, backLinkText }) => (
     <ACC.Page>
         <ACC.Section>
-            <ACC.BackLink route={ProjectDashboardRoute.getLink({})}>{backLinkText || "Main dashboard"}</ACC.BackLink>
+            <ACC.BackLink route={ProjectDashboardRoute.getLink({})}>{backLinkText || "Back to dashboard"}</ACC.BackLink>
         </ACC.Section>
         {validationMessage}
         <ACC.Projects.Title pageTitle="View project" project={project} />
