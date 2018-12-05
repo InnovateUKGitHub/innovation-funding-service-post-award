8394532dc1595fd94714d99f4a3e5008f6ab2f50
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-12-05 13:11:48 +0000

    Left align the accordion button

--- app/src/ui/components/accordionItem.tsx
+++ app/src/ui/components/accordionItem.tsx
@@ -23,17 +23,16 @@ export class AccordionItem extends React.Component<Props, State> {
   }
 
   render() {
-    const styles = {
-      buttonStyle: {
-        background: "none",
-        border: "none"
-      }
+    const buttonStyle: React.CSSProperties = {
+      background: "none",
+      border: "none",
+      textAlign: "left"
     };
 
     return (
       <div className="acc-accordion__section">
         <div className="acc-accordion__section-header" onClick={() => this.setState({ accordionOpen: !this.state.accordionOpen })}>
-          <button style={styles.buttonStyle} className="govuk-heading-m govuk-!-margin-bottom-3 govuk-!-margin-top-3">{this.props.title}</button>
+          <button style={buttonStyle} className="govuk-heading-m govuk-!-margin-bottom-3 govuk-!-margin-top-3">{this.props.title}</button>
           <img className="govuk-!-padding-right-2" src={this.state.accordionOpen ? "/assets/images/icon-minus.png" : "/assets/images/icon-plus.png"} />
         </div>
         {this.state.accordionOpen && <div className="acc-accordion__section-panel">
