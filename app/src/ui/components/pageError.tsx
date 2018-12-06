11d65e9bfb27741823ddcfcd57b6e63a114653ea
Vasilia Themis <vasilia.themis@bjss.com>
2018-12-06 15:41:39 +0000

    removed unnecessary comment

--- app/src/ui/components/pageError.tsx
+++ app/src/ui/components/pageError.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import * as ACC from "./index";
 
-// You can either go back to the page you were previously on or go back to your <ACC.BackLink route={ProjectDashboardRoute.getLink({})}>{"dashboard"}</ACC.BackLink>.
 export const PageError: React.SFC<{title: string}> = ({title, children}) => (
     <ACC.Page>
         <ACC.Title title={title}/>
