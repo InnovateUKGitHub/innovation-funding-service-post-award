e2b83038405206365014532afab18d9e58c70cfc
Vasilia Themis <vasilia.themis@bjss.com>
2018-12-11 11:09:46 +0000

    added 404 page and redirect to there if route not found

--- app/src/ui/components/errorSummary.tsx
+++ app/src/ui/components/errorSummary.tsx
@@ -13,7 +13,7 @@ export const ErrorSummary: React.SFC<Props> = ({ error }) => {
     <div className="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" tabIndex={-1} data-module="error-summary" data-qa="error-summary">
       <h2 className="govuk-error-summary__title" id="error-summary-title">There is a problem</h2>
       <div className="govuk-error-summary__body">
-        Something happened and we couldn't process your request. Please try again later.
+        Something has gone wrong at our end and we were unable to complete the action. Try submitting the page again.
       </div>
     </div>
   );
