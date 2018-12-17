89d8f5744a5418632cc910f9bb9c0bdec3d8115e
Joe Hutchinson <joe.hutchinson@bjss.com>
2018-12-17 16:02:41 +0000

    Changing docker working directory to opt

--- sonarqube/scripts/addCredentialsAndRunSonarScan.sh
+++ sonarqube/scripts/addCredentialsAndRunSonarScan.sh
@@ -5,4 +5,4 @@ SONARQUBE_PROPERTIES_FILE="sonar-project.properties"
 sed -i "s@sonarqubeServerUrl@$1@g" $SONARQUBE_PROPERTIES_FILE 
 sed -i "s/sonarqubePassword/$2/g" $SONARQUBE_PROPERTIES_FILE
 
-/tmp/sonar-scanner-3.2.0.1227-linux/bin/sonar-scanner
+/opt/sonar-scanner-3.2.0.1227-linux/bin/sonar-scanner
