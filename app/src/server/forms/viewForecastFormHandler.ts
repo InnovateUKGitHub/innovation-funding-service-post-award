8d99acb1b6e29c105526b18448116f4d217bf555
Oliver Armfelt <oliver.armfelt@bjss.com>
2018-12-21 09:34:40 +0000

    Adds form handler to update forecast without js

---
+++ app/src/server/forms/viewForecastFormHandler.ts
@@ -0,0 +1,23 @@
+import express, { RequestHandler } from "express";
+import { IFormHandler } from "./formHandlerBase";
+import { UpdateForecastRoute, ViewForecastRoute } from "../../ui/containers";
+import { configureRouter } from "../../ui/routing";
+
+export class ViewForecastFormHandler implements IFormHandler {
+  routePath = ViewForecastRoute.routePath;
+
+  public readonly middleware: RequestHandler[] = [];
+
+
+  handle(req: express.Request, res: express.Response): Promise<void> {
+    const params = ViewForecastRoute.getParams({ name: ViewForecastRoute.routeName, params: req.params, path: req.path });
+
+    const redirectLink = UpdateForecastRoute.getLink(params);
+
+    const router = configureRouter();
+    const url = router.buildPath(redirectLink.routeName, redirectLink.routeParams);
+    res.redirect(url);
+    return Promise.resolve();
+  }
+
+}
