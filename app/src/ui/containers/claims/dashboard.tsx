541e07c06e39ba4bcb05617db56c54b9b9c6a4b2
Vasilia Themis <vasilia.themis@bjss.com>
2019-01-09 14:16:22 +0000

    ACC-2181 added data-qa to iar upload

--- app/src/ui/containers/claims/dashboard.tsx
+++ app/src/ui/containers/claims/dashboard.tsx
@@ -102,7 +102,7 @@ class Component extends ContainerBase<ClaimDashboardPageParams, Data, Callbacks>
     return (
       <React.Fragment>
         <Acc.ValidationMessage messageType={messageType} message={message} />
-        <UploadForm.Form enctype="multipart/form-data" data={editor.data} onChange={(dto) => this.onChange(dto, claim.periodId)}>
+        <UploadForm.Form qa="iar-upload-form" enctype="multipart/form-data" data={editor.data} onChange={(dto) => this.onChange(dto, claim.periodId)}>
           <UploadForm.Fieldset>
             <UploadForm.FileUpload
               label="document"
@@ -115,7 +115,7 @@ class Component extends ContainerBase<ClaimDashboardPageParams, Data, Callbacks>
             <UploadForm.Hidden name="periodId" value={() => claim.periodId} />
             <UploadForm.Hidden name="description" value={() => DocumentDescription.IAR} />
           </UploadForm.Fieldset>
-          <UploadForm.Button name="upload" onClick={() => this.onSave(editor.data, claim.periodId)}>Upload</UploadForm.Button>
+          <UploadForm.Button qa="iar-upload-button" name="upload" onClick={() => this.onSave(editor.data, claim.periodId)}>Upload</UploadForm.Button>
         </UploadForm.Form>
       </React.Fragment>
     );
