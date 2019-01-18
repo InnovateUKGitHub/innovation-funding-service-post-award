4bcce44fa710b43c3c91c1470a1d990285d98604
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-01-18 09:48:03 +0000

    Adds component to render claim detail link base on status and roles Also sorts project tab navigation.

--- app/src/ui/containers/claims/allClaimsDashboard.tsx
+++ app/src/ui/containers/claims/allClaimsDashboard.tsx
@@ -1,15 +1,15 @@
+import { ContainerBase, ReduxContainer } from "../containerBase";
 import * as Actions from "../../redux/actions";
-import React from "react";
 import { ProjectOverviewPage } from "../../components/projectOverview";
 import { Pending } from "../../../shared/pending";
-import { ClaimDto, PartnerDto, ProjectDto } from "../../../types/dtos";
+import { ClaimDto, PartnerDto, ProjectDto, ProjectRole } from "../../../types/dtos";
 import * as Selectors from "../../redux/selectors";
-import { ContainerBase, ReduxContainer } from "../containerBase";
+import React from "react";
 import * as Acc from "../../components";
 import { Accordion, AccordionItem } from "../../components";
-import { ClaimsDetailsRoute, ReviewClaimRoute, } from ".";
+import { ClaimsDetailsRoute, PrepareClaimRoute, ReviewClaimRoute, } from ".";
 import { ClaimStatus } from "../../../types";
-import {DateTime} from "luxon";
+import { DateTime } from "luxon";
 
 interface Params {
   projectId: string;
@@ -48,7 +48,7 @@ class Component extends ContainerBase<Params, Data, {}> {
   }
 
   groupClaimsByPeriod(claims: ClaimDto[]): ProjectPeriod[] {
-    const distinctPeriods = [...new Set(claims.map(x => x.periodId))].sort((a, b) => a-b);
+    const distinctPeriods = [...new Set(claims.map(x => x.periodId))].sort((a, b) => a - b);
     return distinctPeriods.map((period) => {
       const periodClaims = claims.filter(x => x.periodId === period);
       return {
@@ -68,7 +68,7 @@ class Component extends ContainerBase<Params, Data, {}> {
           {this.renderCurrentClaimsPerPeriod(currentClaims, projectDetails, partners)}
         </Acc.Section>
         <Acc.Section qa="closed-claims-section" title="Closed">
-         {this.renderPreviousClaimsSections(projectDetails, partners, previousClaims)}
+          {this.renderPreviousClaimsSections(projectDetails, partners, previousClaims)}
         </Acc.Section>
       </ProjectOverviewPage>
     );
@@ -77,14 +77,14 @@ class Component extends ContainerBase<Params, Data, {}> {
   private renderCurrentClaimsPerPeriod(claims: ClaimDto[], project: ProjectDto, partners: PartnerDto[]) {
     const groupedClaims = this.groupClaimsByPeriod(claims);
     if (groupedClaims.length === 0) {
-        if(!project.periodEndDate) return null;
-        const date = DateTime.fromJSDate(project.periodEndDate).plus({days: 1}).toJSDate();
+      if (!project.periodEndDate) return null;
+      const date = DateTime.fromJSDate(project.periodEndDate).plus({ days: 1 }).toJSDate();
 
-        return (
-          <Acc.Renderers.SimpleString qa="notificationMessage">
-            There are no open claims. The next claim period begins <Acc.Renderers.FullDate value={date} />.
+      return (
+        <Acc.Renderers.SimpleString qa="notificationMessage">
+          There are no open claims. The next claim period begins <Acc.Renderers.FullDate value={date} />.
           </Acc.Renderers.SimpleString>
-        );
+      );
     }
     return groupedClaims.map((x, i) => this.renderCurrentClaims(x, project, partners, i));
   }
@@ -97,9 +97,9 @@ class Component extends ContainerBase<Params, Data, {}> {
         <Acc.SectionPanel qa="claims-summary" title="History">
           <Acc.DualDetails>
             <SummaryDetails.Details data={project} qa="project_summary-col-0">
-              <SummaryDetails.Currency label="Grant offered" qa="gol-costs" value={x => x.grantOfferLetterCosts}/>
-              <SummaryDetails.Currency label="Costs claimed" qa="claimed-costs" value={x => x.costsClaimedToDate}/>
-              <SummaryDetails.Percentage label="Percentage claimed" qa="claimed-percentage" value={x => x.claimedPercentage}/>
+              <SummaryDetails.Currency label="Grant offered" qa="gol-costs" value={x => x.grantOfferLetterCosts} />
+              <SummaryDetails.Currency label="Costs claimed" qa="claimed-costs" value={x => x.costsClaimedToDate} />
+              <SummaryDetails.Percentage label="Percentage claimed" qa="claimed-percentage" value={x => x.claimedPercentage} />
             </SummaryDetails.Details>
           </Acc.DualDetails>
         </Acc.SectionPanel>
@@ -128,18 +128,18 @@ class Component extends ContainerBase<Params, Data, {}> {
     };
 
     const hasClaimNotYetSubmittedToInnovate = currentInfo.claims.find(this.claimHasNotBeenSubmittedToInnovate);
-    const badge = hasClaimNotYetSubmittedToInnovate && <Acc.Claims.ClaimWindow periodEnd={currentInfo.end}/>;
+    const badge = hasClaimNotYetSubmittedToInnovate && <Acc.Claims.ClaimWindow periodEnd={currentInfo.end} />;
 
     return (
       <Acc.Section title={title} qa="current-claims-section" badge={badge} key={index}>
-        <ClaimTable.Table data={currentInfo.claims} qa="current-claims-table" bodyRowFlag={(x) => x.status === ClaimStatus.SUBMITTED ? "info" : null }>
-          <ClaimTable.String header="Partner" qa="partner" value={renderPartnerName}/>
+        <ClaimTable.Table data={currentInfo.claims} qa="current-claims-table" bodyRowFlag={(x) => x.status === ClaimStatus.SUBMITTED ? "info" : null}>
+          <ClaimTable.String header="Partner" qa="partner" value={renderPartnerName} />
           <ClaimTable.Currency header="Forecast costs for period" qa="forecast-cost" value={(x) => x.forecastCost} />
           <ClaimTable.Currency header="Actual costs for period" qa="actual-cost" value={(x) => x.totalCost} />
           <ClaimTable.Currency header="Difference" qa="diff" value={(x) => x.forecastCost - x.totalCost} />
-          <ClaimTable.String header="Status" qa="status" value={(x) => x.status}/>
-          <ClaimTable.ShortDate header="Date of last update" qa="last-update" value={x => x.paidDate || x.approvedDate || x.lastModifiedDate}/>
-          <ClaimTable.Custom header="" qa="link" value={(x) => this.getLink(x, project.id)} />
+          <ClaimTable.String header="Status" qa="status" value={(x) => x.status} />
+          <ClaimTable.ShortDate header="Date of last update" qa="last-update" value={x => x.paidDate || x.approvedDate || x.lastModifiedDate} />
+          <ClaimTable.Custom header="" qa="link" value={(x) => <Acc.Claims.ClaimDetailsLink claim={x} project={project} partner={partners.find(p => p.id === x.partnerId)!} /> }/>
         </ClaimTable.Table>
       </Acc.Section>
     );
@@ -149,13 +149,13 @@ class Component extends ContainerBase<Params, Data, {}> {
     const grouped = partners.map(x => ({ partner: x, claims: previousClaims.filter(y => y.partnerId === x.id) }));
 
     return (
-        <Accordion>
-          {grouped.map((x, i) => (
-            <AccordionItem title={`${x.partner.name} ${x.partner.isLead ? "(Lead)" : ""}`} openAltText="Hide the closed claims" closedAltText="Show the closed claims" key={i}>
-              {this.previousClaimsSection(project, x.partner, x.claims)}
-            </AccordionItem>
-          ))}
-        </Accordion>
+      <Accordion>
+        {grouped.map((x, i) => (
+          <AccordionItem title={`${x.partner.name} ${x.partner.isLead ? "(Lead)" : ""}`} openAltText="Hide the closed claims" closedAltText="Show the closed claims" key={i}>
+            {this.previousClaimsSection(project, x.partner, x.claims)}
+          </AccordionItem>
+        ))}
+      </Accordion>
     );
   }
 
@@ -173,9 +173,9 @@ class Component extends ContainerBase<Params, Data, {}> {
           <ClaimTable.Currency header="Forecast costs for period" qa="forecast-cost" value={(x) => x.forecastCost} />
           <ClaimTable.Currency header="Actual costs for period" qa="actual-cost" value={(x) => x.totalCost} />
           <ClaimTable.Currency header="Difference" qa="diff" value={(x) => x.forecastCost - x.totalCost} />
-          <ClaimTable.String header="Status" qa="status" value={(x) => x.status}/>
-          <ClaimTable.ShortDate header="Date of last update" qa="last-update" value={x => x.paidDate || x.approvedDate || x.lastModifiedDate}/>
-          <ClaimTable.Custom header="" qa="link" value={(x) => this.getLink(x, project.id)} />
+          <ClaimTable.String header="Status" qa="status" value={(x) => x.status} />
+          <ClaimTable.ShortDate header="Date of last update" qa="last-update" value={x => x.paidDate || x.approvedDate || x.lastModifiedDate} />
+          <ClaimTable.Custom header="" qa="link" value={(x) => <Acc.Claims.ClaimDetailsLink claim={x} project={project} partner={partner} />} />
         </ClaimTable.Table>
       </div>
     );
@@ -185,17 +185,6 @@ class Component extends ContainerBase<Params, Data, {}> {
     return (
       <Acc.Claims.ClaimPeriodDate claim={claim} />
     );
-  }
-  private getLink(claim: ClaimDto, projectId: string) {
-    switch (claim.status) {
-      case ClaimStatus.DRAFT:
-      case ClaimStatus.REVIEWING_FORECASTS:
-        return null;
-      case ClaimStatus.SUBMITTED:
-        return <Acc.Link route={ReviewClaimRoute.getLink({projectId, partnerId: claim.partnerId, periodId: claim.periodId})}>Review claim</Acc.Link>;
-      default:
-        return <Acc.Link route={ClaimsDetailsRoute.getLink({projectId, partnerId: claim.partnerId, periodId: claim.periodId})}>View claim</Acc.Link>;
-    }
   }
 }
 
