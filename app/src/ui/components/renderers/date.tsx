59401ba8ab2e0c0038d25fbc1c0b7e234f2cb833
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-01-21 12:41:21 +0000

    Makes sure dates only (eg Project Start Date etc) get rendered in london time.

--- app/src/ui/components/renderers/date.tsx
+++ app/src/ui/components/renderers/date.tsx
@@ -1,19 +1,24 @@
 import * as React from "react";
 import { DateTime } from "luxon";
 
-const renderDate = (value: Date | null, format: string) => {
-    const dateValue = value && DateTime.fromJSDate(value);
+const convertDateAndTime = (jsDate: Date | null): DateTime | null => {
+    return jsDate && DateTime.fromJSDate(jsDate);
+};
 
-    if (dateValue && dateValue.isValid) {
-        return <span>{dateValue.toFormat(format)}</span>;
+const convertDateOnly = (jsDate: Date | null): DateTime | null => {
+    return jsDate && DateTime.fromJSDate(jsDate).setZone("Europe/London");
+};
+
+const render = (value: DateTime | null, format: string) => {
+    if (value && value.isValid) {
+        return <span>{value.toFormat(format)}</span>;
     }
-
     return null;
 };
 
 export const DateRange: React.SFC<{ start: Date | null, end: Date | null }> = props => {
-    const start = props.start && DateTime.fromJSDate(props.start);
-    const end = props.end && DateTime.fromJSDate(props.end);
+    const start = convertDateOnly(props.start);
+    const end = convertDateOnly(props.end);
 
     if (!start || !start.isValid || !end || !end.isValid) {
         return null;
@@ -40,9 +45,9 @@ export const DateRange: React.SFC<{ start: Date | null, end: Date | null }> = pr
     );
 };
 
-export const LongDateRange: React.SFC<{ start: Date | null, end: Date | null , isShortMonth?: boolean}> = props => {
-    const start = props.start && DateTime.fromJSDate(props.start);
-    const end = props.end && DateTime.fromJSDate(props.end);
+export const LongDateRange: React.SFC<{ start: Date | null, end: Date | null, isShortMonth?: boolean }> = props => {
+    const start = convertDateOnly(props.start);
+    const end = convertDateOnly(props.end);
     const isShortMonth = props.isShortMonth;
 
     if (!start || !start.isValid || !end || !end.isValid) {
@@ -67,33 +72,33 @@ export const LongDateRange: React.SFC<{ start: Date | null, end: Date | null , i
 };
 
 export const ShortMonth: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "MMM");
+    return render(convertDateOnly(props.value), "MMM");
 };
 
 export const DayAndLongMonth: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "d MMMM");
+    return render(convertDateOnly(props.value), "d MMMM");
 };
 
 export const LongYear: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "yyyy");
+    return render(convertDateOnly(props.value), "yyyy");
 };
 
 export const FullDate: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "d MMMM yyyy");
+    return render(convertDateOnly(props.value), "d MMMM yyyy");
 };
 
 export const FullDateTime: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "d MMMM yyyy HH:mm");
+    return render(convertDateAndTime(props.value), "d MMMM yyyy HH:mm");
 };
 
 export const FullDateTimeWithSeconds: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "d MMMM yyyy HH:mm:ss");
+    return render(convertDateAndTime(props.value), "d MMMM yyyy HH:mm:ss");
 };
 
 export const ShortDate: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "d MMM yyyy");
+    return render(convertDateOnly(props.value), "d MMM yyyy");
 };
 
 export const ShortDateTime: React.SFC<{ value: Date | null }> = (props) => {
-    return renderDate(props.value, "d MMM yyyy HH:mm");
+    return render(convertDateAndTime(props.value), "d MMM yyyy HH:mm");
 };
