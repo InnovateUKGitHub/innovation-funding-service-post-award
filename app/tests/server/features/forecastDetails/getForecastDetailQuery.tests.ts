c23bf4e3be7df5a3fa0e1e50ba664e7c95a0ce41
Vladimir Pavlicenko <vladimirs.pavlicenko@bjss.com>
2019-02-01 14:10:07 +0000

    added missing feature unit tests

--- app/tests/server/features/forecastDetails/getForecastDetailQuery.tests.ts
+++ app/tests/server/features/forecastDetails/getForecastDetailQuery.tests.ts
@@ -16,4 +16,18 @@ describe("GetForecastDetailQuery", () => {
     expect(item.periodId).toBe(1);
     expect(item.value).toBe(1000);
   });
+
+  it("should return zero value if partner does not exist", async () => {
+    const context = new TestContext();
+    const costCat = context.testData.createCostCategory();
+    const partner = context.testData.createPartner();
+    context.testData.createProfileDetail(costCat, partner);
+
+    const query  = new GetForecastDetailQuery("test", 1, costCat.Id);
+    const item = await context.runQuery(query);
+
+    expect(item.costCategoryId).toBe(costCat.Id);
+    expect(item.periodId).toBe(1);
+    expect(item.value).toBe(0);
+  });
 });
