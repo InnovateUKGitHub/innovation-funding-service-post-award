c30ff0bfc49630e646b3d207d4bd8152204d0eb7
Vasilia Themis <vasilia.themis@bjss.com>
2019-02-11 15:23:45 +0000

    Loading font

--- app/tests/components/loading.tests.tsx
+++ app/tests/components/loading.tests.tsx
@@ -1,124 +1,124 @@
 import "jest";
 import React from "react";
-import Enzyme, { mount, shallow } from "enzyme";
+import Enzyme, { mount, render, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { Loader, PageLoader } from "../../src/ui/components";
+import { Loader } from "../../src/ui/components";
 import { LoadingStatus } from "../../src/shared/pending";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Loader", () => {
-  test("Preload - Renders null", () => {
-    const render = jest.fn();
+  test("Preload - renderFunctions null", () => {
+    const renderFunction = jest.fn();
     const pending = { state: LoadingStatus.Preload } as any;
-    const wrapper = shallow(<Loader render={render} pending={pending} />);
+    const wrapper = shallow(<Loader render={renderFunction} pending={pending} />);
     expect(wrapper.html()).toBeNull();
   });
 
-  test("Done - calls render with data and false", () => {
-    const render  = jest.fn();
+  test("Done - calls renderFunction with data and false", () => {
+    const renderFunction  = jest.fn();
     const data    = { test: 123 };
     const pending = { state: LoadingStatus.Done, data } as any;
-    shallow(<Loader render={render} pending={pending} />);
-    expect(render).toHaveBeenCalledWith(data, false);
+    shallow(<Loader render={renderFunction} pending={pending} />);
+    expect(renderFunction).toHaveBeenCalledWith(data, false);
   });
 
-  test("Done - render returns string", () => {
-    const render  = jest.fn(() => "testing");
+  test("Done - renderFunction returns string", () => {
+    const renderFunction  = jest.fn(() => "testing");
     const data    = { test: 123 };
     const pending = { state: LoadingStatus.Done, data } as any;
-    const wrapper = shallow(<Loader render={render} pending={pending} />);
-    expect(render).toHaveBeenCalledWith(data, false);
+    const wrapper = shallow(<Loader render={renderFunction} pending={pending} />);
+    expect(renderFunction).toHaveBeenCalledWith(data, false);
     expect(wrapper.html()).toBe("<span>testing</span>");
   });
 
-  test("Done - render returns array", () => {
+  test("Done - renderFunction returns array", () => {
     const results = ["testing1", "testing2"];
-    const render  = jest.fn(() => results);
+    const renderFunction  = jest.fn(() => results);
     const data    = { test: 123 };
     const pending = { state: LoadingStatus.Done, data } as any;
-    const wrapper = shallow(<Loader render={render} pending={pending} />);
-    expect(render).toHaveBeenCalledWith(data, false);
+    const wrapper = shallow(<Loader render={renderFunction} pending={pending} />);
+    expect(renderFunction).toHaveBeenCalledWith(data, false);
     expect(wrapper.html()).toBe(`<div>${results.join("")}</div>`);
   });
 
-  test("Loading with data - calls render with data and true", () => {
-    const render  = jest.fn();
+  test("Loading with data - calls renderFunction with data and true", () => {
+    const renderFunction  = jest.fn();
     const data    = { test: 123 };
     const pending = { state: LoadingStatus.Loading, data } as any;
-    shallow(<Loader render={render} pending={pending} />);
-    expect(render).toHaveBeenCalledWith(data, true);
+    shallow(<Loader render={renderFunction} pending={pending} />);
+    expect(renderFunction).toHaveBeenCalledWith(data, true);
   });
 
-  test("Loading without data - renders default loading message", () => {
-    const render  = jest.fn();
+  test("Loading without data - renderFunctions default loading message", () => {
+    const callback  = jest.fn();
     const pending = { state: LoadingStatus.Loading } as any;
-    const wrapper = shallow(<Loader render={render} pending={pending} />);
-    expect(wrapper.text()).toBe("Loading....");
-    expect(render).not.toHaveBeenCalled();
+    const wrapper = render(<Loader render={callback} pending={pending} />);
+    expect(wrapper.text()).toBe("Loading...");
+    expect(callback).not.toHaveBeenCalled();
   });
 
-  test("Loading without data, with renderLoading override - calls given renderLoading", () => {
-    const render  = jest.fn();
+  test("Loading without data, with renderFunctionLoading override - calls given renderFunctionLoading", () => {
+    const renderFunction  = jest.fn();
     const loading = jest.fn();
     const pending = { state: LoadingStatus.Loading } as any;
-    shallow(<Loader render={render} pending={pending} renderLoading={loading} />);
+    shallow(<Loader render={renderFunction} pending={pending} renderLoading={loading} />);
     expect(loading).toHaveBeenCalled();
-    expect(render).not.toHaveBeenCalled();
+    expect(renderFunction).not.toHaveBeenCalled();
   });
 
-  test("Stale with data - calls render with data and true", () => {
-    const render  = jest.fn();
+  test("Stale with data - calls renderFunction with data and true", () => {
+    const renderFunction  = jest.fn();
     const data    = { test: 123 };
     const pending = { state: LoadingStatus.Stale, data } as any;
-    shallow(<Loader render={render} pending={pending} />);
-    expect(render).toHaveBeenCalledWith(data, true);
+    shallow(<Loader render={renderFunction} pending={pending} />);
+    expect(renderFunction).toHaveBeenCalledWith(data, true);
   });
 
-  test("Stale without data - renders default loading message", () => {
-    const render  = jest.fn();
+  test("Stale without data - renderFunctions default loading message", () => {
+    const renderFunction  = jest.fn();
     const pending = { state: LoadingStatus.Stale } as any;
-    const wrapper = shallow(<Loader render={render} pending={pending} />);
-    expect(wrapper.text()).toBe("Loading....");
-    expect(render).not.toHaveBeenCalled();
+    const wrapper = render(<Loader render={renderFunction} pending={pending} />);
+    expect(wrapper.text()).toBe("Loading...");
+    expect(renderFunction).not.toHaveBeenCalled();
   });
 
-  test("Stale without data, with renderLoading override - calls given renderLoading", () => {
-    const render  = jest.fn();
+  test("Stale without data, with renderFunctionLoading override - calls given renderFunctionLoading", () => {
+    const renderFunction  = jest.fn();
     const loading = jest.fn();
     const pending = { state: LoadingStatus.Stale } as any;
-    shallow(<Loader render={render} pending={pending} renderLoading={loading} />);
+    shallow(<Loader render={renderFunction} pending={pending} renderLoading={loading} />);
     expect(loading).toHaveBeenCalled();
-    expect(render).not.toHaveBeenCalled();
+    expect(renderFunction).not.toHaveBeenCalled();
   });
 
-  test("Failed without error - renders null", () => {
-    const render  = jest.fn();
+  test("Failed without error - renderFunctions null", () => {
+    const renderFunction  = jest.fn();
     const pending = { state: LoadingStatus.Failed } as any;
-    const wrapper = mount(<Loader render={render} pending={pending} />);
+    const wrapper = mount(<Loader render={renderFunction} pending={pending} />);
     expect(wrapper.html()).toBeNull();
-    expect(render).not.toHaveBeenCalled();
+    expect(renderFunction).not.toHaveBeenCalled();
   });
 
-  test("Failed with error - renders ErrorSummary", () => {
-    const render  = jest.fn();
+  test("Failed with error - renderFunctions ErrorSummary", () => {
+    const renderFunction  = jest.fn();
     const pending = { state: LoadingStatus.Failed, error: true } as any;
-    const wrapper = mount(<Loader render={render} pending={pending} />);
+    const wrapper = mount(<Loader render={renderFunction} pending={pending} />);
     expect(wrapper.html()).toContain("There is a problem");
-    expect(render).not.toHaveBeenCalled();
+    expect(renderFunction).not.toHaveBeenCalled();
   });
 
   test("Unknown - throws exception", () => {
-    const render  = jest.fn();
+    const renderFunction  = jest.fn();
     const pending = { state: "not a state" } as any;
     let error = null;
     try {
-      shallow(<Loader render={render} pending={pending} />);
+      shallow(<Loader render={renderFunction} pending={pending} />);
     }
     catch(e) {
       error = e;
     }
     expect(error).toBeInstanceOf(Error);
-    expect(render).not.toHaveBeenCalled();
+    expect(renderFunction).not.toHaveBeenCalled();
   });
 });
