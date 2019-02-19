f579fc95a18b21f101f42118b63d58b9e08ddc3f
Seb Zapata <sebastian.zapata@bjss.com>
2019-02-19 16:05:11 +0000

    acc-2581-content changes

--- app/src/ui/components/claims/claimPeriodDate.tsx
+++ app/src/ui/components/claims/claimPeriodDate.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import * as ACC from "..";
-import { ClaimDto } from "../../../types";
+import {ClaimDto, PartnerDto} from "../../../types";
 
 interface Props {
   claim: ClaimDto | null;
+  partner?: PartnerDto | null;
 }
 
-export const ClaimPeriodDate: React.SFC<Props> = (props) => !props.claim ? null : (
-  <React.Fragment>
-    Period {props.claim.periodId}: <ACC.Renderers.LongDateRange start={props.claim.periodStartDate} end={props.claim.periodEndDate} isShortMonth={true} />
-  </React.Fragment>
-);
+export const ClaimPeriodDate: React.SFC<Props> = (props) => {
+  if (!props.claim) return null;
+
+  if (!props.partner) {
+    return (
+      <React.Fragment>
+        Period {props.claim.periodId}: <ACC.Renderers.LongDateRange start={props.claim.periodStartDate} end={props.claim.periodEndDate} isShortMonth={true} />
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {props.partner.name} claim for period {props.claim.periodId}: <ACC.Renderers.LongDateRange start={props.claim.periodStartDate} end={props.claim.periodEndDate} isShortMonth={true}/>
+    </React.Fragment>
+  );
+};
