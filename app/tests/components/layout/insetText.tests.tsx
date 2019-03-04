a89fc9691c3b0ec19f248351121b3d46db9f2c48
Seb Zapata <sebastian.zapata@bjss.com>
2019-03-04 16:22:18 +0000

    inset text unit tests added

---
+++ app/tests/components/layout/insetText.tests.tsx
@@ -0,0 +1,19 @@
+import "jest";
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { InsetText } from "../../../src/ui/components/layout";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("InsetText", () => {
+  it("should render with the correct text", () => {
+    const wrapper = mount(<InsetText text="test text"/>);
+    expect(wrapper.text()).toEqual("test text");
+  });
+
+  it("should render as null if no text is given", () => {
+    const wrapper = mount(<InsetText text={null} />);
+    expect(wrapper.html()).toBe(null);
+  });
+});
