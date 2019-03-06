fdf210700ec56fe65068baecaa34343a7f7e080c
Curtis.Harding <Curtis.Harding@bjss.com>
2019-03-06 14:17:02 +0000

    moving time-zone-polyfill into app control

--- app/public/time-zone-polyfill.js
+++
@@ -1,7 +0,0 @@
-try {
-  new Intl.DateTimeFormat("en", {timeZone: "Europe/London"}).resolvedOptions().timeZone === "Europe/London";
-} catch (e) {
-  var script = document.createElement("script");
-  script.src = "/date-time-format-timezone-golden-zones-no-locale-min-1.0.21.js";
-  document.head.appendChild(script);
-}
