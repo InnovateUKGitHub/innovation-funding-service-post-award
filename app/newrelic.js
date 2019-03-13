7fc2690113d79491b51c5143050e9ae47ad89359
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-03-13 09:35:53 +0000

    Updates example env file and changes default cookie timeout to 10

--- app/newrelic.js
+++ app/newrelic.js
@@ -20,7 +20,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: process.env.NEW_RELIC_LOGGING
+    level: process.env.NEW_RELIC_LOGGING || "info"
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
