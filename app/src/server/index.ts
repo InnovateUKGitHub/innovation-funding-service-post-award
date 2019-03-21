e78292d5929a3f12789f22a5b915b20219804f31
JHutchBJSS <joe.hutchinson@bjss.com>
2019-03-21 08:29:28 +0000

    Setting up Salesforce connection health check in ACC app

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -5,10 +5,12 @@ import "isomorphic-fetch";
 import "isomorphic-form-data";
 
 import { Server } from "./server";
+import { healthCheck } from "./healthCheck";
 
 // Set up New Relic to monitor app when deployed
 if (process.env.NEW_RELIC_ENABLED === "true") {
   require("newrelic"); // tslint:disable-line:no-var-requires
+  setInterval(healthCheck, 60000);
 }
 
 const port = parseInt(process.env.PORT!, 10) || 8080;
