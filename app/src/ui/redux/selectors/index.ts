da1ee33f6aba1e1181f0369e17def73664f9ae32
Curtis.Harding <Curtis.Harding@bjss.com>
2019-03-27 12:53:26 +0000

    removing contacts api and all code references

--- app/src/ui/redux/selectors/index.ts
+++ app/src/ui/redux/selectors/index.ts
@@ -1,6 +1,5 @@
 export * from "./claims";
 export * from "./costCategories";
-export * from "./contacts";
 export * from "./documents";
 export * from "./forecastDetails";
 export * from "./partners";
