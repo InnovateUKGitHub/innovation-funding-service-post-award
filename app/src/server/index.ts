7a63c91e887c53c444ed8f714124afca567ba75b
Curtis.Harding <Curtis.Harding@bjss.com>
2019-04-04 16:34:19 +0100

    moving server to class

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -1,58 +1,16 @@
 import * as DotEnv from "dotenv";
 DotEnv.config();
 
-import cors from "cors";
-import bodyParser from "body-parser";
-
-import express, { NextFunction, Request, Response } from "express";
 import "isomorphic-fetch";
 import "isomorphic-form-data";
 
-import { router as healthRouter} from "./health";
-import { router as authRouter } from "./auth";
-import { router } from "./router";
-import { Configuration, Logger } from "./features/common";
-import { allowCache, noCache} from "./cacheHeaders";
+import { Server } from "./server";
 
 // Set up New Relic to monitor app when deployed
 if (process.env.NEW_RELIC_ENABLED === "true") {
   require("newrelic"); // tslint:disable-line:no-var-requires
 }
 
-const app = express();
-app.enable("trust proxy");
-const port = process.env.PORT || 8080;
-const log = new Logger();
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  log.debug(req.url);
-  next();
-});
-
-const setOwaspHeaders = (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("X-Frame-Options", "deny");
-  res.setHeader("X-Content-Type-Options", "nosniff");
-  res.setHeader("X-XSS-Protection", "1");
-  return next();
-};
-
-// un-authed routes
-// serve the public folder contents
-app.use(setOwaspHeaders, allowCache, express.static("public"));
-
-app.use(noCache, healthRouter);
-
-// auth handler
-app.use(authRouter);
-
-// all our defined routes
-app.use(setOwaspHeaders, noCache, router);
-
-app.listen(port);
-
-log.info(`Listening at ${process.env.SERVER_URL}`);
-log.info("Configuration", Configuration);
+const port = parseInt(process.env.PORT!, 10) || 8080;
+const server = new Server(port);
+server.start();
