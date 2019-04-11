4665ebcce35a89e829dbe40159820ca8a070f0e7
Vladimir Pavlicenko <vladimirs.pavlicenko@bjss.com>
2019-04-11 10:50:33 +0100

    permissions fix for PM to GetAllLineItemsForClaimByCategoryQuery

--- app/src/server/features/claimLineItems/getAllLineItemsForClaimCategoryQuery.ts
+++ app/src/server/features/claimLineItems/getAllLineItemsForClaimCategoryQuery.ts
@@ -14,7 +14,7 @@ export class GetAllLineItemsForClaimByCategoryQuery extends QueryBase<ClaimLineI
 
   protected async accessControl(auth: Authorisation) {
     return auth.forProject(this.projectId).hasRole(ProjectRole.MonitoringOfficer)
-    || auth.forPartner(this.projectId, this.partnerId).hasRole(ProjectRole.FinancialContact);
+    || auth.forPartner(this.projectId, this.partnerId).hasAnyRoles(ProjectRole.FinancialContact, ProjectRole.ProjectManager);
   }
 
   protected async Run(context: IContext) {
