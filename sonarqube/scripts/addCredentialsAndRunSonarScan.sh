b536bfb8ca667c18763a6f7ace7ed2acb2aff462
JHutchBJSS <joe.hutchinson@bjss.com>
2019-04-23 10:50:49 +0100

    Moving security tool files out of root into single directory

--- sonarqube/scripts/addCredentialsAndRunSonarScan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SONARQUBE_PROPERTIES_FILE="sonar-project.properties"
-
-sed -i "s@sonarqubeServerUrl@$1@g" $SONARQUBE_PROPERTIES_FILE 
-sed -i "s/sonarqubePassword/$2/g" $SONARQUBE_PROPERTIES_FILE
-
-/opt/sonar-scanner-3.2.0.1227-linux/bin/sonar-scanner
