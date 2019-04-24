e8bff77efddbae6545c47b1fa258ccfed7f840cc
Curtis.Harding <Curtis.Harding@bjss.com>
2019-04-24 16:16:33 +0100

    adding unit tests for line items features

--- app/tests/server/features/claimLineItems/GetAllLineItemsForClaimByCategoryQuery.tests.ts
+++ app/tests/server/features/claimLineItems/GetAllLineItemsForClaimByCategoryQuery.tests.ts
@@ -1,6 +1,7 @@
 import "jest";
 import { TestContext } from "../../testContextProvider";
 import { GetAllLineItemsForClaimByCategoryQuery } from "@server/features/claimLineItems";
+import { Authorisation, ProjectRole } from "@framework/types";
 
 describe("GetAllLineItemsForClaimByCategoryQuery", () => {
   it("returns objects of correct shape", async () => {
@@ -79,5 +80,65 @@ describe("GetAllLineItemsForClaimByCategoryQuery", () => {
     const result = await context.runQuery(query);
 
     expect(result).toEqual([]);
+  });
+
+  test("accessControl - Project Monitoring officer passes", async () => {
+    const context = new TestContext();
+    const project = context.testData.createProject();
+    const partner = context.testData.createPartner();
+    const command = new GetAllLineItemsForClaimByCategoryQuery(project.Id, partner.Id, "", 1);
+    const auth    = new Authorisation({
+      [project.Id]: {
+        projectRoles: ProjectRole.MonitoringOfficer,
+        partnerRoles: {}
+      }
+    });
+
+    expect(await context.runAccessControl(auth, command)).toBe(true);
+  });
+
+  test("accessControl - Partner Finance contact passes", async () => {
+    const context = new TestContext();
+    const project = context.testData.createProject();
+    const partner = context.testData.createPartner();
+    const command = new GetAllLineItemsForClaimByCategoryQuery(project.Id, partner.Id, "", 1);
+    const auth    = new Authorisation({
+      [project.Id]: {
+        projectRoles: ProjectRole.Unknown,
+        partnerRoles: { [partner.Id]: ProjectRole.FinancialContact }
+      }
+    });
+
+    expect(await context.runAccessControl(auth, command)).toBe(true);
+  });
+
+  test("accessControl - Partner Project Manager passes", async () => {
+    const context = new TestContext();
+    const project = context.testData.createProject();
+    const partner = context.testData.createPartner();
+    const command = new GetAllLineItemsForClaimByCategoryQuery(project.Id, partner.Id, "", 1);
+    const auth    = new Authorisation({
+      [project.Id]: {
+        projectRoles: ProjectRole.Unknown,
+        partnerRoles: { [partner.Id]: ProjectRole.ProjectManager }
+      }
+    });
+
+    expect(await context.runAccessControl(auth, command)).toBe(true);
+  });
+
+  test("accessControl - all other roles fail", async () => {
+    const context = new TestContext();
+    const project = context.testData.createProject();
+    const partner = context.testData.createPartner();
+    const command = new GetAllLineItemsForClaimByCategoryQuery(project.Id, partner.Id, "", 1);
+    const auth    = new Authorisation({
+      [project.Id]: {
+        projectRoles: ProjectRole.FinancialContact | ProjectRole.ProjectManager,
+        partnerRoles: { [partner.Id]: ProjectRole.MonitoringOfficer }
+      }
+    });
+
+    expect(await context.runAccessControl(auth, command)).toBe(false);
   });
 });
