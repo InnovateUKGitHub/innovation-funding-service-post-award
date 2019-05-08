268270975e50b04a7bd86c3103fc5aeb81f17495
Seb Zapata <sebastian.zapata@bjss.com>
2019-05-08 11:28:56 +0100

    ACC-3172 create status change feature

---
+++ app/src/server/features/monitoringReports/getMonitoringReportStatusChanges.ts
@@ -0,0 +1,27 @@
+import { QueryBase } from "@server/features/common";
+import { Authorisation, IContext, MonitoringReportActivityDto, ProjectRole } from "@framework/types";
+
+export class GetMonitoringReportStatusChanges extends QueryBase<MonitoringReportActivityDto[]> {
+  constructor(
+    private readonly projectId: string,
+    private readonly reportId: string
+  ) {
+    super();
+  }
+
+  protected async accessControl(auth: Authorisation, context: IContext) {
+    return context.config.features.monitoringReports && auth.forProject(this.projectId).hasRole(ProjectRole.MonitoringOfficer);
+  }
+
+  protected async Run(context: IContext): Promise<MonitoringReportActivityDto[]> {
+    const statusChanges = await context.repositories.monitoringReportStatusChange.getStatusChanges(this.reportId);
+    return statusChanges.map<MonitoringReportActivityDto>(x => ({
+      id: x.Id,
+      monitoringReport: x.Acc_MonitoringReport__c,
+      newStatus: x.Acc_NewMonitoringReportStatus__c,
+      previousStatus: x.Acc_PreviousMonitoringReportStatus__c,
+      lastModifiedBy: x.LastModifiedById,
+      createdDate: context.clock.parseRequiredSalesforceDateTime(x.CreatedDate)
+    }));
+  }
+}
---
+++ app/tests/server/features/monitoringReports/getMonitoringReportStatusChanges.tests.ts
@@ -0,0 +1,37 @@
+import { TestContext } from "../../testContextProvider";
+import { GetMonitoringReportStatusChanges } from "@server/features/monitoringReports/getMonitoringReportStatusChanges";
+import { DateTime } from "luxon";
+
+describe("GetMonitoringReportStatusChanges", () => {
+  it("returns an object of the right shape", async () => {
+    const context = new TestContext();
+    const testData = context.testData;
+
+    const project = testData.createProject();
+    const report = testData.createMonitoringReportHeader(project, 1);
+    const statusChange = testData.createMonitoringReportStatusChange(report);
+
+    const query = new GetMonitoringReportStatusChanges(project.Id, report.Id);
+    const result = await context.runQuery(query);
+
+    expect(result[0].lastModifiedBy).toBe(statusChange.LastModifiedById);
+    expect(result[0].previousStatus).toBe(statusChange.Acc_PreviousMonitoringReportStatus__c);
+    expect(result[0].newStatus).toBe(statusChange.Acc_NewMonitoringReportStatus__c);
+    expect(result[0].monitoringReport).toBe(report.Id);
+    expect(result[0].createdDate).toEqual(DateTime.fromISO(statusChange.CreatedDate).toJSDate());
+  });
+
+  it("returns the correct number of status changes", async () => {
+    const context = new TestContext();
+    const testData = context.testData;
+
+    const project = testData.createProject();
+    const report = testData.createMonitoringReportHeader(project, 1);
+    testData.createMonitoringReportStatusChange(report);
+    testData.createMonitoringReportStatusChange(report);
+
+    const query = new GetMonitoringReportStatusChanges(project.Id, report.Id);
+    const result = await context.runQuery(query);
+    expect(result.length).toEqual(2);
+  });
+});
