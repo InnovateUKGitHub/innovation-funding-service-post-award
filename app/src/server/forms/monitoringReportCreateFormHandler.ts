f397177d137a85d756ae2c07f70071acd45d956e
Vasilia Themis <vasilia.themis@bjss.com>
2019-05-20 16:47:27 +0100

    Use hash tabs in preare monitoring report page

--- app/src/server/forms/monitoringReportCreateFormHandler.ts
+++ app/src/server/forms/monitoringReportCreateFormHandler.ts
@@ -1,20 +1,18 @@
-import { FormHandlerBase, IFormBody, IFormButton } from "./formHandlerBase";
-import { GetMonitoringReportActiveQuestions } from "@server/features/monitoringReports/getMonitoringReportActiveQuestions";
-import { CreateMonitoringReport } from "@server/features/monitoringReports/createMonitoringReport";
-import { MonitoringReportDashboardRoute } from "@ui/containers/monitoringReports/dashboard";
-import { MonitoringReportCreateRoute, MonitoringReportPrepareParams } from "@ui/containers/monitoringReports/prepare";
+import { FormHandlerBase, IFormBody, IFormButton } from "@server/forms/formHandlerBase";
+import { CreateMonitoringReport, GetMonitoringReportActiveQuestions } from "@server/features/monitoringReports";
+import { MonitoringReportCreateParams, MonitoringReportCreateRoute, MonitoringReportDashboardRoute } from "@ui/containers/monitoringReports";
 import { getMonitoringReportEditor } from "@ui/redux/selectors";
 import { MonitoringReportDtoValidator } from "@ui/validators/MonitoringReportDtoValidator";
 import { MonitoringReportStatus } from "@framework/constants";
 import { MonitoringReportDto } from "@framework/dtos";
 import { IContext, ILinkInfo } from "@framework/types";
 
-export class MonitoringReportCreateFormHandler extends FormHandlerBase<MonitoringReportPrepareParams, MonitoringReportDto> {
+export class MonitoringReportCreateFormHandler extends FormHandlerBase<MonitoringReportCreateParams, MonitoringReportDto> {
   constructor() {
     super(MonitoringReportCreateRoute, [ "save-draft", "save-submitted"]);
   }
 
-  protected async getDto(context: IContext, params: MonitoringReportPrepareParams, button: IFormButton, body: IFormBody): Promise<MonitoringReportDto> {
+  protected async getDto(context: IContext, params: MonitoringReportCreateParams, button: IFormButton, body: IFormBody): Promise<MonitoringReportDto> {
     const questions = await context.runQuery(new GetMonitoringReportActiveQuestions());
 
     questions.forEach(q => {
@@ -36,15 +34,15 @@ export class MonitoringReportCreateFormHandler extends FormHandlerBase<Monitorin
     };
   }
 
-  protected createValidationResult(params: MonitoringReportPrepareParams, dto: MonitoringReportDto) {
+  protected createValidationResult(params: MonitoringReportCreateParams, dto: MonitoringReportDto) {
     return new MonitoringReportDtoValidator(dto, false, false, dto.questions, 100);
   }
 
-  protected getStoreInfo(params: MonitoringReportPrepareParams): { key: string; store: string; } {
-    return getMonitoringReportEditor(params.projectId, params.id);
+  protected getStoreInfo(params: MonitoringReportCreateParams): { key: string; store: string; } {
+    return getMonitoringReportEditor(params.projectId);
   }
 
-  protected async run(context: IContext, params: MonitoringReportPrepareParams, button: IFormButton, dto: MonitoringReportDto): Promise<ILinkInfo> {
+  protected async run(context: IContext, params: MonitoringReportCreateParams, button: IFormButton, dto: MonitoringReportDto): Promise<ILinkInfo> {
     const command = new CreateMonitoringReport(dto, button.name === "save-submitted");
     await context.runCommand(command);
 
