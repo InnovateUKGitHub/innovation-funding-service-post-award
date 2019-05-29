fb4b919f46d5b3d2d2b2d430f8214e10159764eb
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-05-29 13:17:29 +0100

    Change the rendering of the logs

--- app/src/ui/componentsGuide/logsGuide.tsx
+++ app/src/ui/componentsGuide/logsGuide.tsx
@@ -6,7 +6,7 @@ const data: LogItem[] = [
   { newStatus: "Approved", createdDate: DateTime.local().minus({ days: 1 }).toJSDate(), createdBy: "example@test.com", comments: "" },
   { newStatus: "Queried", createdDate: DateTime.local().minus({ days: 2, hours: 1, minutes: 55 }).toJSDate(), createdBy: "another@test.com", comments: "The comments are optional" },
   { newStatus: "Submitted", createdDate: DateTime.local().minus({ days: 3, hours: -1, minutes: 12 }).toJSDate(), createdBy: "geoff.jefty.geoff@test.com", comments: "" },
-  { newStatus: "Draft", createdDate: DateTime.local().minus({ days: 4, hours: -1, minutes: -18 }).toJSDate(), createdBy: "mr.me@test.com", comments: "The comments are optional" },
+  { newStatus: "Draft", createdDate: DateTime.local().minus({ days: 4, hours: -1, minutes: -18 }).toJSDate(), createdBy: "mr.me@test.com", comments: "The comments are optional\n\nAnd and can be mutiline" },
 ];
 
 export const logsGuide: IGuide = {
