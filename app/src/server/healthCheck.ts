d64261c99910ee6d5deb00b658c9275b4aeff84c
Joe Hutchinson <joe.hutchinson@bjss.com>
2019-06-05 09:30:08 +0100

    Correcting variable placement

--- app/src/server/healthCheck.ts
+++ app/src/server/healthCheck.ts
@@ -8,7 +8,7 @@ export const healthCheck = async () => {
 
   const newRelicEventData = {
     eventType:"ACCHealthCheck",
-    env:`${process.env.OPENSHIFT_BUILD_NAMESPACE}`,
+    env:`${process.env.NEW_RELIC_APP_NAME}`,
     sfConnection:`${sfConnection}`
   };
 
@@ -17,7 +17,7 @@ export const healthCheck = async () => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-Insert-Key": `${process.env.NEW_RELIC_APP_NAME}`,
+      "X-Insert-Key": `${process.env.NEW_RELIC_API_KEY}`,
       "Content-Encoding": "gzip"
     },
     body: compressedData
