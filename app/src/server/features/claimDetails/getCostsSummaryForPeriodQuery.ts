5a21fb469b811bd3be1bce92f62eee822d4790e3
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-06-24 10:28:29 +0100

    Returns claims detail summary information even if claim detail data is missing

--- app/src/server/features/claimDetails/getCostsSummaryForPeriodQuery.ts
+++ app/src/server/features/claimDetails/getCostsSummaryForPeriodQuery.ts
@@ -17,33 +17,37 @@ export class GetCostsSummaryForPeriodQuery extends QueryBase<CostsSummaryForPeri
   }
 
   protected async Run(context: IContext) {
+    const partner = await context.repositories.partners.getById(this.partnerId);
     const claimDetailResults = await context.repositories.claimDetails.getAllByPartnerForPeriod(this.partnerId, this.periodId);
     const totalCostCategoryResults = await context.repositories.claimDetails.getAllByPartnerWithPeriodLt(this.partnerId, this.periodId);
     const totalForcastResults = await context.repositories.profileTotalCostCategory.getAllByPartnerId(this.partnerId);
-    const costCategories = await context.runQuery(new GetCostCategoriesQuery());
+    const costCategories = await context.runQuery(new GetCostCategoriesQuery()).then(x => x.filter(y => y.organisationType === partner.Acc_OrganisationType__c && y.competitionType === partner.Acc_ProjectId__r.Acc_CompetitionType__c));
 
-    return claimDetailResults.map(claimDetail => {
+    return costCategories.map(costCategory => {
+
       const forecast = totalForcastResults
-        .filter(y => y.Acc_CostCategory__c === claimDetail.Acc_CostCategory__c)
+        .filter(y => y.Acc_CostCategory__c === costCategory.id)
         .map(y => y.Acc_CostCategoryGOLCost__c)[0] || 0;
 
       const totalCostCategory = totalCostCategoryResults
-        .filter(y => y.Acc_CostCategory__c === claimDetail.Acc_CostCategory__c)
+        .filter(y => y.Acc_CostCategory__c === costCategory.id)
         .map(y => y.Acc_PeriodCostCategoryTotal__c)
         .reduce((t, c) => t + c, 0);
 
+      const claimDetail = claimDetailResults.find(x => x.Acc_CostCategory__c === costCategory.id);
+
       const offerCosts = forecast || 0;
-      const costsClaimedThisPeriod = claimDetail.Acc_PeriodCostCategoryTotal__c || 0;
+      const costsClaimedThisPeriod = claimDetail && claimDetail.Acc_PeriodCostCategoryTotal__c || 0;
       const costsClaimedToDate = totalCostCategory;
       const remainingOfferCosts = offerCosts - totalCostCategory - costsClaimedThisPeriod;
 
       return ({
-        costCategoryId: claimDetail.Acc_CostCategory__c,
+        costCategoryId: costCategory.id,
         offerCosts,
         costsClaimedToDate,
         costsClaimedThisPeriod,
         remainingOfferCosts
       });
-    }).sort((x, y) => costCategories.findIndex((costCat) => costCat.id === x.costCategoryId) - costCategories.findIndex((costCat) => costCat.id === y.costCategoryId));
+    });
   }
 }
