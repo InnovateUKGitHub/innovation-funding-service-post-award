348443382852fa03d555df733cc37b6e5024a3d2
Seb Zapata <sebastian.zapata@bjss.com>
2019-08-27 16:11:38 +0100

    ACC-3950 update details info component for edge

--- app/src/ui/components/layout/info.tsx
+++ app/src/ui/components/layout/info.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-export const Info: React.FunctionComponent<{ summary: string }> = ({ summary, children }) => {
-  return (
-    <details data-module="govuk-details" className="govuk-details">
-      <summary className="govuk-details__summary">
-        <span className="govuk-details__summary-text">{summary}</span>
-      </summary>
-      <div className="govuk-details__text">{children}</div>
-    </details>
-  );
-};
+interface InfoProps {
+  summary: string;
+}
+
+export class Info extends React.PureComponent<InfoProps, {}> {
+
+  private elem: HTMLElement|null = null;
+
+  componentDidMount() {
+    const govFrontend = window && (window as any).GOVUKFrontend;
+    if(this.elem && govFrontend) {
+      new govFrontend.Details(this.elem).init();
+    }
+  }
+
+  render() {
+    return (
+      <details data-module="govuk-details" className="govuk-details" ref={(e) => this.elem = e}>
+        <summary className="govuk-details__summary">
+          <span className="govuk-details__summary-text">{this.props.summary}</span>
+        </summary>
+        <div className="govuk-details__text">{this.props.children}</div>
+      </details>
+    );
+  }
+}
