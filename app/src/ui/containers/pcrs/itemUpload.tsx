b1a28c3c811fb707ea3b642d874156979b22b6b0
Seb Zapata <sebastian.zapata@bjss.com>
2019-09-05 16:15:51 +0100

    ACC-4016 add upload files section

--- app/src/ui/containers/pcrs/itemUpload.tsx
+++ app/src/ui/containers/pcrs/itemUpload.tsx
@@ -6,6 +6,8 @@ import { ProjectDto, ProjectRole } from "@framework/dtos";
 import { Pending } from "@shared/pending";
 import { PCRDetailsRoute } from "@ui/containers";
 import { ContainerBase, ReduxContainer } from "@ui/containers/containerBase";
+import { IEditorStore } from "@ui/redux";
+import { MultipleDocumentUpdloadDtoValidator } from "@ui/validators";
 
 interface Params {
   projectId: string;
@@ -15,18 +17,38 @@ interface Params {
 
 interface Data {
   project: Pending<ProjectDto>;
+  editor: Pending<IEditorStore<MultipleDocumentUploadDto, MultipleDocumentUpdloadDtoValidator>>;
+  documents: Pending<DocumentSummaryDto[]>;
+}
+
+interface CombinedData {
+  project: ProjectDto;
+  editor: IEditorStore<MultipleDocumentUploadDto, MultipleDocumentUpdloadDtoValidator>;
+  documents: DocumentSummaryDto[];
 }
 
 interface Callbacks {}
 
 class ProjectChangeRequestItemUploadContainer extends ContainerBase<Params, Data, Callbacks> {
   render() {
-    const combined = Pending.combine({project: this.props.project});
+    const combined = Pending.combine({
+      project: this.props.project,
+      editor: this.props.editor,
+      documents: this.props.documents
+    });
 
-    return <ACC.PageLoader pending={combined} render={x => this.renderContents(x.project)}/>;
+    return <ACC.PageLoader pending={combined} render={x => this.renderContents(x)}/>;
   }
 
-  private renderContents(project: ProjectDto) {
+  private renderDocumentList(documents: DocumentSummaryDto[]) {
+    return documents.length > 0
+      ? <ACC.DocumentList documents={documents} qa="supporting-documents" />
+      : <ACC.ValidationMessage messageType="info" message="No files uploaded"/>;
+  }
+
+  private renderContents({project, editor, documents}: CombinedData) {
+    const UploadForm = ACC.TypedForm<MultipleDocumentUploadDto>();
+
     return (
       <ACC.Page
         backLink={<ACC.BackLink route={PCRDetailsRoute.getLink({projectId: this.props.projectId, pcrId: this.props.projectChangeRequestId})}>Back to request</ACC.BackLink>}
@@ -34,7 +56,7 @@ class ProjectChangeRequestItemUploadContainer extends ContainerBase<Params, Data
         pageTitle={<ACC.Projects.Title project={project} />}
       >
         <ACC.Section>
-          <div>
+          <div className="govuk-body">
             <p>
               You need to submit a <a href="#1">reallocate project costs spreadsheet</a>. In the yellow boxes enter the names
               of all partner
@@ -50,6 +72,27 @@ class ProjectChangeRequestItemUploadContainer extends ContainerBase<Params, Data
             </p>
             <p>You should not increase the overhead percentage rate.</p>
           </div>
+          <UploadForm.Form
+            enctype="multipart"
+            editor={editor}
+            qa="projectChangeRequestItemUpload"
+          >
+            <UploadForm.Fieldset heading="Upload">
+              <ACC.Renderers.SimpleString>You can upload up to 10 files of any type, as long as their combined file size is less than 10MB.</ACC.Renderers.SimpleString>
+              <UploadForm.MulipleFileUpload
+                label="Upload documents"
+                name="attachment"
+                labelHidden={true}
+                value={data => data.files}
+                update={(dto, files) => dto.files = files || []}
+                validation={editor.validator.files}
+              />
+            </UploadForm.Fieldset>
+            <UploadForm.Submit styling="Secondary">Upload</UploadForm.Submit>
+          </UploadForm.Form>
+        </ACC.Section>
+        <ACC.Section title="Files uploaded">
+          {this.renderDocumentList(documents)}
         </ACC.Section>
       </ACC.Page>
     );
@@ -61,6 +104,8 @@ const definition = ReduxContainer.for<Params, Data, Callbacks>(ProjectChangeRequ
 export const ProjectChangeRequestItemUpload = definition.connect({
   withData: (state, params) => ({
     project: Selectors.getProject(params.projectId).getPending(state),
+    editor: Selectors.getProjectChangeRequestItemDocumentEditor(params.itemId, state.config).get(state),
+    documents: Selectors.getProjectChangeRequestItemDocuments(params.itemId).getPending(state)
   }),
   withCallbacks: () => ({})
 });
@@ -75,7 +120,8 @@ export const ProjectChangeRequestItemUploadRoute = definition.route({
   }),
   getLoadDataActions: (params) => [
     Actions.loadProject(params.projectId),
-    Actions.loadPcr(params.projectId, params.projectChangeRequestId)
+    Actions.loadPcr(params.projectId, params.projectChangeRequestId),
+    Actions.loadProjectChangeRequestItemDocuments(params.projectId, params.itemId)
   ],
   getTitle: (store, params) => {
     const projectChangeRequestItem = Selectors.getPcrItem(params.projectId, params.projectChangeRequestId, params.itemId).getPending(store).then(x => x && x.typeName).data;
