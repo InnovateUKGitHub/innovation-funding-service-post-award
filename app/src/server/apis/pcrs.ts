917ac203cefc81e7741caf3f0086b41aeea53fd4
Vasilia Themis <vasilia.themis@bjss.com>
2019-09-05 10:27:54 +0100

    Added submit & cancle buttons for new PCR

--- app/src/server/apis/pcrs.ts
+++ app/src/server/apis/pcrs.ts
@@ -1,11 +1,14 @@
 import contextProvider from "@server/features/common/contextProvider";
 import { ApiParams, ControllerBaseWithSummary } from "./controllerBase";
-import { PCRDto, PCRItemTypeDto, PCRSummaryDto } from "@framework/dtos/pcrDtos";
+import { PCRDto, PCRItemDto, PCRItemTypeDto, PCRSummaryDto } from "@framework/dtos/pcrDtos";
 import { GetAllPCRsQuery } from "@server/features/pcrs/getAllPCRsQuery";
 import { GetPCRByIdQuery } from "@server/features/pcrs/getPCRByIdQuery";
 import { GetPCRItemTypesQuery } from "@server/features/pcrs/getItemTypesQuery";
+import { processDto } from "@shared/processResponse";
+import { PCRItemStatus, PCRStatus } from "@framework/entities";
 
 export interface IPCRsApi {
+  create: (params: ApiParams<{ projectId: string, projectChangeRequestDto: PCRDto }>) => Promise<PCRDto>;
   getAll: (params: ApiParams<{ projectId: string }>) => Promise<PCRSummaryDto[]>;
   get: (params: ApiParams<{ projectId: string, id: string }>) => Promise<PCRDto>;
   getTypes: (params: ApiParams<{ }>) => Promise<PCRItemTypeDto[]>;
@@ -17,6 +20,7 @@ class Controller extends ControllerBaseWithSummary<PCRSummaryDto, PCRDto> implem
 
     super.getItems("/", (p, q) => ({ projectId: q.projectId }), (p) => this.getAll(p));
     super.getItem("/:projectId/:id", (p, q) => ({ projectId: p.projectId, id: p.id }), (p) => this.get(p));
+    super.postItem("/:projectId", (p, q, b) => ({ projectId: q.projectId, projectChangeRequestDto: processDto(b) }), (p) => this.create(p));
     super.getCustom("/types", () => ({}), p => this.getTypes(p));
   }
 
@@ -28,6 +32,11 @@ class Controller extends ControllerBaseWithSummary<PCRSummaryDto, PCRDto> implem
   get(params: ApiParams<{ projectId: string, id: string}>): Promise<PCRDto> {
     const query = new GetPCRByIdQuery(params.projectId, params.id);
     return contextProvider.start(params).runQuery(query);
+  }
+
+  create({projectId, projectChangeRequestDto}: ApiParams<{ projectId: string, projectChangeRequestDto: PCRDto}>): Promise<PCRDto> {
+    // TODO write command
+    return Promise.resolve({...projectChangeRequestDto, id: "1"});
   }
 
   getTypes(params: ApiParams<{}>): Promise<PCRItemTypeDto[]> {
