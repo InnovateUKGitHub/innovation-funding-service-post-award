797e5c418954fd6f851469ac8a66a6780b84a9c4
ainara.saneduardo <ainara.saneduardo@bjss.com>
2019-09-12 14:52:30 +0100

    change qa values

--- app/src/ui/containers/pcrs/create.tsx
+++ app/src/ui/containers/pcrs/create.tsx
@@ -43,7 +43,7 @@ class PCRCreateComponent extends ContainerBase<CreateProjectChangeRequestParams,
         validator={editor.validator}
         error={editor.error}
       >
-        <ACC.Section qa="section_pcr">
+        <ACC.Section qa="pcr-create">
           {this.renderForm(editor, itemTypes)}
         </ACC.Section>
       </ACC.Page>
@@ -68,7 +68,7 @@ class PCRCreateComponent extends ContainerBase<CreateProjectChangeRequestParams,
 
     return (
       <React.Fragment>
-        <PCRForm.Form editor={pcrEditor} onSubmit={() => this.props.create(this.props.projectId, pcrEditor.data)} onChange={x => this.setState({ pcr: x })} qa="createProjectChangeRequestForm">
+        <PCRForm.Form editor={pcrEditor} onSubmit={() => this.props.create(this.props.projectId, pcrEditor.data)} onChange={x => this.setState({ pcr: x })} qa="pcr-create-form">
           <PCRForm.Fieldset heading="Select request types">
             <PCRForm.Checkboxes
               hint="You can select more than one."
