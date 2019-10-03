048b2c6b56a108233fa3b3b56cdd92b5806a37a0
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2019-10-03 12:26:08 +0100

    ACC-4446 create month year component and tests

--- app/src/ui/components/inputs/dateInput.tsx
+++ app/src/ui/components/inputs/dateInput.tsx
@@ -1,20 +1,21 @@
+// tslint:disable no-duplicate-string
 import * as React from "react";
 import { BaseInput } from "./baseInput";
 import { DateTime } from "luxon";
 import classNames from "classnames";
 
-interface DateInputProps extends InputProps<Date> {
+interface FullDateInputProps extends InputProps<Date> {
 
 }
 
-interface State {
+interface FullDateState {
   day: string;
   month: string;
   year: string;
 }
 
-export class DateInput extends BaseInput<DateInputProps, State> {
-  constructor(props: DateInputProps) {
+export class FullDateInput extends BaseInput<FullDateInputProps, FullDateState> {
+  constructor(props: FullDateInputProps) {
     super(props);
     const val = this.props.value && DateTime.fromJSDate(this.props.value);
 
@@ -62,7 +63,7 @@ export class DateInput extends BaseInput<DateInputProps, State> {
               aria-describedby={this.props.ariaDescribedBy}
               aria-valuemin={1}
               aria-valuemax={31}
-              aria-valuenow={this.getIfNumber(this.state.day)}
+              aria-valuenow={getIfNumber(this.state.day)}
               value={this.state.day}
               onChange={e => this.onChange(e.currentTarget.value, this.state.month, this.state.year)}
             />
@@ -83,7 +84,7 @@ export class DateInput extends BaseInput<DateInputProps, State> {
               aria-describedby={this.props.ariaDescribedBy}
               aria-valuemin={1}
               aria-valuemax={12}
-              aria-valuenow={this.getIfNumber(this.state.month)}
+              aria-valuenow={getIfNumber(this.state.month)}
               value={this.state.month}
               onChange={e => this.onChange(this.state.day, e.currentTarget.value, this.state.year)}
             />
@@ -104,7 +105,7 @@ export class DateInput extends BaseInput<DateInputProps, State> {
               aria-describedby={this.props.ariaDescribedBy}
               aria-valuemin={1000}
               aria-valuemax={3000}
-              aria-valuenow={this.getIfNumber(this.state.year)}
+              aria-valuenow={getIfNumber(this.state.year)}
               value={this.state.year}
               onChange={e => this.onChange(this.state.day, this.state.month, e.currentTarget.value)}
             />
@@ -112,14 +113,6 @@ export class DateInput extends BaseInput<DateInputProps, State> {
         </div>
       </div>
     );
-  }
-
-  private getIfNumber(value: string) {
-    const result = parseInt(value, 10);
-    if (!isNaN(result)) {
-      return result;
-    }
-    return undefined;
   }
 
   private onChange(day: string, month: string, year: string) {
@@ -143,4 +136,124 @@ export class DateInput extends BaseInput<DateInputProps, State> {
       }
     }
   }
+}
+
+interface MonthYearInputProps extends InputProps<Date> {
+  startOrEnd: "start" | "end";
+}
+
+interface MonthYearState {
+  month: string;
+  year: string;
+}
+
+export class MonthYearInput extends BaseInput<MonthYearInputProps, MonthYearState> {
+  constructor(props: MonthYearInputProps) {
+    super(props);
+    const val = this.props.value && DateTime.fromJSDate(this.props.value);
+
+    if (val && val.isValid) {
+      this.state = {
+        month: val.toFormat("MM"),
+        year: val.toFormat("yyyy")
+      };
+    }
+    else {
+      this.state = {
+        month: "",
+        year: ""
+      };
+    }
+  }
+
+  render() {
+    const monthName = `${this.props.name}_month`;
+    const yearName = `${this.props.name}_year`;
+
+    const inputClass = classNames(
+      "govuk-input",
+      "govuk-date-input__input",
+      { "govuk-input--error": this.props.hasError === true }
+    );
+
+    return (
+      <div className="govuk-date-input">
+        <div className="govuk-date-input__item">
+          <div className="govuk-form-group">
+            <label className="govuk-label govuk-date-input__label" htmlFor={monthName}>
+              Month
+            </label>
+            <input
+              className={classNames(inputClass, "govuk-input--width-2")}
+              name={monthName}
+              type="number"
+              pattern="[0-9]*"
+              disabled={!!this.props.disabled}
+              aria-label={this.props.ariaLabel && `${this.props.ariaLabel} month`}
+              aria-describedby={this.props.ariaDescribedBy}
+              aria-valuemin={1}
+              aria-valuemax={12}
+              aria-valuenow={getIfNumber(this.state.month)}
+              value={this.state.month}
+              onChange={e => this.onChange(e.currentTarget.value, this.state.year)}
+            />
+          </div>
+        </div>
+        <div className="govuk-date-input__item">
+          <div className="govuk-form-group">
+            <label className="govuk-label govuk-date-input__label" htmlFor={yearName}>
+              Year
+            </label>
+            <input
+              className={classNames(inputClass, "govuk-input--width-4")}
+              name={yearName}
+              type="number"
+              pattern="[0-9]*"
+              disabled={!!this.props.disabled}
+              aria-label={this.props.ariaLabel && `${this.props.ariaLabel} year`}
+              aria-describedby={this.props.ariaDescribedBy}
+              aria-valuemin={1000}
+              aria-valuemax={3000}
+              aria-valuenow={getIfNumber(this.state.year)}
+              value={this.state.year}
+              onChange={e => this.onChange(this.state.month, e.currentTarget.value)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  private onChange(month: string, year: string) {
+    this.setState({ month, year });
+    this.debounce(() => this.sendUpdate(month), 500);
+  }
+
+  private sendUpdate(monthInput: string) {
+    const monthDateTime = DateTime.fromFormat(monthInput, "M");
+    const day = this.props.startOrEnd === "start" ? "01" : monthDateTime.endOf("month").get("day");
+
+    if (this.props.onChange) {
+      const { month, year } = this.state;
+      const result = DateTime.fromFormat(`${day}/${month}/${year}`, "d/M/yyyy");
+
+      if (result.isValid) {
+        this.props.onChange(result.toJSDate());
+      }
+      else if (month !== "" || year !== "") {
+        this.props.onChange(new Date(NaN));
+      }
+      else {
+        this.props.onChange(null);
+      }
+    }
+  }
+}
+
+function getIfNumber(value: string) {
+  const result = parseInt(value, 10);
+  if (!isNaN(result)) {
+    return result;
+  }
+  return undefined;
 }
---
+++ app/tests/components/inputs/dateInput.tests.tsx
@@ -0,0 +1,143 @@
+// tslint:disable:no-duplicate-string no-identical-functions
+import "jest";
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { FullDateInput, MonthYearInput } from "@ui/components/inputs";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("FullDateInput", () => {
+  it("Renders with given name", () => {
+    const wrapper = mount(<FullDateInput name="testName" />);
+    expect(wrapper.prop("name")).toBe("testName");
+  });
+
+  it("Update component day state with value", () => {
+    const wrapper = mount(<FullDateInput name="testName" />);
+
+    expect(wrapper.state("day")).toBe("");
+
+    const input = wrapper.find("input").first();
+    (input.instance() as any).value = "25";
+    input.simulate("change");
+    expect(wrapper.state("day")).toEqual("25");
+  });
+
+  it("Update component month state with value", () => {
+    const wrapper = mount(<FullDateInput name="testName" />);
+
+    expect(wrapper.state("month")).toBe("");
+
+    const input = wrapper.find("input").at(1);
+    (input.instance() as any).value = "05";
+    input.simulate("change");
+    expect(wrapper.state("month")).toEqual("05");
+  });
+
+  it("Update component year state with value", () => {
+    const wrapper = mount(<FullDateInput name="testName" />);
+
+    expect(wrapper.state("year")).toBe("");
+
+    const input = wrapper.find("input").last();
+    (input.instance() as any).value = "2018";
+    input.simulate("change");
+    expect(wrapper.state("year")).toEqual("2018");
+  });
+
+  it("Doesn't update component state with invalid values", () => {
+    const wrapper = mount(<FullDateInput name="testName" />);
+
+    const dayInput = wrapper.find("input").first();
+    const monthInput = wrapper.find("input").at(1);
+    const yearInput = wrapper.find("input").last();
+    (dayInput.instance() as any).value = "wrong";
+    dayInput.simulate("change");
+    (monthInput.instance() as any).value = "invalid";
+    monthInput.simulate("change");
+    (yearInput.instance() as any).value = "not OK";
+    yearInput.simulate("change");
+
+    expect(wrapper.state("day")).toEqual("");
+    expect(wrapper.state("month")).toEqual("");
+    expect(wrapper.state("year")).toEqual("");
+  });
+});
+
+describe("MonthYearInput", () => {
+  it("Renders with given name", () => {
+    const wrapper = mount(<MonthYearInput name="testName" startOrEnd="start" />);
+    expect(wrapper.prop("name")).toBe("testName");
+  });
+
+  it("Update component month state with value", () => {
+    const wrapper = mount(<MonthYearInput name="testName" startOrEnd="start" />);
+
+    expect(wrapper.state("month")).toBe("");
+
+    const input = wrapper.find("input").first();
+    (input.instance() as any).value = "05";
+    input.simulate("change");
+    expect(wrapper.state("month")).toEqual("05");
+  });
+
+  it("Update component year state with value", () => {
+    const wrapper = mount(<MonthYearInput name="testName" startOrEnd="start" />);
+
+    expect(wrapper.state("year")).toBe("");
+
+    const input = wrapper.find("input").last();
+    (input.instance() as any).value = "2018";
+    input.simulate("change");
+    expect(wrapper.state("year")).toEqual("2018");
+  });
+
+  it("Doesn't update component state with invalid values", () => {
+    const wrapper = mount(<MonthYearInput name="testName" startOrEnd="start" />);
+
+    const monthInput = wrapper.find("input").first();
+    const yearInput = wrapper.find("input").last();
+    (monthInput.instance() as any).value = "invalid";
+    monthInput.simulate("change");
+    (yearInput.instance() as any).value = "not OK";
+    yearInput.simulate("change");
+
+    expect(wrapper.state("month")).toEqual("");
+    expect(wrapper.state("year")).toEqual("");
+  });
+
+  it("Sets the date to the first day of the month", async (done) => {
+    const onChangeMock = jest.fn();
+    const wrapper = mount(<MonthYearInput name="testName" startOrEnd="start" onChange={onChangeMock} />);
+
+    const monthInput = wrapper.find("input").first();
+    (monthInput.instance() as any).value = "11";
+    monthInput.simulate("change");
+    const yearInput = wrapper.find("input").last();
+    (yearInput.instance() as any).value = "2019";
+    yearInput.simulate("change");
+
+    await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+    expect(onChangeMock).toBeCalledWith(new Date("2019/11/01"));
+    done();
+  });
+
+  it("Sets the date to the last day of the month", async (done) => {
+    const onChangeMock = jest.fn();
+    const wrapper = mount(<MonthYearInput name="testName" startOrEnd="end" onChange={onChangeMock} />);
+
+    const monthInput = wrapper.find("input").first();
+    (monthInput.instance() as any).value = "11";
+    monthInput.simulate("change");
+    const yearInput = wrapper.find("input").last();
+    (yearInput.instance() as any).value = "2019";
+    yearInput.simulate("change");
+
+    await new Promise(resolve => setTimeout(() => resolve(), 500));
+
+    expect(onChangeMock).toBeCalledWith(new Date("2019/11/30"));
+    done();
+  });
+});
