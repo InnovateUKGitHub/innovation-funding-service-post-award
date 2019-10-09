daad5115a79ef39eb71f825880c05f2d4761c14f
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-10-09 10:39:17 +0100

    Separates the pcr item files upload

--- app/src/server/forms/projectChangeRequest/projectChangeRequestItemDocumentDeleteHandler.ts
+++ app/src/server/forms/projectChangeRequest/projectChangeRequestItemDocumentDeleteHandler.ts
@@ -1,7 +1,7 @@
 import { IContext, ILinkInfo } from "@framework/types";
 import { DeleteProjectChangeRequestDocumentOrItemDocument } from "@server/features/documents/deleteProjectChangeRequestDocumentOrItemDocument";
 import { IFormBody, IFormButton, StandardFormHandlerBase } from "@server/forms/formHandlerBase";
-import { ProjectChangeRequestPrepareItemParams, ProjectChangeRequestPrepareItemRoute } from "@ui/containers";
+import { ProjectChangeRequestPrepareItemFilesRoute, ProjectChangeRequestPrepareItemParams, ProjectChangeRequestPrepareItemRoute } from "@ui/containers";
 import { getProjectChangeRequestDocumentOrItemDocumentDeleteEditorStoreInfo } from "@ui/redux/selectors";
 import { Results } from "@ui/validation";
 
@@ -11,7 +11,7 @@ interface Document {
 
 export class ProjectChangeRequestItemDocumentDeleteHandler extends StandardFormHandlerBase<ProjectChangeRequestPrepareItemParams, Document, Results<{}>> {
   constructor() {
-    super(ProjectChangeRequestPrepareItemRoute, ["delete"]);
+    super(ProjectChangeRequestPrepareItemFilesRoute, ["delete"]);
   }
 
   protected getDto(context: IContext, params: ProjectChangeRequestPrepareItemParams, button: IFormButton, body: IFormBody) {
@@ -29,6 +29,7 @@ export class ProjectChangeRequestItemDocumentDeleteHandler extends StandardFormH
   protected async run(context: IContext, params: ProjectChangeRequestPrepareItemParams, button: IFormButton, dto: Document): Promise<ILinkInfo> {
     const command = new DeleteProjectChangeRequestDocumentOrItemDocument(dto.id, params.projectId, params.itemId);
     await context.runCommand(command);
-    return ProjectChangeRequestPrepareItemRoute.getLink(params);
+    return ProjectChangeRequestPrepareItemFilesRoute.getLink(params);
   }
+
 }
