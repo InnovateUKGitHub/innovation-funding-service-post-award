3aeefdcb18b2a52416127247faea5603a8505a09
Cosmin-Stefan Dobranici <Cosmin-Stefan.Dobranici@bjss.com>
2019-10-22 11:05:11 +0100

    Final changes applied. Conflicts hopefuly resolved

--- app/src/ui/containers/pcrs/items/projectSuspensionView.tsx
+++ app/src/ui/containers/pcrs/items/projectSuspensionView.tsx
@@ -10,8 +10,10 @@ interface Props {
 }
 
 export const ProjectSuspensionView = (props: Props) => (
-  <ACC.SummaryList qa="projectSuspension">
-    <ACC.SummaryListItem label="First day of pause" content={<ACC.Renderers.ShortDate value={props.projectChangeRequestItem.suspensionStartDate}/>} qa="startDate" />
-    <ACC.SummaryListItem label="Last day of pause (if known)" content={props.projectChangeRequestItem.suspensionEndDate ? <ACC.Renderers.ShortDate value={props.projectChangeRequestItem.suspensionEndDate}/> : "Not set"} qa="endDate" />
-  </ACC.SummaryList>
+  <ACC.Section title="Partner details">
+    <ACC.SummaryList qa="projectSuspension">
+      <ACC.SummaryListItem label="First day of pause" content={<ACC.Renderers.ShortDate value={props.projectChangeRequestItem.suspensionStartDate} />} qa="startDate" />
+      <ACC.SummaryListItem label="Last day of pause (if known)" content={props.projectChangeRequestItem.suspensionEndDate ? <ACC.Renderers.ShortDate value={props.projectChangeRequestItem.suspensionEndDate} /> : "Not set"} qa="endDate" />
+    </ACC.SummaryList>
+  </ACC.Section>
 );
--- app/src/ui/containers/pcrs/items/timeExtensionView.tsx
+++ app/src/ui/containers/pcrs/items/timeExtensionView.tsx
@@ -10,10 +10,10 @@ interface Props {
 }
 
 export const TimeExtensionView = (props: Props) => (
-  <ACC.SummaryList qa="timeExtensionSummaryList">
-    <ACC.SummaryListItem label="Current end date" content={<ACC.Renderers.ShortDate value={props.project.endDate} />} qa="currentEndDate" />
-    <ACC.SummaryListItem label="Current duration" content={<ACC.Renderers.Duration startDate={props.project.startDate} endDate={props.project.endDate} />} qa="currentDuration" />
-    <ACC.SummaryListItem label="New end date" content={<ACC.Renderers.ShortDate value={props.projectChangeRequestItem.projectEndDate} />} qa="newEndDate" />
-    <ACC.SummaryListItem label="New duration" content={<ACC.Renderers.Duration startDate={props.project.startDate} endDate={props.projectChangeRequestItem.projectEndDate} />} qa="newDuration" />
-  </ACC.SummaryList>
+    <ACC.SummaryList qa="timeExtensionSummaryList">
+      <ACC.SummaryListItem label="Current end date" content={<ACC.Renderers.ShortDate value={props.project.endDate} />} qa="currentEndDate" />
+      <ACC.SummaryListItem label="Current duration" content={<ACC.Renderers.Duration startDate={props.project.startDate} endDate={props.project.endDate} />} qa="currentDuration" />
+      <ACC.SummaryListItem label="New end date" content={<ACC.Renderers.ShortDate value={props.projectChangeRequestItem.projectEndDate} />} qa="newEndDate" />
+      <ACC.SummaryListItem label="New duration" content={<ACC.Renderers.Duration startDate={props.project.startDate} endDate={props.projectChangeRequestItem.projectEndDate} />} qa="newDuration" />
+    </ACC.SummaryList>
 );
