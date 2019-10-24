8ad099e4fe81c1aa3c22dab076ee4ab097314e8a
Oliver Armfelt <oliver.armfelt@outlook.com>
2019-10-24 11:07:52 +0100

    Make sure pcr status changes are created after pcr is updated

--- app/src/server/repositories/projectChangeRequestStatusChangeRepository.ts
+++ app/src/server/repositories/projectChangeRequestStatusChangeRepository.ts
@@ -1,17 +1,20 @@
 import SalesforceRepositoryBase from "@server/repositories/salesforceRepositoryBase";
 
-export interface ISalesforceProjectChangeRequestStatusChange {
-  Id: string;
+export interface ICreateProjectChangeRequestStatusChange {
   Acc_ProjectChangeRequest__c: string;
-  Acc_PreviousProjectChangeRequestStatus__c: string;
-  Acc_NewProjectChangeRequestStatus__c: string;
-  CreatedDate: string;
   Acc_ExternalComment__c: string;
   Acc_ParticipantVisibility__c: boolean;
 }
 
+export interface ISalesforceProjectChangeRequestStatusChange extends ICreateProjectChangeRequestStatusChange {
+  Id: string;
+  CreatedDate: string;
+  Acc_NewProjectChangeRequestStatus__c: string;
+  Acc_PreviousProjectChangeRequestStatus__c: string;
+}
+
 export interface IProjectChangeRequestStatusChangeRepository {
-  createStatusChange(statusChange: Partial<ISalesforceProjectChangeRequestStatusChange>): Promise<string>;
+  createStatusChange(statusChange: ICreateProjectChangeRequestStatusChange): Promise<string>;
   getStatusChanges(projectId: string, projectChangeRequestId: string): Promise<ISalesforceProjectChangeRequestStatusChange[]>;
 }
 
@@ -27,8 +30,12 @@ export class ProjectChangeRequestStatusChangeRepository extends SalesforceReposi
     "Acc_ParticipantVisibility__c"
   ];
 
-  public createStatusChange(statusChange: Partial<ISalesforceProjectChangeRequestStatusChange>) {
-    return super.insertItem(statusChange);
+  public createStatusChange(statusChange: ICreateProjectChangeRequestStatusChange) {
+    return super.insertItem({
+      Acc_ProjectChangeRequest__c: statusChange.Acc_ProjectChangeRequest__c,
+      Acc_ExternalComment__c: statusChange.Acc_ExternalComment__c,
+      Acc_ParticipantVisibility__c: statusChange.Acc_ParticipantVisibility__c
+    });
   }
 
   public getStatusChanges(projectId: string, projectChangeRequestId: string): Promise<ISalesforceProjectChangeRequestStatusChange[]> {
