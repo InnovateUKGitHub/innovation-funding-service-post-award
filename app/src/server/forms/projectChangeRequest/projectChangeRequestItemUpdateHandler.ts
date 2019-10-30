963f593b3990173d121a6d2ffd8a317786f090f8
Cosmin-Stefan Dobranici <Cosmin-Stefan.Dobranici@bjss.com>
2019-10-30 15:13:10 +0000

    lint issues

--- app/src/server/forms/projectChangeRequest/projectChangeRequestItemUpdateHandler.ts
+++ app/src/server/forms/projectChangeRequest/projectChangeRequestItemUpdateHandler.ts
@@ -26,10 +26,6 @@ export class ProjectChangeRequestItemUpdateHandler extends StandardFormHandlerBa
 
     item.status = body.itemStatus === "true" ? PCRItemStatus.Complete : PCRItemStatus.Incomplete;
 
-    if (item.type === PCRItemType.TimeExtension) {
-      this.updateTimeExtension(item, body);
-    }
-
     if (item.type === PCRItemType.ScopeChange) {
       this.updateScopeChange(item, body);
     }
@@ -65,8 +61,6 @@ export class ProjectChangeRequestItemUpdateHandler extends StandardFormHandlerBa
     item.publicDescription = body.description || "";
     item.projectSummary = body.summary || "";
   }
-
-  private updateTimeExtension(item: Dtos.PCRItemForTimeExtensionDto, body: IFormBody) {}
 
   protected async run(context: IContext, params: ProjectChangeRequestPrepareItemParams, button: IFormButton, dto: Dtos.PCRDto): Promise<ILinkInfo> {
     await context.runCommand(new UpdatePCRCommand(params.projectId, params.pcrId, dto));
