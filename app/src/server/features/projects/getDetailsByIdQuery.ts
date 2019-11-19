e351e8ab2e0e2ff6785f9604e80a35f2d00772b4
Vasilia Themis <vasilia.themis@mac3932.bjss.co.uk>
2019-11-19 16:55:35 +0000

    Made project mapper function syncronous

--- app/src/server/features/projects/getDetailsByIdQuery.ts
+++ app/src/server/features/projects/getDetailsByIdQuery.ts
@@ -1,7 +1,7 @@
 import { QueryBase } from "../common";
-import { MapToProjectDtoCommand } from "./mapToProjectDto";
 import { GetAllProjectRolesForUser } from "./getAllProjectRolesForUser";
 import { IContext, ProjectDto } from "@framework/types";
+import { mapToProjectDto } from "@server/features/projects/mapToProjectDto";
 
 export class GetByIdQuery extends QueryBase<ProjectDto> {
   constructor(private readonly projectId: string) {
@@ -12,6 +12,6 @@ export class GetByIdQuery extends QueryBase<ProjectDto> {
     const item = await context.repositories.projects.getById(this.projectId);
     const roles = await context.runQuery(new GetAllProjectRolesForUser());
     const roleInfo = roles.forProject(this.projectId).getRoles();
-    return context.runCommand(new MapToProjectDtoCommand(item, roleInfo));
+    return mapToProjectDto(context, item, roleInfo);
   }
 }
