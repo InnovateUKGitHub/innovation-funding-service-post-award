3f1098d1910a66b6027349b5a0f810d3ead61f58
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-11-20 15:25:09 +0000

    Change pcr standard items to use workflow

--- app/src/ui/containers/pcrs/nameChange/prepareItemFilesStep.tsx
+++ app/src/ui/containers/pcrs/nameChange/prepareItemFilesStep.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import * as ACC from "@ui/components";
-import { EditorStatus, IEditorStore, StoresConsumer } from "@ui/redux";
-import { MultipleDocumentUpdloadDtoValidator, PCRAccountNameChangeItemDtoValidator } from "@ui/validators";
-import { Pending } from "@shared/pending";
+import { IEditorStore, StoresConsumer } from "@ui/redux";
+import { MultipleDocumentUpdloadDtoValidator } from "@ui/validators";
 import { StepProps } from "@ui/containers/pcrs/workflow";
 import { PCRItemForAccountNameChangeDto } from "@framework/dtos";
+import { accountNameChangeWorkflow } from "./accountNameChangeWorkflow";
 
 interface InnerProps {
   documents: DocumentSummaryDto[];
-  onFileChange: (saving: boolean, dto: MultipleDocumentUploadDto) => void;
+  onFileChange: (dto: MultipleDocumentUploadDto, saveAndRemain: boolean, saveAndContinue: boolean) => void;
   onFileDelete: (dto: MultipleDocumentUploadDto, document: DocumentSummaryDto) => void;
 }
 
-class Component extends React.Component<StepProps<PCRItemForAccountNameChangeDto, PCRAccountNameChangeItemDtoValidator> & InnerProps> {
+class Component extends React.Component<StepProps<typeof accountNameChangeWorkflow> & InnerProps> {
   render(): React.ReactNode {
-    const {documents, documentsEditor} = this.props;
+    const { documents, documentsEditor } = this.props;
     return (
       <React.Fragment>
         {this.renderFiles(documentsEditor, documents)}
@@ -25,17 +25,16 @@ class Component extends React.Component<StepProps<PCRItemForAccountNameChangeDto
 
   private renderForm(documentsEditor: IEditorStore<MultipleDocumentUploadDto, MultipleDocumentUpdloadDtoValidator>): React.ReactNode {
     const UploadForm = ACC.TypedForm<MultipleDocumentUploadDto>();
-    const ItemForm = ACC.TypedForm<PCRItemForAccountNameChangeDto>();
     return (
       <ACC.Section>
         <UploadForm.Form
           enctype="multipart"
           editor={documentsEditor}
-          onSubmit={() => this.props.onFileChange(true, documentsEditor.data)}
-          onChange={(dto) => this.props.onFileChange(false, dto)}
+          onSubmit={() => this.props.onFileChange(documentsEditor.data, false, true)}
+          onChange={(dto) => this.props.onFileChange(dto, false, false)}
           qa="projectChangeRequestItemUpload"
         >
-          <UploadForm.Fieldset heading="Upload">
+          <UploadForm.Fieldset heading="Upload change of name certificate">
             <ACC.Renderers.SimpleString>You can upload up to 10 files of any type, as long as their combined file size is less than 10MB.</ACC.Renderers.SimpleString>
             <UploadForm.MulipleFileUpload
               label="Upload files"
@@ -46,17 +45,13 @@ class Component extends React.Component<StepProps<PCRItemForAccountNameChangeDto
               validation={documentsEditor.validator.files}
             />
           </UploadForm.Fieldset>
-          <UploadForm.Button name="uploadFile" styling="Secondary">Upload documents</UploadForm.Button>
+          <UploadForm.Fieldset>
+            <UploadForm.Button name="uploadFile" styling="Secondary" onClick={() => this.props.onFileChange(documentsEditor.data, true, false)}>Upload documents</UploadForm.Button>
+          </UploadForm.Fieldset>
+          <UploadForm.Fieldset>
+            <UploadForm.Button name="uploadFileAndContinue" styling="Primary">Upload documents and continue</UploadForm.Button>
+          </UploadForm.Fieldset>
         </UploadForm.Form>
-        <ItemForm.Form
-          qa="changePartnerNameForm"
-          data={this.props.pcrItem}
-          isSaving={this.props.status === EditorStatus.Saving}
-          onSubmit={() => this.props.onSave()}
-          onChange={dto => this.props.onChange(dto)}
-        >
-          <ItemForm.Submit>Save and continue</ItemForm.Submit>
-        </ItemForm.Form>
       </ACC.Section>
     );
   }
@@ -64,42 +59,46 @@ class Component extends React.Component<StepProps<PCRItemForAccountNameChangeDto
   private renderFiles(documentsEditor: IEditorStore<MultipleDocumentUploadDto, MultipleDocumentUpdloadDtoValidator>, documents: DocumentSummaryDto[]) {
     if (documents.length) {
       return (
-        <ACC.Section  title="Files uploaded" subtitle="All documents open in a new window.">
+        <ACC.Section title="Files uploaded" subtitle="All documents open in a new window.">
           <ACC.DocumentListWithDelete onRemove={(document) => this.props.onFileDelete(documentsEditor.data, document)} documents={documents} qa="supporting-documents" />
         </ACC.Section>
       );
     }
     return (
-      <ACC.Section  title="Files uploaded">
+      <ACC.Section title="Files uploaded">
         <ACC.ValidationMessage message="No documents uploaded." messageType="info" />
       </ACC.Section>
     );
   }
 }
 
-export const PCRPrepareItemFilesStep = (props: StepProps<PCRItemForAccountNameChangeDto, PCRAccountNameChangeItemDtoValidator>) => (
-    <StoresConsumer>
-      {
-        stores => {
-          return <ACC.Loader
-            pending={stores.projectChangeRequestDocuments.pcrOrPcrItemDocuments(props.project.id, props.pcrItem.id)}
-            render={documents => (
-              <Component
-                {...props}
-                documents={documents}
-                onFileChange={(saving, dto) => {
-                  stores.messages.clearMessages();
-                  const successMessage = dto.files.length === 1 ? `Your document has been uploaded.` : `${dto.files.length} documents have been uploaded.`;
-                  stores.projectChangeRequestDocuments.updatePcrOrPcrItemDocumentsEditor(saving, props.project.id, props.pcrItem.id, dto, successMessage);
-                }}
-                onFileDelete={(dto, document) => {
-                  stores.messages.clearMessages();
-                  stores.projectChangeRequestDocuments.deletePcrOrPcrItemDocumentsEditor(props.project.id, props.pcrItem.id, dto, document, "Your document has been removed.");
-                }}
-              />
-            )}
-          />;
-        }
+export const PCRPrepareItemFilesStep = (props: StepProps<typeof accountNameChangeWorkflow>) => (
+  <StoresConsumer>
+    {
+      stores => {
+        return <ACC.Loader
+          pending={stores.projectChangeRequestDocuments.pcrOrPcrItemDocuments(props.project.id, props.pcrItem.id)}
+          render={documents => (
+            <Component
+              {...props}
+              documents={documents}
+              onFileChange={(dto, saveAndRemain, saveAndContinue) => {
+                stores.messages.clearMessages();
+                const successMessage = dto.files.length === 1 ? `Your document has been uploaded.` : `${dto.files.length} documents have been uploaded.`;
+                stores.projectChangeRequestDocuments.updatePcrOrPcrItemDocumentsEditor(saveAndRemain || saveAndContinue, props.project.id, props.pcrItem.id, dto, successMessage, () => {
+                  if (saveAndContinue) {
+                    props.onSave();
+                  }
+                });
+              }}
+              onFileDelete={(dto, document) => {
+                stores.messages.clearMessages();
+                stores.projectChangeRequestDocuments.deletePcrOrPcrItemDocumentsEditor(props.project.id, props.pcrItem.id, dto, document, "Your document has been removed.");
+              }}
+            />
+          )}
+        />;
       }
-    </StoresConsumer>
+    }
+  </StoresConsumer>
 );
