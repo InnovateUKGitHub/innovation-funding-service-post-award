c02f836c8896bd9bf1201ee05bdd840a7476d20d
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2019-11-29 15:09:32 +0000

    ACC-4905 create supporting documents page

---
+++ app/src/server/features/documents/uploadClaimDocuments.ts
@@ -0,0 +1,36 @@
+import { BadRequestError, CommandBase, ValidationError } from "@server/features/common";
+import { MultipleDocumentUpdloadDtoValidator } from "@ui/validators/documentUploadValidator";
+import { Authorisation, IContext, ProjectRole } from "@framework/types";
+
+export class UploadClaimDocumentsCommand extends CommandBase<string[]> {
+  constructor(private readonly claimKey: ClaimKey, private readonly documents: MultipleDocumentUploadDto) {
+    super();
+  }
+
+  protected LogMessage() {
+    return [this.constructor.name, this.claimKey, this.documents && this.documents.files && this.documents.files.map(x => x.fileName)];
+  }
+
+  protected async accessControl(auth: Authorisation) {
+    return auth.forPartner(this.claimKey.projectId, this.claimKey.partnerId).hasRole(ProjectRole.FinancialContact);
+  }
+
+  protected async Run(context: IContext) {
+    const claim = await context.repositories.claims.get(this.claimKey.partnerId, this.claimKey.periodId);
+
+    if (!claim) {
+      throw new BadRequestError("No Claim");
+    }
+
+    const result = new MultipleDocumentUpdloadDtoValidator(this.documents, context.config, true);
+
+    if (!result.isValid) {
+      throw new ValidationError(result);
+    }
+
+    return Promise.all(
+      this.documents.files.filter(x => x.fileName && x.size)
+        .map(async (document) => await context.repositories.documents.insertDocument(document, claim.Id))
+    );
+  }
+}
