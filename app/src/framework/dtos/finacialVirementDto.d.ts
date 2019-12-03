b40578415592cbe5fd9d6848f6f78b13b2023fa3
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-12-03 09:28:29 +0000

    Initial work for Financial Virement api

---
+++ app/src/framework/dtos/finacialVirementDto.d.ts
@@ -0,0 +1,19 @@
+interface FinancialVirementDto {
+  pcrItemId: string;
+  originalTotal: number;
+  currentTotal: number;
+  partners: PartnerVirementsDto[]
+}
+
+interface PartnerVirementsDto {
+  partnerId: string;
+  originalTotal: number;
+  currentTotal: number;
+  partnerVirements: VirementDto[];
+}
+
+interface VirementDto {
+  costCategoryId: string;
+  originalAmount: number;
+  currentAmount: number;
+}
