9a98b664d85eea1b18baa030c504de26ae44c63c
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2019-12-05 10:56:10 +0000

    claim documents form handler

---
+++ app/src/server/forms/claimDocuments/claimDocumentsUploadHandler.ts
@@ -0,0 +1,37 @@
+import { IFormBody, IFormButton, MultipleFileFormHandlerBase } from "../formHandlerBase";
+import { ClaimDocumentsPageParams, ClaimDocumentsRoute } from "../../../ui/containers";
+import { IContext } from "@framework/types/IContext";
+import { ILinkInfo } from "@framework/types/ILinkInfo";
+import { Configuration } from "@server/features/common";
+import { UploadClaimDocumentsCommand } from "@server/features/documents/uploadClaimDocuments";
+import { getClaimDocumentsKey } from "@ui/redux/stores/storeKeys";
+import { MultipleDocumentUpdloadDtoValidator } from "@ui/validators";
+
+export class ClaimDocumentsUploadHandler extends MultipleFileFormHandlerBase<ClaimDocumentsPageParams, "multipleDocuments"> {
+  constructor() {
+    super(ClaimDocumentsRoute, ["upload"], "multipleDocuments");
+  }
+
+  protected async getDto(context: IContext, params: ClaimDocumentsPageParams, button: IFormButton, body: IFormBody, files: IFileWrapper[]): Promise<MultipleDocumentUploadDto> {
+    return {
+      files,
+      description: body.description
+    };
+  }
+
+  protected async run(context: IContext, params: ClaimDocumentsPageParams, button: IFormButton, dto: MultipleDocumentUploadDto): Promise<ILinkInfo> {
+    const claimDocumentsKey = { projectId: params.projectId, partnerId: params.partnerId, periodId: params.periodId };
+
+    await context.runCommand(new UploadClaimDocumentsCommand(claimDocumentsKey, dto));
+
+    return ClaimDocumentsRoute.getLink(params);
+  }
+
+  protected getStoreKey(params: ClaimDocumentsPageParams) {
+    return getClaimDocumentsKey(params.partnerId, params.periodId);
+  }
+
+  protected createValidationResult(params: ClaimDocumentsPageParams, dto: MultipleDocumentUploadDto) {
+    return new MultipleDocumentUpdloadDtoValidator(dto, Configuration, true, true);
+  }
+}
