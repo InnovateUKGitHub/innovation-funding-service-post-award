2957cdd922b1cc606a2d7d4e84b33679b72157ba
Oliver Armfelt <oliver.armfelt@bjss.com>
2019-12-19 17:43:53 +0000

    Ensure that calculations with award rate resolve percentage first

--- app/src/ui/components/claims/claimTable.tsx
+++ app/src/ui/components/claims/claimTable.tsx
@@ -92,7 +92,7 @@ const renderFooters = (project: ProjectDto, partner: PartnerDto, claimsCosts: Co
       <tr key="2" className="govuk-table__row">
         <td className="govuk-table__cell govuk-table__cell--numeric govuk-!-font-weight-bold" colSpan={3}>Costs to be paid this period</td>
         <td className="govuk-table__cell govuk-table__cell--numeric">
-          <Currency value={claimsCosts.reduce((total, item) => total + item.costsClaimedThisPeriod, 0) * partner.awardRate! / 100} />
+          <Currency value={claimsCosts.reduce((total, item) => total + item.costsClaimedThisPeriod, 0) * (partner.awardRate! / 100)} />
         </td>
         <td className="govuk-table__cell"><AccessibilityText>No data</AccessibilityText></td>
       </tr>
