d84ffdbf49960777b41ad3f54ee3b0bed4fb7846
Cosmin-Stefan Dobranici <cosmin-stefan.dobranici@bjss.com>
2020-01-28 11:56:05 +0000

    Created mapPCRStatus method

--- app/src/server/features/pcrs/getPcrStatusesQuery.ts
+++ app/src/server/features/pcrs/getPcrStatusesQuery.ts
@@ -1,6 +1,7 @@
 import { QueryBase } from "../common/queryBase";
 import { IContext, PCRStatus } from "@framework/types";
 import { OptionsQueryBase } from "../common/optionsQueryBase";
+import { mapToPCRStatus } from "@server/repositories/mappers/projectChangeRequestMapper";
 
 export class GetPcrStatusesQuery extends OptionsQueryBase<PCRStatus> {
   constructor() {
@@ -12,34 +13,6 @@ export class GetPcrStatusesQuery extends OptionsQueryBase<PCRStatus> {
   }
 
   protected mapToEnumValue(value: string) {
-    switch (value) {
-        case "Draft":
-          return PCRStatus.Draft;
-        case "Submitted to Monitoring Officer":
-          return PCRStatus.SubmittedToMonitoringOfficer;
-        case "Queried by Monitoring Officer":
-          return PCRStatus.QueriedByMonitoringOfficer;
-        case "Submitted to Innovation Lead":
-          return PCRStatus.SubmittedToInnovationLead;
-        case "Queried by Innovate UK":
-          return PCRStatus.QueriedByInnovateUK;
-        case "In External Review":
-          return PCRStatus.InExternalReview;
-        case "In Review with Innovate UK":
-          return PCRStatus.InReviewWithInnovateUK;
-        case "Rejected":
-          return PCRStatus.Rejected;
-        case "Withdrawn":
-          return PCRStatus.Withdrawn;
-        case "Approved":
-          return PCRStatus.Approved;
-        case "Actioned":
-          return PCRStatus.Actioned;
-        case "Queried by Innovation Lead":
-          // This PCR Status is currently not intended for ACC, as it is meant only for Innovate internal use;
-          return PCRStatus.InExternalReview;
-        default:
-          return PCRStatus.Unknown;
-      }
-    }
+      return mapToPCRStatus(value);
+  }
 }
