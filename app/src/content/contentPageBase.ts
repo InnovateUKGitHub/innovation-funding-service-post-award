3354e38ce0ad9d97f616e03b2d89d278e10b5e40
Oliver Armfelt <oliver.armfelt@bjss.com>
2020-02-03 14:41:20 +0000

    Adds content to the projects dashboard

---
+++ app/src/content/contentPageBase.ts
@@ -0,0 +1,38 @@
+import i18next from "i18next";
+import { ContentBase } from "./contentBase";
+import { Content } from "./content";
+
+export abstract class ContentPageBase extends ContentBase {
+
+  constructor(parent: Content, pageName: string) {
+    super(parent, `pages.${pageName}`);
+  }
+
+  public title() {
+    const prefix = super.getNameParts().join(".");
+    const htmlKey = `${prefix}.title.html`;
+    const displayKey = `${prefix}.title.display`;
+
+    const html = i18next.exists(htmlKey) ? i18next.t(htmlKey) : null;
+    const display = i18next.exists(displayKey) ? i18next.t(displayKey) : null;
+
+    if (html || display) {
+      return {
+        htmlTitle: html || htmlKey,
+        htmlTitleKey: htmlKey,
+        displayTitle: display || displayKey,
+        displayTitleKey: displayKey,
+      };
+    }
+
+    const generalKey = `${prefix}.title`;
+    const general = i18next.exists(generalKey) ? i18next.t(generalKey) : null;
+    return {
+      htmlTitle: general || generalKey,
+      htmlTitleKey: generalKey,
+      displayTitle: general || generalKey,
+      displayTitleKey: generalKey,
+    };
+
+  }
+}
--- app/src/ui/redux/actions/pageTitle.ts
+++ app/src/ui/redux/actions/pageTitle.ts
@@ -1,14 +1,15 @@
 import { AsyncThunk, createAction } from "./common/createAction";
 import { MatchedRoute } from "@ui/routing/matchRoute";
 import { IStores } from "../storesProvider";
+import { Content } from "@content/content";
 
 export type SetPageTitleAction = ReturnType<typeof setPageTitle>;
 export const setPageTitle = (title: { displayTitle: string, htmlTitle: string }) => createAction("SET_PAGE_TITLE", title);
 
-export const udpatePageTitle = (route: MatchedRoute, params: {}, stores: IStores): AsyncThunk<void, SetPageTitleAction> => {
+export const udpatePageTitle = (route: MatchedRoute, params: {}, stores: IStores, content: Content): AsyncThunk<void, SetPageTitleAction> => {
   return (dispatch, getState) => {
     const state = getState();
-    const title = route.getTitle(state, params, stores);
+    const title = route.getTitle(state, params, stores, content);
 
     if(!state.title || title.displayTitle !== state.title.displayTitle || title.htmlTitle !== state.title.htmlTitle) {
       dispatch(setPageTitle(title));
