f259d8bff936ca88e614cb03266d83778ccc435e
Oliver Armfelt <oliver.armfelt@bjss.com>
2020-02-11 12:17:35 +0000

    Adds content to the error pages

--- app/src/ui/components/notFoundErrorPage.tsx
+++ app/src/ui/components/notFoundErrorPage.tsx
@@ -3,16 +3,23 @@ import { Page } from "./layout/page";
 import { Section } from "./layout/section";
 import { SimpleString } from "./renderers/simpleString";
 import { PageTitle } from "@ui/components/layout";
+import { ContentConsumer } from "@ui/redux/contentProvider";
 
 export const NotFoundErrorPage = () => (
-  <Page
-    pageTitle={<PageTitle title="Page not found" />}
-    backLink={null}
-  >
-    <Section>
-      <SimpleString qa="errorMessage">
-        Please check the web address or search term you entered for any errors. You can return to <a href="/projects/dashboard">your dashboard</a> or go back to the <a href="/">Innovate UK homepage</a>.
-      </SimpleString>
-    </Section>
-  </Page>
+  <ContentConsumer>
+    {
+      content => (
+        <Page
+          pageTitle={<PageTitle title={content.errors.notfound.title().displayTitle} />}
+          backLink={null}
+        >
+          <Section>
+            <SimpleString qa="errorMessage">
+              Please check the web address or search term you entered for any errors. You can return to <a href="/projects/dashboard">your dashboard</a> or go back to the <a href="/">Innovate UK homepage</a>.
+            </SimpleString>
+          </Section>
+        </Page>
+      )
+    }
+  </ContentConsumer>
 );
--- app/src/ui/containers/errors.tsx
+++ app/src/ui/containers/errors.tsx
@@ -3,11 +3,11 @@ import { BaseProps, defineRoute } from "./containerBase";
 import { StandardErrorPage } from "../components/standardErrorPage";
 import { NotFoundErrorPage } from "../components/notFoundErrorPage";
 
-interface Params {
+interface Props {
   errorType: "standard" | "notFound";
 }
 
-const ErrorContainer = (props: Params & BaseProps) => (
+const ErrorContainer = (props: Props & BaseProps) => (
   props.errorType === "notFound" ? <NotFoundErrorPage /> : <StandardErrorPage />
 );
 
@@ -16,10 +16,7 @@ export const ErrorRoute = defineRoute<{}>({
   routePath: "/error",
   container: (params) => <ErrorContainer errorType="standard" {...params}/>,
   getParams: () => ({}),
-  getTitle: () => ({
-    htmlTitle: "Error",
-    displayTitle: "Something has gone wrong at our end"
-  })
+  getTitle: ({content}) => content.errors.unexpected.title()
 });
 
 export const ErrorNotFoundRoute = defineRoute<{}>({
@@ -27,8 +24,5 @@ export const ErrorNotFoundRoute = defineRoute<{}>({
   routePath: "/error-not-found",
   container: (params) => <ErrorContainer errorType="notFound" {...params}/>,
   getParams: () => ({}),
-  getTitle: () => ({
-    htmlTitle: "Page not found",
-    displayTitle: "Page not found"
-  })
+  getTitle: ({content}) => content.errors.notfound.title()
 });
