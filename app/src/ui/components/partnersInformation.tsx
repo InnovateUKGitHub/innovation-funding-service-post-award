4acb38718d3d297ae12ad7206255cb195dc761f4
ainara.saneduardo <ainara.saneduardo@bjss.com>
2020-02-14 10:21:34 +0000

    mend

--- app/src/ui/components/partnersInformation.tsx
+++ app/src/ui/components/partnersInformation.tsx
@@ -15,7 +15,7 @@ export const PartnersContactInformation: React.FunctionComponent<Props> = (props
     const PartnersTable = TypedTable<typeof props.partners[0]>();
 
     return (
-        <PartnersTable.Table qa="partner-details" data={props.partners}>
+        <PartnersTable.Table qa="partner-information" data={props.partners}>
             <PartnersTable.Custom headerContent={x => props.projectContactLabels(x).partnerName()} value={x => <PartnerName partner={x} showIsLead={true} />} qa="partner-name" />
             <PartnersTable.String headerContent={x => props.projectContactLabels(x).partnerType()} value={x => x.type} qa="partner-type" />
             <PartnersTable.String headerContent={x => props.projectContactLabels(x).partnerPostcode()} value={x => x.postcode} qa="partner-postcode" />
