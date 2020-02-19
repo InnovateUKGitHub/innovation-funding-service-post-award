bb4f60e7e39ad57c3caad7064ae189afdf9253dd
Oliver Armfelt <oliver.armfelt@bjss.com>
2020-02-19 10:34:42 +0000

    Adds goggle analytics to health check end point

--- app/src/server/health.ts
+++ app/src/server/health.ts
@@ -10,25 +10,58 @@ export const router = express.Router();
 
 const endpoint = "/api/health";
 
-export const health = async (logger: ILogger) => {
-  const salesforce = await new CostCategoryRepository(() => salesforceConnectionWithToken({
+const checkSalesforce = (logger: ILogger) => {
+  const getSalesforceConnection = () => salesforceConnectionWithToken({
     clientId: Configuration.salesforce.clientId,
     connectionUrl: Configuration.salesforce.connectionUrl,
     currentUsername: Configuration.salesforce.serivceUsername
-  }), logger)
+  });
+
+  return new CostCategoryRepository(getSalesforceConnection, logger)
     .getAll()
-    .then(_ => true)
+    .then(() => true)
     .catch(e => {
       logger.error("SALESFORCE HEALTH CHECK", new AppError(ErrorCode.UNKNOWN_ERROR, e.message, e));
       return false;
     });
-
-  const status = salesforce ? 200 : 500;
-  const response = { salesforce };
-  return { status, response };
 };
 
-// health check endpoint tests dependencies ie slaesforce connection
+const checkGoogleAnalytics = (logger: ILogger) => {
+  if (!Configuration.googleTagManagerCode) {
+    return Promise.resolve(null);
+  }
+
+  const url = `https://www.googletagmanager.com/ns.html?id=${Configuration.googleTagManagerCode}`;
+  return fetch(url)
+    .then((e) => {
+      if (e.ok) {
+        return true;
+      }
+      throw new Error(`Failed get request to ${url}`);
+    })
+    .catch(e => {
+      logger.error("GOOGLE ANALYTICS HEALTH CHECK", new AppError(ErrorCode.UNKNOWN_ERROR, e.message, e));
+      return false;
+    });
+};
+
+export const health = async (logger: ILogger) => {
+  const salesforce = checkSalesforce(logger);
+  const googleAnalytics = checkGoogleAnalytics(logger);
+
+  const results = await Promise.all([salesforce, googleAnalytics]);
+
+  // awaits in response are required to get result but will have already run in promise all
+  return {
+    status: results.some(x => x === false) ? 500 : 200,
+    response: {
+      salesforce: await salesforce,
+      googleAnalytics: await googleAnalytics
+    }
+  };
+};
+
+// health check endpoint tests dependencies ie salesforce connection
 router.get(`${endpoint}/details`, async (req, res) => {
   const logger = new Logger("Health check");
   const result = await health(logger);
