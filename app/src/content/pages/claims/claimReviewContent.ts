e832bd670f2bb4cdea248b2d63ebf1a6da9fad9d
Cosmin-Stefan Dobranici <Cosmin-Stefan.Dobranici@bjss.com>
2020-03-03 16:43:27 +0000

    .

--- app/src/content/pages/claims/claimReviewContent.ts
+++ app/src/content/pages/claims/claimReviewContent.ts
@@ -2,12 +2,14 @@ import { ContentPageBase } from "../../contentPageBase";
 import { Content } from "../../content";
 import { ClaimMessages } from "@content/messages/claimMessages";
 import { ClaimsLabels } from "@content/labels/claimsLabels";
+import { DocumentMessages } from "@content/messages/documentMessages";
 
 export class ClaimReviewContent extends ContentPageBase {
   constructor(private content: Content) {
     super(content, "claim-review");
   }
   public readonly messages = new ClaimMessages(this);
+  public readonly documentMessages = new DocumentMessages(this);
   public readonly labels = new ClaimsLabels(this);
   public readonly backLink = () => this.getContent("back-link");
   public readonly queryClaimOption = () => this.getContent("option-query-claim");
@@ -17,10 +19,7 @@ export class ClaimReviewContent extends ContentPageBase {
   public readonly additionalInfoHint = () => this.getContent("additional-info-hint");
   public readonly submitButton = () => this.getContent("button-submit");
   public readonly sendQueryButton = () => this.getContent("button-send-query");
-
   public readonly uploadButton = () => this.getContent("button-upload");
-  public readonly uploadInput = () => this.getContent("input-button");
-
-  public readonly uploadClaimValidationFormInstructions = () => this.getContent("claims-messages.upload-instruction-claim-validation-form", { markdown: true });
+  public readonly uploadInputLabel = () => this.getContent("label-input-upload");
   public readonly uploadClaimValidationFormAccordionTitle = () => this.getContent("accordion-title-upload-claim-validation-form");
 }
