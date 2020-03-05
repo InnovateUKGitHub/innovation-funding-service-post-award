230edecdd0342044f5e45733ffe9d04771872445
Vasilia <vasilia.themis@bjss.com>
2020-03-05 12:08:16 +0000

    Added current claim cost to forecast dashboard totals

--- app/src/ui/containers/forecasts/dashboard.tsx
+++ app/src/ui/containers/forecasts/dashboard.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BaseProps, ContainerBase, defineRoute } from "../containerBase";
 import * as ACC from "../../components";
 import { Pending } from "@shared/pending";
-import { PartnerDto, ProjectDto, ProjectRole } from "@framework/dtos";
+import { ClaimDto, PartnerDto, ProjectDto, ProjectRole } from "@framework/dtos";
 import { StoresConsumer } from "@ui/redux";
 
 interface Data {
   projectDetails: Pending<ProjectDto>;
   partners: Pending<PartnerDto[]>;
+  activeClaims: Pending<ClaimDto[]>;
 }
 
 interface Params {
@@ -18,12 +19,18 @@ interface Callbacks {}
 
 class ProjectForecastComponent extends ContainerBase<Params, Data, Callbacks> {
   render() {
-    const combined = Pending.combine({projectDetails: this.props.projectDetails, partners: this.props.partners});
-    return <ACC.PageLoader pending={combined} render={x => this.renderContents(x.projectDetails, x.partners)} />;
+    const combined = Pending.combine({projectDetails: this.props.projectDetails, partners: this.props.partners, activeClaims: this.props.activeClaims});
+    return <ACC.PageLoader pending={combined} render={x => this.renderContents(x.projectDetails, x.partners, x.activeClaims)} />;
   }
 
-  private renderContents(project: ProjectDto, partners: PartnerDto[]) {
+  private renderContents(project: ProjectDto, partners: PartnerDto[], activeClaims: ClaimDto[]) {
     const Table = ACC.TypedTable<PartnerDto>();
+
+    const getForecastsAndCosts = (partner: PartnerDto) => {
+      const claim = activeClaims.find(x => x.partnerId === partner.id);
+      const claimCost = claim ? claim.totalCost || 0 : 0;
+      return claimCost + (partner.totalFutureForecastsForParticipants || 0) + (partner.totalCostsSubmitted || 0);
+    };
 
     return (
       <ACC.Page
@@ -35,8 +42,8 @@ class ProjectForecastComponent extends ContainerBase<Params, Data, Callbacks> {
           <Table.Table data={partners} qa="partner-table">
             <Table.Custom header="Partner" value={x => <ACC.PartnerName partner={x} showIsLead={true}/>} qa="partner" />
             <Table.Currency header="Total eligible costs" value={x => x.totalParticipantGrant} qa="grant-offered" />
-            <Table.Currency header="Forecasts and costs" value={x => (x.totalFutureForecastsForParticipants || 0) + (x.totalCostsSubmitted || 0)} qa="forecasts-and-costs" />
-            <Table.Currency header="Underspend" value={x => x.totalParticipantGrant! - (x.totalFutureForecastsForParticipants || 0) - (x.totalCostsSubmitted || 0)} qa="underspend" />
+            <Table.Currency header="Forecasts and costs" value={x => getForecastsAndCosts(x)} qa="forecasts-and-costs" />
+            <Table.Currency header="Underspend" value={x => x.totalParticipantGrant! - getForecastsAndCosts(x)} qa="underspend" />
             <Table.ShortDate header="Date of last update" value={x => x.forecastLastModifiedDate} qa="last-updated" />
             <Table.Link header="Action" hideHeader={true} value={x => this.props.routes.forecastDetails.getLink({ projectId: this.props.projectId, partnerId: x.id })} content="View forecast" qa="view-partner-forecast" />
           </Table.Table>
@@ -52,6 +59,7 @@ const ForecastDashboardContainer = (props: Params & BaseProps) => (
       <ProjectForecastComponent
         projectDetails={stores.projects.getById(props.projectId)}
         partners={stores.partners.getPartnersForProject(props.projectId)}
+        activeClaims={stores.claims.getActiveClaimsForProject(props.projectId)}
         {...props}
       />
     )}
