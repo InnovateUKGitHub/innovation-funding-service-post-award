aaedc56839eef1ae38a98823928f8b07cac4b969
Vasilia <vasilia.themis@bjss.com>
2020-03-19 16:53:16 +0000

    project details page improvements

--- app/src/ui/components/projectContact.tsx
+++ app/src/ui/components/projectContact.tsx
@@ -2,27 +2,30 @@ import React from "react";
 import { Email } from "./renderers/email";
 import { PartnerDto } from "@framework/dtos";
 import { PartnerName } from "@ui/components/partners";
+import { Section } from "@ui/components/layout";
+import { SimpleString } from "@ui/components/renderers";
 
 interface Props {
-    contact?: ProjectContactDto | null;
-    partner?: PartnerDto | null;
-    qa: string;
+  contact?: ProjectContactDto | null;
+  partner?: PartnerDto | null;
+  qa: string;
 }
 
 export const ProjectContact: React.FunctionComponent<Props> = (props) => {
 
-    const contact = props.contact;
+  const contact = props.contact;
 
-    const partner = props.partner;
+  const partner = props.partner;
 
-    if (!contact) { return null; }
+  if (!contact) {
+    return null;
+  }
 
-    return (
-        <div className="govuk-heading-m govuk-!-margin-bottom-9">
-            <h3 className="govuk-heading-s govuk-!-margin-bottom-0" data-qa={`${props.qa}-roleName`}>{contact.roleName}</h3>
-            <p className="govuk-body govuk-!-margin-bottom-0" data-qa={`${props.qa}-name`}>{contact.name}</p>
-            <p className="govuk-body govuk-!-margin-bottom-0" data-qa={`${props.qa}-partner`}><PartnerName partner={partner}/></p>
-            <Email value={contact.email} qa={props.qa + "-email"} />
-        </div>
-    );
+  return (
+    <Section title={contact.roleName}>
+      <SimpleString className={"govuk-!-margin-bottom-0"} data-qa={`${props.qa}-roleName`}>{contact.name}</SimpleString>
+      <SimpleString className={"govuk-!-margin-bottom-0"} data-qa={`${props.qa}-name`}><PartnerName partner={partner}/></SimpleString>
+      <Email value={contact.email} qa={props.qa + "-email"}/>
+    </Section>
+  );
 };
