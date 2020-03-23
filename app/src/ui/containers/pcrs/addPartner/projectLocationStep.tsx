cee1c3667998940cc0c83ae29181c0fabc0be937
Chris <christopher.juckes@bjss.com>
2020-03-23 11:36:08 +0000

    Add Project Location steps to addPartner workflow, also API tests

---
+++ app/src/ui/containers/pcrs/addPartner/projectLocationStep.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import * as ACC from "@ui/components";
+import { PCRItemForPartnerAdditionDto } from "@framework/dtos";
+import { EditorStatus } from "@ui/redux";
+import { PcrStepProps } from "@ui/containers/pcrs/pcrWorkflow";
+import { PCRPartnerAdditionItemDtoValidator } from "@ui/validators";
+
+export const ProjectLocationStep = (props: PcrStepProps<PCRItemForPartnerAdditionDto, PCRPartnerAdditionItemDtoValidator>) => {
+  const Form = ACC.TypedForm<PCRItemForPartnerAdditionDto>();
+  return (
+    <ACC.Section>
+      <Form.Form
+        qa="addPartnerForm"
+        data={props.pcrItem}
+        isSaving={props.status === EditorStatus.Saving}
+        onSubmit={() => props.onSave()}
+        onChange={dto => props.onChange(dto)}
+      >
+        <Form.Fieldset heading="Name of town or city">
+          <Form.String
+            name="projectCity"
+            value={dto => dto.projectCity}
+            update={(x, val) => {
+              x.projectCity = val;
+            }}
+            validation={props.validator.projectCity}
+          />
+        </Form.Fieldset>
+        <Form.Fieldset heading="Postcode, postal code or zip code">
+          <Form.String
+            name="projectPostcode"
+            hint="If this is not available, leave this blank."
+            value={dto => dto.projectPostcode}
+            update={(x, val) => {
+              x.projectPostcode = val;
+            }}
+            validation={props.validator.projectPostcode}
+          />
+        </Form.Fieldset>
+        <Form.Fieldset qa="save-and-continue">
+          <Form.Submit>Save and continue</Form.Submit>
+        </Form.Fieldset>
+      </Form.Form>
+    </ACC.Section>
+  );
+};
