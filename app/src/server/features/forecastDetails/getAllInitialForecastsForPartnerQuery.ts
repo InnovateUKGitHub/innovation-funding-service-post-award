7a618fda1db8bd07ae009bc34629b54a2941f216
Vasilia <vasilia.themis@bjss.com>
2020-04-15 12:26:31 +0100

    Initial forecasts apis

--- app/src/server/features/forecastDetails/getAllInitialForecastsForPartnerQuery.ts
+++ app/src/server/features/forecastDetails/getAllInitialForecastsForPartnerQuery.ts
@@ -1,14 +1,14 @@
 import { QueryBase } from "../common";
 import { IContext } from "@framework/types";
-import { mapLatestForecastDetail } from "@server/features/forecastDetails/mapForecastDetail";
+import { mapInitialForecastDetail } from "@server/features/forecastDetails/mapForecastDetail";
 
-export class GetAllForecastsForPartnerQuery extends QueryBase<ForecastDetailsDTO[]> {
+export class GetAllInitialForecastsForPartnerQuery extends QueryBase<ForecastDetailsDTO[]> {
   constructor(private readonly partnerId: string) {
     super();
   }
 
   protected async Run(context: IContext) {
     const results = await context.repositories.profileDetails.getAllByPartner(this.partnerId);
-    return results.map(mapLatestForecastDetail(context));
+    return results.map(mapInitialForecastDetail(context));
   }
 }
--- app/src/server/features/forecastDetails/mapForecastDetail.ts
+++ app/src/server/features/forecastDetails/mapForecastDetail.ts
@@ -2,11 +2,20 @@ import { ISalesforceProfileDetails } from "../../repositories";
 import { IContext } from "@framework/types";
 import { SALESFORCE_DATE_FORMAT } from "../common";
 
-export default (context: IContext) => (forecastDetail: ISalesforceProfileDetails): ForecastDetailsDTO => ({
+const mapCommon = (context: IContext, forecastDetail: ISalesforceProfileDetails): Pick<ForecastDetailsDTO, Exclude<keyof ForecastDetailsDTO, "value">> => ({
   id: forecastDetail.Id,
   costCategoryId: forecastDetail.Acc_CostCategory__c,
   periodId: forecastDetail.Acc_ProjectPeriodNumber__c,
   periodStart: context.clock.parse(forecastDetail.Acc_ProjectPeriodStartDate__c, SALESFORCE_DATE_FORMAT),
   periodEnd: context.clock.parse(forecastDetail.Acc_ProjectPeriodEndDate__c, SALESFORCE_DATE_FORMAT),
+});
+
+export const mapLatestForecastDetail = (context: IContext) => (forecastDetail: ISalesforceProfileDetails): ForecastDetailsDTO => ({
+  ...mapCommon(context, forecastDetail),
   value: forecastDetail.Acc_LatestForecastCost__c
+});
+
+export const mapInitialForecastDetail = (context: IContext) => (forecastDetail: ISalesforceProfileDetails): ForecastDetailsDTO => ({
+  ...mapCommon(context, forecastDetail),
+  value: forecastDetail.Acc_InitialForecastCost__c
 });
