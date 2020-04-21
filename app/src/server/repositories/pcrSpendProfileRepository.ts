d690cd4bc9a4f5587efcca648d324cc273f5d30f
Vasilia <vasilia.themis@bjss.com>
2020-04-21 11:50:00 +0100

    Update & tests for Spend Profile repository & get API

--- app/src/server/repositories/pcrSpendProfileRepository.ts
+++ app/src/server/repositories/pcrSpendProfileRepository.ts
@@ -1,22 +1,30 @@
 import { Connection } from "jsforce";
-import SalesforceRepositoryBase, { SalesforceRepositoryBaseWithMapping } from "./salesforceRepositoryBase";
+import SalesforceRepositoryBase, {
+  Insertable,
+  SalesforceRepositoryBaseWithMapping,
+  Updatable
+} from "./salesforceRepositoryBase";
 import { ILogger } from "@server/features/common/logger";
-import { PcrSpendProfileEntity } from "@framework/entities/pcrSpendProfile";
+import { PcrSpendProfileEntity, PcrSpendProfileEntityForCreate } from "@framework/entities/pcrSpendProfile";
 import { SalesforcePcrSpendProfileMapper } from "@server/repositories/mappers/pcrSpendProfileMapper";
+import { ISalesforceFinancialVirement } from "@server/repositories/financialVirementRepository";
 
 export interface ISalesforcePcrSpendProfile {
   Id: string;
   Acc_ProjectChangeRequest__c: string;
   Acc_CostCategoryID__c: string;
   RecordTypeId: string;
-  Acc_CostOfRole__c: number;
+  Acc_CostOfRole__c?: number;
 }
 
 export interface IPcrSpendProfileRepository {
   getAllForPcr(pcrItemId: string): Promise<PcrSpendProfileEntity[]>;
+  insertSpendProfiles(items: PcrSpendProfileEntityForCreate[]): Promise<string[]>;
+  updateSpendProfiles(items: PcrSpendProfileEntity[]): Promise<boolean>;
+  deleteSpendProfiles(items: string[]): Promise<void>;
 }
 
-export class PcrSpendProfileRepository extends SalesforceRepositoryBaseWithMapping<ISalesforcePcrSpendProfile, PcrSpendProfileEntity> implements IPcrSpendProfileRepository {
+export class PcrSpendProfileRepository extends SalesforceRepositoryBase<ISalesforcePcrSpendProfile> implements IPcrSpendProfileRepository {
   constructor(private getRecordTypeId: (objectName: string, recordType: string) => Promise<string>, getSalesforceConnection: () => Promise<Connection>, logger: ILogger) {
     super(getSalesforceConnection, logger);
   }
@@ -30,13 +38,33 @@ export class PcrSpendProfileRepository extends SalesforceRepositoryBaseWithMappi
     "Acc_CostOfRole__c",
   ];
 
-  protected mapper = new SalesforcePcrSpendProfileMapper();
+  private recordType = "PCR Spend Profile";
 
   public async getAllForPcr(pcrItemId: string): Promise<PcrSpendProfileEntity[]> {
-    const pcrRecordType = await this.getRecordTypeId(this.salesforceObjectName, "PCR Spend Profile");
-    return super.where({
+    const pcrRecordTypeId = await this.getRecordTypeId(this.salesforceObjectName, this.recordType);
+    const records = await super.where({
       Acc_ProjectChangeRequest__c: pcrItemId,
-      RecordTypeId: pcrRecordType
+      RecordTypeId: pcrRecordTypeId
     });
+    const mapper = new SalesforcePcrSpendProfileMapper(pcrRecordTypeId);
+    return records.map(x => mapper.map(x));
+  }
+
+  public async insertSpendProfiles(items: PcrSpendProfileEntityForCreate[]) {
+    const pcrRecordTypeId = await this.getRecordTypeId(this.salesforceObjectName, this.recordType);
+    const mapper = new SalesforcePcrSpendProfileMapper(pcrRecordTypeId);
+    const insertables: Insertable<ISalesforcePcrSpendProfile>[] = items.map(x => mapper.mapToSalesforceForCreate(x));
+    return super.insertAll(insertables);
+  }
+
+  public async updateSpendProfiles(items: PcrSpendProfileEntity[]) {
+    const pcrRecordTypeId = await this.getRecordTypeId(this.salesforceObjectName, this.recordType);
+    const mapper = new SalesforcePcrSpendProfileMapper(pcrRecordTypeId);
+    const updatables: ISalesforcePcrSpendProfile[] = items.map(x => mapper.mapToSalesforce(x));
+    return super.updateAll(updatables);
+  }
+
+  public async deleteSpendProfiles(ids: string[]) {
+    return super.deleteAll(ids);
   }
 }
