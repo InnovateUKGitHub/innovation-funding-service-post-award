0dedf0b181c8b30819f747763a9a3ca308d469e0
Vasilia <vasilia.themis@bjss.com>
2020-06-04 12:14:36 +0100

    Uses content solution for change period pcr

---
+++ app/src/content/pages/pcrs/periodLengthChangeContent.ts
@@ -0,0 +1,16 @@
+import { PCRPeriodLengthChangeLabels } from "@content/labels/pcrPeriodLengthChangeLabels";
+import { ContentPageBase } from "@content/contentPageBase";
+import { Content } from "@content/content";
+import { PCRItem } from "@content/pages/pcrs/pcrItem";
+
+export class PCRPeriodLengthChangeContent extends ContentPageBase {
+  constructor(private content: Content) {
+    super(content, "pcr-period-length-change");
+  }
+
+  public readonly pcrItem = new PCRItem(this);
+  public readonly labels = new PCRPeriodLengthChangeLabels(this);
+  public readonly periodLengthQuarterly = () => this.getContent("period-length-quarterly");
+  public readonly periodLengthMonthly = () => this.getContent("period-length-monthly");
+  public readonly guidance = () => this.getContent("guidance", { markdown: true });
+}
--- app/src/ui/containers/pcrs/periodLengthChange/periodLengthChangeSummary.tsx
+++ app/src/ui/containers/pcrs/periodLengthChange/periodLengthChangeSummary.tsx
@@ -4,23 +4,23 @@ import { PcrSummaryProps } from "@ui/containers/pcrs/pcrWorkflow";
 import { ClaimFrequency, PCRItemForPeriodLengthChangeDto } from "@framework/dtos";
 import { PCRPeriodLengthChangeItemDtoValidator } from "@ui/validators";
 
-const guidance  = `This request is to change the length of a period for the remainder of the project. The frequency of how often you submit a claim and the project forecasts will be affected as a result. This will apply to all partners.\n\nIt will be rejected if submitted with any other request type.`;
-
 export const PeriodLengthChangeSummary = (props: PcrSummaryProps<PCRItemForPeriodLengthChangeDto, PCRPeriodLengthChangeItemDtoValidator, "">) => {
+  const monthlyContent = <ACC.Content value={(x) => x.pcrPeriodLengthChangeContent.periodLengthMonthly()} />;
+  const quarterlyContent = <ACC.Content value={(x) => x.pcrPeriodLengthChangeContent.periodLengthQuarterly()} />;
   return (
     /*TODO look at title sizing*/
     <ACC.Section title="">
       <ACC.Section qa="guidance">
-        <ACC.Renderers.Markdown value={guidance} />
+        <ACC.Content value={(x) => x.pcrPeriodLengthChangeContent.guidance()} />
       </ACC.Section>
-      <ACC.Section title="Current period length">
+      <ACC.Section titleContent={x => x.pcrPeriodLengthChangeContent.labels.currentPeriodLength()} qa="current-length">
         <ACC.Renderers.SimpleString>
-          {props.project.claimFrequency === ClaimFrequency.Monthly ? "1 month" : "3 months"}
+          {props.project.claimFrequency === ClaimFrequency.Monthly ? monthlyContent : quarterlyContent}
         </ACC.Renderers.SimpleString>
       </ACC.Section>
-      <ACC.Section title="New period length">
+      <ACC.Section titleContent={x => x.pcrPeriodLengthChangeContent.labels.previousPeriodLength()} qa="previous-length">
         <ACC.Renderers.SimpleString>
-          {props.project.claimFrequency === ClaimFrequency.Monthly ? "3 months" : "1 month"}
+          {props.project.claimFrequency === ClaimFrequency.Monthly ? quarterlyContent : monthlyContent}
         </ACC.Renderers.SimpleString>
       </ACC.Section>
     </ACC.Section>
