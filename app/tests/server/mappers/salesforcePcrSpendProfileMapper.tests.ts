e23e7b7a31c9e4e5f965f5a9f8cd07fdf6e37a87
Vasilia <vasilia.themis@bjss.com>
2020-06-08 11:19:23 +0100

    Updates SpendProfile entity mapper tests

--- app/tests/server/mappers/salesforcePcrSpendProfileMapper.tests.ts
+++ app/tests/server/mappers/salesforcePcrSpendProfileMapper.tests.ts
@@ -1,7 +1,8 @@
 import { createDto } from "@framework/util/dtoHelpers";
 import { ISalesforcePcrSpendProfile } from "@server/repositories";
 import { SalesforcePcrSpendProfileMapper } from "@server/repositories/mappers/pcrSpendProfileMapper";
-import { PcrSpendProfileEntity } from "@framework/entities";
+import { PcrSpendProfileEntity, PcrSpendProfileEntityForCreate } from "@framework/entities";
+import { PCRSpendProfileCapitalUsageType } from "@framework/constants";
 
 const PCR_SPEND_PROFILE_TYPE = "PCR_SPEND_PROFILE_TYPE";
 
@@ -28,54 +29,105 @@ const createPcrSpendProfileEntity = (update?: Partial<PcrSpendProfileEntity>): P
 
 describe("SalesforcePcrSpendProfileMapper", () => {
   it("Maps spend profile correctly to entity", () => {
-    const update: Partial<ISalesforcePcrSpendProfile> = {
-      Id: "1234",
-      RecordTypeId: "xxx",
-      Acc_CostCategoryID__c: "Expected Partner",
-      Acc_ProjectChangeRequest__c: "Expected PCR",
-      Acc_CostOfRole__c: 10,
-      Acc_Country__c: "Spain",
-      Acc_RoleAndDescription__c: "Developer",
+    const expectedEntity: PcrSpendProfileEntity = {
+      id: "id1",
+      costCategoryId: "costCatId1",
+      pcrItemId: "pcrItemId1",
+      value: 50,
+      description: "A description",
+      grossCostOfRole: 10,
+      daysSpentOnProject: 11,
+      ratePerDay: 12,
+      quantity: 13,
+      costPerItem: 14,
+      subcontractorCountry: "Brazil",
+      subcontractorRoleAndDescription: "Works on it",
+      // TODO rename type
+      type: PCRSpendProfileCapitalUsageType.Existing,
+      typeLabel: "Existing",
+      depreciationPeriod: 6,
+      netPresentValue: 120,
+      residualValue: 110,
+      utilisation: 12.45,
+      numberOfTimes: 6,
+      costOfEach: 7,
+    };
+
+    const update: ISalesforcePcrSpendProfile = {
+      Id: expectedEntity.id,
+      RecordTypeId: PCR_SPEND_PROFILE_TYPE,
+      Acc_CostCategoryID__c: expectedEntity.costCategoryId,
+      Acc_ProjectChangeRequest__c: expectedEntity.pcrItemId,
+      Acc_CostOfRole__c: expectedEntity.value,
+      Acc_Country__c: expectedEntity.subcontractorCountry,
+      Acc_RoleAndDescription__c: expectedEntity.subcontractorRoleAndDescription,
+      Acc_CostPerItem__c: expectedEntity.costPerItem,
+      Acc_Quantity__c: expectedEntity.quantity,
+      Acc_Role__c: expectedEntity.description,
+      Acc_Rate__c: expectedEntity.ratePerDay,
+      Acc_GrossCostOfRole__c: expectedEntity.grossCostOfRole,
+      Acc_DaysSpentOnProject__c: expectedEntity.daysSpentOnProject,
+      Acc_CostEach__c: expectedEntity.costOfEach,
+      Acc_DepreciationPeriod__c: expectedEntity.depreciationPeriod,
+      Acc_NetPresentValue__c: expectedEntity.netPresentValue,
+      Acc_NewOrExisting__c: "Existing",
+      Acc_NumberOfTimes__c: expectedEntity.numberOfTimes,
+      Acc_ResidualValue__c: expectedEntity.residualValue,
+      Acc_Utilisation__c: expectedEntity.utilisation,
+      NewOrExistingLabel: expectedEntity.typeLabel,
     };
     const pcrSpendProfile = createPcrSpendProfileSalesforceRecord(update);
     const mapped = new SalesforcePcrSpendProfileMapper(PCR_SPEND_PROFILE_TYPE).map(pcrSpendProfile);
-    expect(mapped.id).toEqual(update.Id);
-    expect(mapped.costCategoryId).toEqual(update.Acc_CostCategoryID__c);
-    expect(mapped.value).toEqual(update.Acc_CostOfRole__c);
-    expect(mapped.subcontractorCountry).toEqual(update.Acc_Country__c);
-    expect(mapped.subcontractorRoleAndDescription).toEqual(update.Acc_RoleAndDescription__c);
+    expect(mapped).toStrictEqual(expectedEntity);
   });
   it("Maps spend profile entity correctly to sf record", () => {
-    const update: Partial<PcrSpendProfileEntity> = {
-      id: "1234",
-      costCategoryId: "Expected Partner Id",
-      pcrItemId: "Expected PCR Id",
-      value: 10,
-      subcontractorCountry: "Spain",
-      subcontractorRoleAndDescription: "Developer"
+    const entity: PcrSpendProfileEntity = {
+      id: "id1",
+      costCategoryId: "costCatId1",
+      pcrItemId: "pcrItemId1",
+      value: 50,
+      description: "A description",
+      grossCostOfRole: 10,
+      daysSpentOnProject: 11,
+      ratePerDay: 12,
+      quantity: 13,
+      costPerItem: 14,
+      subcontractorCountry: "Brazil",
+      subcontractorRoleAndDescription: "Works on it",
+      // TODO rename type
+      type: PCRSpendProfileCapitalUsageType.Existing,
+      typeLabel: "Existing",
+      depreciationPeriod: 6,
+      netPresentValue: 120,
+      residualValue: 110,
+      utilisation: 12.45,
+      numberOfTimes: 6,
+      costOfEach: 7,
     };
-    const pcrSpendProfile = createPcrSpendProfileEntity(update);
+    const sfRecord: ISalesforcePcrSpendProfile = {
+      Id: entity.id,
+      RecordTypeId: PCR_SPEND_PROFILE_TYPE,
+      Acc_CostCategoryID__c: entity.costCategoryId,
+      Acc_ProjectChangeRequest__c: entity.pcrItemId,
+      Acc_CostOfRole__c: entity.value,
+      Acc_Country__c: entity.subcontractorCountry,
+      Acc_RoleAndDescription__c: entity.subcontractorRoleAndDescription,
+      Acc_CostPerItem__c: entity.costPerItem,
+      Acc_Quantity__c: entity.quantity,
+      Acc_Role__c: entity.description,
+      Acc_Rate__c: entity.ratePerDay,
+      Acc_GrossCostOfRole__c: entity.grossCostOfRole,
+      Acc_DaysSpentOnProject__c: entity.daysSpentOnProject,
+      Acc_CostEach__c: entity.costOfEach,
+      Acc_DepreciationPeriod__c: entity.depreciationPeriod,
+      Acc_NetPresentValue__c: entity.netPresentValue,
+      Acc_NewOrExisting__c: "Existing",
+      Acc_NumberOfTimes__c: entity.numberOfTimes,
+      Acc_ResidualValue__c: entity.residualValue,
+      Acc_Utilisation__c: entity.utilisation,
+    };
+    const pcrSpendProfile = createPcrSpendProfileEntity(entity);
     const mapped = new SalesforcePcrSpendProfileMapper(PCR_SPEND_PROFILE_TYPE).mapToSalesforce(pcrSpendProfile);
-    expect(mapped.Id).toEqual(update.id);
-    expect(mapped.Acc_CostCategoryID__c).toEqual(update.costCategoryId);
-    expect(mapped.Acc_CostOfRole__c).toEqual(update.value);
-    expect(mapped.Acc_ProjectChangeRequest__c).toEqual(update.pcrItemId);
-    expect(mapped.Acc_Country__c).toEqual(update.subcontractorCountry);
-    expect(mapped.Acc_RoleAndDescription__c).toEqual(update.subcontractorRoleAndDescription);
-    expect(mapped.RecordTypeId).toEqual(PCR_SPEND_PROFILE_TYPE);
-  });
-  it("Maps spend profile entity for create correctly to sf record", () => {
-    const update: Partial<PcrSpendProfileEntity> = {
-      costCategoryId: "Expected Partner Id",
-      pcrItemId: "Expected PCR Id",
-      value: 10
-    };
-    const pcrSpendProfile = createPcrSpendProfileEntity(update);
-    const mapped = new SalesforcePcrSpendProfileMapper(PCR_SPEND_PROFILE_TYPE).mapToSalesforceForCreate(pcrSpendProfile);
-    expect((mapped as any).Id).toBe(undefined);
-    expect(mapped.Acc_CostCategoryID__c).toEqual(update.costCategoryId);
-    expect(mapped.Acc_CostOfRole__c).toEqual(update.value);
-    expect(mapped.Acc_ProjectChangeRequest__c).toEqual(update.pcrItemId);
-    expect(mapped.RecordTypeId).toEqual(PCR_SPEND_PROFILE_TYPE);
+    expect(mapped).toStrictEqual(sfRecord);
   });
 });
