a53a02db0f2c10df681028a7c85fde31f3466746
Chris <christopher.juckes@bjss.com>
2020-06-08 15:13:08 +0100

    Form component for delete other cost

--- app/src/ui/containers/pcrs/addPartner/spendProfile/index.ts
+++ app/src/ui/containers/pcrs/addPartner/spendProfile/index.ts
@@ -1,5 +1,6 @@
 export * from "./deleteCapitalUsageCostFormComponent";
 export * from "./deleteLabourCostFormComponent";
 export * from "./deleteMaterialCostFormComponent";
+export * from "./deleteOtherCostFormComponent";
 export * from "./deleteSubcontractingCostFormComponent";
 export * from "./deleteTravelAndSubsCostFormComponent";
