4fb29a581228aa733c76074dc0ec9bfb10828c03
Chris <christopher.juckes@bjss.com>
2020-06-17 16:21:15 +0100

    Initial form handler for project setup spend profile

---
+++ app/src/server/forms/projectSetupSpendProfileFormHandler.ts
@@ -0,0 +1,50 @@
+import { ILinkInfo } from "@framework/types/ILinkInfo";
+import { IFormButton, StandardFormHandlerBase } from "./formHandlerBase";
+import { UpdateInitialForecastDetailsCommand } from "../features/forecastDetails";
+import { GetByIdQuery } from "../features/projects";
+import { InitialForecastDetailsDtosValidator } from "../../ui/validators";
+import { IContext } from "@framework/types/IContext";
+import { GetByIdQuery as GetPartnerByIdQuery } from "../features/partners";
+import { GetCostCategoriesForPartnerQuery } from "../features/claims/getCostCategoriesForPartnerQuery";
+import { Params } from "@ui/containers/forecasts/update";
+import { storeKeys } from "@ui/redux/stores/storeKeys";
+import { ProjectSetupRoute, ProjectSetupSpendProfileParams, ProjectSetupSpendProfileRoute } from "@ui/containers";
+import { GetAllInitialForecastsForPartnerQuery } from "@server/features/forecastDetails/getAllInitialForecastsForPartnerQuery";
+
+export class ProjectSetupSpendProfileFormHandler extends StandardFormHandlerBase<ProjectSetupSpendProfileParams, "initialForecastDetails"> {
+  constructor() {
+    super(ProjectSetupSpendProfileRoute, ["default"], "initialForecastDetails");
+  }
+  protected async getDto(context: IContext, params: Params, button: IFormButton, body: { [key: string]: string; }): Promise<ForecastDetailsDTO[]> {
+    const dto = await context.runQuery(new GetAllInitialForecastsForPartnerQuery(params.partnerId));
+    const project = await context.runQuery(new GetByIdQuery(params.projectId));
+    const partner = await context.runQuery(new GetPartnerByIdQuery(params.partnerId));
+    const costCategories = await context.runQuery(new GetCostCategoriesForPartnerQuery(project, partner));
+
+    const costCategoriesIdsToUpdate = costCategories
+      .filter(x => !x.isCalculated)
+      .map(x => x.id);
+
+    dto.forEach(x => {
+      if (costCategoriesIdsToUpdate.indexOf(x.costCategoryId) >= 0) {
+        x.value = parseFloat(body[`value_${x.periodId}_${x.costCategoryId}`]);
+      }
+    });
+
+    return dto;
+  }
+
+  protected async run(context: IContext, params: Params, button: IFormButton, dto: ForecastDetailsDTO[]): Promise<ILinkInfo> {
+    await context.runCommand(new UpdateInitialForecastDetailsCommand(params.projectId, params.partnerId, dto));
+    return ProjectSetupRoute.getLink(params);
+  }
+
+  protected getStoreKey(params: Params) {
+    return storeKeys.getPartnerKey(params.partnerId);
+  }
+
+  protected createValidationResult(params: Params, dto: ForecastDetailsDTO[]) {
+    // TODO: Update once correct validator created as part of ACC-5850
+    return new InitialForecastDetailsDtosValidator(dto, [], undefined, false);
+  }
+}
