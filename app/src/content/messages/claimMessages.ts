a2715f3fe183b725b79cb78bdfb88381920e7645
Chris <christopher.juckes@bjss.com>
2020-06-26 16:17:05 +0100

    Show message on claim forecast screen to warn about frequency change

--- app/src/content/messages/claimMessages.ts
+++ app/src/content/messages/claimMessages.ts
@@ -6,18 +6,20 @@ export class ClaimMessages extends ContentBase {
     super(parent, "claims-messages");
   }
 
-  public readonly guidanceMessage = () => this.getContent("guidance-message", {markdown: true});
+  public readonly guidanceMessage = () => this.getContent("guidance-message", { markdown: true });
   public readonly submitClaimConfirmation = () => this.getContent("submit-claim-confirmation");
-  public readonly noOpenClaimsMessage = (nextClaimStartDate: Date) => this.getContent("no-open-claims", {nextClaimStartDate: formatDate(nextClaimStartDate, DateFormat.FULL_DATE) });
+  public readonly noOpenClaimsMessage = (nextClaimStartDate: Date) => this.getContent("no-open-claims", { nextClaimStartDate: formatDate(nextClaimStartDate, DateFormat.FULL_DATE) });
   public readonly noRemainingClaims = () => this.getContent("no-remaining-claims");
   public readonly noClosedClaims = () => this.getContent("no-closed-claims");
   public readonly finalClaim = () => this.getContent("final-claim");
   public readonly iarRequired = () => this.getContent("iar-required");
   public readonly claimQueried = () => this.getContent("claim-queried");
   public readonly claimApproved = () => this.getContent("claim-approved");
-  public readonly finalClaimGuidance = () => this.getContent("final-claim-guidance", {markdown: true});
+  public readonly finalClaimGuidance = () => this.getContent("final-claim-guidance", { markdown: true });
   public readonly uploadClaimValidationFormInstructions = () => this.getContent("upload-instruction-claim-validation-form", { markdown: true });
   public readonly interimClaimGuidanceFC = () => this.getContent("interim-claim-guidance-fc");
   public readonly interimClaimGuidanceMO = () => this.getContent("interim-claim-guidance-mo");
-  public readonly interimClaimReviewGuidanceMO = () => this.getContent("interim-claim-review-guidance-mo", {markdown: true});
+  public readonly interimClaimReviewGuidanceMO = () => this.getContent("interim-claim-review-guidance-mo", { markdown: true });
+  public readonly frequencyChangeMessage = () => this.getContent("frequency-change-message");
+  public readonly lastChanceToChangeForecast = (periodId: number) => this.getContent("last-chance-to-change-forecast", { periodId });
 }
