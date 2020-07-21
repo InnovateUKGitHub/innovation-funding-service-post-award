c9293997eea8724a4ec9955de0fe61c84c2ac75d
Chris <christopher.juckes@bjss.com>
2020-07-21 12:22:18 +0100

    Add comment field to summary form

--- app/src/content/labels/monitoringReportsLabels.ts
+++ app/src/content/labels/monitoringReportsLabels.ts
@@ -4,4 +4,7 @@ export class MonitoringReportsLabels extends ContentBase {
   constructor(parent: ContentBase) {
     super(parent, "monitoring-reports-labels");
   }
+
+  public readonly statusAndCommentsLog = () => this.getContent("status-and-comments-log");
+  public readonly additionalComments = () => this.getContent("additional-comments");
 }
