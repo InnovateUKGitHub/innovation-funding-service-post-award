ca045417dc8022439f4269b9cab4310e9939a90e
Chris <christopher.juckes@bjss.com>
2020-08-04 08:01:37 +0100

    Tests for update partner

--- app/src/ui/validators/partnerValidator.ts
+++ app/src/ui/validators/partnerValidator.ts
@@ -86,10 +86,6 @@ export class PartnerDtoValidator extends Results<PartnerDto> {
         () => Validation.required(this, this.model.postcode, "Postcode field cannot be empty")
     ) : Validation.valid(this);
 
-    public companyNumber = this.conditionallyValidateBankDetails(() => Validation.all(this,
-        () => Validation.required(this, this.model.bankDetails.companyNumber, "Company number cannot be empty")
-    ));
-
     public bankCheckValidation = this.conditionallyValidateBankDetails(() => Validation.isFalse(this, !!this.options.failBankValidation, "Check your sort code and account number."));
 
     public sortCode = this.conditionallyValidateBankDetails(() => Validation.all(this,
@@ -101,33 +97,6 @@ export class PartnerDtoValidator extends Results<PartnerDto> {
         () => Validation.required(this, this.model.bankDetails.accountNumber, "Account number cannot be empty"),
         () => Validation.accountNumber(this, this.model.bankDetails.accountNumber, "Please enter a valid account number")
     ), this.original.bankCheckStatus === BankCheckStatus.NotValidated);
-
-    public firstName = this.conditionallyValidateBankDetails(() => Validation.all(this,
-        () => Validation.required(this, this.model.bankDetails.firstName, "First name cannot be empty")
-    ));
-
-    public lastName = this.conditionallyValidateBankDetails(() => Validation.all(this,
-        () => Validation.required(this, this.model.bankDetails.lastName, "Last name cannot be empty")
-    ));
-
-    public accountStreet = this.conditionallyValidateBankDetails(() => Validation.all(this,
-        () => Validation.required(this, this.model.bankDetails.address.accountStreet, "Account street cannot be empty")
-    ));
-
-    public accountTownOrCity = this.conditionallyValidateBankDetails(() => Validation.all(this,
-        () => Validation.required(this, this.model.bankDetails.address.accountTownOrCity, "Account town or city cannot be empty")
-    ));
-
-    // TODO: Should these values be required?
-    public accountBuilding = this.conditionallyValidateBankDetails(() => Validation.all(this,
-        () => Validation.required(this, this.model.bankDetails.address.accountBuilding, "Account building name cannot be empty")
-    ));
-
-    public accountLocality = this.conditionallyValidateBankDetails(() => Validation.all(this,
-        () => Validation.required(this, this.model.bankDetails.address.accountLocality, "Account locality cannot be empty")
-    ));
-
-    public accountPostcode = this.conditionallyValidateBankDetails(() => Validation.required(this, this.model.bankDetails.address.accountPostcode, "Account postcode cannot be empty"));
 
     private conditionallyValidateBankDetails(test: () => Result, condition: boolean = true) {
         if (condition && this.options.validateBankDetails) {
