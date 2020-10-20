e389b7f6f6c90f58a09fd9b457843f0dad6adb44
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-10-20 11:20:39 +0100

    ACC-7165: Validation removed to allow negative amount

--- app/src/ui/validators/claimDetailsValidator.ts
+++ app/src/ui/validators/claimDetailsValidator.ts
@@ -15,7 +15,6 @@ export class ClaimLineItemDtoValidator extends Results<ClaimLineItemDto> {
     () => typeof this.model.description === "string" && this.model.description.length > 0
       ? Validation.required(this, this.model.value, "Enter a cost.")
       : Validation.valid(this),
-    () => Validation.isCurrency(this, this.model.value, "Costs must be a number."),
-    () => Validation.isTrue(this, this.model.value === undefined || this.model.value === null || this.model.value >= 0, "Costs must be a number.")
-  );
+    () => Validation.isCurrency(this, this.model.value, "Costs must be a number.")
+    );
 }
