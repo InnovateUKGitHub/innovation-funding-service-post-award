75987ce53d714443f62599bb19c2bdcdbe349a74
Neil Little <neil.little@ukri.org>
2020-11-06 11:11:09 +0000

    ACC-7218: Refactor <TextHint>
    * Fix element type bug <div>'s should not render text 😟
    * Updated to use `children` as it makes more sense
    * Add never to stop use of className and style

--- app/src/ui/componentsGuide/textHintGuide.tsx
+++ app/src/ui/componentsGuide/textHintGuide.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { TextHint} from "../components/layout/textHint";
 import { IGuide } from "@framework/types";
+import { TextHint } from "../components/layout/textHint";
+
+const sampleText = "Enter all labour costs in this period, describing each person’s role, how much time they spent on the project and so on. We will calculate your payment based on the agreed award offer rate.";
 
 export const textHint: IGuide = {
   name: "TextHint",
@@ -8,8 +10,8 @@ export const textHint: IGuide = {
     {
       name: "Simple",
       comments: "Renders a text hint",
-      example: "<TextHint text=\"Enter all labour costs in this period, describing each person’s role, how much time they spent on the project and so on. We will calculate your payment based on the agreed award offer rate.\"/>",
-      render: () => <TextHint text="Enter all labour costs in this period, describing each person’s role, how much time they spent on the project and so on. We will calculate your payment based on the agreed award offer rate." />
-    }
-  ]
+      example: `<TextHint>${sampleText}</TextHint>`,
+      render: () => <TextHint>{sampleText}</TextHint>,
+    },
+  ],
 };
--- app/tests/components/layout/textHint.tests.tsx
+++ app/tests/components/layout/textHint.tests.tsx
@@ -1,18 +1,44 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
-import { TextHint } from "../../../src/ui/components/layout/textHint";
+import { GOVUK_SECONDARY_TEXT_COLOUR } from "@ui/styles/colours";
+import { TextHint, TextHintReactProps } from "../../../src/ui/components/layout/textHint";
+import { findByQa } from "../helpers/find-by-qa";
 
-describe("TextHint", () => {
+describe("<TextHint />", () => {
+  const setup = (text: TextHintReactProps["children"]) => {
+    const wrapper = mount(<TextHint>{text}</TextHint>);
+    const textElement = findByQa(wrapper, "text-hint");
+
+    return {
+      wrapper,
+      textElement,
+    };
+  };
+
+  it("should render as a <p> tag", () => {
+    const { textElement } = setup("show-me-a-p");
+
+    expect(textElement.type()).toEqual("p");
+  });
+
+  it("should render with unique color", () => {
+    const { textElement } = setup("coloured-text");
+    const inlineStyles = textElement.prop("style")!;
+
+    expect(inlineStyles.color).toEqual(GOVUK_SECONDARY_TEXT_COLOUR);
+  });
+
   it("should render with the correct text", () => {
-    const wrapper = shallow(<TextHint text="test text" />);
+    const stubText = "stub-text";
+    const { textElement } = setup(stubText);
 
-    expect(wrapper.text()).toEqual("test text");
+    expect(textElement.text()).toEqual(stubText);
   });
 
   it("should render null if no text is given", () => {
-    const wrapper = shallow(<TextHint text={null} />);
+    const { textElement } = setup("");
 
-    expect(wrapper.html()).toBe(null);
+    expect(textElement.exists()).toBe(false);
   });
 });
