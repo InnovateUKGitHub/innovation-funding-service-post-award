0b97275108a3326ff09e89835d7bc9b56aa22018
Jake Harrison <jake.harrison@ukri.org>
2020-11-06 17:11:23 +0000

    ACC-7218: <NavigationArrows />

--- app/tests/components/navigationArrows.tests.tsx
+++ app/tests/components/navigationArrows.tests.tsx
@@ -5,25 +5,25 @@ import { mount } from "enzyme";
 import { NavigationArrows } from "../../src/ui/components";
 import { createStore } from "redux";
 import { rootReducer } from "../../src/ui/redux/reducers";
-import { RouterProvider } from "react-router5";
-import { Provider } from "react-redux";
 import { IClientUser, ProjectRole } from "@framework/types";
-import { configureRouter, routeConfig } from "@ui/routing";
+import { routeConfig } from "@ui/routing";
 import TestBed from "./helpers/TestBed";
+import { IStores } from "@ui/redux/storesProvider";
 
 const routes = routeConfig;
-const router = configureRouter(routes);
 
-const store = createStore(rootReducer, { user: {
-  email: "iuk.accproject@bjss.com.bjsspoc2",
-  roleInfo: {
-    a0C0Q000001tr5yUAA: {
-      projectRoles: ProjectRole.MonitoringOfficer,
-      partnerRoles: {}
-    }
-  },
-  csrf: "CSFR"
-} as IClientUser});
+const store = createStore(rootReducer, {
+  user: {
+    email: "iuk.accproject@bjss.com.bjsspoc2",
+    roleInfo: {
+      a0C0Q000001tr5yUAA: {
+        projectRoles: ProjectRole.MonitoringOfficer,
+        partnerRoles: {},
+      },
+    },
+    csrf: "CSFR",
+  } as IClientUser,
+}) as IStores;
 
 const previousLink = {
   label: "Overheads",
@@ -31,8 +31,8 @@ const previousLink = {
     partnerId: "a0B0Q000001e3HdUAI",
     projectId: "a0C0Q000001tr5yUAA",
     periodId: 2,
-    costCategoryId: "a060Q000000oAYZQA2"
-  })
+    costCategoryId: "a060Q000000oAYZQA2",
+  }),
 };
 
 const nextLink = {
@@ -41,17 +41,15 @@ const nextLink = {
     partnerId: "a0B0Q000001e3HdUAI",
     projectId: "a0C0Q000001tr5yUAA",
     periodId: 2,
-    costCategoryId: "a060Q000000oAYYQA2"
-  })
+    costCategoryId: "a060Q000000oAYYQA2",
+  }),
 };
 
 describe("NavigationArrows", () => {
   it("renders only the next arrow if no previous link is given", () => {
     const wrapper = mount(
-      <TestBed stores={store} providedRoute={router}>
-        <RouterProvider router={router}>
-          <NavigationArrows previousLink={null} nextLink={nextLink} />
-        </RouterProvider>
+      <TestBed stores={store}>
+        <NavigationArrows previousLink={null} nextLink={nextLink} />
       </TestBed>
     );
 
@@ -62,43 +60,45 @@ describe("NavigationArrows", () => {
 
   it("renders the previous and the next arrow", () => {
     const wrapper = mount(
-      <Provider store={store}>
-        <RouterProvider router={router}>
-          <NavigationArrows previousLink={previousLink} nextLink={nextLink} />
-        </RouterProvider>
-      </Provider>
+      <TestBed stores={store}>
+        <NavigationArrows previousLink={previousLink} nextLink={nextLink} />
+      </TestBed>
     );
 
     expect(wrapper.find("a").length).toEqual(2);
 
-    expect(wrapper.find("a").at(0).find("span").at(0).text()).toEqual("Previous");
-    expect(wrapper.find("a").at(0).find("span").at(1).text()).toEqual(previousLink.label);
+    expect(wrapper.find("a").at(0).find("span").at(0).text()).toEqual(
+      "Previous"
+    );
+    expect(wrapper.find("a").at(0).find("span").at(1).text()).toEqual(
+      previousLink.label
+    );
 
     expect(wrapper.find("a").at(1).find("span").at(0).text()).toEqual("Next");
-    expect(wrapper.find("a").at(1).find("span").at(1).text()).toEqual(nextLink.label);
+    expect(wrapper.find("a").at(1).find("span").at(1).text()).toEqual(
+      nextLink.label
+    );
   });
 
   it("renders only the previous arrow if no next link is given", () => {
     const wrapper = mount(
-      <Provider store={store}>
-        <RouterProvider router={router}>
-          <NavigationArrows previousLink={previousLink} nextLink={null} />
-        </RouterProvider>
-      </Provider>
+      <TestBed stores={store}>
+        <NavigationArrows previousLink={previousLink} nextLink={null} />
+      </TestBed>
     );
 
     expect(wrapper.find("a").length).toEqual(1);
     expect(wrapper.find("a").find("span").at(0).text()).toEqual("Previous");
-    expect(wrapper.find("a").find("span").at(1).text()).toEqual(previousLink.label);
+    expect(wrapper.find("a").find("span").at(1).text()).toEqual(
+      previousLink.label
+    );
   });
 
   it("renders no arrows if no links are given", () => {
     const wrapper = mount(
-      <Provider store={store}>
-        <RouterProvider router={router}>
-          <NavigationArrows previousLink={null} nextLink={null} />
-        </RouterProvider>
-      </Provider>
+      <TestBed stores={store}>
+        <NavigationArrows previousLink={null} nextLink={null} />
+      </TestBed>
     );
 
     expect(wrapper.find("a").length).toEqual(0);
