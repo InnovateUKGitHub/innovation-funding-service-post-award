60ce5de299dbaf91d6244b6d7fb9075cd31e679d
Jake Harrison <jake.harrison@ukri.org>
2020-11-06 17:11:23 +0000

    ACC-7218: <NavigationArrows />

--- app/src/ui/components/navigationArrows.tsx
+++ app/src/ui/components/navigationArrows.tsx
@@ -4,45 +4,35 @@ import { ILinkInfo } from "@framework/types";
 import {ArrowLeft} from "./svg/arrowLeft";
 import {ArrowRight} from "./svg/arrowRight";
 
+interface LinkProps {
+  label: string;
+  route: ILinkInfo;
+}
 interface Props {
-  previousLink?: {label: string, route: ILinkInfo} | null;
-  nextLink?: {label: string, route: ILinkInfo} | null;
+  previousLink?: LinkProps | null;
+  nextLink?: LinkProps | null;
 }
 
 export class NavigationArrows extends React.Component<Props> {
   render() {
     return (
       <div className="govuk-navigation-arrows">
-        {this.renderLeftHalf()}
-        {this.renderRightHalf()}
+        {this.renderLink(this.props.previousLink, "Previous", "arrow-right")}
+        {this.renderLink(this.props.nextLink, "Next", "arrow-left")}
       </div>
     );
   }
 
-  private renderLeftHalf() {
-    if (!this.props.previousLink) return null;
+  private renderLink(linkType: LinkProps | null | undefined, name: string, qa: string) {
+    if (!linkType) return null;
     return (
-      <Link route={this.props.previousLink.route} replace={true} className="govuk-navigation-arrows__button">
+      <Link route={linkType.route} replace={true} className="govuk-navigation-arrows__button">
         <ArrowLeft className="govuk-navigation-arrows__button__arrow" />
-        <div data-qa="arrow-left" className="govuk-navigation-arrows__button__label">
-          <span>Previous</span>
-          <span className="govuk-navigation-arrows__button__label__category">{this.props.previousLink.label}</span>
+        <div data-qa={qa} className="govuk-navigation-arrows__button__label">
+          <span>{name}</span>
+          <span className="govuk-navigation-arrows__button__label__category">{linkType.label}</span>
         </div>
       </Link>
     );
   }
-
-  private renderRightHalf() {
-    if (!this.props.nextLink) return null;
-    return (
-      <Link route={this.props.nextLink.route} replace={true} className="govuk-navigation-arrows__button">
-        <ArrowRight className="govuk-navigation-arrows__button__arrow" />
-        <div data-qa="arrow-right" className="govuk-navigation-arrows__button__label govuk-navigation-arrows__button__label">
-          <span>Next</span>
-          <span className="govuk-navigation-arrows__button__label__category">{this.props.nextLink.label}</span>
-        </div>
-      </Link>
-    );
-  }
-
 }
