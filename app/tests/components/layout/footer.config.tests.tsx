0b5ae90207400c2f5af0046c2533bf26105356d9
Neil Little <neil.little@ukri.org>
2020-11-10 16:41:51 +0000

    ACC-7218: Create footer config hook test

---
+++ app/tests/components/layout/footer.config.tests.tsx
@@ -0,0 +1,35 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useExternalContent } from "@ui/components/layout/footer.config";
+import { Content } from "../../../src/content";
+import { hookTestBed } from "../helpers/TestBed";
+
+describe("Footer config", () => {
+  describe("useExternalContent", () => {
+    const setup = () => {
+      const stubContent = {
+        footer: {
+          supportLinks: { content: "stub-content-supportLinks" },
+          explainCookies: { content: "stub-content-explainCookies" },
+          cookieFindOutMore: { content: "stub-content-cookieFindOutMore" },
+          govLicenseLinkPart1: { content: "stub-content-govLicenseLinkPart1" },
+          govLicenseLinkPart2: { content: "stub-content-govLicenseLinkPart2" },
+          govLicenseLinkPart3: { content: "stub-content-govLicenseLinkPart3" },
+          crownCopyright: { content: "stub-content-crownCopyright" },
+        },
+      } as Partial<Content>;
+
+      const { result } = renderHook(
+        useExternalContent,
+        hookTestBed({ content: stubContent })
+      );
+
+      return result.current;
+    };
+
+    it("should return with content", () => {
+      const footerContent = setup();
+
+      expect(footerContent).toMatchSnapshot();
+    });
+  });
+});
