5cb52d179d22eece2c528903f92095f67fec5250
Jake Harrison <jake.harrison@ukri.org>
2020-11-10 18:52:58 +0000

    ACC-7218: <HashTabs />

--- app/tests/components/layout/hashTabs.tests.tsx
+++ app/tests/components/layout/hashTabs.tests.tsx
@@ -1,64 +1,95 @@
 import React from "react";
-import Enzyme from "enzyme";
+import { mount } from "enzyme";
 
-import { HashTabItem, HashTabs } from "../../../src/ui/components/layout/hashTabs";
+import {
+  HashTabItem,
+  HashTabs,
+} from "../../../src/ui/components/layout/hashTabs";
+import { findByQa } from "../helpers/find-by-qa";
 
 describe("Hash Tabs", () => {
-  const aTabList: HashTabItem[] = [
-    { text: "Claims", hash: "claims", content: "Claims content" },
-    { text: "Project change requests", hash: "pcr", content: "Project change requests content" }
+
+  const selectedTabClass = "govuk-tabs__list-item--selected";
+
+  const tabStub: HashTabItem[] = [
+    { text: "Claims", hash: "claims", content: "Claims content", qa: "claim-tab" },
+    {
+      text: "Project change requests",
+      hash: "pcr",
+      content: "Project change requests content",
+      qa: "pcr-tab"
+    },
   ];
 
-  it("should render the list of tabs", () => {
-    const wrapper = Enzyme.mount(<HashTabs tabList={aTabList}/>);
-    const items = wrapper.find("li");
-    expect(items.length).toBe(2);
-    items.forEach(item => {
-      expect(item.props().className).toContain("govuk-tabs__list-item");
-      expect(item.props().role).toBe("presentation");
-      expect(item.childAt(0).props().role).toBe("tab");
+  const setup = (tabs: any) => {
+    return mount(<HashTabs tabList={tabs} qa="tab-list-qa"/>);
+  };
+
+  it("should render the list of tabs with the first selected", () => {
+    const wrapper = setup(tabStub);
+
+    const tabs = findByQa(wrapper, "tab-list-qa").children();
+    expect(tabs.length).toBe(2);
+
+    // verify first item is selected
+    const selectedTab = findByQa(wrapper, "claim-tab");
+    expect(selectedTab.hasClass(selectedTabClass)).toBeTruthy();
+    expect(selectedTab.children().first().props()["aria-selected"]).toBeTruthy();
+
+    tabs.forEach((item, i) => {
+      // validate li
+      const li = item.props();
+      expect(li.className).toContain("govuk-tabs__list-item");
+      expect(li.role).toBe("presentation");
+      expect(li["data-qa"]).toBe(tabStub[i].qa); // a bit ugly
+
+      // validate tab child
+      const a = item.children().first().props();
+      expect(a.className).toBe("govuk-tabs__tab");
+      expect(a.role).toBe("tab");
     });
   });
 
-  it("should render the first tab as selected if no hash", () => {
-    const wrapper = Enzyme.shallow(<HashTabs tabList={aTabList}/>);
-    const selected = wrapper.find("li").filterWhere(x => x.hasClass("govuk-tabs__list-item--selected")).map(x => x);
-    expect(selected.length).toBe(1);
-    expect(selected[0].html()).toContain(`<a href="#claims" class="govuk-tabs__tab" aria-selected="true" role="tab">Claims</a>`);
-  });
-
   it("should render the unselected tab", () => {
-    const wrapper = Enzyme.shallow(<HashTabs tabList={aTabList}/>);
-    expect(wrapper.html()).toContain(`<a href="#pcr" class="govuk-tabs__tab" aria-selected="false" role="tab">Project change requests</a>`);
+    const wrapper = setup(tabStub);
+
+    expect(wrapper.html()).toContain(
+      `<a href="#pcr" class="govuk-tabs__tab" aria-selected="false" role="tab">Project change requests</a>`
+    );
   });
 
   it("should not render tabs if passed an empty tab list", () => {
-    const tabList = [] as any;
-    const wrapper = Enzyme.shallow(<HashTabs tabList={tabList}/>);
+    const wrapper = setup([]);
+
     expect(wrapper.html()).toBeNull();
   });
 
   it("should not render tabs if passed null tab list", () => {
-    const tabList = null as any;
-    const wrapper = Enzyme.shallow(<HashTabs tabList={tabList}/>);
+    const wrapper = setup(null);
     expect(wrapper.html()).toBeNull();
   });
 
   it("should render all tab contents in sections", () => {
-    const wrapper = Enzyme.shallow(<HashTabs tabList={aTabList}/>);
-    const sections = wrapper.find("section").map(x => x);
-    expect(sections.length).toBe(aTabList.length);
-    aTabList.forEach((tab,i) => {
+    const wrapper = setup(tabStub);
+
+    const sections = wrapper.find("section").map((x) => x);
+    expect(sections.length).toBe(tabStub.length);
+    tabStub.forEach((tab, i) => {
       expect(sections[i].props().className).toContain("govuk-tabs__panel");
       expect(sections[i].html()).toContain(tab.content);
     });
   });
 
   it("should render hidden tabs with hidden class", () => {
-    const wrapper = Enzyme.shallow(<HashTabs tabList={aTabList}/>);
-    const sections = wrapper.find("section").map(x => x);
+    const wrapper = setup(tabStub);
+
+    const sections = wrapper.find("section").map((x) => x);
     expect(sections.length).toBe(2);
-    expect(sections[0].props().className).not.toContain("govuk-tabs__panel--hidden");
-    expect(sections[1].props().className).toContain("govuk-tabs__panel--hidden");
+    expect(sections[0].props().className).not.toContain(
+      "govuk-tabs__panel--hidden"
+    );
+    expect(sections[1].props().className).toContain(
+      "govuk-tabs__panel--hidden"
+    );
   });
 });
