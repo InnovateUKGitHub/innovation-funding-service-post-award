ef2dc79acf394ccee8c58157d81a4071e584f322
Neil Little <neil.little@ukri.org>
2020-11-11 20:20:49 +0000

    ACC-7218: <DocumentSingle />

--- app/src/ui/components/documentSingle.tsx
+++ app/src/ui/components/documentSingle.tsx
@@ -1,43 +1,44 @@
 import React from "react";
 import { DocumentSummaryDto } from "@framework/dtos/documentDto";
 import { useContent } from "@ui/redux/contentProvider";
+import { SimpleString } from "./renderers";
 
 export interface DocumentSingleProps {
-    message?: string;
-    document: DocumentSummaryDto;
-    openNewWindow?: boolean;
-    qa?: string;
-    renderRemove?: () => React.ReactNode;
+  document: Pick<DocumentSummaryDto, "link" | "fileName">;
+  message?: string;
+  openNewWindow?: boolean;
+  qa?: string;
+  removeElement?: React.ReactNode;
 }
 
-const Message: React.FunctionComponent<{message?: string}> = ({message}: {message?: string}) => {
-    if (!message) {
-        return null;
-    }
-    return (
-      <div className="govuk-!-padding-bottom-3">
-        <p className="govuk-body">{message}</p>
-      </div>
-    );
-};
+export function DocumentSingle({ message, document, openNewWindow, qa, removeElement }: DocumentSingleProps) {
+  const { getContent } = useContent();
+  const newWindowText = getContent((x) => x.components.documentSingle.newWindow);
+  const newWindowMessage = ` (${newWindowText})`;
 
-export const DocumentSingle: React.FunctionComponent<DocumentSingleProps> = ({ message, document, openNewWindow, qa, renderRemove }: DocumentSingleProps) => {
-
-    const textStyle = {
-        paddingRight: "20px"
-    };
-    const { getContent } = useContent();
-    const newWindowText = getContent((x) => x.components.documentSingle.newWindow);
-    const newWindowMessage = ` (${newWindowText})`;
-
-    return (
-        <div>
-            <Message message={message}/>
-            <div className="govuk-!-padding-bottom-3">
-                <a target={openNewWindow ? "_blank" : ""} href={document.link} style={textStyle} className="govuk-link govuk-!-font-size-19" data-qa={qa}>{document.fileName}</a>
-                <span className="govuk-body">{newWindowMessage}</span>
-            </div>
-            {renderRemove && renderRemove()}
+  return (
+    <div>
+      {message && (
+        <div className="govuk-!-padding-bottom-3">
+          <SimpleString qa="document-single-message">{message}</SimpleString>
         </div>
-    );
-};
+      )}
+
+      <div className="govuk-!-padding-bottom-3">
+        <a
+          className="govuk-link govuk-!-font-size-19"
+          target={openNewWindow ? "_blank" : undefined}
+          style={{ paddingRight: 20 }}
+          href={document.link}
+          data-qa={qa}
+        >
+          {document.fileName}
+        </a>
+
+        <SimpleString>{newWindowMessage}</SimpleString>
+      </div>
+
+      {removeElement}
+    </div>
+  );
+}
