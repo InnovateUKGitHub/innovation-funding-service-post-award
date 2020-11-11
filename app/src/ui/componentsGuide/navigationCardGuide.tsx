8106329b4d8d9292e782f4d99dc70b1b34f0233c
Neil Little <neil.little@ukri.org>
2020-11-11 16:29:59 +0000

    ACC-7218: <NavigationCard>
    * NavigationCard remove unneeded children prop
    * Add unit tests
    * Change to FC components
    * Rewrite the chunk logic + test, fixes future tsc bug with interface
    * Remove unneeded styles
    * Remove invalid guide usages

--- app/src/ui/componentsGuide/navigationCardGuide.tsx
+++ app/src/ui/componentsGuide/navigationCardGuide.tsx
@@ -6,7 +6,6 @@ import { RouterProvider } from "react-router5";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { rootReducer } from "@ui/redux";
-import { SimpleString } from "@ui/components/renderers";
 import { IGuide, ILinkInfo } from "@framework/types";
 
 const route = { name: "test", path: "/components" } as any;
@@ -34,44 +33,30 @@ export const navigationCardGuide: IGuide = {
       )
     },
     {
-      name: "With content",
-      comments: "A card which renders a navigation link with associated text",
-      example: `
-        <NavigationCard route={dummyLink} label="The link" qa="example">
-          <SimpleString>Some text</SimpleString>
-        </NavigationCard>
-      `,
-      render: () => (
-        <Provider store={createStore(rootReducer)}>
-          <RouterProvider router={router}>
-            <NavigationCard route={dummyLink} label="The link" qa="example">
-              <SimpleString>Some text</SimpleString>
-            </NavigationCard>
-          </RouterProvider>
-        </Provider>
-      )
-    },
-    {
       name: "Dashboard",
       comments: "A number of card which renders a 3 column dashboard",
       example: `
         <NavigationCardsGrid>
-          <NavigationCard route={dummyLink} label="Example link" qa="example">Row 1 column 1</NavigationCard>
-          <NavigationCard route={dummyLink} label="Example link" qa="example">Row 1 column 2</NavigationCard>
-          <NavigationCard route={dummyLink} label="Example link" qa="example">Row 1 column 3</NavigationCard>
-          <NavigationCard route={dummyLink} label="Example link" qa="example">Row 2 column 1</NavigationCard>
-          <NavigationCard route={dummyLink} label="Example link" qa="example">Row 2 column 2</NavigationCard>
+          <NavigationCard route={dummyLink} label="Row 1 column 1" qa="row-1-column-1" messages={[{message: "Custom Message 1", qa: "row-1-column-1-message-1"}, {message: "Custom Message 2", qa: "row-1-column-1-message-2"} ]} />
+          <NavigationCard route={dummyLink} label="Row 1 column 2" qa="row-1-column-2" />
+          <NavigationCard route={dummyLink} label="Row 1 column 3" qa="row-1-column-3" />
+          <NavigationCard route={dummyLink} label="Row 2 column 1" qa="row-2-column-1" />
+          <NavigationCard route={dummyLink} label="Row 2 column 2" qa="row-2-column-2" />
+          <NavigationCard route={dummyLink} label="Row 2 column 3" qa="row-2-column-3" />
+          <NavigationCard route={dummyLink} label="Row 3 column 1" qa="row-3-column-1" />
         </NavigationCardsGrid>
       `,
       render: () => (
         <Provider store={createStore(rootReducer)}>
           <RouterProvider router={router}>
             <NavigationCardsGrid>
-              <NavigationCard route={dummyLink} label="Example link" qa="example">Row 1 column 1</NavigationCard>
-              <NavigationCard route={dummyLink} label="Example link" qa="example">Row 1 column 2</NavigationCard>
-              <NavigationCard route={dummyLink} label="Example link" qa="example">Row 1 column 3</NavigationCard>
-              <NavigationCard route={dummyLink} label="Example link" qa="example">Row 2 column 1</NavigationCard>
-              <NavigationCard route={dummyLink} label="Example link" qa="example">Row 2 column 2</NavigationCard>
+              <NavigationCard route={dummyLink} label="Row 1 column 1" qa="row-1-column-1" messages={[{message: "Custom Message 1", qa: "row-1-column-1-message-1"}, {message: "Custom Message 2", qa: "row-1-column-1-message-2"} ]} />
+              <NavigationCard route={dummyLink} label="Row 1 column 2" qa="row-1-column-2" />
+              <NavigationCard route={dummyLink} label="Row 1 column 3" qa="row-1-column-3" />
+              <NavigationCard route={dummyLink} label="Row 2 column 1" qa="row-2-column-1" />
+              <NavigationCard route={dummyLink} label="Row 2 column 2" qa="row-2-column-2" />
+              <NavigationCard route={dummyLink} label="Row 2 column 3" qa="row-2-column-3" />
+              <NavigationCard route={dummyLink} label="Row 3 column 1" qa="row-3-column-1" />
             </NavigationCardsGrid>
           </RouterProvider>
         </Provider>
