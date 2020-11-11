b8d9dbd1d5c1c6bcc2b038837a6bd34e1cd87e60
Neil Little <neil.little@ukri.org>
2020-11-11 16:29:59 +0000

    ACC-7218: <NavigationCard>
    * NavigationCard remove unneeded children prop
    * Add unit tests
    * Change to FC components
    * Rewrite the chunk logic + test, fixes future tsc bug with interface
    * Remove unneeded styles
    * Remove invalid guide usages

--- app/src/ui/components/navigationCard.tsx
+++ app/src/ui/components/navigationCard.tsx
@@ -1,67 +1,80 @@
 import React from "react";
 import { ILinkInfo } from "@framework/types";
 import { Link } from "./links";
-import classNames from "classnames";
 import { SimpleString } from "./renderers";
 
-interface NavigationCardProps {
+export interface NavigationCardMessage {
+  message: React.ReactNode;
+  qa?: string;
+}
+
+export interface NavigationCardProps {
   label: React.ReactNode;
   route: ILinkInfo;
   qa: string;
   messages?: NavigationCardMessage[];
 }
 
-export interface NavigationCardMessage {
-  message: React.ReactNode;
-  isAlert?: boolean;
-  qa?: string;
+// TODO: Refactor this component to be consumed via parent. E.G NavigationCardsGrid.Item = NavigationCard (Consume as <NavigationCardsGrid.Item ... />)
+export function NavigationCard({ qa, label, route, messages = [] }: NavigationCardProps) {
+  const hasMessages: boolean = !!messages.length;
+
+  return (
+    <div className="card-link" data-qa={qa}>
+      <Link className="card-link__link" route={route}>
+        <h2 className="card-link__title" data-qa="navigation-card-label">{label}</h2>
+
+        {hasMessages && (
+          <div className="card-link__messages" data-qa="navigation-card-list">
+            {messages.map((x) => (
+              <SimpleString key={x.qa} {...x} className="card-link__message">
+                {x.message}
+              </SimpleString>
+            ))}
+          </div>
+        )}
+      </Link>
+    </div>
+  );
 }
 
-export class NavigationCard extends React.Component<NavigationCardProps> {
-  render() {
-    return (
-      <div className="card-link" data-qa={this.props.qa}>
-        <Link className="card-link__link" route={this.props.route}>
-          <h2 className="card-link__title">{this.props.label}</h2>
-          {this.renderMessages()}
-        </Link>
-      </div>
-    );
-  }
-
-  private renderMessages() {
-    return (
-      this.props.messages ?
-        <div className="card-link__messages"> {this.props.messages.map((x,i) => ( <SimpleString key={i} className={classNames( "card-link__message", { "card-link__message--alert": x.isAlert } )} qa={x.qa}> {x.message} </SimpleString> ) )} </div>
-        : null
-    );
-  }
-}
+type NavigationCardsList = React.ReactElement<NavigationCardProps>[];
 
 interface NavigationCardsGridProps {
-  children: React.ReactElement<NavigationCardProps>[];
+  children: NavigationCardsList;
 }
 
-export class NavigationCardsGrid extends React.Component<NavigationCardsGridProps> {
-  render() {
-    const children = React.Children.toArray(this.props.children);
+export function NavigationCardsGrid({ children }: NavigationCardsGridProps) {
+  const childrenArray = React.Children.toArray(children);
+  const chunkSize = 3;
 
-    let current = 0;
-    const results = [];
-    while (current < children.length) {
-      // TODO: FIX this!!
-      // @ts-ignore
-      results.push(this.renderRow(children.slice(current, current + 3), Math.floor(current / 3)));
-      current = current + 3;
-    }
-    return results;
+  const gridLayout = chunks(childrenArray, chunkSize) as NavigationCardsList[];
+
+  return (
+    <>
+      {gridLayout.map((group, groupIndex) => {
+        const groupUid = `group-${groupIndex}`;
+
+        return (
+          <div key={groupUid} className="govuk-grid-row card-link-grid" data-qa="navigation-group">
+            {group.map((navCard, rowIndex) => (
+              <div key={`${groupUid}-row-${rowIndex}`} className="govuk-grid-column-one-third" data-qa="navigation-group-item">
+                {navCard}
+              </div>
+            ))}
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export function chunks(array: any[], size: number): any[][] {
+  const results = [];
+
+  while (array.length) {
+    results.push(array.splice(0, size));
   }
 
-  private renderRow(items: React.ReactElement<NavigationCardProps>[], rowIndex: number) {
-    return (
-      <div className="govuk-grid-row card-link-grid" key={rowIndex}>
-        {items.map((x, i) => <div key={i} className="govuk-grid-column-one-third">{x}</div>)}
-      </div>
-    );
-  }
+  return results;
 }
---
+++ app/tests/components/navigationCard.tests.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { chunks, NavigationCard, NavigationCardProps, NavigationCardsGrid } from "@ui/components";
+import { mount } from "enzyme";
+import TestBed from "./helpers/TestBed";
+import { findByQa } from "./helpers/find-by-qa";
+
+const createRoute = (uid: string) => ({
+  routeName: `${uid}-routeName`,
+  routeParams: { projectId: `${uid}-projectId` },
+  accessControl: () => true,
+});
+
+function createMessages(totalMessages: number, uid: string) {
+  return Array.from({ length: totalMessages }, (_, i) => ({
+    message: `${uid}-message-${i + 1}`,
+    qa: `${uid}-message-qa-${i + 1}`,
+  }));
+}
+
+function createStubData(length: number): NavigationCardProps[] {
+  return Array.from(
+    { length },
+    (_, uid): NavigationCardProps => {
+      const uidString = `stub-${uid}`;
+
+      return {
+        label: `${uidString}-content`,
+        qa: `${uidString}-qa`,
+        messages: createMessages(2, uidString),
+        route: createRoute(uidString),
+      };
+    },
+  );
+}
+
+describe("chunks()", () => {
+  test.each`
+    name                                  | chunkSize | array                          | expected
+    ${"with 1 chunks with a length of 2"} | ${2}      | ${[1, 2]}                      | ${[[1, 2]]}
+    ${"with 2 chunks with a length of 2"} | ${2}      | ${[1, 2, 3, 4]}                | ${[[1, 2], [3, 4]]}
+    ${"with 1 chunk with a length of 3"}  | ${3}      | ${[1, 2, 3]}                   | ${[[1, 2, 3]]}
+    ${"with 2 chunks with a length of 3"} | ${3}      | ${[1, 2, 3, 4, 5, 6]}          | ${[[1, 2, 3], [4, 5, 6]]}
+    ${"with 3 chunks with a length of 3"} | ${3}      | ${[1, 2, 3, 4, 5, 6, 7, 8, 9]} | ${[[1, 2, 3], [4, 5, 6], [7, 8, 9]]}
+  `("should return $name", ({ chunkSize, array, expected }) => {
+    expect(chunks(array, chunkSize)).toStrictEqual(expected);
+  });
+});
+
+describe("<NavigationCard>", () => {
+  const defaultProps = {
+    label: "stub-label",
+    route: createRoute("stub-route"),
+    qa: "stub-qa",
+  };
+
+  const setup = (props?: Partial<NavigationCardProps>) => {
+    const wrapper = mount(
+      <TestBed>
+        <NavigationCard {...defaultProps} {...props} />
+      </TestBed>,
+    );
+
+    const labelElement = findByQa(wrapper, "navigation-card-label");
+    const listElement = findByQa(wrapper, "navigation-card-list");
+
+    return {
+      wrapper,
+      labelElement,
+      listElement,
+    };
+  };
+
+  it("should render with default props", () => {
+    const { labelElement, listElement } = setup();
+
+    expect(labelElement.exists()).toBe(true);
+    expect(listElement.exists()).toBe(false);
+  });
+
+  describe("should render label", () => {
+    it("as a string", () => {
+      const { labelElement } = setup();
+
+      expect(labelElement.text()).toBe(defaultProps.label);
+    });
+
+    it("as a react element", () => {
+      const stubLabel = <span>label as node</span>;
+      const { labelElement } = setup({ label: stubLabel });
+
+      const labelElementType = labelElement.children().type();
+
+      expect(labelElementType).toBe(stubLabel.type);
+    });
+  });
+
+  it("should render with one message", () => {
+    const stubMessage = { message: "stub-message-1", qa: "stub-message-1-qa" };
+    const { wrapper } = setup({ messages: [stubMessage] });
+
+    const firstMessage = findByQa(wrapper, stubMessage.qa);
+
+    expect(firstMessage.exists()).toBe(true);
+  });
+
+  it("should render with multiple messages", () => {
+    const stubMessages = createMessages(2, "stub-multiple-message-test");
+    const { wrapper } = setup({ messages: stubMessages });
+
+    stubMessages.forEach((mssg) => {
+      const firstMessage = findByQa(wrapper, mssg.qa);
+
+      expect(firstMessage.text()).toBe(mssg.message);
+    });
+  });
+});
+
+describe("<NavigationCardsGrid>", () => {
+  const setup = (totalChildren: number) => {
+    const stubNavigation = createStubData(totalChildren);
+    const wrapper = mount(
+      <TestBed>
+        <NavigationCardsGrid>
+          {stubNavigation.map((navItem, i) => (
+            <NavigationCard key={i} {...navItem} />
+          ))}
+        </NavigationCardsGrid>
+      </TestBed>,
+    );
+
+    const navGroupElement = findByQa(wrapper, "navigation-group");
+    const navGroupItemElement = findByQa(wrapper, "navigation-group-item");
+
+    return {
+      wrapper,
+      navGroupElement,
+      navGroupItemElement,
+    };
+  };
+
+  test.each`
+    name                        | totalItems | totalGroups
+    ${"with 1 group, 1 item"}   | ${1}       | ${1}
+    ${"with 1 group, 2 items"}  | ${2}       | ${1}
+    ${"with 2 groups, 4 item"}  | ${4}       | ${2}
+    ${"with 2 groups, 6 items"} | ${6}       | ${2}
+    ${"with 3 groups, 9 items"} | ${9}       | ${3}
+  `("should render $name", ({ totalItems, totalGroups }) => {
+    const { navGroupElement, navGroupItemElement } = setup(totalItems);
+
+    expect(navGroupItemElement.length).toBe(totalItems);
+    expect(navGroupElement.length).toBe(totalGroups);
+  });
+});
