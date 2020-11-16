661f0c8ce39b2bf911d76164696a55fe84c49c4d
Neil Little <neil.little@ukri.org>
2020-11-16 12:52:48 +0000

    ACC-7218: Refactor <PartnerName /> for a function support

--- app/src/ui/components/partners/partnerName.tsx
+++ app/src/ui/components/partners/partnerName.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 
 interface Props {
-  partner: { name: string, isWithdrawn: boolean, isLead: boolean } | null | undefined;
+  partner: { name: string; isWithdrawn: boolean; isLead: boolean } | null | undefined;
   showWithdrawn?: boolean;
   showIsLead?: boolean;
 }
 
-export const PartnerName: React.FunctionComponent<Props> = ({ partner, showIsLead = false, showWithdrawn = true }) => {
-  if (!partner) return null;
+export function getPartnerName(
+  partner: Props["partner"],
+  showIsLead: Props["showIsLead"] = false,
+  showWithdrawn: Props["showWithdrawn"] = true,
+) {
+  if (!partner) return "";
+
   let name = partner.name;
+
   if (showWithdrawn && partner.isWithdrawn) {
     name += " (withdrawn)";
   }
   if (showIsLead && partner.isLead) {
     name += " (Lead)";
   }
+  return name;
+}
+
+/**
+ * @deprecated Please use getPartnerName()
+ */
+export const PartnerName: React.FunctionComponent<Props> = ({ partner, showIsLead = false, showWithdrawn = true }) => {
+  const name = getPartnerName(partner, showIsLead, showWithdrawn);
+
+  if (!partner) return null;
+
   return <React.Fragment>{name}</React.Fragment>;
 };
