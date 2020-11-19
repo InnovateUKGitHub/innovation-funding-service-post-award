555cc7cb98d9c5fa748c36d20c751b3d7dd7b2e5
Neil Little <neil.little@ukri.org>
2020-11-19 15:36:19 +0000

    ACC-7218: <Loader /> & <PageLoader /> refactor

--- app/tests/components/loading.tests.tsx
+++ app/tests/components/loading.tests.tsx
@@ -1,171 +1,247 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { render } from "@testing-library/react";
 
-import { Loader, LoadingProps } from "../../src/ui/components";
-import { LoadingStatus } from "../../src/shared/pending";
+import { Loader, LoadingProps, PageLoader } from "../../src/ui/components";
+import { LoadingStatus, Pending } from "../../src/shared/pending";
 import TestBed from "./helpers/TestBed";
-import { findByQa } from "./helpers/find-by-qa";
+import { ErrorCode } from "@framework/types";
 
-describe("<Loader />", () => {
-  const pendingPreload = { state: LoadingStatus.Preload } as any;
-  const pendingDone = { state: LoadingStatus.Done } as any;
-  const pendingLoading = { state: LoadingStatus.Loading } as any;
-  const pendingStale = { state: LoadingStatus.Stale } as any;
-  const pendingFailed = { state: LoadingStatus.Failed } as any;
-
-  const stubContent = {
-    components: {
-      errorSummary: {
-        errorTitle: {
-          content: "stub-errorContent",
-        },
-        expiredMessageContent: {
-          content: "stub-expiredMessageContent",
-        },
-        unsavedWarningContent: {
-          content: "stub-unsavedWarningContent",
-        },
-        somethingGoneWrongContent: {
-          content: "stub-somethingGoneWrongContent",
-        },
-      },
-      loading: {
-        message: {
-          content: "stub-loading-message",
-        },
+const stubContent = {
+  errors: {
+    notfound: {
+      errorTitle: {
+        content: "stub-notfound-errorTitle",
       },
     },
-  };
+    unexpected: {
+      errorTitle: {
+        content: "stub-unexpected-errorTitle",
+      },
+    },
+  },
+  components: {
+    notFoundErrorPage: {
+      notFoundError: { content: "stub-notFoundError" },
+      goBackMessage: { content: "stub-goBackMessage" },
+      innovateUKMessage: { content: "stub-innovateUKMessage" },
+      yourDashboardMessage: { content: "stub-yourDashboardMessage" },
+    },
+    standardErrorPage: {
+      dashboardText: {
+        content: "stub-dashboardText",
+      },
+      standardError: {
+        content: "stub-standardError",
+      },
+    },
+    errorSummary: {
+      errorTitle: {
+        content: "stub-errorContent",
+      },
+      expiredMessageContent: {
+        content: "stub-expiredMessageContent",
+      },
+      unsavedWarningContent: {
+        content: "stub-unsavedWarningContent",
+      },
+      somethingGoneWrongContent: {
+        content: "stub-somethingGoneWrongContent",
+      },
+    },
+    loading: {
+      message: {
+        content: "stub-loading-message",
+      },
+    },
+  },
+};
 
-  const setup = (props: LoadingProps<{}>) => {
-    const shallowWrapper = shallow(
+describe("<PageLoader />", () => {
+  const setup = (props: LoadingProps<{}>) =>
+    render(
+      <TestBed content={stubContent as any}>
+        <PageLoader {...props} />
+      </TestBed>,
+    );
+
+  describe("@renders", () => {
+    it("with <NotFoundErrorPage />", () => {
+      const stubRender = jest.fn();
+      const { getByText } = setup({
+        render: stubRender,
+        pending: new Pending(LoadingStatus.Failed, {}, { code: ErrorCode.REQUEST_ERROR }),
+      });
+
+      const targetElement = getByText(stubContent.errors.notfound.errorTitle.content);
+
+      expect(targetElement).toBeInTheDocument();
+      expect(stubRender).toBeCalledTimes(0);
+    });
+
+    it("with <StandardErrorPage />", () => {
+      const stubRender = jest.fn();
+      const { getByText } = setup({ render: stubRender, pending: new Pending(LoadingStatus.Failed) });
+
+      const targetElement = getByText(stubContent.errors.unexpected.errorTitle.content);
+
+      expect(targetElement).toBeInTheDocument();
+      expect(stubRender).toBeCalledTimes(0);
+    });
+
+    it("with custom renderError()", () => {
+      const stubCustomErrorContent = "custom-error";
+      const stubRender = jest.fn();
+      const { getByText } = setup({
+        render: stubRender,
+        pending: new Pending(LoadingStatus.Failed),
+        renderError: () => <div>{stubCustomErrorContent}</div>,
+      });
+
+      const targetElement = getByText(stubCustomErrorContent);
+
+      expect(targetElement).toBeInTheDocument();
+      expect(stubRender).toBeCalledTimes(0);
+    });
+  });
+});
+
+describe("<Loader />", () => {
+  const pendingDone = new Pending(LoadingStatus.Done);
+  const pendingPreload = new Pending(LoadingStatus.Preload);
+  const pendingLoading = new Pending(LoadingStatus.Loading);
+  const pendingFailed = new Pending(LoadingStatus.Failed);
+
+  const setup = (props: LoadingProps<{}>) =>
+    render(
       <TestBed content={stubContent as any}>
         <Loader {...props} />
       </TestBed>,
     );
-    const mountWrapper = mount(
-      <TestBed content={stubContent as any}>
-        <Loader {...props} />
-      </TestBed>,
-    );
 
-    const loadingMessageElement = findByQa(mountWrapper, "loading-message");
+  beforeEach(jest.clearAllMocks);
 
-    return {
-      shallowWrapper,
-      mountWrapper,
-      loadingMessageElement,
-    };
-  };
+  describe("@renders", () => {
+    it("should render with Preloaded state", () => {
+      const { container } = setup({ render: jest.fn(), pending: pendingPreload });
+      expect(container).toBeEmptyDOMElement();
+    });
 
-  test("Preload - renderFunctions null", () => {
-    const renderFunction = jest.fn();
-    const { mountWrapper } = setup({ render: renderFunction, pending: pendingPreload });
-    expect(mountWrapper.html()).toBeNull();
-  });
+    describe("should render conditional element", () => {
+      it.each`
+        name                                 | renderFn                        | queryText        | expectedTagName
+        ${"when string renders as span"}     | ${() => "string"}               | ${"string"}      | ${"SPAN"}
+        ${"when number renders as span"}     | ${() => 0}                      | ${"0"}           | ${"SPAN"}
+        ${"when jsx element renders as div"} | ${() => <div>jsx element</div>} | ${"jsx element"} | ${"DIV"}
+      `("$name", ({ renderFn, queryText, expectedTagName }) => {
+        const { getByText } = setup({ render: renderFn, pending: pendingDone });
+        const renderedElementTagName = getByText(queryText).tagName;
 
-  test("Done - calls renderFunction with data and false", () => {
-    const renderFunction = jest.fn(() => null);
-    const data = { test: 123 };
-    const pendingDoneWithData = { ...pendingDone, data };
-    setup({ render: renderFunction, pending: pendingDoneWithData });
-    expect(renderFunction).toHaveBeenCalledWith(data, false);
-  });
+        expect(renderedElementTagName).toBe(expectedTagName);
+      });
+    });
 
-  test("Done - renderFunction returns string", () => {
-    const renderFunction = jest.fn(() => "testing");
-    const data = { test: 123 };
-    const pendingDoneWithData = { ...pendingDone, data };
-    const { shallowWrapper } = setup({ render: renderFunction, pending: pendingDoneWithData });
+    describe.each`
+      name                    | loadingStatus
+      ${"with Stale state"}   | ${LoadingStatus.Stale}
+      ${"with Loading state"} | ${LoadingStatus.Loading}
+    `("should render $name", ({ loadingStatus }) => {
+      it("should render when data is present", () => {
+        const stubData = { stubData: "Hello World" };
+        const stubRender = jest.fn();
+        setup({
+          render: stubRender,
+          pending: new Pending(loadingStatus, stubData),
+        });
 
-    expect(renderFunction).toHaveBeenCalledWith(data, false);
-    expect(shallowWrapper.html()).toBeDefined();
-  });
+        expect(stubRender).toBeCalledTimes(1);
+        expect(stubRender).toBeCalledWith(stubData, true);
+      });
 
-  test("Done - renderFunction returns array", () => {
-    const results = ["testing1", "testing2"];
-    const renderFunction = jest.fn(() => results);
-    const data = { test: 123 };
-    const pendingDoneWithData = { ...pendingDone, data };
-    const { shallowWrapper } = setup({ render: renderFunction, pending: pendingDoneWithData });
-    expect(renderFunction).toHaveBeenCalledWith(data, false);
-    expect(shallowWrapper.html()).toBeDefined();
-  });
+      it("with renderLoading", () => {
+        const stubLoaderQa = "custom-loader";
+        const { getByTestId } = setup({
+          render: jest.fn(),
+          pending: pendingLoading,
+          renderLoading: () => <div data-qa={stubLoaderQa}>some custom loader</div>,
+        });
+        expect(getByTestId(stubLoaderQa)).toBeInTheDocument();
+      });
 
-  test("Loading with data - calls renderFunction with data and true", () => {
-    const renderFunction = jest.fn(() => null);
-    const data = { test: 123 };
-    const pendingLoadingWithData = { ...pendingLoading, data };
-    setup({ render: renderFunction, pending: pendingLoadingWithData });
-    expect(renderFunction).toHaveBeenCalledWith(data, true);
-  });
+      it("with <LoadingMessage />", () => {
+        const { getByText } = setup({
+          render: jest.fn(),
+          pending: pendingLoading,
+        });
 
-  test("Loading without data - renderFunctions default loading message", () => {
-    const callback = jest.fn();
-    const { loadingMessageElement } = setup({ render: callback, pending: pendingLoading });
+        expect(getByText(stubContent.components.loading.message.content)).toBeInTheDocument();
+      });
+    });
 
-    expect(loadingMessageElement.text()).toBe(stubContent.components.loading.message.content);
-    expect(callback).not.toHaveBeenCalled();
-  });
+    it.each`
+      name                                  | loadingStatus
+      ${"should render with Updated state"} | ${LoadingStatus.Updated}
+      ${"should render with Done state"}    | ${LoadingStatus.Done}
+    `("$name", ({ loadingStatus }) => {
+      const stubData = { stubData: "Hello rendered code!" };
+      const stubRender = jest.fn();
+      setup({
+        render: stubRender,
+        pending: new Pending(loadingStatus, stubData),
+      });
 
-  test("Loading without data, with renderFunctionLoading override - calls given renderFunctionLoading", () => {
-    const renderFunction = jest.fn();
-    const loading = jest.fn(() => null);
-    setup({ render: renderFunction, pending: pendingLoading, renderLoading: loading });
-    expect(loading).toHaveBeenCalled();
-    expect(renderFunction).not.toHaveBeenCalled();
-  });
+      expect(stubRender).toBeCalledTimes(1);
+      expect(stubRender).toBeCalledWith(stubData, false);
+    });
 
-  test("Stale with data - calls renderFunction with data and true", () => {
-    const renderFunction = jest.fn(() => null);
-    const data = { test: 123 };
-    const pendingStaleWithData = { ...pendingStale, data };
-    setup({ render: renderFunction, pending: pendingStaleWithData });
-    expect(renderFunction).toHaveBeenCalledWith(data, true);
-  });
+    describe("should render with Failed state", () => {
+      describe("should render renderError", () => {
+        it("with no error code", () => {
+          const stubWithoutErrorCodeQa = "renderError()-without-errorCode";
+          const { getByTestId } = setup({
+            render: jest.fn(),
+            pending: pendingFailed,
+            renderError: (e) => (
+              <div data-qa={e && e.code ? "renderError()-with-code" : stubWithoutErrorCodeQa}>renderError test</div>
+            ),
+          });
 
-  test("Stale without data - renderFunctions default loading message", () => {
-    const renderFunction = jest.fn();
-    const { mountWrapper } = setup({ render: renderFunction, pending: pendingStale });
-    expect(mountWrapper.text()).toBe(stubContent.components.loading.message.content);
-    expect(renderFunction).not.toHaveBeenCalled();
-  });
+          const errorElement = getByTestId(stubWithoutErrorCodeQa);
 
-  test("Stale without data, with renderFunctionLoading override - calls given renderFunctionLoading", () => {
-    const renderFunction = jest.fn();
-    const loading = jest.fn(() => null);
-    setup({ render: renderFunction, pending: pendingStale, renderLoading: loading });
-    expect(loading).toHaveBeenCalled();
-    expect(renderFunction).not.toHaveBeenCalled();
-  });
+          expect(errorElement).toBeDefined();
+        });
 
-  test("Failed without error - renderFunctions null", () => {
-    const renderFunction = jest.fn();
-    const { mountWrapper } = setup({ render: renderFunction, pending: pendingFailed });
-    expect(mountWrapper.html()).toBeNull();
-    expect(renderFunction).not.toHaveBeenCalled();
-  });
+        it("with an error code", () => {
+          const stubWithoutErrorCodeQa = "renderError()-with-errorCode";
+          const { getByTestId } = setup({
+            render: jest.fn(),
+            pending: new Pending(LoadingStatus.Failed, {}, { code: "stub-error" }),
+            renderError: (e) => (
+              <div data-qa={e && e.code ? stubWithoutErrorCodeQa : "renderError-no-code"}>renderError test</div>
+            ),
+          });
 
-  test("Failed with error - renderFunctions ErrorSummary", () => {
-    const renderFunction = jest.fn();
-    const error = true;
-    const pendingWithError = { ...pendingFailed, error };
-    const { mountWrapper } = setup({ render: renderFunction, pending: pendingWithError });
-    expect(mountWrapper.html()).toBeDefined();
-    expect(renderFunction).not.toHaveBeenCalled();
-  });
+          const errorElement = getByTestId(stubWithoutErrorCodeQa);
 
-  // TODO: This test needs a refactor and does not require a <TestBed>
-  test("Unknown - throws exception", () => {
-    const renderFunction = jest.fn();
-    const pending = { state: "not a state" } as any;
-    let error = null;
-    try {
-      shallow(<Loader render={renderFunction} pending={pending} />);
-    } catch (e) {
-      error = e;
-    }
-    expect(error).toBeInstanceOf(Error);
-    expect(renderFunction).not.toHaveBeenCalled();
+          expect(errorElement).toBeDefined();
+        });
+      });
+
+      describe("with ErrorSummary", () => {
+        it("with no error code", () => {
+          const { container, queryByTestId } = setup({ render: jest.fn(), pending: pendingPreload });
+
+          expect(container).toBeEmptyDOMElement();
+          expect(queryByTestId("error-summary")).toBeNull();
+        });
+
+        it("with an error code", () => {
+          const { getByTestId } = setup({
+            render: jest.fn(),
+            pending: new Pending(LoadingStatus.Failed, {}, { code: "some-error" }),
+          });
+          expect(getByTestId("error-summary")).toBeInTheDocument();
+        });
+      });
+    });
   });
 });
