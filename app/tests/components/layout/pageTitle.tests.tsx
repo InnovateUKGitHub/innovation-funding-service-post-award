9eeba0a0f51a1c89f2bdb1e55e9ae033bfa6905a
Neil Little <neil.little@ukri.org>
2020-11-20 15:32:45 +0000

    ACC-7218: Refactor <PageTitle /> update test to RTL

--- app/tests/components/layout/pageTitle.tests.tsx
+++ app/tests/components/layout/pageTitle.tests.tsx
@@ -1,65 +1,109 @@
 import React from "react";
-// tslint:disable-next-line: import-blacklist
-import { mount } from "enzyme";
+import { render } from "@testing-library/react";
 
 import { PageTitle, PageTitleProps } from "../../../src/ui/components/layout/pageTitle";
-import { findByQa } from "../helpers/find-by-qa";
-import TestBed, { ITestBedProps, stubStores } from "../helpers/TestBed";
+import TestBed, { stubStores, TestBedStore } from "../helpers/TestBed";
 
-describe("PageTitle", () => {
-  const setup = (props?: PageTitleProps, stores?: ITestBedProps["stores"]) => {
-    const wrapper = mount(
+describe("<PageTitle />", () => {
+  const setup = (props?: PageTitleProps, stores?: TestBedStore) => {
+    const results = render(
       <TestBed stores={stores}>
         <PageTitle {...props} />
       </TestBed>,
     );
 
-    const titleElement = findByQa(wrapper, "page-title-value");
-    const captionElement = findByQa(wrapper, "page-title-caption");
+    const captionElementQa = "page-title-caption";
 
     return {
-      wrapper,
-      titleElement,
-      captionElement,
+      ...results,
+      captionElementQa,
     };
   };
 
-  test.each`
-    name                 | caption           | matches
-    ${"with caption"}    | ${"stub-caption"} | ${"stub-caption"}
-    ${"without caption"} | ${null}           | ${null}
-  `("renders $name", ({ caption, matches }) => {
-    const { captionElement } = setup({ caption });
+  describe("@renders", () => {
+    describe("with caption", () => {
+      test("when defined", () => {
+        const stubCaptionText = "stub-caption-content";
+        const { queryByTestId, captionElementQa } = setup({ caption: stubCaptionText });
 
-    expect(captionElement.exists()).toBe(!!matches);
+        expect(queryByTestId(captionElementQa)).toBeInTheDocument();
+      });
 
-    if (matches) {
-      expect(captionElement.text()).toBe(matches);
-    }
-  });
+      test("when not set", () => {
+        const { queryByTestId, captionElementQa } = setup({ caption: "" });
 
-  test.each`
-    name                                                              | title                | displayTitle            | matches
-    ${"returns null if no valid title is set"}                        | ${""}                | ${""}                   | ${null}
-    ${"returns title from props"}                                     | ${"stub-only-title"} | ${""}                   | ${"stub-only-title"}
-    ${"returns title from props if defined rather than displayTitle"} | ${"stub-title"}      | ${"stub-fallbackTitle"} | ${"stub-title"}
-    ${"returns displayTitle value when title is not defined"}         | ${""}                | ${"stub-getPageTitle"}  | ${"stub-getPageTitle"}
-  `("$name", ({ title, displayTitle, matches }) => {
-    const stubTestBedStore = {
-      ...stubStores,
-      navigation: {
-        getPageTitle: () => ({
-          displayTitle,
-        }),
-      },
-    };
+        expect(queryByTestId(captionElementQa)).not.toBeInTheDocument();
+      });
+    });
 
-    const { titleElement } = setup({ title }, stubTestBedStore as any);
+    describe("with title", () => {
+      test.each`
+        name                                         | title                  | contextTitle
+        ${"returns title prop"}                      | ${"stub-only-title"}   | ${""}
+        ${"returns title prop in favour of context"} | ${"stub-favour-title"} | ${"stub-custom-context-title"}
+      `("$name", ({ title, contextTitle }) => {
+        const stubTestBedStore = {
+          ...stubStores,
+          navigation: {
+            getPageTitle: () => ({
+              displayTitle: contextTitle,
+            }),
+          },
+        } as TestBedStore;
 
-    if (matches) {
-      expect(titleElement.text()).toBe(matches);
-    } else {
-      expect(titleElement.exists()).toBeFalsy();
-    }
+        const { queryByText } = setup({ title }, stubTestBedStore);
+        const titleValue = queryByText(title);
+
+        expect(titleValue).toBeInTheDocument();
+      });
+
+      test("returns context title when title prop is not defined", () => {
+        const stubContextTitle = "stub-context-title";
+        const stubTestBedStore = {
+          ...stubStores,
+          navigation: {
+            getPageTitle: () => ({
+              displayTitle: stubContextTitle,
+            }),
+          },
+        } as TestBedStore;
+
+        const { queryByText } = setup({ title: "" }, stubTestBedStore);
+        const titleValue = queryByText(stubContextTitle);
+
+        expect(titleValue).toBeInTheDocument();
+      });
+    });
+
+    test("should display both title and caption", () => {
+      const stubTitleText = "stub-title-content";
+      const { queryByText, queryByTestId, captionElementQa } = setup({
+        title: stubTitleText,
+        caption: "stub-caption-content",
+      });
+
+      expect(queryByText(stubTitleText)).toBeInTheDocument();
+      expect(queryByTestId(captionElementQa)).toBeInTheDocument();
+    });
+
+    test("should render null when title is empty", () => {
+      const stubTestBedStore = {
+        ...stubStores,
+        navigation: {
+          getPageTitle: () => ({
+            displayTitle: "",
+          }),
+        },
+      };
+
+      const { container } = setup(
+        {
+          title: "",
+        },
+        stubTestBedStore as TestBedStore,
+      );
+
+      expect(container).toBeEmpty();
+    });
   });
 });
