47a77f93d28db6aeb2a19b07e1116388bd0905b9
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-11-20 10:06:17 +0000

    ACC-7198: PartnerLabels updated

--- app/src/content/labels/partnerLabels.ts
+++ app/src/content/labels/partnerLabels.ts
@@ -6,19 +6,19 @@ export class PartnerLabels extends ContentBase {
     super(parent, "partner-labels", project);
   }
 
-  public readonly organisationName = () => this.getContent("organisation-name");
-  public readonly companyNumber = () => this.getContent("company-number");
-  public readonly companyNumberHint = () => this.getContent("company-number-hint");
-  public readonly sortCode = () => this.getContent("sort-code");
-  public readonly sortCodeHint = () => this.getContent("sort-code-hint");
-  public readonly accountNumber = () => this.getContent("account-number");
-  public readonly accountNumberHint = () => this.getContent("account-number-hint");
-  public readonly firstName = () => this.getContent("first-name");
-  public readonly lastName = () => this.getContent("last-name");
-  public readonly accountBuilding = () => this.getContent("account-building");
-  public readonly accountStreet = () => this.getContent("account-street");
-  public readonly accountLocality = () => this.getContent("account-locality");
-  public readonly accountTownOrCity = () => this.getContent("account-town-or-city");
-  public readonly accountPostcode = () => this.getContent("account-postcode");
+  public readonly organisationName = this.getContent("organisation-name");
+  public readonly companyNumber = this.getContent("company-number");
+  public readonly companyNumberHint = this.getContent("company-number-hint");
+  public readonly sortCode = this.getContent("sort-code");
+  public readonly sortCodeHint = this.getContent("sort-code-hint");
+  public readonly accountNumber = this.getContent("account-number");
+  public readonly accountNumberHint = this.getContent("account-number-hint");
+  public readonly firstName = this.getContent("first-name");
+  public readonly lastName = this.getContent("last-name");
+  public readonly accountBuilding = this.getContent("account-building");
+  public readonly accountStreet = this.getContent("account-street");
+  public readonly accountLocality = this.getContent("account-locality");
+  public readonly accountTownOrCity = this.getContent("account-town-or-city");
+  public readonly accountPostcode = this.getContent("account-postcode");
 
 }
