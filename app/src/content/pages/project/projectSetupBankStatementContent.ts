de2a2dae40c52c7abd0d920b8c5d7b51ab3f2f41
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-11-23 09:57:40 +0000

    ACC-7198: ProjectSetupBankStatementContent updated

--- app/src/content/pages/project/projectSetupBankStatementContent.ts
+++ app/src/content/pages/project/projectSetupBankStatementContent.ts
@@ -13,9 +13,9 @@ export class ProjectSetupBankStatementContent extends ContentPageBase {
   public readonly partnerLabels = new PartnerLabels(this, this.project);
   public readonly documentLabels = new DocumentLabels(this, this.project);
   public readonly documentMessages = new DocumentMessages(this, this.project);
-  public readonly guidanceMessage = () => this.getContent("guidance-message", {markdown: true});
-  public readonly submitButton = () => this.getContent("button-submit");
-  public readonly returnButton = () => this.getContent("button-return");
-  public readonly backLink = () => this.getContent("back-link");
-  public readonly uploadDocumentSectionTitle = () => this.getContent("section-title-upload-document");
+  public readonly guidanceMessage = this.getContent("guidance-message", {markdown: true});
+  public readonly submitButton = this.getContent("button-submit");
+  public readonly returnButton = this.getContent("button-return");
+  public readonly backLink = this.getContent("back-link");
+  public readonly uploadDocumentSectionTitle = this.getContent("section-title-upload-document");
 }
--- app/src/ui/containers/projects/projectSetupBankStatement.tsx
+++ app/src/ui/containers/projects/projectSetupBankStatement.tsx
@@ -40,7 +40,7 @@ class ProjectSetupBankStatementComponent extends ContainerBase<ProjectSetupBankS
       <ACC.Page
         backLink={
           <ACC.BackLink route={this.props.routes.projectSetup.getLink({ projectId: this.props.projectId, partnerId: this.props.partnerId })}>
-            <ACC.Content value={(x) => x.projectSetupBankStatement.backLink()} />
+            <ACC.Content value={(x) => x.projectSetupBankStatement.backLink} />
           </ACC.BackLink>
         }
         error={editor.error}
@@ -59,9 +59,9 @@ class ProjectSetupBankStatementComponent extends ContainerBase<ProjectSetupBankS
             qa="submit-bank-statement-form"
           >
             <Form.Fieldset>
-              <Form.Submit><ACC.Content value={x => x.projectSetupBankStatement.submitButton()}/></Form.Submit>
+              <Form.Submit><ACC.Content value={x => x.projectSetupBankStatement.submitButton}/></Form.Submit>
               <ACC.Link styling="SecondaryButton" route={this.props.routes.projectSetup.getLink({projectId: this.props.projectId, partnerId: this.props.partnerId})}>
-                <ACC.Content value={x => x.projectSetupBankStatement.returnButton()}/>
+                <ACC.Content value={x => x.projectSetupBankStatement.returnButton}/>
               </ACC.Link>
             </Form.Fieldset>
           </Form.Form>
@@ -127,7 +127,7 @@ class ProjectSetupBankStatementComponent extends ContainerBase<ProjectSetupBankS
   private renderGuidance() {
     return (
       <ACC.Section qa={"guidance"}>
-        <ACC.Content value={x => x.projectSetupBankStatement.guidanceMessage()}/>
+        <ACC.Content value={x => x.projectSetupBankStatement.guidanceMessage}/>
       </ACC.Section>
     );
   }
