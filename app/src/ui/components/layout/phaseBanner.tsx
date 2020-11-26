2ae0e9dc34f2aa07538e10b65f53043f59f94daf
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-11-26 13:25:08 +0000

    ACC-7258: PhaseBanner updated

--- app/src/ui/components/layout/phaseBanner.tsx
+++ app/src/ui/components/layout/phaseBanner.tsx
@@ -1,14 +1,21 @@
 import * as React from "react";
 import { ExternalLink } from "../renderers";
+import { useContent } from "@ui/hooks";
 
 export function PhaseBanner() {
+  const { getContent } = useContent();
+  const newServiceMessage = getContent(x => x.components.phaseBannerContent.newServiceMessage);
+  const feedbackMessage = getContent(x => x.components.phaseBannerContent.feedbackMessage);
+  const helpImproveMessage = getContent(x => x.components.phaseBannerContent.helpImprove);
+  const betaText = getContent(x => x.components.phaseBannerContent.betaText);
+
   return (
     <div className="govuk-phase-banner">
       <p className="govuk-phase-banner__content">
-        <strong className="govuk-tag govuk-phase-banner__content__tag" data-qa="phase-banner">beta</strong>
+        <strong className="govuk-tag govuk-phase-banner__content__tag" data-qa="phase-banner">{betaText}</strong>
 
         <span className="govuk-phase-banner__text" data-qa="phase-text">
-          This is a new service – your <ExternalLink className="govuk-link" href="https://www.surveymonkey.co.uk/r/IFSPostAwardFeedback" data-qa="phase-banner-link">feedback</ExternalLink> will help us to improve it.
+          {newServiceMessage} <ExternalLink className="govuk-link" href="https://www.surveymonkey.co.uk/r/IFSPostAwardFeedback" data-qa="phase-banner-link">{feedbackMessage}</ExternalLink> {helpImproveMessage}
         </span>
       </p>
     </div>
--- app/tests/components/layout/phaseBanner.tests.tsx
+++ app/tests/components/layout/phaseBanner.tests.tsx
@@ -4,16 +4,32 @@ import { mount } from "enzyme";
 import React from "react";
 import { PhaseBanner } from "@ui/components";
 import { findByQa } from "../helpers/find-by-qa";
+import { TestBed, TestBedContent } from "@shared/TestBed";
 
 describe("<PhaseBanner />", () => {
-  const setup = () => mount(<PhaseBanner />);
+  const stubContent = {
+    components: {
+      phaseBannerContent: {
+        newServiceMessage: { content: "stub-newServiceMessage" },
+        feedbackMessage: { content: "stub-feedback" },
+        helpImprove: { content: "stub-helpImprove" },
+        betaText: {content: "stub-betaText" }
+      },
+    },
+  };
+  const setup = () =>
+    mount(
+      <TestBed content={stubContent as TestBedContent}>
+        <PhaseBanner />
+      </TestBed>,
+    );
 
   describe("@renders", () => {
     it("a beta element", () => {
       const wrapper = setup();
       const target = findByQa(wrapper, "phase-banner");
 
-      expect(target.text()).toBe("beta");
+      expect(target.text()).toBe(stubContent.components.phaseBannerContent.betaText.content);
     });
 
     it("a link", () => {
@@ -21,7 +37,7 @@ describe("<PhaseBanner />", () => {
       const target = findByQa(wrapper, "phase-banner-link");
 
       expect(target.prop("href")).toBe("https://www.surveymonkey.co.uk/r/IFSPostAwardFeedback");
-      expect(target.text()).toBe("feedback");
+      expect(target.text()).toBe(stubContent.components.phaseBannerContent.feedbackMessage.content);
     });
   });
 });
