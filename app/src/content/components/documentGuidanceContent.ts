03232987006a149f5e825bbdc3d39e4f6b2a58bb
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-11-28 23:16:15 +0000

    ACC-7260: DocumentGuidance bug fixed

--- app/src/content/components/documentGuidanceContent.ts
+++ app/src/content/components/documentGuidanceContent.ts
@@ -9,13 +9,13 @@ export class DocumentGuidanceContent extends ContentPageBase {
 
   public readonly uploadGuidance = this.getContent("components.documentGuidance.uploadGuidance");
   public readonly fileSize = this.getContent("components.documentGuidance.fileSize");
-  public readonly uniqueFilename = this.getContent("components.documentGuidance.uniqueFilename");
+  public readonly uniqueFilename = this.getContent("components.documentGuidance.uniqueFileName");
   public readonly noFilesNumberLimit = this.getContent("components.documentGuidance.noFilesNumberLimit");
   public readonly fileTypesUpload = this.getContent("components.documentGuidance.fileTypesUpload");
   public readonly pdfFiles = this.getContent("components.documentGuidance.pdfFiles");
   public readonly textFiles = this.getContent("components.documentGuidance.textFiles");
   public readonly presentationFiles = this.getContent("components.documentGuidance.presentationFiles");
   public readonly spreadsheetFiles = this.getContent("components.documentGuidance.spreadsheetFiles");
-  public readonly imagesFiles = this.getContent("components.documentGuidance.imagesFiles");
+  public readonly availableImageExtensions = this.getContent("components.documentGuidance.availableImageExtensions");
   public readonly fileTypesQuestion = this.getContent("components.documentGuidance.fileTypesQuestion");
 }
