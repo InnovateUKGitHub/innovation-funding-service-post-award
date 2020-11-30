4c1ac2de464840e82e035df061b01c804b6889c2
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-11-30 10:59:15 +0000

    ACC-7260: DashboardProjectCount content updated

--- app/src/content/pages/project/projectDashboardContent.ts
+++ app/src/content/pages/project/projectDashboardContent.ts
@@ -11,6 +11,21 @@ export class ProjectDashboardContent extends ContentPageBase {
   public readonly searchTitle = this.getContent("searchTitle");
   public readonly searchHint = this.getContent("searchHint");
   public readonly searchLabel = this.getContent("searchLabel");
+  public readonly backToDashboard = this.getContent("backToDashboard");
+  public readonly backToHomepage = this.getContent("backToHomepage");
+
+  public readonly projectCountPrefixMessage = (projectCount: number) => {
+    const hasSingleProject = projectCount === 1;
+
+    return hasSingleProject
+      ? this.getContent("project-prefix-count.singular", { projectCount })
+      : this.getContent("project-prefix-count.plural", { projectCount });
+  }
+
+  public readonly pendingMessage = (projectCount: number) => this.getContent("countMessages.pendingMessage", { projectCount });
+  public readonly liveMessage = (projectCount: number) => this.getContent("countMessages.liveMessage", { projectCount });
+  public readonly upcomingMessage = (projectCount: number) => this.getContent("countMessages.upcomingMessage", { projectCount });
+  public readonly archivedMessage = (projectCount: number) => this.getContent("countMessages.archivedMessage", { projectCount });
 
   public readonly live = {
     noProjects: this.getContent("noLiveProjectsMessage"),
@@ -28,5 +43,4 @@ export class ProjectDashboardContent extends ContentPageBase {
   };
 
   public readonly messages = new ProjectMessages(this, this.project);
-
 }
--- app/src/ui/containers/projects/dashboard/DashboardProjectCount.tsx
+++ app/src/ui/containers/projects/dashboard/DashboardProjectCount.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SimpleString } from "@ui/components/renderers";
 
 import { CuratedSection } from "./Dashboard.interface";
+import { useContent } from "@ui/hooks";
 
 interface DashboardProjectCountProps {
   curatedTotals: CuratedSection<number>;
@@ -9,18 +10,19 @@ interface DashboardProjectCountProps {
 }
 
 export function DashboardProjectCount({ curatedTotals, totalProjectCount }: DashboardProjectCountProps) {
+  const { getContent } = useContent();
   const { open: openTotal, upcoming: upcomingTotal, archived: archivedTotal, pending: pendingTotal } = curatedTotals;
 
   if (!totalProjectCount) return null;
 
   const results: string[] = [];
 
-  if (!!pendingTotal) results.push(`${pendingTotal} in project setup`);
-  if (!!openTotal) results.push(`${openTotal} live`);
-  if (!!upcomingTotal) results.push(`${upcomingTotal} upcoming`);
-  if (!!archivedTotal) results.push(`${archivedTotal} archived`);
+  if (!!pendingTotal) results.push(getContent(x => x.projectsDashboard.pendingMessage(pendingTotal)));
+  if (!!openTotal) results.push(getContent(x => x.projectsDashboard.liveMessage(openTotal)));
+  if (!!upcomingTotal) results.push(getContent(x => x.projectsDashboard.upcomingMessage(upcomingTotal)));
+  if (!!archivedTotal) results.push(getContent(x => x.projectsDashboard.archivedMessage(archivedTotal)));
 
-  const prefixMessage = `${totalProjectCount} ${totalProjectCount > 1 ? "projects" : "project"}`;
+  const prefixMessage = getContent(x => x.projectsDashboard.projectCountPrefixMessage(totalProjectCount));
   const listOfProjects = `(${results.join(", ")})`;
 
   return (
