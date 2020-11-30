84c7ac299e8dcf5b937eaa377f67fa108427511b
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-11-30 01:48:16 +0000

    ACC-7260: ProjectSetupSpendProfile updated

--- app/src/content/pages/project/projectSetupSpendProfileContent.ts
+++ app/src/content/pages/project/projectSetupSpendProfileContent.ts
@@ -10,4 +10,7 @@ export class ProjectSetupSpendProfileContent extends ContentPageBase {
   public readonly guidanceMessage = this.getContent("guidance-message");
   public readonly submitButton = this.getContent("submit-button");
   public readonly backLink = this.getContent("back-link");
+  public readonly readyToSubmitMessage = this.getContent("ready_to_submit-message");
+  public readonly markAsComplete = this.getContent("mark-as-complete");
+  public readonly spendProfileUpdatedMessage = this.getContent("spend-profile-updated-message");
 }
