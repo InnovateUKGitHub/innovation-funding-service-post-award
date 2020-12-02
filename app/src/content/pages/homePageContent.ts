3f2d3b1adcd2b8b5373132518528cd9b780405f9
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-12-02 15:05:42 +0000

    ACC-7260: Home updated

--- app/src/content/pages/homePageContent.ts
+++ app/src/content/pages/homePageContent.ts
@@ -9,5 +9,10 @@ export class HomePageContent extends ContentPageBase {
 
   public readonly exampleContentTitle = this.getContent("example.contentTitle");
   public readonly exampleContent = this.getContent("example.content");
+  public readonly currentUserHeading = this.getContent("current-user-heading");
+  public readonly changeUserMessage = this.getContent("change-user-message");
+  public readonly resetUserMessage = this.getContent("reset-user-message");
+  public readonly projectsHeading = this.getContent("projects-heading");
+  public readonly projectsDashboardHeading = this.getContent("projects-dashboard-heading");
 
 }
