84d752a8c7389e3162e225eee82a91183898880a
Neil Little <neil.little@ukri.org>
2020-12-07 14:30:56 +0000

    ACC-7303: consume gds hook <Button>
    * Refactor to FC
    * Tidy unit tests
    * Add missing test when no styling prop is defined
    * Remove enzyme from unit test

--- app/tests/components/styledButton.tests.tsx
+++ app/tests/components/styledButton.tests.tsx
@@ -1,29 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
 
-import React, { CSSProperties } from "react";
-
-// tslint:disable-next-line: import-blacklist
-import { shallow } from "enzyme";
-import { Button } from "../../src/ui/components";
-import { GOVUK_LINK_COLOUR } from "../../src/ui/styles/colours";
+import { Button, StyledButtonProps } from "../../src/ui/components";
 
 describe("StyledButton", () => {
-  it("should be styled as a primary button when given a primary styling", () => {
-    const wrapper = shallow(<Button styling="Primary" />);
-    expect(wrapper.prop("className")).toEqual("govuk-button govuk-!-margin-right-1");
-  });
+  const setup = (props: StyledButtonProps) => render(<Button {...props} />);
 
-  it("should be styled as a secondary button when given a secondary styling", () => {
-    const wrapper = shallow(<Button styling="Secondary" />);
-    expect(wrapper.prop("className")).toEqual("govuk-button govuk-!-margin-right-1 govuk-button--secondary");
-  });
-
-  it("should be styled as a warning button when given a warning styling", () => {
-    const wrapper = shallow(<Button styling="Warning" />);
-    expect(wrapper.prop("className")).toEqual("govuk-button govuk-!-margin-right-1 govuk-button--warning");
-  });
-
-  it("should be styled as a link when given a link styling", () => {
-    const wrapper = shallow(<Button styling="Link" />);
-    expect(wrapper.prop("className")).toEqual("govuk-link");
+  describe("@renders", () => {
+    test.each`
+      name                                                        | props                       | expectedClasses
+      ${"when a primary button when given a primary styling"}     | ${{ styling: "Primary" }}   | ${"govuk-button govuk-!-margin-right-1"}
+      ${"when a secondary button when given a secondary styling"} | ${{ styling: "Secondary" }} | ${"govuk-button govuk-!-margin-right-1 govuk-button--secondary"}
+      ${"when a warning button when given a warning styling"}     | ${{ styling: "Warning" }}   | ${"govuk-button govuk-!-margin-right-1 govuk-button--warning"}
+      ${"when a link when given a link styling"}                  | ${{ styling: "Link" }}      | ${"govuk-link"}
+      ${"when a button with no styling returns a default class"}  | ${{}}                       | ${"govuk-button govuk-!-margin-right-1"}
+    `("$name", ({ props, expectedClasses }) => {
+      const { container } = setup(props);
+      expect(container.firstChild).toHaveClass(expectedClasses, { exact: true });
+    });
   });
 });
