d17fa57a2675867f7a44e88a730c7f52af9f556e
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-12-11 10:39:28 +0000

    ACC-7325: Claim Review updated with new content

--- app/src/content/pages/claims/claimReviewContent.ts
+++ app/src/content/pages/claims/claimReviewContent.ts
@@ -18,6 +18,7 @@ export class ClaimReviewContent extends ContentPageBase {
   public readonly howToProceedSectionTitle = this.getContent("section-title-how-to-proceed");
   public readonly additionalInfoSectionTitle = this.getContent("section-title-additional-info");
   public readonly additionalInfoHint = this.getContent("additional-info-hint");
+  public readonly claimReviewDeclaration = this.getContent("claim-review-declaration");
   public readonly monitoringReportReminder = this.getContent("monitoring-report-reminder");
   public readonly submitButton = this.getContent("button-submit");
   public readonly sendQueryButton = this.getContent("button-send-query");
