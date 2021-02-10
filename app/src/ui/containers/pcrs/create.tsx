ba6bf5f6403bebcd9866ca294ecb1e87c38e810f
Neil Little <neil.little@ukri.org>
2021-02-10 18:22:49 +0000

    ACC-5928: Initial refactor + tidy

--- app/src/ui/containers/pcrs/create.tsx
+++ app/src/ui/containers/pcrs/create.tsx
@@ -1,161 +1,189 @@
 import * as Dtos from "@framework/dtos";
+import * as ACC from "@ui/components";
+
 import { Pending } from "@shared/pending";
+
+import { BaseProps, defineRoute } from "@ui/containers/containerBase";
+import { useContent } from "@ui/hooks";
 import { IEditorStore, useStores } from "@ui/redux";
 import { PCRDtoValidator } from "@ui/validators";
-import { useContent } from "@ui/hooks";
-import { ExpandedItem, FormGuidanceExpander } from "../../components/inputs/formGuidanceExpander";
 
-import * as ACC from "../../components";
-import { BaseProps, ContainerBase, defineRoute } from "../containerBase";
-export interface CreateProjectChangeRequestParams {
+export interface CreatePcrParams {
   projectId: string;
 }
 
-interface Data {
+interface CreatePcrProps extends CreatePcrParams, BaseProps {
   project: Pending<Dtos.ProjectDto>;
   itemTypes: Pending<Dtos.PCRItemTypeDto[]>;
   editor: Pending<IEditorStore<Dtos.PCRDto, PCRDtoValidator>>;
-}
-
-interface Callbacks {
   content: Record<string, string>;
   onChange: (saving: boolean, dto: Dtos.PCRDto) => void;
   createNewChangeRequestItem: (itemType: Dtos.PCRItemTypeDto) => Dtos.PCRItemDto;
+}
+
+function PCRCreateComponent({ content, ...props }: CreatePcrProps) {
+  const getPcrFromData = (editorItems: Dtos.PCRItemDto[], itemTypes: Dtos.PCRItemTypeDto[]) => {
+    const filteredOptions = itemTypes.reduce<ACC.SelectOption[][]>(
+      (acc, item) => {
+        // Note: ignore values which are not enabled
+        if (!item.enabled) return acc;
+
+        const newOption = {
+          id: item.type.toString(),
+          value: item.displayName,
+          disabled: false,
+        };
+        const hasSelectedOption = editorItems.some(x => `${x.type}` === newOption.id);
+
+        const [allOptions, allSelectedItems] = acc;
+
+        const newOptions = [...allOptions, newOption];
+        const newSelectedOptions = hasSelectedOption ? [...allSelectedItems, newOption] : allSelectedItems;
+
+        return [newOptions, newSelectedOptions];
+      },
+      [[], []],
+    );
+
+    const [options, selected] = filteredOptions;
+
+    return {
+      options,
+      selected,
+    };
+  };
+
+  const calculateUpdatedOptions = (
+    options: Dtos.PCRItemTypeDto[],
+    model: Dtos.PCRDto,
+    selectedValues: ACC.SelectOption[],
+  ): Dtos.PCRItemDto[] => {
+    const filteredItems = options.filter(x => selectedValues.some(y => y.id === x.type.toString()));
+    return filteredItems.map(x => model.items.find(y => x.type === y.type) || props.createNewChangeRequestItem(x));
+  };
+
+  const renderPage = (
+    project: Dtos.ProjectDto,
+    itemTypes: Dtos.PCRItemTypeDto[],
+    editor: IEditorStore<Dtos.PCRDto, PCRDtoValidator>,
+  ) => {
+    const pcrDashboardLink = props.routes.pcrsDashboard.getLink({ projectId: props.projectId });
+    const pcrDashboardBackLink = <ACC.BackLink route={pcrDashboardLink}>{content.backLink}</ACC.BackLink>;
+
+    const PCRForm = ACC.TypedForm<Dtos.PCRDto>();
+    const { options, selected } = getPcrFromData(editor.data.items, itemTypes);
+
+    const createGuidanceContent = [
+      { header: content.reallocateCostsTitle, description: content.reallocateCostsMessage },
+      { header: content.removePartnerTitle, description: content.removePartnerMessage },
+      { header: content.addPartnerTitle, description: content.addPartnerMessage },
+      { header: content.changeScopeTitle, description: content.changeScopeMessage },
+      { header: content.changeDurationTitle, description: content.changeDurationMessage },
+      { header: content.changePartnersNameTitle, description: content.changePartnersNameMessage },
+      { header: content.putProjectOnHoldTitle, description: content.putProjectOnHoldMessage },
+      { header: content.endProjectEarlyTitle, description: content.endProjectEarlyMessage },
+    ];
+
+    return (
+      <ACC.Page
+        backLink={pcrDashboardBackLink}
+        pageTitle={<ACC.Projects.Title {...project} />}
+        project={project}
+        validator={editor.validator}
+        error={editor.error}
+      >
+        <ACC.Renderers.SimpleString>{content.guidanceIntroMessage}</ACC.Renderers.SimpleString>
+
+        <ACC.UnorderedList>
+          <li>{content.guidanceListRow1}</li>
+          <li>{content.guidanceListRow2}</li>
+        </ACC.UnorderedList>
+
+        <ACC.Section qa="pcr-create">
+          <PCRForm.Form
+            qa="pcr-create-form"
+            editor={editor}
+            onSubmit={() => props.onChange(true, editor.data)}
+            onChange={dto => props.onChange(false, dto)}
+          >
+            <PCRForm.Fieldset heading={content.selectRequestTypesTitle}>
+              <ACC.Renderers.SimpleString className="govuk-hint">{content.selectTypesHint}</ACC.Renderers.SimpleString>
+
+              <ACC.Inputs.FormGuidanceExpander
+                title={content.learnMoreAboutTitle}
+                items={createGuidanceContent}
+                qa="pcr-create-guidance"
+              />
+
+              <PCRForm.Checkboxes
+                name="types"
+                hint={content.selectTypesHint}
+                options={options}
+                validation={editor.validator.items}
+                value={() => selected}
+                update={(model, selectedValue) => {
+                  const updatedItems = calculateUpdatedOptions(itemTypes, model, selectedValue || []);
+
+                  model.items = updatedItems;
+                }}
+              />
+            </PCRForm.Fieldset>
+
+            <PCRForm.Fieldset>
+              <PCRForm.Submit>{content.createRequestButton}</PCRForm.Submit>
+
+              <ACC.Link styling="SecondaryButton" route={pcrDashboardLink}>
+                {content.cancelRequestButton}
+              </ACC.Link>
+            </PCRForm.Fieldset>
+          </PCRForm.Form>
+        </ACC.Section>
+      </ACC.Page>
+    );
+  };
+
+  const combined = Pending.combine({
+    project: props.project,
+    editor: props.editor,
+    itemTypes: props.itemTypes,
+  });
+
+  return <ACC.PageLoader pending={combined} render={x => renderPage(x.project, x.itemTypes, x.editor)} />;
 }
 
 export function useCreatePcrContent() {
   const { getContent } = useContent();
 
   return {
-  learnMoreAboutTitle: getContent(x => x.pcrCreate.learnMoreAboutTitle),
-  reallocateCostsTitle: getContent(x => x.pcrCreate.reallocateCostsTitle),
-  reallocateCostsMessage: getContent(x => x.pcrCreate.reallocateCostsMessage),
-  removePartnerTitle: getContent(x => x.pcrCreate.removePartnerTitle),
-  removePartnerMessage: getContent(x => x.pcrCreate.removePartnerMessage),
-  addPartnerTitle: getContent(x => x.pcrCreate.addPartnerTitle),
-  addPartnerMessage: getContent(x => x.pcrCreate.addPartnerMessage),
-  changeScopeTitle: getContent(x => x.pcrCreate.changeScopeTitle),
-  changeScopeMessage: getContent(x => x.pcrCreate.changeScopeMessage),
-  changeDurationTitle: getContent(x => x.pcrCreate.changeDurationTitle),
-  changeDurationMessage: getContent(x => x.pcrCreate.changeDurationMessage),
-  changePartnersNameTitle: getContent(x => x.pcrCreate.changePartnersNameTitle),
-  changePartnersNameMessage: getContent(x => x.pcrCreate.changePartnersNameMessage),
-  putProjectOnHoldTitle: getContent(x => x.pcrCreate.putProjectOnHoldTitle),
-  putProjectOnHoldMessage: getContent(x => x.pcrCreate.putProjectOnHoldMessage),
-  endProjectEarlyTitle: getContent(x => x.pcrCreate.endProjectEarlyTitle),
-  endProjectEarlyMessage: getContent(x => x.pcrCreate.endProjectEarlyMessage),
-  selectTypesHint: getContent(x => x.pcrCreate.selectTypesHint),
-  backLink: getContent(x => x.pcrCreate.backLink),
-  selectRequestTypesTitle: getContent(x => x.pcrCreate.selectRequestTypesTitle),
-  createRequestButton: getContent(x => x.pcrCreate.createRequestButton),
-  cancelRequestButton: getContent(x => x.pcrCreate.cancelRequestButton),
-  guidanceIntroMessage: getContent(x => x.pcrCreate.guidanceIntroMessage),
-  guidanceListRow1: getContent(x => x.pcrCreate.guidanceListRow1),
-  guidanceListRow2: getContent(x => x.pcrCreate.guidanceListRow2)
+    learnMoreAboutTitle: getContent(x => x.pcrCreate.learnMoreAboutTitle),
+    reallocateCostsTitle: getContent(x => x.pcrCreate.reallocateCostsTitle),
+    reallocateCostsMessage: getContent(x => x.pcrCreate.reallocateCostsMessage),
+    removePartnerTitle: getContent(x => x.pcrCreate.removePartnerTitle),
+    removePartnerMessage: getContent(x => x.pcrCreate.removePartnerMessage),
+    addPartnerTitle: getContent(x => x.pcrCreate.addPartnerTitle),
+    addPartnerMessage: getContent(x => x.pcrCreate.addPartnerMessage),
+    changeScopeTitle: getContent(x => x.pcrCreate.changeScopeTitle),
+    changeScopeMessage: getContent(x => x.pcrCreate.changeScopeMessage),
+    changeDurationTitle: getContent(x => x.pcrCreate.changeDurationTitle),
+    changeDurationMessage: getContent(x => x.pcrCreate.changeDurationMessage),
+    changePartnersNameTitle: getContent(x => x.pcrCreate.changePartnersNameTitle),
+    changePartnersNameMessage: getContent(x => x.pcrCreate.changePartnersNameMessage),
+    putProjectOnHoldTitle: getContent(x => x.pcrCreate.putProjectOnHoldTitle),
+    putProjectOnHoldMessage: getContent(x => x.pcrCreate.putProjectOnHoldMessage),
+    endProjectEarlyTitle: getContent(x => x.pcrCreate.endProjectEarlyTitle),
+    endProjectEarlyMessage: getContent(x => x.pcrCreate.endProjectEarlyMessage),
+    selectTypesHint: getContent(x => x.pcrCreate.selectTypesHint),
+    backLink: getContent(x => x.pcrCreate.backLink),
+    selectRequestTypesTitle: getContent(x => x.pcrCreate.selectRequestTypesTitle),
+    createRequestButton: getContent(x => x.pcrCreate.createRequestButton),
+    cancelRequestButton: getContent(x => x.pcrCreate.cancelRequestButton),
+    guidanceIntroMessage: getContent(x => x.pcrCreate.guidanceIntroMessage),
+    guidanceListRow1: getContent(x => x.pcrCreate.guidanceListRow1),
+    guidanceListRow2: getContent(x => x.pcrCreate.guidanceListRow2),
   };
 }
 
-class PCRCreateComponent extends ContainerBase<CreateProjectChangeRequestParams, Data, Callbacks> {
-  render() {
-    const combined = Pending.combine({
-      project: this.props.project,
-      pcr: this.props.editor,
-      itemTypes: this.props.itemTypes,
-    });
-    return <ACC.PageLoader pending={combined} render={d => this.renderContents(d.project, d.pcr, d.itemTypes)} />;
-  }
-
-  private renderContents(
-    project: Dtos.ProjectDto,
-    editor: IEditorStore<Dtos.PCRDto, PCRDtoValidator>,
-    itemTypes: Dtos.PCRItemTypeDto[],
-  ) {
-    return (
-      <ACC.Page
-        backLink={
-          <ACC.BackLink route={this.props.routes.pcrsDashboard.getLink({ projectId: this.props.projectId })}>
-            {this.props.content.backLink}
-          </ACC.BackLink>
-        }
-        pageTitle={<ACC.Projects.Title {...project} />}
-        project={project}
-        validator={editor.validator}
-        error={editor.error}
-      >
-        <ACC.Renderers.SimpleString>{this.props.content.guidanceIntroMessage}</ACC.Renderers.SimpleString>
-
-        <ACC.UnorderedList>
-          <li>{this.props.content.guidanceListRow1}</li>
-          <li>{this.props.content.guidanceListRow2}</li>
-        </ACC.UnorderedList>
-
-        <ACC.Section qa="pcr-create">{this.renderForm(editor, itemTypes)}</ACC.Section>
-      </ACC.Page>
-    );
-  }
-
-  private readonly createGuidanceContent: ExpandedItem[] = [
-    { header: this.props.content.reallocateCostsTitle, description: this.props.content.reallocateCostsMessage },
-    { header: this.props.content.removePartnerTitle, description: this.props.content.removePartnerMessage },
-    { header: this.props.content.addPartnerTitle, description: this.props.content.addPartnerMessage },
-    { header: this.props.content.changeScopeTitle, description: this.props.content.changeScopeMessage },
-    { header: this.props.content.changeDurationTitle, description: this.props.content.changeDurationMessage },
-    { header: this.props.content.changePartnersNameTitle, description: this.props.content.changePartnersNameMessage },
-    { header: this.props.content.putProjectOnHoldTitle, description: this.props.content.putProjectOnHoldMessage },
-    { header: this.props.content.endProjectEarlyTitle, description: this.props.content.endProjectEarlyMessage },
-  ];
-
-  private renderForm(
-    pcrEditor: IEditorStore<Dtos.PCRDto, PCRDtoValidator>,
-    itemTypes: Dtos.PCRItemTypeDto[],
-  ): React.ReactNode {
-    const PCRForm = ACC.TypedForm<Dtos.PCRDto>();
-    const options = itemTypes
-      .filter(x => x.enabled)
-      .map<ACC.SelectOption>(x => ({ id: x.type.toString(), value: x.displayName }));
-    const selected = options.filter(x => pcrEditor.data.items.some(y => y.type.toString() === x.id));
-
-    return (
-      <PCRForm.Form
-        editor={pcrEditor}
-        onSubmit={() => this.props.onChange(true, pcrEditor.data)}
-        onChange={dto => this.props.onChange(false, dto)}
-        qa="pcr-create-form"
-      >
-        <PCRForm.Fieldset heading={this.props.content.selectRequestTypesTitle}>
-          <ACC.Renderers.SimpleString className="govuk-hint">
-            {this.props.content.selectTypesHint}
-          </ACC.Renderers.SimpleString>
-
-          <FormGuidanceExpander title={this.props.content.learnMoreAboutTitle} items={this.createGuidanceContent} qa="pcr-create-guidance"/>
-
-          <PCRForm.Checkboxes
-            options={options}
-            name="types"
-            validation={pcrEditor.validator.items}
-            value={x => selected}
-            update={(model, selectedValue) => {
-              model.items = itemTypes
-                .filter(x => (selectedValue || []).some(y => y.id === x.type.toString()))
-                .map(x => model.items.find(y => x.type === y.type) || this.props.createNewChangeRequestItem(x));
-            }}
-          />
-        </PCRForm.Fieldset>
-        <PCRForm.Fieldset>
-          <PCRForm.Submit>{this.props.content.createRequestButton}</PCRForm.Submit>
-          <ACC.Link
-            styling="SecondaryButton"
-            route={this.props.routes.pcrsDashboard.getLink({ projectId: this.props.projectId })}
-          >
-            {this.props.content.cancelRequestButton}
-          </ACC.Link>
-        </PCRForm.Fieldset>
-      </PCRForm.Form>
-    );
-  }
-}
-
-const PCRCreateContainer = (props: CreateProjectChangeRequestParams & BaseProps) => {
+function PCRCreateContainer(props: CreatePcrParams & BaseProps) {
   const { projects, projectChangeRequests, navigation } = useStores();
   const pcrContent = useCreatePcrContent();
 
@@ -166,26 +194,26 @@ const PCRCreateContainer = (props: CreateProjectChangeRequestParams & BaseProps)
       project={projects.getById(props.projectId)}
       itemTypes={projectChangeRequests.getAllPcrTypes()}
       editor={projectChangeRequests.getPcrCreateEditor(props.projectId)}
+      createNewChangeRequestItem={itemType => projectChangeRequests.createNewChangeRequestItem(itemType)}
       onChange={(saving, dto) =>
         projectChangeRequests.updatePcrEditor(saving, props.projectId, dto, undefined, created =>
-          navigation.navigateTo(props.routes.pcrPrepare.getLink({ projectId: dto.projectId, pcrId: created.id })),
+          navigation.navigateTo(
+            props.routes.pcrPrepare.getLink({
+              projectId: dto.projectId,
+              pcrId: created.id,
+            }),
+          ),
         )
-      }
-      createNewChangeRequestItem={(itemType: Dtos.PCRItemTypeDto) =>
-        projectChangeRequests.createNewChangeRequestItem(itemType)
       }
     />
   );
-};
+}
 
 export const PCRCreateRoute = defineRoute({
   routeName: "pcrCreate",
   routePath: "/projects/:projectId/pcrs/create",
   container: PCRCreateContainer,
-  getParams: route => ({
-    projectId: route.params.projectId,
-  }),
+  getParams: route => ({ projectId: route.params.projectId }),
   getTitle: ({ content }) => content.pcrCreate.title(),
-  accessControl: (auth, { projectId }, config) =>
-    auth.forProject(projectId).hasAnyRoles(Dtos.ProjectRole.ProjectManager),
+  accessControl: (auth, { projectId }) => auth.forProject(projectId).hasAnyRoles(Dtos.ProjectRole.ProjectManager),
 });
