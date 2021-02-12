705fd399aa9a6f0072b18dfd0f9e3e7f82c68aa3
Grace Williams <grace.williams@innovateuk.gov.uk>
2021-02-12 19:17:38 +0000

    ACC-7276: Content changes to Change remaining grant and Reallocate project costs screen

--- app/src/ui/containers/pcrs/financialVirements/editPartnerLevel.tsx
+++ app/src/ui/containers/pcrs/financialVirements/editPartnerLevel.tsx
@@ -1,9 +1,33 @@
 import * as ACC from "@ui/components";
 import { Pending } from "@shared/pending";
 import { BaseProps, ContainerBase, defineRoute } from "@ui/containers/containerBase";
-import { EditorStatus, IEditorStore, StoresConsumer } from "@ui/redux";
+import { EditorStatus, IEditorStore, useStores } from "@ui/redux";
 import { FinancialVirementDtoValidator, PartnerVirementsDtoValidator } from "@ui/validators";
 import { FinancialVirementDto, PartnerDto, PartnerVirementsDto, ProjectDto } from "@framework/dtos";
+import { useContent } from "@ui/hooks";
+
+export function useEditPartnerLevelContent() {
+  const { getContent } = useContent();
+
+  return {
+    saveButton: getContent(x => x.financialVirementEditPartnerLevel.saveButton),
+
+    remainingGrantInfoIntro: getContent(x => x.financialVirementEditPartnerLevel.remainingGrantInfo.intro),
+    remainingGrantInfoCheckRules: getContent(x => x.financialVirementEditPartnerLevel.remainingGrantInfo.checkRules),
+    remainingGrantInfoRemainingGrant: getContent(x => x.financialVirementEditPartnerLevel.remainingGrantInfo.remainingGrant),
+    remainingGrantInfoFundingLevel: getContent(x => x.financialVirementEditPartnerLevel.remainingGrantInfo.fundingLevel),
+
+    partnerName: getContent(x => x.financialVirementEditPartnerLevel.labels.partnerName),
+    partnerOriginalRemainingCosts: getContent(x => x.financialVirementEditPartnerLevel.labels.partnerOriginalRemainingCosts),
+    partnerOriginalRemainingGrant: getContent(x => x.financialVirementEditPartnerLevel.labels.partnerOriginalRemainingGrant),
+    originalFundingLevel: getContent(x => x.financialVirementEditPartnerLevel.labels.originalFundingLevel),
+    partnerNewRemainingCosts: getContent(x => x.financialVirementEditPartnerLevel.labels.partnerNewRemainingCosts),
+    partnerNewRemainingGrant: getContent(x => x.financialVirementEditPartnerLevel.labels.partnerNewRemainingGrant),
+    newFundingLevel: getContent(x => x.financialVirementEditPartnerLevel.labels.newFundingLevel),
+    projectTotals: getContent(x => x.financialVirementEditPartnerLevel.labels.projectTotals),
+    backToSummary: getContent(x => x.financialVirementEditPartnerLevel.labels.backToSummary),
+  };
+}
 
 export interface FinancialVirementParams {
   projectId: string;
@@ -15,10 +39,11 @@ interface Props {
   project: Pending<ProjectDto>;
   partners: Pending<PartnerDto[]>;
   editor: Pending<IEditorStore<FinancialVirementDto, FinancialVirementDtoValidator>>;
+  content: Record<string, string>;
   onChange: (saving: boolean, dto: FinancialVirementDto) => void;
 }
 
-class Component extends ContainerBase<FinancialVirementParams, Props, {}> {
+class EditPartnerLevelComponent extends ContainerBase<FinancialVirementParams, Props, {}> {
   render() {
     const combined = Pending.combine({
       project: this.props.project,
@@ -44,21 +69,24 @@ class Component extends ContainerBase<FinancialVirementParams, Props, {}> {
         validator={editor.validator}
       >
         <ACC.Section>
-          <ACC.Renderers.SimpleString><ACC.Content value={x => x.financialVirementEditPartnerLevel.remainingGrantInfo} /></ACC.Renderers.SimpleString>
+          <ACC.Renderers.SimpleString>{this.props.content.remainingGrantInfoIntro}</ACC.Renderers.SimpleString>
+          <ACC.Renderers.SimpleString>{this.props.content.remainingGrantInfoCheckRules}</ACC.Renderers.SimpleString>
+          <ACC.Renderers.SimpleString>{this.props.content.remainingGrantInfoRemainingGrant}</ACC.Renderers.SimpleString>
+          <ACC.Renderers.SimpleString>{this.props.content.remainingGrantInfoFundingLevel}</ACC.Renderers.SimpleString>
           <VirementForm.Form editor={editor} onChange={(dto) => this.props.onChange(false, dto)} onSubmit={() => this.props.onChange(true, editor.data)} qa="partner_level_form">
             <VirementForm.Fieldset>
               <VirementTable.Table qa="partnerVirements" data={data} validationResult={validation}>
-                <VirementTable.String qa="partner" headerContent={x => x.financialVirementEditPartnerLevel.labels.partnerName} value={x => x.partner.name} footer={<ACC.Content value={x => x.financialVirementEditPartnerLevel.labels.projectTotals} />} isDivider="normal" />
-                <VirementTable.Currency qa="remainingCosts" headerContent={x => x.financialVirementEditPartnerLevel.labels.partnerOriginalRemainingCosts} value={x => x.virement.originalRemainingCosts} footer={<ACC.Renderers.Currency value={editor.data.originalRemainingCosts} />} />
-                <VirementTable.Currency qa="remainingGrant" headerContent={x => x.financialVirementEditPartnerLevel.labels.partnerOriginalRemainingGrant} value={x => x.virement.originalRemainingGrant} footer={<ACC.Renderers.Currency value={editor.data.originalRemainingGrant} />} />
-                <VirementTable.Percentage qa="fundingLevel" headerContent={x => x.financialVirementEditPartnerLevel.labels.originalFundingLevel} value={(x, i) => x.virement.originalFundingLevel} footer={<ACC.Renderers.Percentage value={editor.data.originalFundingLevel} />} isDivider="normal" />
-                <VirementTable.Currency qa="newCosts" headerContent={x => x.financialVirementEditPartnerLevel.labels.partnerNewRemainingCosts} value={x => x.virement.newRemainingCosts} footer={<ACC.Renderers.Currency value={editor.data.newRemainingCosts} />} />
-                <VirementTable.Custom qa="newGrant" headerContent={x => x.financialVirementEditPartnerLevel.labels.partnerNewRemainingGrant} value={x => this.renderInput(x.partner, x.virement, editor.status === EditorStatus.Saving, x.validator)} footer={this.renderFooter(editor)} classSuffix="numeric" />
-                <VirementTable.Percentage qa="newLevel" headerContent={x => x.financialVirementEditPartnerLevel.labels.newFundingLevel} value={(x, i) => x.virement.newFundingLevel} footer={<ACC.Renderers.Percentage value={editor.data.newFundingLevel} />} />
+                <VirementTable.String qa="partner" header={this.props.content.partnerName} value={x => x.partner.name} footer={this.props.content.projectTotals} isDivider="normal" />
+                <VirementTable.Currency qa="remainingCosts" header={this.props.content.partnerOriginalRemainingCosts} value={x => x.virement.originalRemainingCosts} footer={<ACC.Renderers.Currency value={editor.data.originalRemainingCosts} />} />
+                <VirementTable.Currency qa="remainingGrant" header={this.props.content.partnerOriginalRemainingGrant} value={x => x.virement.originalRemainingGrant} footer={<ACC.Renderers.Currency value={editor.data.originalRemainingGrant} />} />
+                <VirementTable.Percentage qa="fundingLevel" header={this.props.content.originalFundingLevel} value={(x, i) => x.virement.originalFundingLevel} footer={<ACC.Renderers.Percentage value={editor.data.originalFundingLevel} />} isDivider="normal" />
+                <VirementTable.Currency qa="newCosts" header={this.props.content.partnerNewRemainingCosts} value={x => x.virement.newRemainingCosts} footer={<ACC.Renderers.Currency value={editor.data.newRemainingCosts} />} />
+                <VirementTable.Custom qa="newGrant" header={this.props.content.partnerNewRemainingGrant} value={x => this.renderInput(x.partner, x.virement, editor.status === EditorStatus.Saving, x.validator)} footer={this.renderFooter(editor)} classSuffix="numeric" />
+                <VirementTable.Percentage qa="newLevel" header={this.props.content.newFundingLevel} value={(x, i) => x.virement.newFundingLevel} footer={<ACC.Renderers.Percentage value={editor.data.newFundingLevel} />} />
               </VirementTable.Table>
             </VirementForm.Fieldset>
             <VirementForm.Fieldset>
-              <VirementForm.Submit><ACC.Content value={x => x.financialVirementEditPartnerLevel.saveButton} /></VirementForm.Submit>
+              <VirementForm.Submit>{this.props.content.saveButton}</VirementForm.Submit>
             </VirementForm.Fieldset>
           </VirementForm.Form>
         </ACC.Section>
@@ -107,26 +135,43 @@ class Component extends ContainerBase<FinancialVirementParams, Props, {}> {
       itemId: this.props.itemId
     };
 
-    return <ACC.BackLink route={this.props.routes.pcrPrepareItem.getLink(params)} preserveData={true}><ACC.Content value={x => x.financialVirementEditPartnerLevel.labels.backToSummary}/></ACC.BackLink>;
+    return <ACC.BackLink route={this.props.routes.pcrPrepareItem.getLink(params)} preserveData={true}>{this.props.content.backToSummary}</ACC.BackLink>;
   }
 }
 
-const Container = (props: FinancialVirementParams & BaseProps) => (
-  <StoresConsumer>
-    {
-      stores => (
-        <Component
-          project={stores.projects.getById(props.projectId)}
-          partners={stores.partners.getPartnersForProject(props.projectId)}
-          editor={stores.financialVirements.getFiniancialVirementEditor(props.projectId, props.pcrId, props.itemId)}
-          onChange={(saving, dto) => stores.financialVirements.updateFiniancialVirementEditor(saving, props.projectId, props.pcrId, props.itemId, dto, true, () =>
-            stores.navigation.navigateTo(props.routes.pcrPrepareItem.getLink({ projectId: props.projectId, pcrId: props.pcrId, itemId: props.itemId }), true))}
-          {...props}
-        />
-      )
-    }
-  </StoresConsumer>
-);
+const Container = (props: FinancialVirementParams & BaseProps) => {
+  const { projects, partners, financialVirements, navigation } = useStores();
+  const editPartnerLevelContent = useEditPartnerLevelContent();
+
+  return (
+    <EditPartnerLevelComponent
+      content={editPartnerLevelContent}
+      project={projects.getById(props.projectId)}
+      partners={partners.getPartnersForProject(props.projectId)}
+      editor={financialVirements.getFiniancialVirementEditor(props.projectId, props.pcrId, props.itemId)}
+      onChange={(saving, dto) =>
+        financialVirements.updateFiniancialVirementEditor(
+          saving,
+          props.projectId,
+          props.pcrId,
+          props.itemId,
+          dto,
+          true,
+          () =>
+            navigation.navigateTo(
+              props.routes.pcrPrepareItem.getLink({
+                projectId: props.projectId,
+                pcrId: props.pcrId,
+                itemId: props.itemId,
+              }),
+              true,
+            ),
+        )
+      }
+      {...props}
+    />
+  );
+};
 
 export const FinancialVirementEditPartnerLevelRoute = defineRoute({
   routeName: "financial-virement-edit-partner-level",
