2a0962cec4846decf39dbc697807b15cdb30244a
Neil Little <neil.little@ukri.org>
2021-02-12 16:13:21 +0000

    ACC-7410: roundCurrency on originalRemainingGrant
    - Add coverage for framework/utils
    - Add negative unit test check
    - Move test file to correct directory
    - Add unit tests on helpers + updated to jsdoc comments

--- app/src/framework/util/numberHelper.ts
+++ app/src/framework/util/numberHelper.ts
@@ -1,18 +1,45 @@
 const EPISILON = Math.pow(2, -52);
-export const isNumber = (value: number | null | undefined): value is number => typeof value === "number" && !isNaN(value);
-// Adding epsilon to ensure things like 1.005 round correctly
-export const roundCurrency = (value: number) => Math.round((value + EPISILON) * 100) / 100;
 
-/*
-* Converts a string to a number where possible.
-* If the object passed in is empty, will return null.
-* If the object passed in cannot be parsed as a number, returns NaN
-* */
-export const parseNumber = (x: string | null | undefined) => {
-  if (x === null || x === undefined || x === "") return null;
-  return Number(x);
+export const isNumber = (value?: number | null): value is number => {
+  // Note: JS treats Zero as false 👀
+  if (value !== 0 && !value) return false;
+
+  const isValueNumber = typeof value === "number";
+  const isNotNaN = !isNaN(value);
+
+  return isValueNumber && isNotNaN;
 };
 
-export const sum = <T>(items: T[], value: (item: T) => number) => {
+/**
+ * @description Added an Epsilon cover edge cases such as 1.005 to round correctly
+ */
+export function roundCurrency(value: number) {
+  const valueToBeRounded = (value + EPISILON) * 100;
+  const roundedValue = Math.round(valueToBeRounded);
+
+  return roundedValue / 100;
+}
+
+/**
+ * @description Converts a string to a number where possible.
+ * - If the object passed in is empty, will return null.
+ * - If the object passed in cannot be parsed as a number, returns NaN
+ */
+export function parseNumber(x?: string | number | null) {
+  // Note: explicit check since !x captures zero
+  if (x === null || x === undefined) return null;
+
+  const emptyString = typeof x === "string" && !x.trim().length;
+
+  if (emptyString) return null;
+
+  const value = Number(x);
+
+  return isNumber(value) ? value : null;
+}
+
+export function sum<T>(items: T[], value: (item: T) => number): number {
+  if (!items.length) return 0;
+
   return items.reduce((total, item) => total + value(item), 0);
-};
+}
