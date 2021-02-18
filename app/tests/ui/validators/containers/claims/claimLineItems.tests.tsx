aeb79a5b9d096a814121869c53c8674b9b745358
Neil Little <neil.little@ukri.org>
2021-02-18 13:49:13 +0000

    ACC-7476: Fix supporting docs display state
    - Added unit tests for content

---
+++ app/tests/ui/validators/containers/claims/claimLineItems.tests.tsx
@@ -0,0 +1,41 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useClaimLineItemsContent } from "@ui/containers";
+import { hookTestBed, TestBedContent } from "@shared/TestBed";
+import { Content } from "@content/content";
+
+describe("claimLineItems", () => {
+  describe("useClaimLineItemsContent()", () => {
+    const stubContent = {
+      claimLineItems: {
+        supportingDocumentsTitle: { content: "stub-supportingDocumentsTitle" },
+        documentsInNewWindow: { content: "stub-documentsInNewWindow" },
+        noDocumentsUploaded: { content: "stub-noDocumentsUploaded" },
+        additionalInfoTitle: { content: "stub-additionalInfoTitle" },
+        totalCostTitle: { content: "stub-totalCostTitle" },
+        forecastCostTitle: { content: "stub-forecastCostTitle" },
+        differenceTitle: { content: "stub-differenceTitle" },
+        descriptionHeader: { content: "stub-descriptionHeader" },
+        costHeader: { content: "stub-costHeader" },
+        lastUpdatedHeader: { content: "stub-lastUpdatedHeader" },
+        noDataMessage: { content: "stub-noDataMessage" },
+      },
+    };
+
+    const setup = () => renderHook(useClaimLineItemsContent, hookTestBed({ content: stubContent as TestBedContent }));
+
+    describe("@returns", () => {
+      type ClaimLineItemsKeys = Exclude<keyof Content["claimLineItems"], "title">[];
+
+      const contentKeys = Object.keys(stubContent.claimLineItems) as ClaimLineItemsKeys;
+
+      test.each(contentKeys)("with %s", contentKey => {
+        const { current } = setup().result;
+
+        const contentTarget = current[contentKey];
+        const expectedValue = stubContent.claimLineItems[contentKey].content;
+
+        expect(contentTarget).toBe(expectedValue);
+      });
+    });
+  });
+});
