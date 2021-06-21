f8c5c1fdf417123548ff1e4c3f0718870e40168c
Neil Little <neil.little@ukri.org>
2021-06-21 14:50:44 +0100

    ACC-7843: Remove enzyme from NavigationArrows

--- app/src/ui/components/navigationArrows.tsx
+++ app/src/ui/components/navigationArrows.tsx
@@ -7,31 +7,39 @@ interface LinkProps {
   label: string;
   route: ILinkInfo;
 }
-interface Props {
+
+interface NavigationArrowsProps {
   previousLink?: LinkProps | null;
   nextLink?: LinkProps | null;
 }
 
-export class NavigationArrows extends React.Component<Props> {
-  render() {
-    return (
-      <div className="govuk-navigation-arrows">
-        {this.renderLink(this.props.previousLink, "Previous", "arrow-right")}
-        {this.renderLink(this.props.nextLink, "Next", "arrow-left")}
-      </div>
-    );
-  }
+export function NavigationArrows({ previousLink, nextLink }: NavigationArrowsProps) {
+  if (!previousLink && !nextLink) return null;
 
-  private renderLink(linkType: LinkProps | null | undefined, name: string, qa: string) {
-    if (!linkType) return null;
-    return (
-      <Link route={linkType.route} replace className="govuk-navigation-arrows__button">
-        <ArrowLeft className="govuk-navigation-arrows__button__arrow" />
-        <div data-qa={qa} className="govuk-navigation-arrows__button__label">
-          <span>{name}</span>
-          <span className="govuk-navigation-arrows__button__label__category">{linkType.label}</span>
-        </div>
-      </Link>
-    );
-  }
+  return (
+    <div className="govuk-navigation-arrows">
+      {previousLink && <NavigationArrow {...previousLink} name="Previous" qa="arrow-right" />}
+
+      {nextLink && <NavigationArrow {...nextLink} name="Next" qa="arrow-left" />}
+    </div>
+  );
+}
+
+type NavigationArrowProps = LinkProps & {
+  name: string;
+  qa?: string;
+};
+
+function NavigationArrow({ label, route, name, qa }: NavigationArrowProps) {
+  return (
+    <Link route={route} replace className="govuk-navigation-arrows__button">
+      <ArrowLeft className="govuk-navigation-arrows__button__arrow" />
+
+      <div data-qa={qa} className="govuk-navigation-arrows__button__label">
+        <span>{name}</span>
+
+        <span className="govuk-navigation-arrows__button__label__category">{label}</span>
+      </div>
+    </Link>
+  );
 }
