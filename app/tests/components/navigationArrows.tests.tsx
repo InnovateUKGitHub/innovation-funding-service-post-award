96c7db49822ff9bb65358689908f28252aa9a8f4
Neil Little <neil.little@ukri.org>
2021-06-21 14:50:44 +0100

    ACC-7843: Remove enzyme from NavigationArrows

--- app/tests/components/navigationArrows.tests.tsx
+++ app/tests/components/navigationArrows.tests.tsx
@@ -1,104 +1,88 @@
-import { mount } from "enzyme";
-
 import { createStore } from "redux";
+import { render } from "@testing-library/react";
+
 import { IClientUser, ProjectRole } from "@framework/types";
 import { routeConfig } from "@ui/routing";
-import TestBed from "@shared/TestBed";
 import { IStores } from "@ui/redux/storesProvider";
-import { rootReducer } from "../../src/ui/redux/reducers";
-import { NavigationArrows } from "../../src/ui/components";
+import { rootReducer } from "@ui/redux/reducers";
+import { NavigationArrows } from "@ui/components";
+import { TestBed } from "@shared/TestBed";
 
-const routes = routeConfig;
+describe("<NavigationArrows />", () => {
+  const routes = routeConfig;
 
-const store = createStore(rootReducer, {
-  user: {
-    email: "iuk.accproject@bjss.com.bjsspoc2",
-    roleInfo: {
-      a0C0Q000001tr5yUAA: {
-        projectRoles: ProjectRole.MonitoringOfficer,
-        partnerRoles: {},
+  const store = createStore(rootReducer, {
+    user: {
+      email: "iuk.accproject@bjss.com.bjsspoc2",
+      roleInfo: {
+        a0C0Q000001tr5yUAA: {
+          projectRoles: ProjectRole.MonitoringOfficer,
+          partnerRoles: {},
+        },
       },
-    },
-    csrf: "CSFR",
-  } as IClientUser,
-}) as IStores;
+      csrf: "CSFR",
+    } as IClientUser,
+  }) as IStores;
 
-const previousLink = {
-  label: "Overheads",
-  route: routes.reviewClaimLineItems.getLink({
-    partnerId: "a0B0Q000001e3HdUAI",
-    projectId: "a0C0Q000001tr5yUAA",
-    periodId: 2,
-    costCategoryId: "a060Q000000oAYZQA2",
-  }),
-};
+  const previousLink = {
+    label: "Overheads",
+    route: routes.reviewClaimLineItems.getLink({
+      partnerId: "a0B0Q000001e3HdUAI",
+      projectId: "a0C0Q000001tr5yUAA",
+      periodId: 2,
+      costCategoryId: "a060Q000000oAYZQA2",
+    }),
+  };
 
-const nextLink = {
-  label: "Labour",
-  route: routes.reviewClaimLineItems.getLink({
-    partnerId: "a0B0Q000001e3HdUAI",
-    projectId: "a0C0Q000001tr5yUAA",
-    periodId: 2,
-    costCategoryId: "a060Q000000oAYYQA2",
-  }),
-};
+  const nextLink = {
+    label: "Labour",
+    route: routes.reviewClaimLineItems.getLink({
+      partnerId: "a0B0Q000001e3HdUAI",
+      projectId: "a0C0Q000001tr5yUAA",
+      periodId: 2,
+      costCategoryId: "a060Q000000oAYYQA2",
+    }),
+  };
 
-describe("NavigationArrows", () => {
-  it("renders only the next arrow if no previous link is given", () => {
-    const wrapper = mount(
-      <TestBed stores={store}>
-        <NavigationArrows previousLink={null} nextLink={nextLink} />
-      </TestBed>
-    );
+  const setup = (element: React.ReactElement) => render(<TestBed stores={store}>{element}</TestBed>);
 
-    expect(wrapper.find("a").length).toEqual(1);
-    expect(wrapper.find("a").find("span").at(0).text()).toEqual("Next");
-    expect(wrapper.find("a").find("span").at(1).text()).toEqual(nextLink.label);
-  });
+  describe("@returns", () => {
+    test("with next link only", () => {
+      const { queryByText } = setup(<NavigationArrows nextLink={nextLink} />);
 
-  it("renders the previous and the next arrow", () => {
-    const wrapper = mount(
-      <TestBed stores={store}>
-        <NavigationArrows previousLink={previousLink} nextLink={nextLink} />
-      </TestBed>
-    );
+      expect(queryByText("Next")).toBeInTheDocument();
+      expect(queryByText(nextLink.label)).toBeInTheDocument();
 
-    expect(wrapper.find("a").length).toEqual(2);
+      expect(queryByText("Previous")).not.toBeInTheDocument();
+    });
 
-    expect(wrapper.find("a").at(0).find("span").at(0).text()).toEqual(
-      "Previous"
-    );
-    expect(wrapper.find("a").at(0).find("span").at(1).text()).toEqual(
-      previousLink.label
-    );
+    test("with previous link only", () => {
+      const { queryByText } = setup(<NavigationArrows previousLink={previousLink} />);
 
-    expect(wrapper.find("a").at(1).find("span").at(0).text()).toEqual("Next");
-    expect(wrapper.find("a").at(1).find("span").at(1).text()).toEqual(
-      nextLink.label
-    );
-  });
+      expect(queryByText("Previous")).toBeInTheDocument();
+      expect(queryByText(previousLink.label)).toBeInTheDocument();
 
-  it("renders only the previous arrow if no next link is given", () => {
-    const wrapper = mount(
-      <TestBed stores={store}>
-        <NavigationArrows previousLink={previousLink} nextLink={null} />
-      </TestBed>
-    );
+      expect(queryByText("Next")).not.toBeInTheDocument();
+    });
 
-    expect(wrapper.find("a").length).toEqual(1);
-    expect(wrapper.find("a").find("span").at(0).text()).toEqual("Previous");
-    expect(wrapper.find("a").find("span").at(1).text()).toEqual(
-      previousLink.label
-    );
-  });
+    test("with both previous and next link concurrently", () => {
+      const { container, queryByText } = setup(<NavigationArrows previousLink={previousLink} nextLink={nextLink} />);
 
-  it("renders no arrows if no links are given", () => {
-    const wrapper = mount(
-      <TestBed stores={store}>
-        <NavigationArrows previousLink={null} nextLink={null} />
-      </TestBed>
-    );
+      const targetButtons = container.querySelectorAll(".govuk-navigation-arrows__button");
 
-    expect(wrapper.find("a").length).toEqual(0);
+      expect(targetButtons).toHaveLength(2);
+
+      expect(queryByText("Previous")).toBeInTheDocument();
+      expect(queryByText(previousLink.label)).toBeInTheDocument();
+
+      expect(queryByText("Next")).toBeInTheDocument();
+      expect(queryByText(nextLink.label)).toBeInTheDocument();
+    });
+
+    test("with no prev or next link provided", () => {
+      const { container } = setup(<NavigationArrows />);
+
+      expect(container.firstChild).toBeNull();
+    });
   });
 });
