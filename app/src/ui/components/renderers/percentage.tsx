8ac0dfee3f3020f1a87c4916b7cd18c4dc0f2fdf
Neil Little <neil.little@ukri.org>
2021-07-01 13:15:15 +0100

    ACC-7877: Replace enzyme for RTL in Percentage

--- app/src/ui/components/renderers/percentage.tsx
+++ app/src/ui/components/renderers/percentage.tsx
@@ -1,28 +1,24 @@
 import { isNumber } from "@framework/util";
 
-interface Props {
+export interface Props {
   value: number | null;
   fractionDigits?: number;
 }
 
 export const Percentage: React.FunctionComponent<Props> = ({ value, fractionDigits = 2 }) => {
-  if (!isNumber(value)) {
-    return null;
-  }
+  if (!isNumber(value)) return null;
 
   const options = {
     style: "percent",
     minimumFractionDigits: fractionDigits,
-    maximumFractionDigits: fractionDigits
+    maximumFractionDigits: fractionDigits,
   };
 
-  // @TODO we need to find out if Salesforce will be returning 0.75 vs 75 and possibly remove this "/100"
-  let valToRender = new Intl.NumberFormat("en-GB", options).format(value/100);
+  // Note: If the input matches a negative zero, then remove the change of a minus later on
+  const absoluteNumber = Object.is(Math.sign(value), -0) ? Math.abs(value) : value;
 
-  // we don't want to display negative zero percent
-  if (valToRender === "-0.00%") {
-    valToRender = valToRender.substring(1, valToRender.length);
-  }
+  // TODO we need to find out if Salesforce will be returning 0.75 vs 75 and possibly remove this "/100"
+  const percentage = new Intl.NumberFormat("en-GB", options).format(absoluteNumber / 100);
 
-  return <span style={{ whiteSpace: "nowrap" }}>{valToRender}</span>;
+  return <span style={{ whiteSpace: "nowrap" }}>{percentage}</span>;
 };
