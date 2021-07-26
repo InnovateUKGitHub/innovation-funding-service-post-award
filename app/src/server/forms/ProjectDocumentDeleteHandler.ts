79887e4f9815e1384466e0f5935d91c1db374440
Neil Little <neil.little@ukri.org>
2021-07-26 17:50:23 +0100

    ACC-7952: Create ProjectDocumentDeleteHandler
    * Fix invalid HTML
    * Add form to standardFormHandlers

---
+++ app/src/server/forms/ProjectDocumentDeleteHandler.ts
@@ -0,0 +1,47 @@
+import { IContext, ILinkInfo } from "@framework/types";
+import { MultipleDocumentUploadDto } from "@framework/dtos";
+
+import { configuration } from "@server/features/common";
+import { IFormButton, StandardFormHandlerBase } from "@server/forms/formHandlerBase";
+import { DeleteProjectDocumentCommand } from "@server/features/documents/deleteProjectDocument";
+
+import { storeKeys } from "@ui/redux/stores/storeKeys";
+import { MultipleDocumentUpdloadDtoValidator } from "@ui/validators";
+
+import { ProjectDocumentPageParams, ProjectDocumentsRoute } from "@ui/containers";
+
+interface Document extends MultipleDocumentUploadDto {
+  id: string;
+}
+
+export class ProjectDocumentDeleteHandler extends StandardFormHandlerBase<
+  ProjectDocumentPageParams,
+  "multipleDocuments"
+> {
+  constructor() {
+    super(ProjectDocumentsRoute, ["delete"], "multipleDocuments");
+  }
+
+  protected getDto(context: IContext, params: ProjectDocumentPageParams, button: IFormButton) {
+    return Promise.resolve({ id: button.value, files: [] });
+  }
+
+  protected getStoreKey(params: ProjectDocumentPageParams) {
+    return storeKeys.getProjectKey(params.projectId);
+  }
+
+  protected async run(
+    context: IContext,
+    params: ProjectDocumentPageParams,
+    button: IFormButton,
+    dto: Document,
+  ): Promise<ILinkInfo> {
+    await context.runCommand(new DeleteProjectDocumentCommand(params.projectId, dto.id));
+
+    return ProjectDocumentsRoute.getLink(params);
+  }
+
+  protected createValidationResult(params: ProjectDocumentPageParams, dto: Document) {
+    return new MultipleDocumentUpdloadDtoValidator(dto, configuration.options, false, false, null);
+  }
+}
