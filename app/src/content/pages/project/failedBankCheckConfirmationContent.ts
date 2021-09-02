d0a209ab3869a4727156e6f39a11bf9c4ac98a83
Neil Little <neil.little@ukri.org>
2021-09-02 13:18:55 +0100

    ACC-8039: Fix content UI not being shown

--- app/src/content/pages/project/failedBankCheckConfirmationContent.ts
+++ app/src/content/pages/project/failedBankCheckConfirmationContent.ts
@@ -2,11 +2,20 @@ import { Content } from "@content/content";
 import { ContentPageBase } from "@content/contentPageBase";
 
 export class FailedBankCheckConfirmationContent extends ContentPageBase {
-  constructor(content: Content, competitionType?: string) {
-    super(content, "failed-bank-check-confirmation", competitionType);
+  constructor(content: Content) {
+    super(content, "failed-bank-check-confirmation");
   }
 
   public readonly backLink = this.getContent("back-link");
   public readonly returnButton = this.getContent("return-to-setup");
-  public readonly guidance = this.getContent("guidance", { markdown: true });
+
+  public readonly guidanceLine1 = this.getContent("guidance.line1");
+  public readonly guidanceLine2 = this.getContent("guidance.line2");
+  public readonly guidanceLine3 = this.getContent("guidance.line3");
+
+  public readonly guidanceListIntro = this.getContent("guidance.list.intro");
+
+  public readonly guidanceListItem1 = this.getContent("guidance.list.item1");
+  public readonly guidanceListItem2 = this.getContent("guidance.list.item2");
+  public readonly guidanceListItem3 = this.getContent("guidance.list.item3");
 }
--- app/src/ui/containers/projects/failedBankCheckConfirmation.tsx
+++ app/src/ui/containers/projects/failedBankCheckConfirmation.tsx
@@ -1,61 +1,83 @@
-import {StoresConsumer} from "@ui/redux";
-import {ProjectDto} from "@framework/dtos";
-import { BaseProps, ContainerBase, defineRoute } from "@ui/containers/containerBase";
+import { ProjectDto } from "@framework/dtos";
+import { Pending } from "@shared/pending";
 import { ProjectRole } from "@framework/constants";
-import * as ACC from "../../components";
+
+import { BaseProps, defineRoute } from "@ui/containers/containerBase";
+import { useStores } from "@ui/redux";
+import * as ACC from "@ui/components";
+import { useContent } from "@ui/hooks";
 
 export interface FailedBankCheckConfirmationParams {
   projectId: string;
   partnerId: string;
 }
 
-interface Data {
-  project: ProjectDto;
+interface FailedBankCheckConfirmationProps extends BaseProps, FailedBankCheckConfirmationParams {
+  project: Pending<ProjectDto>;
 }
 
-class FailedBankCheckConfirmationComponent extends ContainerBase<FailedBankCheckConfirmationParams, Data> {
-  render() {
-    const { projectId, partnerId, project } = this.props;
-    const projectSetupRoute = this.props.routes.projectSetup.getLink({ projectId, partnerId });
+function FailedBankCheckConfirmation({ projectId, partnerId, routes, ...props }: FailedBankCheckConfirmationProps) {
+  const { getContent } = useContent();
+
+  const renderContents = (project: ProjectDto) => {
+    const projectSetupRoute = routes.projectSetup.getLink({ projectId, partnerId });
+
+    const failedConfirmationBackLink = getContent(x => x.failedBankCheckConfirmation.backLink);
+    const backLink = <ACC.BackLink route={projectSetupRoute}>{failedConfirmationBackLink}</ACC.BackLink>;
+    const pageTitle = <ACC.Projects.Title {...project} />;
 
     return (
-      <ACC.Page
-        backLink={<ACC.BackLink route={projectSetupRoute}> <ACC.Content value={x => x.failedBankCheckConfirmation.backLink} /></ACC.BackLink>}
-        pageTitle={<ACC.Projects.Title {...project} />}
-        project={project}
-      >
+      <ACC.Page backLink={backLink} pageTitle={pageTitle} project={project}>
         <ACC.Section qa="guidance">
           <ACC.Renderers.SimpleString>
-            <ACC.Content value={x => x.failedBankCheckConfirmation.guidance} />
+            {getContent(x => x.failedBankCheckConfirmation.guidanceLine1)}
           </ACC.Renderers.SimpleString>
+
+          <ACC.Renderers.SimpleString>
+            {getContent(x => x.failedBankCheckConfirmation.guidanceLine2)}
+          </ACC.Renderers.SimpleString>
+
+          <ACC.Renderers.SimpleString>
+            {getContent(x => x.failedBankCheckConfirmation.guidanceLine3)}
+          </ACC.Renderers.SimpleString>
+
+          <ACC.Renderers.SimpleString>
+            {getContent(x => x.failedBankCheckConfirmation.guidanceListIntro)}
+          </ACC.Renderers.SimpleString>
+
+          <ACC.UL>
+            <li>{getContent(x => x.failedBankCheckConfirmation.guidanceListItem1)}</li>
+            <li>{getContent(x => x.failedBankCheckConfirmation.guidanceListItem2)}</li>
+            <li>{getContent(x => x.failedBankCheckConfirmation.guidanceListItem3)}</li>
+          </ACC.UL>
         </ACC.Section>
+
         <ACC.Section qa="return-to-setup-button">
-          <ACC.Link styling="PrimaryButton" route={projectSetupRoute}><ACC.Content value={x => x.failedBankCheckConfirmation.returnButton} /></ACC.Link>
+          <ACC.Link styling="PrimaryButton" route={projectSetupRoute}>
+            {getContent(x => x.failedBankCheckConfirmation.returnButton)}
+          </ACC.Link>
         </ACC.Section>
       </ACC.Page>
     );
-  }
+  };
+
+  return <ACC.Loader pending={props.project} render={x => renderContents(x)} />;
 }
 
-const FailedBankCheckConfirmationContainer = (props: FailedBankCheckConfirmationParams & BaseProps) => (
-  <StoresConsumer>
-    {
-      stores => {
-        return <ACC.Loader
-          pending={stores.projects.getById(props.projectId)}
-          render={x => <FailedBankCheckConfirmationComponent project={x} {...props}/>}
-        />;
-      }
-    }
-  </StoresConsumer>
-);
+function FailedBankCheckConfirmationContainer(props: FailedBankCheckConfirmationParams & BaseProps) {
+  const { projects } = useStores();
+
+  return <FailedBankCheckConfirmation {...props} project={projects.getById(props.projectId)} />;
+}
 
 export const FailedBankCheckConfirmationRoute = defineRoute<FailedBankCheckConfirmationParams>({
   routeName: "failedBankCheckConfirmation",
   routePath: "/projects/:projectId/setup/:partnerId/further-information-required",
-  getParams: (r) => ({ projectId: r.params.projectId, partnerId: r.params.partnerId }),
+  getParams: r => ({
+    projectId: r.params.projectId,
+    partnerId: r.params.partnerId,
+  }),
   container: FailedBankCheckConfirmationContainer,
-  accessControl: (auth, params) => auth.forProject(params.projectId)
-    .hasRole(ProjectRole.FinancialContact),
-  getTitle: ({ content }) => content.failedBankCheckConfirmation.title(),
+  accessControl: (auth, params) => auth.forProject(params.projectId).hasRole(ProjectRole.FinancialContact),
+  getTitle: x => x.content.failedBankCheckConfirmation.title(),
 });
