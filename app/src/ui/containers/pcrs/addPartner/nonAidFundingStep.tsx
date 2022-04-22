2a2d85023e5e50184a92941766d6f902936b790f
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2022-04-22 11:29:32 +0100

    ACC-8519: change add partner workflow for ktp

--- app/src/ui/containers/pcrs/addPartner/nonAidFundingStep.tsx
+++ app/src/ui/containers/pcrs/addPartner/nonAidFundingStep.tsx
@@ -3,24 +3,37 @@ import { PCRPartnerAdditionItemDtoValidator } from "@ui/validators";
 import { PcrStepProps } from "@ui/containers/pcrs/pcrWorkflow";
 import { PCRItemForPartnerAdditionDto } from "@framework/dtos";
 import { useContent } from "@ui/hooks";
+import { checkProjectCompetition } from "@ui/helpers/check-competition-type";
 
-export const NonAidFundingStep = (
-  props: PcrStepProps<PCRItemForPartnerAdditionDto, PCRPartnerAdditionItemDtoValidator>,
-) => {
+export const NonAidFundingStep = ({
+  project,
+  pcrItem,
+  onSave,
+}: PcrStepProps<PCRItemForPartnerAdditionDto, PCRPartnerAdditionItemDtoValidator>) => {
   const { getContent } = useContent();
 
   const Form = ACC.TypedForm<PCRItemForPartnerAdditionDto>();
+
+  const { isKTP } = checkProjectCompetition(project.competitionType);
+
   return (
     <>
       <ACC.Section qa="non-aid" title={x => x.pcrAddPartnerStateAidEligibilityContent.nonAidFundingTitle}>
-        <ACC.Content value={x => x.pcrAddPartnerStateAidEligibilityContent.nonAidFundingGuidance} />
+        {isKTP ? (
+          <ACC.ValidationMessage
+            messageType="info"
+            message={x => x.pcrAddPartnerStateAidEligibilityContent.nonAidFundingGuidance}
+          />
+        ) : (
+          <ACC.Content value={x => x.pcrAddPartnerStateAidEligibilityContent.nonAidFundingGuidance} />
+        )}
       </ACC.Section>
 
-      <Form.Form qa="saveAndContinue" data={props.pcrItem} onSubmit={() => props.onSave(false)}>
+      <Form.Form qa="saveAndContinue" data={pcrItem} onSubmit={() => onSave(false)}>
         <Form.Fieldset>
           <Form.Submit>{getContent(x => x.pcrAddPartnerStateAidEligibilityContent.pcrItem.submitButton)}</Form.Submit>
 
-          <Form.Button name="saveAndReturnToSummary" onClick={() => props.onSave(true)}>
+          <Form.Button name="saveAndReturnToSummary" onClick={() => onSave(true)}>
             {getContent(x => x.pcrAddPartnerStateAidEligibilityContent.pcrItem.returnToSummaryButton)}
           </Form.Button>
         </Form.Fieldset>
