81c8391a6a9f889c04956c0d99b8f057522da354
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2022-07-12 11:31:53 +0100

    ACC-8690: change generated id function to avoid server side clash

--- app/src/ui/validation/result.ts
+++ app/src/ui/validation/result.ts
@@ -1,44 +1,69 @@
+/** global window */
 import { Results } from "./results";
 
 interface ResultsInternal {
-    add(result: Result): void;
+  add(result: Result): void;
 }
 
-let valSeed = 0;
+/**
+ * implementation inspired by reach-ui/autoid
+ *
+ * defaults to Val:0 for server side, but once mounted will be given an incremental id
+ *
+ * @see https://github.com/reach/reach-ui/blob/dev/packages/auto-id/src/index.tsx
+ */
+
+let hasMounted = false;
+let keyVal = 0;
+
+function getKey() {
+  return `Val:${++keyVal}`;
+}
 
 // A single validation result, typically for a single field
 export class Result {
-    constructor(
-        results: Results<{}> | null,
-        public readonly showValidationErrors: boolean,
-        public readonly isValid: boolean,
-        public readonly errorMessage: string | null,
-        public readonly isRequired: boolean,
-    ) {
-        const internalResults = (results as any) as ResultsInternal;
+  private pKey = "Val:0";
 
-        if (internalResults) {
-            internalResults.add(this);
-        }
+  constructor(
+    results: Results<{}> | null,
+    public readonly showValidationErrors: boolean,
+    public readonly isValid: boolean,
+    public readonly errorMessage: string | null,
+    public readonly isRequired: boolean,
+  ) {
+    const internalResults = results as any as ResultsInternal;
 
-        this.key = "Val" + valSeed;
-        valSeed++;
+    if (internalResults) {
+      internalResults.add(this);
     }
 
-    public readonly key: string;
-
-    public combine(other: Result) {
-        return new Result(
-            null,
-            this.showValidationErrors || other.showValidationErrors,
-            this.isValid && other.isValid,
-            this.errorMessage || other.errorMessage || "",
-            this.isRequired || other.isRequired
-        );
+    // replace static id with incremental id after mounting
+    if (hasMounted) {
+      this.pKey = getKey();
+    } else {
+      global.window?.requestAnimationFrame(() => {
+        hasMounted = true;
+        this.pKey = getKey();
+      });
     }
+  }
 
-    public log() {
-        if (this.isValid) return null;
-        return this.errorMessage;
-    }
+  get key() {
+    return this.pKey;
+  }
+
+  public combine(other: Result) {
+    return new Result(
+      null,
+      this.showValidationErrors || other.showValidationErrors,
+      this.isValid && other.isValid,
+      this.errorMessage || other.errorMessage || "",
+      this.isRequired || other.isRequired,
+    );
+  }
+
+  public log() {
+    if (this.isValid) return null;
+    return this.errorMessage;
+  }
 }
