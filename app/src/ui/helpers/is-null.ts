7dac60bff628221623aa96a702d62c0a7c42871d
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2022-09-06 12:28:10 +0100

    ACC-8806: fix change loan duration pcr

---
+++ app/src/ui/helpers/is-null.ts
@@ -0,0 +1,23 @@
+/**
+ * Is null check and type guard.
+ *
+ * Typechecking whether val `is null` helps the typechecker type-narrow whenever
+ * the input is not null, by removing the possibility that val is null.
+ *
+ * @example
+ * let x: string | null
+ *
+ * console.log(x.length) // type error, x is possibly null
+ *
+ * if(isNull(x)) {
+ *  // do something
+ * } else {
+ *   console.log(x.length) // type narrowed to string by this point
+ * }
+ */
+
+function isNull(val: unknown): val is null {
+  return val === null;
+}
+
+export default isNull;
