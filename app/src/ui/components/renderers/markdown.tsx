554849cb20f825e0fef2cd9c10673e8249b8e987
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2022-09-08 15:27:32 +0100

    ACC-8813: update marked package and types

--- app/src/ui/components/renderers/markdown.tsx
+++ app/src/ui/components/renderers/markdown.tsx
@@ -1,14 +1,15 @@
-import Marked from "marked";
+import { marked } from "marked";
 
 export interface IMarkdownProps {
   value: string;
   style?: React.CSSProperties;
 }
 
+/**
+ * Renders a component that renders a string using markdown syntax
+ */
 export function Markdown({ value, ...props }: IMarkdownProps) {
   if (!value.length) return null;
 
-  const dangerousHTML = Marked(value);
-
-  return <span {...props} className="govuk-body markdown" dangerouslySetInnerHTML={{ __html: dangerousHTML }} />;
+  return <span {...props} className="govuk-body markdown" dangerouslySetInnerHTML={{ __html: marked.parse(value) }} />;
 }
