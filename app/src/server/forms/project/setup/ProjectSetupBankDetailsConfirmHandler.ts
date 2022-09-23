36c1dbf64181332b6c02bc9ea95f82a7a44fbe1c
Leondro Lio <leondro.lio@iuk.ukri.org>
2022-09-23 12:43:35 +0100

    ACC-8832: Forward users onto correct page after bank detail verification with JavaScript disabled

--- app/src/server/forms/project/setup/ProjectSetupBankDetailsConfirmHandler.ts
+++ app/src/server/forms/project/setup/ProjectSetupBankDetailsConfirmHandler.ts
@@ -1,10 +1,13 @@
-import { IContext, ILinkInfo, PartnerDto } from "@framework/types";
+import { BankCheckStatus, IContext, ILinkInfo, PartnerDto } from "@framework/types";
 import { GetByIdQuery } from "@server/features/partners";
+import { BankCheckStatusMapper } from "@server/features/partners/mapToPartnerDto";
 import { UpdatePartnerCommand } from "@server/features/partners/updatePartnerCommand";
 import {
+  FailedBankCheckConfirmationRoute,
   PartnerDetailsParams,
   ProjectSetupBankDetailsParams,
   ProjectSetupBankDetailsVerifyRoute,
+  ProjectSetupRoute,
 } from "@ui/containers";
 import { storeKeys } from "@ui/redux/stores/storeKeys";
 import { PartnerDtoValidator } from "@ui/validators/partnerValidator";
@@ -34,8 +37,26 @@ export class ProjectSetupBankDetailsConfirmHandler extends StandardFormHandlerBa
     button: IFormButton,
     dto: PartnerDto,
   ): Promise<ILinkInfo> {
+    // Obtain a mapper, which converts Salesforce status names to our enums.
+    const bankCheckStatusMapper = new BankCheckStatusMapper();
+
+    // Run an Experian verification on the DTO
     await context.runCommand(new UpdatePartnerCommand(dto, false, true));
-    return ProjectSetupBankDetailsVerifyRoute.getLink(params);
+
+    // Re-obtain the new results
+    const partnerDto = await context.repositories.partners.getById(dto.id);
+    const bankStatus = bankCheckStatusMapper.mapFromSalesforce(partnerDto.bankCheckStatus);
+
+    if (bankStatus === BankCheckStatus.VerificationPassed) {
+      // We've passed - Go to the ProjectSetupRoute.
+      return ProjectSetupRoute.getLink(params);
+    } else if (bankStatus === BankCheckStatus.VerificationFailed) {
+      // We've failed - Go to fallback screen.
+      return FailedBankCheckConfirmationRoute.getLink(params);
+    } else {
+      // Unknown state! Go back to the first page entirely.
+      return ProjectSetupRoute.getLink(params);
+    }
   }
 
   protected getStoreKey(params: ProjectSetupBankDetailsParams) {
