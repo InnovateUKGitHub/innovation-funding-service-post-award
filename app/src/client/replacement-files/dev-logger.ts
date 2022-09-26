5d0a3884128a961beefe54a52286ed89c7a84dce
Leondro Lio <leondro.lio@iuk.ukri.org>
2022-09-26 16:27:20 +0100

    ACC-8853: Unify logging between client/server

--- app/src/client/replacement-files/dev-logger.ts
+++
@@ -1,9 +0,0 @@
-import { Logger } from "@shared/developmentLogger";
-
-export function devLogger(warning: string): void {
-  const isProdUrl = !/localhost|acc-dev/.test(window.location.hostname);
-
-  if (isProdUrl) return;
-
-  return new Logger("Develop Warning").warn(warning);
-}
--- app/src/ui/helpers/dev-logger.ts
+++
@@ -1,9 +0,0 @@
-import { Logger } from "@shared/developmentLogger";
-
-export function devLogger(warning: string): void {
-  const isProdUrl: boolean = process.env.SERVER_URL?.includes("localhost") ?? true;
-
-  if (isProdUrl) return;
-
-  return new Logger("Develop Warning").warn(warning);
-}
