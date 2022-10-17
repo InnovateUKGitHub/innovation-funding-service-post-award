df46909e0e7af844ad54ab117c9ba0efb1948c09
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2022-10-17 14:39:54 +0100

    ACC-8924: fix stability of text input

--- app/src/ui/components/inputs/textInput.tsx
+++ app/src/ui/components/inputs/textInput.tsx
@@ -15,8 +15,6 @@ export const TextInput = (props: TextInputProps) => {
   const [state, setState] = useState({ value: props.value ?? "" });
   const debouncedOnChange = useDebounce(props.onChange, props.debounce);
 
-  useUpdateStateValueOnProps(props.value ?? "", setState);
-
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement> | React.KeyboardEvent<HTMLInputElement>,
     debounce: boolean,
