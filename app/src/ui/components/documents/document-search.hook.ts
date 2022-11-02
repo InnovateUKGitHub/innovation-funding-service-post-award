65ad246885d9caa698b973fdfd4401252c506502
Leondro Lio <leondro.lio@iuk.ukri.org>
2022-11-02 11:36:52 +0000

    ACC-8923: Increase specificity of document search

--- app/src/ui/components/documents/document-search.hook.ts
+++ app/src/ui/components/documents/document-search.hook.ts
@@ -6,14 +6,21 @@ import { useStores } from "@ui/redux";
 import { formatDate, getFileSize } from "@framework/util";
 import { DateFormat } from "@framework/constants/enums";
 
-import { DocumentsBase } from "./documents.interface";
 import { DocumentSummaryDto } from "@framework/dtos";
 
+/**
+ * Perform a fuzzy search on a list of items.
+ *
+ * @param value The user's input search value
+ * @param items The list of items to search through
+ * @param keysToSearch Keys in `item` that can be searched through
+ * @returns A list of all items that match the user's input value.
+ */
 const fuzzySearch = <T>(value: string, items: T[], keysToSearch: string[]) => {
   const valueToSearch = value.trim();
 
   // TODO: reconfirm this value after migrating to Node16
-  const scoreThreshold = 0.3;
+  const scoreThreshold = 0.19;
 
   const fusedQuery = new Fuse(items, {
     threshold: scoreThreshold,
