acb53edb061766f4ecb0d68f7dd8bc41d11bac03
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2022-12-14 16:30:39 +0000

    ACC-9111: (cy) move typings and reconfigure baseurl

--- cypress-test/cypress.config.ts
+++ cypress-test/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:8080",
+    // baseUrl: "https://www-acc-dev.apps.ocp4.innovateuk.ukri.org",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
