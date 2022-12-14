36e454b346d8341b3714efb9cc69dc764b1bb26c
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2022-12-14 16:28:07 +0000

    ACC-9111: use string helper function to force cost categories to only capitalise first word

--- app/src/shared/string-helpers.ts
+++ app/src/shared/string-helpers.ts
@@ -1,3 +1,6 @@
 export const removeSpaces = (value: string, replacementValue = "") => value.trim().replace(/\s/g, replacementValue);
 
 export const removeUndefinedString = (value: string): string => value.replace(/undefined/g, "");
+
+export const capitalizeFirstWord = (str: string) =>
+  str.toLowerCase().replace(/^(\w)(.*)$/, (m, p1, p2) => `${p1.toUpperCase()}${p2}`);
