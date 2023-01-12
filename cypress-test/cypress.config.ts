877dc234de711f9e627284c8deabe747bba76b9c
Allan Haines <allan.haines@iuk.ukri.org>
2023-01-12 13:35:35 +0000

    ACC-9164:(cy) Added command to run localhost and allow for branch numbers

--- cypress-test/cypress.config.ts
+++ cypress-test/cypress.config.ts
@@ -1,10 +1,31 @@
 require("dotenv").config();
 import { defineConfig } from "cypress";
 
+/**
+ * To target specific deployment branches with ACC number
+ *
+ * Windows powershell
+ * ------------------
+ * `$env:ACC=****; npm start`
+ *
+ * Windows command prompt
+ * ----------------------
+ * `set ACC=**** && npm start`
+ *
+ * MAC/Linux
+ * ---------
+ * `ACC=**** npm start`
+ *
+ * To run against local host:
+ * `npm run local`
+ */
+
+const accNumber = (process.env.ACC || "").trim();
+let accDevUrl = `https://www-acc-dev${accNumber}.apps.ocp4.innovateuk.ukri.org"`;
+
 export default defineConfig({
   e2e: {
-    // baseUrl: process.env.TEST_URL || "https://www-acc-dev.apps.ocp4.innovateuk.ukri.org",
-    baseUrl: process.env.TEST_URL || "http://localhost:8080",
+    baseUrl: process.env.TEST_URL || accDevUrl,
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
