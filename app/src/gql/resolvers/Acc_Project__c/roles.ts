fdb70309c842647486c490b32341ae63c1048733
Leondro Lio <leondro.lio@iuk.ukri.org>
2023-01-26 16:12:33 +0000

    ACC-9150: Increase null handling resilliancy in Acc_Project__c resolver

--- app/src/gql/resolvers/Acc_Project__c/roles.ts
+++ app/src/gql/resolvers/Acc_Project__c/roles.ts
@@ -27,11 +27,11 @@ interface ProjectData {
         node: {
           Acc_ProjectRole__c: {
             value: string;
-          };
+          } | null;
           Acc_AccountId__c: {
             value: string;
-          };
-        };
+          } | null;
+        } | null;
       }[];
     };
     Project_Contact_Links__r: {
@@ -39,16 +39,16 @@ interface ProjectData {
         node: {
           Acc_Role__c: {
             value: string;
-          };
+          } | null;
           Acc_ContactId__r: {
             Email__c: {
               value: string;
-            };
-          };
+            } | null;
+          } | null;
           Acc_AccountId__c: {
             value: string;
-          };
-        };
+          } | null;
+        } | null;
       }[];
     };
   };
@@ -135,31 +135,33 @@ const rolesResolver = (salesforceSubschema: ExecutableSchema): IFieldResolverOpt
       };
 
       for (const { node: projectContactLink } of project.Project_Contact_Links__r.edges) {
-        if (ctx.email === projectContactLink.Acc_ContactId__r.Email__c.value) {
-          if (projectContactLink.Acc_Role__c.value === "Monitoring officer") permissions.isMo = true;
-          if (projectContactLink.Acc_Role__c.value === "Finance contact") permissions.isFc = true;
-          if (projectContactLink.Acc_Role__c.value === "Project Manager") permissions.isPm = true;
+        if (ctx.email === projectContactLink?.Acc_ContactId__r?.Email__c?.value) {
+          if (projectContactLink?.Acc_Role__c?.value === "Monitoring officer") permissions.isMo = true;
+          if (projectContactLink?.Acc_Role__c?.value === "Finance contact") permissions.isFc = true;
+          if (projectContactLink?.Acc_Role__c?.value === "Project Manager") permissions.isPm = true;
         }
 
         for (const { node: projectParticipant } of project.Acc_ProjectParticipantsProject__r.edges) {
-          const existingPartnerPermissions = permissions.partnerRoles.find(
-            x => x.partnerId === projectParticipant.Acc_AccountId__c.value,
-          );
-          const partnerPermissions = existingPartnerPermissions ?? {
-            isMo: isSalesforceSystemUser,
-            isFc: isSalesforceSystemUser,
-            isPm: isSalesforceSystemUser,
-            isSalesforceSystemUser: ctx.email === configuration.salesforceServiceUser.serviceUsername,
-            partnerId: projectParticipant.Acc_AccountId__c.value,
-          };
+          if (projectParticipant && projectParticipant.Acc_AccountId__c?.value) {
+            const existingPartnerPermissions = permissions.partnerRoles.find(
+              x => x.partnerId === projectParticipant?.Acc_AccountId__c?.value,
+            );
+            const partnerPermissions = existingPartnerPermissions ?? {
+              isMo: isSalesforceSystemUser,
+              isFc: isSalesforceSystemUser,
+              isPm: isSalesforceSystemUser,
+              isSalesforceSystemUser: ctx.email === configuration.salesforceServiceUser.serviceUsername,
+              partnerId: projectParticipant.Acc_AccountId__c.value,
+            };
 
-          if (projectContactLink.Acc_AccountId__c.value === projectParticipant.Acc_AccountId__c.value) {
-            if (permissions.isFc) partnerPermissions.isFc = true;
-            if (permissions.isPm && projectParticipant.Acc_ProjectRole__c.value === "Lead")
-              partnerPermissions.isPm = true;
+            if (projectContactLink?.Acc_AccountId__c?.value === projectParticipant?.Acc_AccountId__c?.value) {
+              if (permissions.isFc) partnerPermissions.isFc = true;
+              if (permissions.isPm && projectParticipant?.Acc_ProjectRole__c?.value === "Lead")
+                partnerPermissions.isPm = true;
+            }
+
+            if (!existingPartnerPermissions) permissions.partnerRoles.push(partnerPermissions);
           }
-
-          if (!existingPartnerPermissions) permissions.partnerRoles.push(partnerPermissions);
         }
       }
 
