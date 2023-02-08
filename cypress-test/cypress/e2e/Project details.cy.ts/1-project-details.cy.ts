075280b176100c77cfa51228d1a629962eae42a8
Allan Haines <allan.haines@iuk.ukri.org>
2023-02-08 16:32:57 +0000

    ACC-9221: (cy) Project details page complete along with steps

--- cypress-test/cypress/e2e/Project details.cy.ts/1-project-details.cy.ts
+++ cypress-test/cypress/e2e/Project details.cy.ts/1-project-details.cy.ts
@@ -1,8 +1,17 @@
 import { visitApp } from "../../common/visit";
-import { shouldShowProjectTitle } from "./steps";
-
-var currentYear = new Date();
-var thisYear = currentYear.getFullYear();
+import {
+  detailsGuidanceMessage,
+  financeDetailsSection,
+  ilDetailsSection,
+  ipmDetailsSection,
+  moDetailsSection,
+  otherDetailsSection,
+  partnerDetailsSection,
+  pmDetailsSection,
+  projectDetailsSection,
+  projectPeriodSubHeading,
+  shouldShowProjectTitle,
+} from "./steps";
 
 describe("Projects details", () => {
   before(() => {
@@ -19,10 +28,7 @@ describe("Projects details", () => {
 
   it("Should have the project title", shouldShowProjectTitle);
 
-  it("Should show the 'Project period' subheading and date", () => {
-    cy.get("h2").contains("Project period");
-    cy.get("span").contains(thisYear);
-  });
+  it("Should show the 'Project period' subheading and date", projectPeriodSubHeading);
 
   it("Should have a back link", () => {
     cy.backLink("Back to project");
@@ -32,111 +38,21 @@ describe("Projects details", () => {
     cy.get("h2").contains("Project members");
   });
 
-  it("Should have Monitoring Officer section", () => {
-    cy.getByQA("partner-information-monitoringOfficers");
-    cy.get("h3").contains("Monitoring Officer");
-    cy.get("tr").contains("Javier Baez").next().contains("testman2@testing.comnoemail");
-  });
+  it("Should have Monitoring Officer section", moDetailsSection);
 
-  it("Should have Project Manager section", () => {
-    cy.getByQA("partner-information-projectManagers");
-    cy.get("h3").contains("Project Manager");
-    cy.get("tr")
-      .contains("James Black")
-      .next()
-      .contains("EUI Small Ent Health (Lead)")
-      .next()
-      .contains("james.black@euimeabs.testnoemail");
-  });
+  it("Should have Project Manager section", pmDetailsSection);
 
-  it("Should have Finance contacts section", () => {
-    cy.getByQA("partner-information-financeContacts");
-    cy.get("h3").contains("Finance contacts");
-    cy.get("tr")
-      .contains("ken Charles")
-      .next()
-      .contains("A B Cad Services")
-      .next()
-      .contains("contact77@test.co.uknoemail");
-    cy.get("tr")
-      .contains("Sarah Shuang")
-      .next()
-      .contains("ABS EUI Medium Enterprise")
-      .next()
-      .contains("s.shuang@irc.trde.org.uk.testnoemail");
-  });
+  it("Should have Finance contacts section", financeDetailsSection);
 
-  it("Should have Other contacts section", () => {
-    cy.getByQA("partner-information-otherContacts");
-    cy.get("h3").contains("Other contacts");
-  });
+  it("Should have an Innovation Lead section", ilDetailsSection);
 
-  it("Should have an Innovation Lead section", () => {
-    cy.getByQA("partner-information-innovationLeads");
-  });
+  it("Should have an IPM section", ipmDetailsSection);
 
-  it("Should have an IPM section", () => {
-    cy.getByQA("partner-information-ipms");
-  });
+  it("Should have Other contacts section", otherDetailsSection);
 
-  it("Should have correct table headers", () => {
-    cy.tableHeader("Name");
-    cy.tableHeader("Email");
-    cy.tableHeader("Partner");
-    cy.tableHeader("Funding status");
-    cy.tableHeader("Location");
-  });
+  it("Should display a Partner information section", partnerDetailsSection);
 
-  it("Should display a Partner information section", () => {
-    cy.get("tr")
-      .contains("a", "EUI Small Ent Health")
-      .next()
-      .contains("Business")
-      .next()
-      .contains("Active")
-      .next()
-      .contains("Funded");
-    cy.get("tr")
-      .contains("a", "ABS EUI Medium Enterprise")
-      .next()
-      .contains("Business")
-      .next()
-      .contains("Active")
-      .next()
-      .contains("Funded");
-    cy.get("tr")
-      .contains("a", "A B Cad Services")
-      .next()
-      .contains("Business")
-      .next()
-      .contains("Active")
-      .next()
-      .contains("Funded");
-  });
+  it("Should present correct guidance messaging", detailsGuidanceMessage);
 
-  it("Should display the lead partner with a suffix of '(Lead)'", () => {
-    cy.get("tr").contains("(Lead)");
-  });
-
-  it("Should present correct guidance messaging", () => {
-    cy.get("p").contains("Only project managers can raise project change requests.");
-    cy.get("p").contains("Only finance contacts can submit claims.");
-    cy.get("p").contains(
-      "If you need to change the lead project manager or finance contact, please email grants_service@iuk.ukri.org.",
-    );
-  });
-
-  it("Should have 'Project information' heading", () => {
-    cy.get("h2").contains("Project information");
-  });
-
-  it("Should have correct fields in Project information section", () => {
-    cy.getByQA("competition-name");
-    cy.getByQA("competition-type");
-    cy.getByQA("start-date");
-    cy.getByQA("end-date");
-    cy.getByQA("duration");
-    cy.getByQA("periods");
-    cy.getByQA("scope");
-  });
+  it("Should have 'Project information' heading and correct fields", projectDetailsSection);
 });
--- cypress-test/cypress/e2e/Project details.cy.ts/steps.ts
+++ cypress-test/cypress/e2e/Project details.cy.ts/steps.ts
@@ -1,3 +1,110 @@
+var currentYear = new Date();
+var thisYear = currentYear.getFullYear();
+
 export const shouldShowProjectTitle = () => {
   cy.getByQA("page-title-caption").should("contain.text", "CYPRESS");
+};
+
+export const projectPeriodSubHeading = () => {
+  cy.get("h2").contains("Project period");
+  cy.get("span").contains(thisYear);
+};
+
+export const moDetailsSection = () => {
+  cy.get("h3").contains("Monitoring Officer");
+  ["Javier Baez", "testman2@testing.comnoemail"].forEach(moInfo => {
+    cy.getByQA("partner-information-monitoring-officer").contains(moInfo);
+  });
+};
+
+export const pmDetailsSection = () => {
+  ["James Black", "EUI Small Ent Health (Lead)", "james.black@euimeabs.testnoemail"].forEach(pmInfo => {
+    cy.getByQA("partner-information-project-manager").contains(pmInfo);
+  });
+  cy.get("h3").contains("Project Manager");
+  cy.get("p").contains("Only project managers can raise project change requests.");
+};
+
+export const financeDetailsSection = () => {
+  [
+    "ken Charles",
+    "A B Cad Services",
+    "contact77@test.co.uknoemail",
+    "Sarah Shuang",
+    "ABS EUI Medium Enterprise",
+    "s.shuang@irc.trde.org.uk.testnoemail",
+  ].forEach(fcInfo => {
+    cy.getByQA("partner-information-finance-contacts").contains(fcInfo);
+  });
+  cy.get("h3").contains("Finance contacts");
+  cy.get("p").contains("Only finance contacts can submit claims.");
+};
+
+export const ilDetailsSection = () => {
+  ["Name", "Email", "Nicole Hedges", "nicole.hedges@iuk.ukri.org"].forEach(ilInfo => {
+    cy.getByQA("partner-information-innovation-lead").contains(ilInfo);
+  });
+  cy.get("h3").contains("Innovation lead");
+};
+
+export const ipmDetailsSection = () => {
+  ["Name", "Email", "Wednesday Addams", "wed.addams@test.test.co.uk"].forEach(ipmInfo => {
+    cy.getByQA("partner-information-ipm").contains(ipmInfo);
+  });
+  cy.get("h3").contains("IPM");
+};
+
+export const otherDetailsSection = () => {
+  ["Name", "Role", "Relationship Manager", "Email", "allan.haines@iuk.ukri.org"].forEach(otherInfo => {
+    cy.getByQA("partner-information-other-contacts").contains(otherInfo);
+  });
+  cy.get("h3").contains("Other contacts");
+};
+
+export const partnerDetailsSection = () => {
+  [
+    "Partner",
+    "Name",
+    "Email",
+    "Funding status",
+    "Location",
+    "EUI Small Ent Health",
+    "Business",
+    "Active",
+    "Funded",
+    "ABS EUI Medium Enterprise",
+    "A B Cad Services",
+  ].forEach(partnerInfo => {
+    cy.getByQA("partner-information").contains(partnerInfo);
+  });
+  cy.get("h2").contains("Partner information");
+};
+
+export const detailsGuidanceMessage = () => {
+  cy.get("p").contains("Only project managers can raise project change requests.");
+  cy.get("p").contains("Only finance contacts can submit claims.");
+  cy.get("p").contains(
+    "If you need to change the lead project manager or finance contact, please email grants_service@iuk.ukri.org.",
+  );
+};
+
+/**
+ * Start and end date are prone to change to keep a project in the correct state for testing.
+ * Therefore these are not included in the test.
+ */
+export const projectDetailsSection = () => {
+  [
+    "Competition name",
+    "Competition type",
+    "Project start date",
+    "Project end date",
+    "Duration",
+    "Number of periods",
+    "Project scope statement",
+    "a002600000C6rp9",
+    "CR&D",
+    "12 months",
+  ].forEach(projInfo => {
+    cy.getByQA("project-details").contains(projInfo);
+  });
 };
