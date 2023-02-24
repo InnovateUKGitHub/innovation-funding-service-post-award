b7d2e55e24812aa503662810e2f9ac06bd62510b
Leondro Lio <leondro.lio@iuk.ukri.org>
2023-02-24 15:36:40 +0000

    ACC-9284: Add default Logger identifier

--- app/src/shared/developmentLogger.ts
+++ app/src/shared/developmentLogger.ts
@@ -304,7 +304,10 @@ export class Logger implements ILogger {
 
     // Print the log type name, followed by the identifier, followed by the message.
     output += colouring.byNum(logLevelName, fg);
-    output += colouring.byNum(this.identifier.padEnd(Logger.LOG_IDENTIFIER_PADDING), ForegroundColourCode.CYAN);
+    output += colouring.byNum(
+      (this.identifier ?? "Logger").padEnd(Logger.LOG_IDENTIFIER_PADDING),
+      ForegroundColourCode.CYAN,
+    );
     output += " ";
     output += message;
 
