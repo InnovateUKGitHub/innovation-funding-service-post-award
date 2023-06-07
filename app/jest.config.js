52d332d3a85dd7c976ce98812f67581dc7003cc9
Leondro Lio <leondro.lio@iuk.ukri.org>
2023-06-07 10:18:43 +0100

    ACC-9645: (storybooks) Mock jest MDX with empty object

--- app/jest.config.js
+++ app/jest.config.js
@@ -42,5 +42,6 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/tests/__mocks__/fileMock.js",
     "\\.(css|less|apex)$": "<rootDir>/tests/__mocks__/styleMock.js",
+    "\\.mdx$": "<rootDir>/tests/__mocks__/mdxMock.js",
   },
 };
