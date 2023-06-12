a0dadbda28d72abcbcdcc24fd720249b4fd5ff01
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2023-06-12 14:41:45 +0100

    ACC-9654: (setup bank details) show required error if empty sortcode/accountnumber

--- app/src/ui/containers/projects/setup/projectSetupBankDetails.zod.ts
+++ app/src/ui/containers/projects/setup/projectSetupBankDetails.zod.ts
@@ -3,13 +3,15 @@ import { makeZodI18nMap } from "@shared/zodi18n";
 
 export const projectSetupBankDetailsErrorMap = makeZodI18nMap({ keyPrefix: ["projectSetupBankDetails"] });
 
-export const projectSetupBankDetailsSchema = z.object({
-  // companyNumber: z.string().min(1).max(10),
-  sortCode: z.string().regex(/\d{6}|\d\d-\d\d-\d\d/),
-  accountNumber: z.string().min(6).max(8).regex(/^\d+$/),
-  // accountBuilding: z.string().min(1).max(10),
-  // accountStreet: z.string().min(1).max(10),
-  // accountLocality: z.string().min(1).max(10),
-  // accountTownOrCity: z.string().min(1).max(10),
-  // accountPostcode: z.string().min(1).max(10),
-});
+export const projectSetupBankDetailsSchema = z
+  .object({
+    sortCode: z
+      .string()
+      .min(1)
+      .regex(/\d{6}|\d\d-\d\d-\d\d/),
+    accountNumber: z
+      .string()
+      .min(1)
+      .regex(/^\d{6,8}$/),
+  })
+  .required();
