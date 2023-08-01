7eaa86d37c057a34912175788e0e40bb7f3b9849
Joe Grundman <jonathan.grundman@iuk.ukri.org>
2023-08-01 10:24:55 +0100

    ACC-9661: (scroll) fix scrolling to id

--- app/src/framework/util/windowHelpers.ts
+++ app/src/framework/util/windowHelpers.ts
@@ -1,3 +1,7 @@
+import { Logger } from "@shared/developmentLogger";
+
+const logger = new Logger("Window Helpers");
+
 export const scrollToTheTopSmoothly = () =>
   requestAnimationFrame(() => window.scrollTo({ top: 0, behavior: "smooth" }));
 export const scrollToTheTopInstantly = () => requestAnimationFrame(() => window.scrollTo(0, 0));
@@ -5,11 +9,13 @@ export const scrollToTheTopInstantly = () => requestAnimationFrame(() => window.
 export const scrollToTheTagSmoothly = (tag: string) => {
   requestAnimationFrame(() => {
     // Find the first instance of the NAME or ID
-    const [nameTag] = [...document.getElementsByName(tag)];
+    const element = [...document.getElementsByName(tag)]?.[0] ?? document.getElementById(tag);
 
     // If it exists, scroll to it.
-    if (nameTag) {
-      nameTag.scrollIntoView({ behavior: "smooth" });
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    } else {
+      logger.error("Failed to find element to scroll to", tag, element);
     }
   });
 };
