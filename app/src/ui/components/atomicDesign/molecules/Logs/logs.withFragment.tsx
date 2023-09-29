98123cc57de77c95d56f589a2c276bf908b8ac62
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2023-09-29 10:05:39 +0100

    ACC-10039: (gql) add fragment for logs and claim period date

---
+++ app/src/ui/components/atomicDesign/molecules/Logs/logs.withFragment.tsx
@@ -0,0 +1,30 @@
+import { useFragment } from "relay-hooks";
+import { Logs as LogsComponent } from "./logs";
+import { statusChangesLogsFragment } from "./StatusChangesLogs.fragment";
+import { useFragmentContext } from "@gql/utils/fragmentContextHook";
+import { StatusChangesLogsFragment$key } from "./__generated__/StatusChangesLogsFragment.graphql";
+import { isValidFragmentKey } from "@gql/utils/isValidFragmentKey";
+import { mapToClaimStatusChangeDtoArray } from "@gql/dtoMapper/mapClaimStatusChange";
+import { mapToProjectDto } from "@gql/dtoMapper/mapProjectDto";
+import { getFirstEdge } from "@gql/selectors/edges";
+
+export const Logs = (props: { qa: string }) => {
+  const fragmentRef = useFragmentContext();
+
+  if (!isValidFragmentKey<StatusChangesLogsFragment$key>(fragmentRef, "StatusChangesLogsFragment")) {
+    throw new Error("Logs component is missing a StatusChangesLogsFragment reference");
+  }
+
+  const fragment = useFragment(statusChangesLogsFragment, fragmentRef);
+  const { node: projectNode } = getFirstEdge(fragment?.query?.StatusChanges_Project?.edges);
+
+  const project = mapToProjectDto(projectNode, ["competitionType", "roles"]);
+
+  const statusChanges = mapToClaimStatusChangeDtoArray(
+    fragment.query?.StatusChanges_StatusChanges?.edges ?? [],
+    ["comments", "createdBy", "createdDate", "newStatusLabel"],
+    { roles: project.roles, competitionType: project.competitionType },
+  );
+
+  return <LogsComponent data={statusChanges} {...props} />;
+};
---
+++ app/src/ui/components/atomicDesign/organisms/claims/ClaimPeriodDate/ClaimPeriodDate.fragment.ts
@@ -0,0 +1,61 @@
+import { graphql } from "react-relay";
+
+export const claimPeriodDateFragment = graphql`
+  fragment ClaimPeriodDateFragment on UIAPI {
+    query {
+      ClaimPeriodDate_Claims: Acc_Claims__c(
+        where: {
+          and: [
+            { Acc_ProjectID__c: { eq: $projectIdStr } }
+            { Acc_ProjectParticipant__c: { eq: $partnerId } }
+            { Acc_ProjectPeriodNumber__c: { eq: $periodId } }
+            { RecordType: { Name: { eq: "Total Project Period" } } }
+          ]
+        }
+        first: 2000
+        orderBy: { Acc_ProjectParticipant__r: { Acc_AccountId__r: { Name: { order: ASC } } } }
+      ) {
+        edges {
+          node {
+            Id
+            RecordType {
+              Name {
+                value
+              }
+            }
+            Acc_ProjectPeriodEndDate__c {
+              value
+            }
+            Acc_ProjectPeriodStartDate__c {
+              value
+            }
+            Acc_ProjectPeriodNumber__c {
+              value
+            }
+          }
+        }
+      }
+      ClaimPeriodDate_ProjectParticipant: Acc_ProjectParticipant__c(
+        where: { and: [{ Acc_ProjectId__c: { eq: $projectId } }, { Id: { eq: $partnerId } }] }
+        first: 2000
+      ) {
+        edges {
+          node {
+            Id
+            Acc_AccountId__r {
+              Name {
+                value
+              }
+            }
+            Acc_ProjectRole__c {
+              value
+            }
+            Acc_ParticipantStatus__c {
+              value
+            }
+          }
+        }
+      }
+    }
+  }
+`;
