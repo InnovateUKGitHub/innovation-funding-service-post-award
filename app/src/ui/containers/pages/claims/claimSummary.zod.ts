4e37bdd4b312bd250a272e61679f4cb520fe14ed
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2023-10-30 14:35:52 +0000

    ACC-10205: (claim summary) only validate iar document on submit

--- app/src/ui/containers/pages/claims/claimSummary.zod.ts
+++ app/src/ui/containers/pages/claims/claimSummary.zod.ts
@@ -4,12 +4,19 @@ import { DocumentDescription } from "@framework/constants/documentDescription";
 
 export const claimSummaryErrorMap = makeZodI18nMap({ keyPrefix: ["claimSummary"] });
 
-export const claimSummarySchema = z.object({
-  status: z.string(),
-  comments: z.string().max(1000),
-  button_submit: z.string(),
-  documents: z
-    .object({ description: z.nullable(z.number()).optional() })
-    .array()
-    .refine(x => x.some(doc => doc.description === DocumentDescription.IAR)),
-});
+export const claimSummarySchema = z.discriminatedUnion("button_submit", [
+  z.object({
+    button_submit: z.literal("submit"),
+    status: z.string(),
+    comments: z.string().max(1000),
+    documents: z
+      .object({ description: z.nullable(z.number()).optional() })
+      .array()
+      .refine(x => x.some(doc => doc.description === DocumentDescription.IAR)),
+  }),
+  z.object({
+    button_submit: z.literal("saveAndReturnToClaims"),
+    status: z.string(),
+    comments: z.string().max(1000),
+  }),
+]);
