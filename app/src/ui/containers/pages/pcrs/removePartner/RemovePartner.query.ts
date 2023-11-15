67542eb6816b61577b39fc1b3228acfca995f0be
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2023-11-15 15:05:41 +0000

    ACC-10268: (remove partner) add the word valid to the add a removal period error

--- app/src/ui/containers/pages/pcrs/removePartner/RemovePartner.query.ts
+++ app/src/ui/containers/pages/pcrs/removePartner/RemovePartner.query.ts
@@ -1,4 +1,4 @@
-import { graphql } from "relay-hooks";
+import { graphql } from "react-relay";
 
 export const removePartnerWorkflowQuery = graphql`
   query RemovePartnerWorkflowQuery($projectId: ID!, $pcrItemId: ID!) {
