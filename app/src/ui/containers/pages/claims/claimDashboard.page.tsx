23892188cce5f811cdf23b848234a139ed0d4789
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2023-11-17 14:02:04 +0000

    ACC-10396: (claims dashboard) convert to fc

--- app/src/ui/containers/pages/claims/claimDashboard.page.tsx
+++ app/src/ui/containers/pages/claims/claimDashboard.page.tsx
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon";
 import { ProjectParticipantsHoc } from "@ui/components/atomicDesign/atoms/providers/ProjectParticipants/project-participants";
 import { Pending } from "../../../../shared/pending";
-import { BaseProps, ContainerBase, defineRoute } from "../../containerBase";
+import { BaseProps, defineRoute } from "../../containerBase";
 import { GetProjectStatus } from "../../app/project-active";
 import { ClaimsDashboardGuidance } from "./components/ClaimsDashboardGuidance";
 import { DateFormat } from "@framework/constants/enums";
@@ -84,147 +84,199 @@ const ClaimTable =
     >
   >();
 
-class ClaimDashboardComponent extends ContainerBase<ClaimDashboardPageParams, Data> {
-  public render() {
-    const { project, partner, previousClaims, currentClaim } = this.props;
-    return (
-      <Page
-        pageTitle={<Title title={project.title} projectNumber={project.projectNumber} />}
-        backLink={<ProjectBackLink routes={this.props.routes} projectId={project.id} />}
-        projectStatus={project.status}
-        partnerStatus={partner.partnerStatus}
-      >
-        <ProjectParticipantsHoc>
-          {state => state.isMultipleParticipants && <ClaimsDashboardGuidance {...partner} />}
-        </ProjectParticipantsHoc>
+const ClaimDashboardComponent = (props: BaseProps & Data & ClaimDashboardPageParams) => {
+  const { project, partner, previousClaims, currentClaim } = props;
+  return (
+    <Page
+      pageTitle={<Title title={project.title} projectNumber={project.projectNumber} />}
+      backLink={<ProjectBackLink routes={props.routes} projectId={project.id} />}
+      projectStatus={project.status}
+      partnerStatus={partner.partnerStatus}
+    >
+      <ProjectParticipantsHoc>
+        {state => state.isMultipleParticipants && <ClaimsDashboardGuidance {...partner} />}
+      </ProjectParticipantsHoc>
 
-        <Messages messages={this.props.messages} />
-        <Section qa="current-claims-section" title={x => x.claimsLabels.openSectionTitle}>
-          {this.renderCurrentClaims(
-            currentClaim ? [currentClaim] : [],
-            "current-claims-table",
-            project,
-            partner,
-            previousClaims,
-          )}
-        </Section>
-        <Section qa="previous-claims-section" title={x => x.claimsLabels.closedSectionTitle}>
-          {this.renderPreviousClaims(previousClaims, "previous-claims-table", project, partner)}
-        </Section>
-      </Page>
-    );
-  }
-
-  private renderNoCurrentClaimsMessage(endDate: Date, previousClaims: Data["previousClaims"]) {
-    const date = DateTime.fromJSDate(endDate).plus({ days: 1 }).toJSDate();
-    // If the final claim has been approved
-    if (previousClaims && previousClaims.find(x => x.isFinalClaim)) {
-      return (
-        <SimpleString qa="yourFinalClaimApprovedNotificationMessage">
-          <Content value={x => x.claimsMessages.noRemainingClaims} />
-        </SimpleString>
-      );
-    }
-    return (
-      <SimpleString>
-        <Content
-          value={x => x.claimsMessages.noOpenClaims({ nextClaimStartDate: formatDate(date, DateFormat.FULL_DATE) })}
+      <Messages messages={props.messages} />
+      <Section qa="current-claims-section" title={x => x.claimsLabels.openSectionTitle}>
+        <CurrentClaims
+          currentClaims={currentClaim ? [currentClaim] : []}
+          tableQa="current-claims-table"
+          project={project}
+          partner={partner}
+          previousClaims={previousClaims}
+          routes={props.routes}
         />
+      </Section>
+      <Section qa="previous-claims-section" title={x => x.claimsLabels.closedSectionTitle}>
+        <PreviousClaims
+          tableQa="previous-claims-table"
+          data={previousClaims}
+          project={project}
+          partner={partner}
+          routes={props.routes}
+        />
+      </Section>
+    </Page>
+  );
+};
+
+const NoCurrentClaimsMessage = ({
+  endDate,
+  previousClaims,
+}: {
+  endDate: Date;
+  previousClaims: Data["previousClaims"];
+}) => {
+  const date = DateTime.fromJSDate(endDate).plus({ days: 1 }).toJSDate();
+  // If the final claim has been approved
+  if (previousClaims && previousClaims.find(x => x.isFinalClaim)) {
+    return (
+      <SimpleString qa="yourFinalClaimApprovedNotificationMessage">
+        <Content value={x => x.claimsMessages.noRemainingClaims} />
       </SimpleString>
     );
   }
+  return (
+    <SimpleString>
+      <Content
+        value={x => x.claimsMessages.noOpenClaims({ nextClaimStartDate: formatDate(date, DateFormat.FULL_DATE) })}
+      />
+    </SimpleString>
+  );
+};
 
-  private renderCurrentClaims(
-    currentClaims: NonNullable<Data["currentClaim"]>[],
-    tableQa: string,
-    project: Data["project"],
-    partner: Data["partner"],
-    previousClaims: Data["previousClaims"],
-  ) {
-    if (currentClaims.length) {
-      return this.renderClaimsTable(currentClaims, tableQa, project, partner, "Open");
-    }
-
-    if (project.periodEndDate) {
-      return this.renderNoCurrentClaimsMessage(project.periodEndDate, previousClaims);
-    }
-
-    return null;
-  }
-
-  private renderPreviousClaims(
-    data: Data["previousClaims"],
-    tableQa: string,
-    project: Data["project"],
-    partner: Data["partner"],
-  ) {
-    if (data.length) {
-      return this.renderClaimsTable(data, tableQa, project, partner, "Closed");
-    }
-
+const CurrentClaims = ({
+  currentClaims,
+  tableQa,
+  project,
+  partner,
+  previousClaims,
+  routes,
+}: {
+  currentClaims: NonNullable<Data["currentClaim"]>[];
+  tableQa: string;
+  project: Data["project"];
+  partner: Data["partner"];
+  previousClaims: Data["previousClaims"];
+  routes: BaseProps["routes"];
+}) => {
+  if (currentClaims.length) {
     return (
-      <SimpleString>
-        <Content value={x => x.claimsMessages.noClosedClaims} />
-      </SimpleString>
+      <ClaimsTable
+        data={currentClaims}
+        tableQa={tableQa}
+        project={project}
+        partner={partner}
+        tableCaption="Open"
+        routes={routes}
+      />
     );
   }
 
-  private renderClaimsTable(
-    data: Data["previousClaims"],
-    tableQa: string,
-    project: Data["project"],
-    partner: Data["partner"],
-    tableCaption?: string,
-  ) {
+  if (project.periodEndDate) {
+    return <NoCurrentClaimsMessage endDate={project.periodEndDate} previousClaims={previousClaims} />;
+  }
+
+  return null;
+};
+
+const PreviousClaims = ({
+  data,
+  tableQa,
+  project,
+  partner,
+  routes,
+}: {
+  data: Data["previousClaims"];
+  tableQa: string;
+  project: Data["project"];
+  partner: Data["partner"];
+  routes: BaseProps["routes"];
+}) => {
+  if (data.length) {
     return (
-      <GetProjectStatus>
-        {projectStatus => (
-          <ClaimTable.Table
-            data={data.sort((a, b) => a.periodId - b.periodId)}
-            bodyRowFlag={claim => (projectStatus.isActive ? this.hasBodyRowFlag(claim, project, partner) : null)}
-            qa={tableQa}
-            caption={tableCaption}
-          >
-            <ClaimTable.Custom
-              header={x => x.claimsLabels.period}
-              qa="period"
-              value={x => <ClaimPeriodDate claim={x} />}
-            />
-            <ClaimTable.Currency
-              header={x => x.claimsLabels.forecastCosts}
-              qa="forecast-cost"
-              value={x => x.forecastCost}
-            />
-            <ClaimTable.Currency header={x => x.claimsLabels.actualCosts} qa="actual-cost" value={x => x.totalCost} />
-            <ClaimTable.Currency
-              header={x => x.claimsLabels.difference}
-              qa="diff"
-              value={x => roundCurrency(x.forecastCost - x.totalCost)}
-            />
-            <ClaimTable.Custom header={x => x.claimsLabels.status} qa="status" value={x => x.statusLabel} />
-            <ClaimTable.ShortDate
-              header={x => x.claimsLabels.lastUpdatedDate}
-              qa="date"
-              value={x => x.paidDate || x.approvedDate || x.lastModifiedDate}
-            />
-            <ClaimTable.Custom
-              header={x => x.claimsLabels.actionHeader}
-              hideHeader
-              qa="link"
-              value={x => <ClaimDetailsLink claim={x} project={project} partner={partner} routes={this.props.routes} />}
-            />
-          </ClaimTable.Table>
-        )}
-      </GetProjectStatus>
+      <ClaimsTable
+        data={data}
+        tableQa={tableQa}
+        project={project}
+        partner={partner}
+        tableCaption="Closed"
+        routes={routes}
+      />
     );
   }
 
-  private hasBodyRowFlag(claim: Pick<ClaimDto, "status">, project: Data["project"], partner: Data["partner"]) {
-    const linkType = getClaimDetailsStatusType({ claim, project, partner });
+  return (
+    <SimpleString>
+      <Content value={x => x.claimsMessages.noClosedClaims} />
+    </SimpleString>
+  );
+};
 
-    return linkType === "edit" ? "edit" : null;
-  }
-}
+const ClaimsTable = ({
+  data,
+  tableQa,
+  project,
+  partner,
+  tableCaption,
+  routes,
+}: {
+  data: Data["previousClaims"];
+  tableQa: string;
+  project: Data["project"];
+  partner: Data["partner"];
+  tableCaption?: string;
+  routes: BaseProps["routes"];
+}) => {
+  return (
+    <GetProjectStatus>
+      {projectStatus => (
+        <ClaimTable.Table
+          data={data.sort((a, b) => a.periodId - b.periodId)}
+          bodyRowFlag={claim => (projectStatus.isActive ? hasBodyRowFlag(claim, project, partner) : null)}
+          qa={tableQa}
+          caption={tableCaption}
+        >
+          <ClaimTable.Custom
+            header={x => x.claimsLabels.period}
+            qa="period"
+            value={x => <ClaimPeriodDate claim={x} />}
+          />
+          <ClaimTable.Currency
+            header={x => x.claimsLabels.forecastCosts}
+            qa="forecast-cost"
+            value={x => x.forecastCost}
+          />
+          <ClaimTable.Currency header={x => x.claimsLabels.actualCosts} qa="actual-cost" value={x => x.totalCost} />
+          <ClaimTable.Currency
+            header={x => x.claimsLabels.difference}
+            qa="diff"
+            value={x => roundCurrency(x.forecastCost - x.totalCost)}
+          />
+          <ClaimTable.Custom header={x => x.claimsLabels.status} qa="status" value={x => x.statusLabel} />
+          <ClaimTable.ShortDate
+            header={x => x.claimsLabels.lastUpdatedDate}
+            qa="date"
+            value={x => x.paidDate || x.approvedDate || x.lastModifiedDate}
+          />
+          <ClaimTable.Custom
+            header={x => x.claimsLabels.actionHeader}
+            hideHeader
+            qa="link"
+            value={x => <ClaimDetailsLink claim={x} project={project} partner={partner} routes={routes} />}
+          />
+        </ClaimTable.Table>
+      )}
+    </GetProjectStatus>
+  );
+};
+
+const hasBodyRowFlag = (claim: Pick<ClaimDto, "status">, project: Data["project"], partner: Data["partner"]) => {
+  const linkType = getClaimDetailsStatusType({ claim, project, partner });
+
+  return linkType === "edit" ? "edit" : null;
+};
 
 const ClaimsDashboardRouteContainer = (props: ClaimDashboardPageParams & BaseProps) => {
   const stores = useStores();
