23a88d82074a4f35fb3251c1f016846cc5bb5768
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2023-12-05 11:19:15 +0000

    ACC-10433: (add partner) add useLinks hook

--- app/src/ui/containers/pages/pcrs/utils/useNextLink.ts
+++ app/src/ui/containers/pages/pcrs/utils/useNextLink.ts
@@ -15,3 +15,22 @@ export const useNextLink = () => {
     });
   }, [projectId, pcrId, itemId, routes, workflow]);
 };
+
+export const useSummaryLink = () => {
+  const { projectId, pcrId, itemId, workflow, routes } = usePcrWorkflowContext();
+
+  return useMemo(() => {
+    return routes.pcrPrepareItem.getLink({
+      projectId,
+      pcrId,
+      itemId,
+      step: undefined,
+    });
+  }, [projectId, pcrId, itemId, routes, workflow]);
+};
+
+export const useLinks = () => {
+  const nextLink = useNextLink();
+  const summaryLink = useSummaryLink();
+  return (data: { button_submit: string }) => ({ link: data.button_submit === "submit" ? nextLink : summaryLink });
+};
