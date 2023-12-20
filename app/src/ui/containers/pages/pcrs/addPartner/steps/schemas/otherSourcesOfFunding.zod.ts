f66f9960bc9052d915857ff4668bdad7e0553a87
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2023-12-20 14:44:55 +0000

    ACC-10433: (add partner) reject non digits in date secured field

--- app/src/ui/containers/pages/pcrs/addPartner/steps/schemas/otherSourcesOfFunding.zod.ts
+++ app/src/ui/containers/pages/pcrs/addPartner/steps/schemas/otherSourcesOfFunding.zod.ts
@@ -11,11 +11,19 @@ const valueDescription = z.object({
  */
 const dateSecured = z
   .object({
-    dateSecured_month: z.string().optional().nullable(),
-    dateSecured_year: z.string().optional().nullable(),
+    dateSecured_month: z.string(),
+    dateSecured_year: z.string(),
     dateSecured: z.date().nullable(),
   })
   .superRefine((data, ctx) => {
+    if (!/^\d*$/.test(data.dateSecured_month) || !/^\d*$/.test(data.dateSecured_year)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_type,
+        received: "string",
+        expected: "number",
+        path: ["dateSecured"],
+      });
+    }
     if (isEmptyDate(data.dateSecured_month, data.dateSecured_year)) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
