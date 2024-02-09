690fa3490eae13c48cb47fbb202db5b60275352b
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2024-02-09 11:10:07 +0000

    ACC-10652: (revalidate) add revalidate hook to help force revalidation of derived fields

--- app/src/ui/containers/pages/pcrs/pcrItemWorkflowHooks.tsx
+++ app/src/ui/containers/pages/pcrs/pcrItemWorkflowHooks.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { UseFormTrigger } from "react-hook-form";
 import { usePcrWorkflowContext } from "./pcrItemWorkflowMigrated";
-import { noop } from "lodash";
+import { useFormRevalidate } from "@ui/hooks/useFormRevalidate";
 
 /**
  * Hook returns a suite of hooks for handling errors on the migrated workflow pages
@@ -21,35 +21,8 @@ export const usePcrItemWorkflowHooks = () => {
     }, [trigger, markedAsCompleteHasBeenChecked]);
   };
 
-  const useFormValidate = <TFormValues extends AnyObject>(trigger: UseFormTrigger<TFormValues>) => {
-    const { markedAsCompleteHasBeenChecked } = usePcrWorkflowContext();
-
-    // ref prevents revalidating until after the submit event has been triggered once unless pcr marked as complete
-    const hasSubmitted = useRef(markedAsCompleteHasBeenChecked);
-
-    const submitFormHandler = () => (hasSubmitted.current = true);
-    const changeFormHandler = () => (hasSubmitted.current ? trigger() : noop());
-
-    useEffect(() => {
-      if (markedAsCompleteHasBeenChecked) {
-        // we need to revalidate the form immediately if marked as complete
-        // trigger validation
-        trigger();
-      }
-
-      // sets the hasSubmitted to true on the event
-      window.addEventListener("submit", submitFormHandler);
-      // triggers reevaluation on keypress
-      window.addEventListener("change", changeFormHandler);
-      // cleans up event listeners on unmount
-      return () => {
-        window.removeEventListener("submit", submitFormHandler);
-        window.removeEventListener("change", changeFormHandler);
-      };
-    }, [trigger, markedAsCompleteHasBeenChecked]);
-  };
   return {
-    useFormValidate,
+    useFormValidate: useFormRevalidate,
     useInitialValidate,
   };
 };
