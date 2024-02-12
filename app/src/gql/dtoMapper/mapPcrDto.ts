de34a9630297e3ae04e87800a3d5f745a82f1acf
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-02-12 11:12:11 +0000

    ACC-10194: (pcr) Display uplift justification

--- app/src/gql/dtoMapper/mapPcrDto.ts
+++ app/src/gql/dtoMapper/mapPcrDto.ts
@@ -65,6 +65,7 @@ export type PcrNode = GQL.PartialNode<{
   Country_where_work_will_be_carried_out__c: GQL.Value<string>;
   Cost_of_work__c: GQL.Value<number>;
   Justification__c: GQL.Value<string>;
+  Override_Justification__c: GQL.Value<string>;
   Acc_Project_Change_Requests__r: GQL.ArrayValue<PcrNode>;
 }>;
 
@@ -131,6 +132,7 @@ export type PcrItemDtoMapping = Pick<
   | "subcontractorJustification"
   | "subcontractorCost"
   | "reasoningComments"
+  | "upliftJustification"
 >;
 
 const mapChangeOffsetToQuarter = (currentMonthOffset: number, changedMonthOffset: number) => {
@@ -304,6 +306,9 @@ const itemMapper: GQL.DtoMapper<PcrItemDtoMapping, PcrNode, { typeOfAid?: string
   },
   subcontractorJustification(node) {
     return node?.Justification__c?.value ?? null;
+  },
+  upliftJustification(node) {
+    return node?.Override_Justification__c?.value ?? null;
   },
 };
 
