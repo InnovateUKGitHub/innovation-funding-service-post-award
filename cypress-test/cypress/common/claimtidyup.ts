213f45b4244a3cd9860d13d0687aa493cf330bf8
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2024-02-14 13:49:30 +0000

    ACC-10750: (cy) improve resilience of textarea clear action

--- cypress-test/cypress/common/claimtidyup.ts
+++ cypress-test/cypress/common/claimtidyup.ts
@@ -29,9 +29,10 @@ export const fcClaimTidyUp = async (claimType: string) => {
         cy.log(`Change claim status of ${claimType} claim`);
         cy.switchUserTo("testman2@testing.com");
         cy.get("td").contains("ABS EUI Medium Enterprise").siblings().clickOn("a", "Review");
-        cy.getByQA("status_MO Queried").click({ force: true });
-        cy.get("textarea").wait(200).clear();
-        cy.get("textarea").type(comments);
+        cy.getByLabel("Query claim").click();
+        cy.get("textarea").as("txt").wait(200);
+        cy.get("@txt").clear();
+        cy.get("@txt").type(comments);
         cy.paragraph("You have");
         cy.paragraph("I am satisfied that the costs claimed appear to comply");
         cy.getByQA("cr&d-reminder").contains("You must submit a monitoring report");
