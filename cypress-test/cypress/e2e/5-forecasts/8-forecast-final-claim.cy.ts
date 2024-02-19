f83c384a0fe2caa964f67371216e412bf2016b9b
Allan Haines - IUK <allan.haines@iuk.ukri.org>
2024-02-19 11:18:28 +0000

    ACC-10763: (cy) Added test steps for Forecast final claim to test 10641.

---
+++ cypress-test/cypress/e2e/5-forecasts/8-forecast-final-claim.cy.ts
@@ -0,0 +1,27 @@
+import { visitApp } from "common/visit";
+const fcContact = "pauline.o'jones@uobcw.org.uk.test.prod";
+describe("Forecast > Final claim", () => {
+  before(() => {
+    visitApp({ asUser: fcContact, path: "projects/a0E2600000kSvOGEA0/overview" });
+  });
+
+  it("Should display project Overview and click into the Forecast tile", () => {
+    cy.heading("Project overview");
+    cy.selectTile("Forecast");
+  });
+
+  it("Should display the Forecast heading and guidance", () => {
+    cy.heading("Forecast");
+    cy.paragraph(
+      "You can now amend your forecasted costs at any time (as long as the related period's claim has not yet been approved).",
+    );
+    cy.getByQA("final-claim-message-FC-content").contains("You cannot change your forecast. You must ");
+    cy.get("a").contains("submit your final claim");
+  });
+
+  it("Should click 'Submit your final claim' and land on the correct claim page", () => {
+    cy.clickOn("submit your final claim");
+    cy.heading("Costs to be claimed");
+    cy.validationNotification("This is the final claim.");
+  });
+});
