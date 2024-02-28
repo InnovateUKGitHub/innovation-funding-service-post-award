d568a71fd263ddfa342f6381683c78f3e399e53e
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-02-28 10:44:46 +0000

    stash

---
+++ test-framework/salesforce-factory/src/factory/SalesforceFactory.ts
@@ -0,0 +1,84 @@
+interface SffFieldBase {
+  sfdcName: Readonly<string>;
+  sffName: Readonly<string>;
+  sfdcType: Readonly<SffFieldType>;
+}
+
+enum SffFieldType {
+  STRING,
+  NUMBER,
+  SINGLE_PICKLIST,
+  MULTI_PICKLIST,
+  CHECKBOX,
+}
+
+interface SffFieldTypeToJsMap {
+  [SffFieldType.STRING]: string;
+  [SffFieldType.NUMBER]: number;
+  [SffFieldType.SINGLE_PICKLIST]: string;
+  [SffFieldType.MULTI_PICKLIST]: string[];
+  [SffFieldType.CHECKBOX]: boolean;
+}
+
+type SffTypeToJsType<T extends SffFieldType> = SffFieldTypeToJsMap[T];
+
+interface SffStringField extends SffFieldBase {
+  sfdcType: SffFieldType.STRING;
+}
+interface SffNumberField extends SffFieldBase {
+  sfdcType: SffFieldType.NUMBER;
+}
+interface SffPicklistField extends SffFieldBase {
+  sfdcType: SffFieldType.SINGLE_PICKLIST;
+}
+interface SffCheckboxField extends SffFieldBase {
+  sfdcType: SffFieldType.CHECKBOX;
+}
+
+type SffField = SffStringField | SffNumberField | SffPicklistField | SffCheckboxField;
+
+interface SffRelationship {
+  sfdcName: Readonly<string>;
+  sffName: Readonly<string>;
+}
+
+interface SffFactoryObjectDefinition {
+  sfdcName: Readonly<string>;
+  fields: ReadonlyArray<SffField>;
+  relationships: ReadonlyArray<SffRelationship>;
+}
+
+class SffBuilder<T extends SffFactoryObjectDefinition> {
+  private readonly definition: Readonly<T>;
+
+  constructor({ definition }: { definition: Readonly<T> }) {
+    this.definition = definition;
+  }
+
+  public readonly setField: <K extends T["fields"][number]>(
+    sffName: K["sffName"],
+    value: SffTypeToJsType<K["sfdcType"]>,
+  ) => this = (sffName, value) => {
+    const sfdcName: SffField = this.definition.fields.find(x => x.sffName === sffName)!;
+
+    return this;
+  };
+}
+
+const project = new SffBuilder({
+  definition: {
+    sfdcName: "Acc_Project__c",
+    fields: [
+      { sfdcName: "Acc_ProjectNumber__c", sffName: "string", sfdcType: SffFieldType.STRING },
+      { sfdcName: "Acc_SomethingElse__c", sffName: "number", sfdcType: SffFieldType.NUMBER },
+    ],
+    relationships: [],
+  },
+} as const);
+
+project.setField("string", 42).setField("number", "sdaasd");
+
+abstract class SffObject {
+  create() {}
+  delete() {}
+}
