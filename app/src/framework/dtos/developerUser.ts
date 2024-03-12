1bd41a7fa811f9225fa9fdb2b8e20ce7b81fca1d
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-03-12 09:52:42 +0000

    ACC-10813: (user switcher) Show all roles for users with multiple roles

--- app/src/framework/dtos/developerUser.ts
+++ app/src/framework/dtos/developerUser.ts
@@ -1,8 +1,5 @@
-import { SalesforceRole } from "@server/repositories/projectContactsRepository";
-
 export interface DeveloperUser {
   externalUsername: string | null;
   internalUsername: string | null;
   name: string;
-  role: SalesforceRole;
 }
