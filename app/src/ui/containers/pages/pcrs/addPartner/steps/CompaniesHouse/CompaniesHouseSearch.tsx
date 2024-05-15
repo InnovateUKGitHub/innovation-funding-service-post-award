81fa4e951ff5d45eb1683ea40c59d2448d869cca
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-05-15 14:35:51 +0100

    ACC-11047: (pcr) Only setCompanyInfo if radioValue is defined

--- app/src/ui/containers/pages/pcrs/addPartner/steps/CompaniesHouse/CompaniesHouseSearch.tsx
+++ app/src/ui/containers/pages/pcrs/addPartner/steps/CompaniesHouse/CompaniesHouseSearch.tsx
@@ -46,16 +46,18 @@ const CompaniesHouseSearchResults = ({ searchQuery, setCompanyInfo }: CompaniesH
   const radioValue = watch("companiesHouseResult");
 
   useEffect(() => {
-    // We are using a serialised JSON string to allow for JS disabled,
-    // since a radio option can only "remember" a single value.
-    try {
-      const selectedOption = JSON.parse(radioValue) as DeserialisedCompaniesHouseSearchData;
-      setCompanyInfo({
-        organisationName: selectedOption?.title,
-        registeredAddress: selectedOption?.addressFull,
-        registrationNumber: selectedOption?.registrationNumber,
-      });
-    } catch {}
+    if (radioValue) {
+      // We are using a serialised JSON string to allow for JS disabled,
+      // since a radio option can only "remember" a single value.
+      try {
+        const selectedOption = JSON.parse(radioValue) as DeserialisedCompaniesHouseSearchData;
+        setCompanyInfo({
+          organisationName: selectedOption?.title,
+          registeredAddress: selectedOption?.addressFull,
+          registrationNumber: selectedOption?.registrationNumber,
+        });
+      } catch {}
+    }
   }, [handleSubmit, radioValue, setCompanyInfo]);
 
   if (data.companies.length === 0) {
