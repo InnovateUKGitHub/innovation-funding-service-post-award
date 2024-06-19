947d9e47d6325fab7a439963226efea6d86ec6a4
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-06-19 15:26:35 +0100

    ACC-11159: (js disabled) Allow multer to process 11 files (so Zod can refuse it)

--- app/src/server/htmlFormHandler/diskStorage.ts
+++ app/src/server/htmlFormHandler/diskStorage.ts
@@ -6,8 +6,8 @@ const storage = multer.diskStorage({});
 export const upload = multer({
   storage,
   limits: {
-    // Add extra leeway for multer to accept larger files.
+    // Add extra leeway for multer to accept more than allowed (only to then validate against it)
     fileSize: configuration.options.maxFileSize * 1.2,
-    files: configuration.options.maxUploadFileCount,
+    files: configuration.options.maxUploadFileCount + 1,
   },
 });
