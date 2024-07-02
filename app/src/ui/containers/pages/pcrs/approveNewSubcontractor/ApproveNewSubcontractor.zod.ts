71d8a7a0a4fedc244fe675c12e19e15e95291c5b
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2024-07-02 10:27:48 +0100

    ACC-11111: (subcontractor) make all validations appear simultaneously on summary

--- app/src/ui/containers/pages/pcrs/approveNewSubcontractor/ApproveNewSubcontractor.zod.ts
+++ app/src/ui/containers/pages/pcrs/approveNewSubcontractor/ApproveNewSubcontractor.zod.ts
@@ -3,7 +3,6 @@ import { FormTypes } from "@ui/zod/FormTypes";
 import { getGenericCurrencyValidation } from "@ui/zod/currencyValidator.zod";
 import {
   booleanValidation,
-  evaluateObject,
   pcrIdValidation,
   pcrItemIdValidation,
   projectIdValidation,
@@ -19,62 +18,65 @@ const subcontractorLocationMaxChars = 100 as const;
 const subcontractorDescriptionMaxChars = 255 as const;
 const subcontractorJustificationMaxChars = 32000 as const;
 
-const approveNewSubcontractorSchema = evaluateObject((data: { markedAsComplete: boolean }) => ({
-  projectId: projectIdValidation,
-  pcrId: pcrIdValidation,
-  pcrItemId: pcrItemIdValidation,
-  form: z.union([
-    z.literal(FormTypes.PcrApproveNewSubcontractorStep),
-    z.literal(FormTypes.PcrApproveNewSubcontractorSummary),
-  ]),
-  subcontractorName: z.string().max(subcontractorNameMaxChars),
-  subcontractorRegistrationNumber: z.string().max(subcontractorRegistrationNumberMaxChars),
-  subcontractorRelationship: booleanValidation,
-  subcontractorRelationshipJustification: z.string(),
-  subcontractorLocation: z.string().max(subcontractorLocationMaxChars),
-  subcontractorDescription: z.string().max(subcontractorDescriptionMaxChars),
-  subcontractorJustification: z.string().max(subcontractorJustificationMaxChars),
-  subcontractorCost: getGenericCurrencyValidation({
-    label: "forms.pcr.approveNewSubcontractor.subcontractorCost.label",
-    required: data.markedAsComplete,
-  }),
-  markedAsComplete: z.boolean(),
-})).superRefine((data, ctx) => {
-  if (data.markedAsComplete) {
-    const checkIfFalsy = <T extends keyof typeof data>(key: T, value: unknown) => {
-      if (!value) {
+const approveNewSubcontractorSchema = z
+  .object({
+    projectId: projectIdValidation,
+    pcrId: pcrIdValidation,
+    pcrItemId: pcrItemIdValidation,
+    form: z.union([
+      z.literal(FormTypes.PcrApproveNewSubcontractorStep),
+      z.literal(FormTypes.PcrApproveNewSubcontractorSummary),
+    ]),
+    subcontractorName: z.string().max(subcontractorNameMaxChars),
+    subcontractorRegistrationNumber: z.string().max(subcontractorRegistrationNumberMaxChars),
+    subcontractorRelationship: booleanValidation,
+    subcontractorRelationshipJustification: z.string(),
+    subcontractorLocation: z.string().max(subcontractorLocationMaxChars),
+    subcontractorDescription: z.string().max(subcontractorDescriptionMaxChars),
+    subcontractorJustification: z.string().max(subcontractorJustificationMaxChars),
+    subcontractorCost: getGenericCurrencyValidation({
+      label: "forms.pcr.approveNewSubcontractor.subcontractorCost.label",
+    }),
+    markedAsComplete: z.boolean(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.markedAsComplete) {
+      const checkIfFalsy = <T extends keyof typeof data>(key: T, value: unknown) => {
+        if (!value) {
+          ctx.addIssue({
+            type: "string",
+            code: z.ZodIssueCode.too_small,
+            minimum: 1,
+            inclusive: true,
+            path: [key],
+          });
+        }
+      };
+      console.log("data", data);
+
+      checkIfFalsy("subcontractorName", data.subcontractorName);
+      checkIfFalsy("subcontractorRegistrationNumber", data.subcontractorRegistrationNumber);
+      checkIfFalsy("subcontractorLocation", data.subcontractorLocation);
+      checkIfFalsy("subcontractorDescription", data.subcontractorDescription);
+      checkIfFalsy("subcontractorJustification", data.subcontractorJustification);
+      checkIfFalsy("subcontractorCost", data.subcontractorCost);
+      if (data.subcontractorRelationship) {
+        checkIfFalsy("subcontractorRelationshipJustification", data.subcontractorRelationshipJustification);
+      }
+    }
+
+    if (data.subcontractorRelationship) {
+      if (data.subcontractorRelationshipJustification.length > subcontractorRelationshipJustificationMaxChars) {
         ctx.addIssue({
           type: "string",
-          code: z.ZodIssueCode.too_small,
-          minimum: 1,
+          code: z.ZodIssueCode.too_big,
+          maximum: subcontractorRelationshipJustificationMaxChars,
           inclusive: true,
-          path: [key],
+          path: ["subcontractorRelationshipJustification"],
         });
       }
-    };
-
-    checkIfFalsy("subcontractorName", data.subcontractorName);
-    checkIfFalsy("subcontractorRegistrationNumber", data.subcontractorRegistrationNumber);
-    checkIfFalsy("subcontractorLocation", data.subcontractorLocation);
-    checkIfFalsy("subcontractorDescription", data.subcontractorDescription);
-    checkIfFalsy("subcontractorJustification", data.subcontractorJustification);
-    if (data.subcontractorRelationship) {
-      checkIfFalsy("subcontractorRelationshipJustification", data.subcontractorRelationshipJustification);
     }
-  }
-
-  if (data.subcontractorRelationship) {
-    if (data.subcontractorRelationshipJustification.length > subcontractorRelationshipJustificationMaxChars) {
-      ctx.addIssue({
-        type: "string",
-        code: z.ZodIssueCode.too_big,
-        maximum: subcontractorRelationshipJustificationMaxChars,
-        inclusive: true,
-        path: ["subcontractorRelationshipJustification"],
-      });
-    }
-  }
-});
+  });
 
 type ApproveNewSubcontractorSchemaType = typeof approveNewSubcontractorSchema;
 
