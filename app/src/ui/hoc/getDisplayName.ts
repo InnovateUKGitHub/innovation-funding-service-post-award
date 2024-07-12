074f9446eb887c76f115baaa5f1502eef6cf9120
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2024-07-12 09:41:08 +0100

    ACC-11347:(getDisplayName) Delete unused file

--- app/src/ui/hoc/getDisplayName.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentType } from "react";
-
-const getDisplayName = (Element: ComponentType<AnyObject> | keyof React.JSX.IntrinsicElements): string => {
-  if (typeof Element === "string") return Element;
-  return Element.displayName ?? Element.name ?? "Component";
-};
-
-export { getDisplayName };
