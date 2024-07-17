4c5a1cee481f87fe3f430643d284439035d7c499
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2024-07-17 14:18:37 +0100

    ACC-11282: (document filter) remove bjss form from document filter

--- app/src/ui/components/atomicDesign/organisms/documents/DocumentFilter/DocumentFilter.tsx
+++ app/src/ui/components/atomicDesign/organisms/documents/DocumentFilter/DocumentFilter.tsx
@@ -1,17 +1,9 @@
 import { useCallback, useEffect, useState } from "react";
 
 import { noop } from "@ui/helpers/noop";
-// import { createTypedForm } from "../../../../bjss/form/form";
 import { Form } from "@ui/components/atomicDesign/atoms/form/Form/Form";
 import { FormGroup } from "@ui/components/atomicDesign/atoms/form/FormGroup/FormGroup";
 import { SearchInput } from "@ui/components/bjss/inputs/searchInput";
-
-// Note: TypedForm enforces an object shaped payload, so this can't a single primitive string
-interface DocumentFilterState {
-  filteredText: string;
-}
-
-// const FilterForm = createTypedForm<DocumentFilterState>();
 
 export interface DocumentFilterProps {
   value: string;
@@ -28,22 +20,19 @@ export const DocumentFilter = ({
   placeholder = "Search documents",
   onSearch,
 }: DocumentFilterProps) => {
-  const handleOnSearch = useCallback(
-    (newState: DocumentFilterState): void => onSearch(newState.filteredText),
-    [onSearch],
-  );
+  const handleOnSearch = useCallback((filterText: string): void => onSearch(filterText), [onSearch]);
 
   const [searchInputValue, setSearchInputValue] = useState<string>(value);
-  //   // const formData: DocumentFilterState = { filteredText: value };
-  // useEffect(() => {
-  // handleOnSearch();
-  // },    [searchInputValue, handleOnSearch]))
+
+  useEffect(() => {
+    handleOnSearch(searchInputValue);
+  }, [searchInputValue, handleOnSearch]);
   return (
     <Form data-qa={qa} onSubmit={noop}>
       <FormGroup>
         <SearchInput
           autoComplete="off"
-          width="one-half"
+          width="full"
           name={name || "filter-item"}
           value={searchInputValue}
           onChange={searchValue => setSearchInputValue(searchValue?.trim() || "")}
@@ -52,16 +41,5 @@ export const DocumentFilter = ({
         />
       </FormGroup>
     </Form>
-    //   <FilterForm.Form qa={qa} data={formData} onSubmit={noop} onChange={handleOnSearch}>
-    //     <FilterForm.Search
-    //       name={name || "filter-item"}
-    //       labelHidden
-    //       autoComplete="off"
-    //       placeholder={placeholder}
-    //       value={x => x.filteredText}
-    //       // Note: SearchInput will always return a string, form controls always return TValue | null poor generic defaults :(
-    //       update={(x, v) => (x.filteredText = v as string)}
-    //     />
-    //   </FilterForm.Form>
   );
 };
