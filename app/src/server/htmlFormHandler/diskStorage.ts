a16e2f4507707d99e3a6cc3b248edf41affbc3ff
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-08-13 10:32:04 +0100

    ACC-11320: (multer) Parse filename as latin1 and convert to utf-8

--- app/src/server/htmlFormHandler/diskStorage.ts
+++ app/src/server/htmlFormHandler/diskStorage.ts
@@ -10,4 +10,10 @@ export const upload = multer({
     fileSize: configuration.options.maxFileSize * 1.2,
     files: configuration.options.maxUploadFileCount + 1,
   },
+
+  // TODO: Give support to https://github.com/expressjs/multer/pull/1210
+  fileFilter: (req, file, cb) => {
+    file.originalname = Buffer.from(file.originalname, "latin1").toString("utf8");
+    cb(null, true);
+  },
 });
