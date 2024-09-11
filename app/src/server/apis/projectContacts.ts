bec9a1a6444df0c4a55c8ee988a130d89b3bcb5c
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-09-11 14:00:40 +0100

    FPD-1090: (ktp) Make pclId optional

--- app/src/server/apis/projectContacts.ts
+++ app/src/server/apis/projectContacts.ts
@@ -1,10 +1,5 @@
 import { ProjectContactDto } from "@framework/dtos/projectContactDto";
 import {
-  CreateProjectContactCommand,
-  ServerCreateProjectContactsCommandContact,
-} from "@server/features/projectContacts/createProjectContactCommand";
-import { GetProjectContactLinkByIdQuery } from "@server/features/projectContacts/GetProjectContactLinkByIdQuery";
-import {
   ServerUpdateProjectContactsAssociateDetailsCommand,
   UpdateProjectContactLinkCommand,
 } from "@server/features/projectContacts/UpdateProjectContactLinkCommand";
@@ -14,10 +9,6 @@ import { GetAllForProjectQuery } from "../features/projectContacts/getAllForProj
 import { ApiParams, ControllerBase } from "./controllerBase";
 
 export interface IProjectContactsApi<Context extends "client" | "server"> {
-  create: (
-    params: ApiParams<Context, { projectId: ProjectId; contact: ServerCreateProjectContactsCommandContact }>,
-  ) => Promise<ProjectContactDto>;
-
   update: (
     params: ApiParams<
       Context,
@@ -30,16 +21,6 @@ class Controller extends ControllerBase<"server", ProjectContactDto> implements 
   constructor() {
     super("project-contacts");
 
-    this.postItem(
-      "/:projectId",
-      (p, _, b: ServerCreateProjectContactsCommandContact) => ({
-        projectId: p.projectId,
-        pcrId: p.pcrId,
-        contact: processDto(b),
-      }),
-      p => this.create(p),
-    );
-
     this.putItems(
       "/:projectId",
       (p, _, b: ServerUpdateProjectContactsAssociateDetailsCommand[]) => ({
@@ -48,15 +29,6 @@ class Controller extends ControllerBase<"server", ProjectContactDto> implements 
       }),
       p => this.update(p),
     );
-  }
-
-  public async create(
-    params: ApiParams<"server", { projectId: ProjectId; contact: ServerCreateProjectContactsCommandContact }>,
-  ) {
-    const command = new CreateProjectContactCommand(params.projectId, params.contact);
-    const pclId = await contextProvider.start(params).runCommand(command);
-    const query = new GetProjectContactLinkByIdQuery(pclId);
-    return await contextProvider.start(params).runQuery(query);
   }
 
   public async update(
