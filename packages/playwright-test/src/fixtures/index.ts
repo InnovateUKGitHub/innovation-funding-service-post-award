78de34db75edb193b89981972ab97edd44b033a6
Leondro Lio <leondro.lio@iuk.ukri.org>
2024-09-19 14:37:43 +0100

    ACC-11425: (cy) Use correct accProjectKTP fixture in manage team member tests

--- packages/playwright-test/src/fixtures/index.ts
+++ packages/playwright-test/src/fixtures/index.ts
@@ -88,6 +88,8 @@ export const test = base.extend<AccFixtures, Workers>({
     use(new RemovePartner({ page, commands, projectChangeRequests, validators })),
   projectDetails: ({ page, commands }, use) => use(new ProjectDetails({ page, commands })),
   projectDocuments: ({ page, commands, validators }, use) => use(new ProjectDocuments({ page, commands, validators })),
+  manageTeamMember: ({ page, commands, accProjectKTP, accUserSwitcher, accNavigation }, use) =>
+    use(new ManageTeamMember({ page, commands, ktp, accUserSwitcher, accNavigation })),
 
   // Project Factory
   accProjectBase: [
