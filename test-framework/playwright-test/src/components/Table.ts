662740e671bc0fed437e9706ece3670207fdb656
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2024-10-07 12:34:07 +0100

    ACC-11497: (pw) add methods for table selector

--- test-framework/playwright-test/src/components/Table.ts
+++ test-framework/playwright-test/src/components/Table.ts
@@ -1,19 +1,41 @@
 import { Page } from "@playwright/test";
 
-interface Column {
-  name: string;
-}
-
-class Table<Columns extends Record<string, Column>> {
+class Table<ColumnHeader extends string> {
   private readonly page: Page;
-  private readonly columns: Columns;
+  private readonly caption: string;
 
-  constructor({ page, columns }: { page: Page; columns: Columns }) {
+  constructor({ page, caption }: { page: Page; caption: string }) {
     this.page = page;
-    this.columns = columns;
+    this.caption = caption;
   }
 
-  async lookup<Column extends keyof Columns>(lookupColumn: Column, lookupValue: string) {}
+  get() {
+    return this.page.locator("table", { has: this.page.locator("caption", { hasText: this.caption }) });
+  }
+
+  /**
+   * Gets table cell from the column header and a row containing the string value.
+   * @example
+   * lookupCell("Forecast for period", "Labour"); // 123.45
+   */
+  lookupCell(columnHeader: ColumnHeader, row: string) {
+    return this.get().evaluate(table => {
+      const rows = table.querySelectorAll("tr");
+      let index: number = 0;
+      rows.evaluateAll(rows =>
+        rows.forEach((element, i) => {
+          if (element.textContent === columnHeader) {
+            index = i;
+          }
+        }),
+      );
+      return this.page.locator("table tr", { hasText: row }).locator(`td:nth(${index + 1})`);
+    });
+  }
+
+  static fromCaption(page: Page, caption: string) {
+    return new Table({ page, caption });
+  }
 }
 
 export { Table };
