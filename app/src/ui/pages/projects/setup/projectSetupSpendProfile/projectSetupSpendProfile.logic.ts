ff618e866e715dc843a1ad449e7a3e5f0428a903
Jonathan Grundman <jonathan.grundman@iuk.ukri.org>
2024-10-10 09:01:20 +0100

    ACC-11382: (project setup) prepopulate marked as complete status and show api errors

--- app/src/ui/pages/projects/setup/projectSetupSpendProfile/projectSetupSpendProfile.logic.ts
+++ app/src/ui/pages/projects/setup/projectSetupSpendProfile/projectSetupSpendProfile.logic.ts
@@ -1,6 +1,8 @@
 import { useLazyLoadQuery } from "react-relay";
 import { projectSetupSpendProfileQuery } from "./ProjectSetupSpendProfile.query";
 import { ProjectSetupSpendProfileQuery } from "./__generated__/ProjectSetupSpendProfileQuery.graphql";
+import { mapToPartnerDtoArray } from "@gql/dtoMapper/mapPartnerDto";
+import { head } from "lodash";
 
 const useProjectSetupSpendProfileData = ({ partnerId, projectId }: { partnerId: PartnerId; projectId: ProjectId }) => {
   const data = useLazyLoadQuery<ProjectSetupSpendProfileQuery>(
@@ -9,7 +11,16 @@ const useProjectSetupSpendProfileData = ({ partnerId, projectId }: { partnerId: 
     { fetchPolicy: "network-only" },
   );
 
+  const partnerPage = head(
+    mapToPartnerDtoArray(
+      data?.salesforce?.uiapi?.query?.Acc_ProjectParticipant__c?.edges ?? [],
+      ["spendProfileStatus"],
+      {},
+    ),
+  );
+
   return {
+    partnerPage,
     fragmentRef: data?.salesforce.uiapi,
   };
 };
