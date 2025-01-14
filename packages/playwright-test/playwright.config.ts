2908886d60cb1a8a4231833d429ae8cd3e00af9e
Leondro Lio <leondro.lio@iuk.ukri.org>
2025-01-14 09:29:09 +0000

    ACC-11715: (cy) Replace playwright baseURL with accdev

--- packages/playwright-test/playwright.config.ts
+++ packages/playwright-test/playwright.config.ts
@@ -45,9 +45,9 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: "http://127.0.0.1:3000",
-    // baseURL: "https://www-acc-dev.apps.ocp4.innovateuk.ukri.org",
-    //baseURL: "https://www-acc-at.apps.ocp4.innovateuk.ukri.org",
+    baseURL: "https://www-acc-dev.apps.ocp4.innovateuk.ukri.org",
+    // baseURL: "https://www-acc-at.apps.ocp4.innovateuk.ukri.org",
+    // baseURL: "http://127.0.0.1:3000",
     httpCredentials,
     trace: "on-first-retry",
     testIdAttribute: "data-qa",
