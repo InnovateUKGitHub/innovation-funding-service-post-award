6b89c30c948dcc5d5197e7f5c3e7a3710c2f09a0
Leondro Lio <leondro.lio@iuk.ukri.org>
2025-01-21 09:57:17 +0000

    ACC-11744: (pcr) Internationalise manage team member "partner to be replaced" role

--- packages/app/src/ui/pages/pcrs/manageTeamMembers/summary/components/ManageTeamMemberDeletedPartnerSummaryCard.tsx
+++ packages/app/src/ui/pages/pcrs/manageTeamMembers/summary/components/ManageTeamMemberDeletedPartnerSummaryCard.tsx
@@ -1,9 +1,11 @@
+import { mapProjectRoleToInternal } from "@framework/mappers/pcr";
 import { ProjectContactDtoGql } from "@gql/dtoMapper/mapContactDto";
 import { FullDate } from "@ui/components/atoms/Date";
 import { SummaryCard } from "@ui/components/atoms/SummaryCard/SummaryCard";
 import { SummaryCardContent } from "@ui/components/atoms/SummaryCardContent/SummaryCardContent";
 import { SummaryCardTitle } from "@ui/components/atoms/SummaryCardTitle/SummaryCardTitle";
 import { SummaryList, SummaryListItem } from "@ui/components/molecules/SummaryList/summaryList";
+import { useContent } from "@ui/hooks/content.hook";
 
 const ManageTeamMemberDeletedPartnerSummaryCard = ({
   pcl,
@@ -11,33 +13,47 @@ const ManageTeamMemberDeletedPartnerSummaryCard = ({
 }: {
   pcl: Pick<ProjectContactDtoGql, "firstName" | "lastName" | "email" | "endDate" | "role">;
   title: string;
-}) => (
-  <SummaryCard>
-    <SummaryCardTitle header="h2">{title}</SummaryCardTitle>
-    <SummaryCardContent>
-      <SummaryList>
-        <SummaryListItem
-          label={x => x.pages.manageTeamMembers.summary.firstName}
-          content={pcl.firstName}
-          qa="before-firstName"
-        />
-        <SummaryListItem
-          label={x => x.pages.manageTeamMembers.summary.lastName}
-          content={pcl.lastName}
-          qa="before-lastName"
-        />
-        <SummaryListItem label={x => x.pages.manageTeamMembers.summary.email} content={pcl.email} qa="before-email" />
-        <SummaryListItem label={x => x.pages.manageTeamMembers.summary.role} content={pcl.role} qa="before-role" />
-        {pcl.endDate && (
+}) => {
+  const { getContent } = useContent();
+  const { role } = pcl;
+  const manageTeamMemberRole = mapProjectRoleToInternal(role) ?? null;
+
+  return (
+    <SummaryCard>
+      <SummaryCardTitle header="h2">{title}</SummaryCardTitle>
+      <SummaryCardContent>
+        <SummaryList>
           <SummaryListItem
-            label={x => x.pages.manageTeamMembers.summary.endDate}
-            content={<FullDate value={pcl.endDate} />}
-            qa="before-endDate"
+            label={x => x.pages.manageTeamMembers.summary.firstName}
+            content={pcl.firstName}
+            qa="before-firstName"
           />
-        )}
-      </SummaryList>
-    </SummaryCardContent>
-  </SummaryCard>
-);
+          <SummaryListItem
+            label={x => x.pages.manageTeamMembers.summary.lastName}
+            content={pcl.lastName}
+            qa="before-lastName"
+          />
+          <SummaryListItem label={x => x.pages.manageTeamMembers.summary.email} content={pcl.email} qa="before-email" />
+          <SummaryListItem
+            label={x => x.pages.manageTeamMembers.summary.role}
+            content={
+              manageTeamMemberRole !== null
+                ? getContent(x => x.projectLabels[manageTeamMemberRole]({ count: 1 }))
+                : null
+            }
+            qa="before-role"
+          />
+          {pcl.endDate && (
+            <SummaryListItem
+              label={x => x.pages.manageTeamMembers.summary.endDate}
+              content={<FullDate value={pcl.endDate} />}
+              qa="before-endDate"
+            />
+          )}
+        </SummaryList>
+      </SummaryCardContent>
+    </SummaryCard>
+  );
+};
 
 export { ManageTeamMemberDeletedPartnerSummaryCard };
