b89bfbab55c13430641e7bdaca2b6682afb7be4e
Allan Haines - IUK <allan.haines@iuk.ukri.org>
2025-02-14 15:16:03 +0000

    ACC-11847: (playwright) Added further drawdown tests.

--- packages/playwright-test/src/features/loans.feature
+++ packages/playwright-test/src/features/loans.feature
@@ -6,7 +6,7 @@ Feature: Loan Drawdowns
         And the user is the "pmUser" user
         And the user is on the project overview
         When the user selects the "Drawdowns" tile
-        Then the user will see the Drawdowns page
+        Then the user will see the "PM" Drawdowns page
             | Drawdown | Drawdown amount |
             | 1        | £110,000        |
             | 2        | £121,000        |
@@ -22,7 +22,53 @@ Feature: Loan Drawdowns
             | 12       | £231,000        |
 
         When the user clicks the View button
-        Then the user will see the read-only drawdown
+        Then the user will see the "PM" drawdown
 
         When this user clicks the change drawdown link
         Then the user will see the PCR Start a new request page
+
+    Scenario: Submitting a drawdown as a Finance Contact
+        Given a standard Loans project exists
+        And the user is the "mainFcUser" user
+        And the user is on the project overview
+        When the user selects the "Drawdowns" tile
+        Then the user will see the "FC" Drawdowns page
+            | Drawdown | Drawdown amount |
+            | 1        | £110,000        |
+            | 2        | £121,000        |
+            | 3        | £132,000        |
+            | 4        | £143,000        |
+            | 5        | £154,000        |
+            | 6        | £165,000        |
+            | 7        | £176,000        |
+            | 8        | £187,000        |
+            | 9        | £198,000        |
+            | 10       | £209,000        |
+            | 11       | £220,000        |
+            | 12       | £231,000        |
+
+        When the user clicks the Request button
+        Then the user will see the "FC" drawdown
+
+        When the user attempts to submit the Drawdown without a document and too many comments
+        Then the user will see Drawdown validation messaging
+            | Message                                                                  |
+            | The request is only accepted when at least 1 document has been uploaded. |
+            | Comments must be between 5 and 32768 characters.                         |
+
+        When the enters 4 characters only and attempts to submit
+        Then the user will see Drawdown validation messaging
+            | Message                                                                  |
+            | The request is only accepted when at least 1 document has been uploaded. |
+            | Comments must be between 5 and 32768 characters.                         |
+
+        When the user uploads a document and enters 5 characters
+        Then the Drawdown validation messages will no longer appear
+
+        When the user submits the Drawdown request
+        Then the Drawdown status will be Requested
+        And the Drawdown request button will be disabled
+
+
+
+
--- packages/playwright-test/src/fixtures/acc/pages/Loans/LoanDrawdowns.ts
+++ packages/playwright-test/src/fixtures/acc/pages/Loans/LoanDrawdowns.ts
@@ -3,6 +3,7 @@ import { Fixture, Then, When } from "playwright-bdd/decorators";
 import { Commands } from "../../../Commands";
 import { PageHeading } from "../../../../components/PageHeading";
 import { DataTable } from "playwright-bdd";
+import { getLorem } from "../../../../components/lorem";
 
 export
 @Fixture("loanDrawdowns")
@@ -11,25 +12,49 @@ class LoanDrawdowns {
   protected readonly commands: Commands;
   private readonly drawdownsHeading: PageHeading;
   private readonly drawdownHeading: PageHeading;
+  private readonly backToProject: Locator;
+  private readonly backToLoansSummary: Locator;
   private readonly viewButton: Locator;
   private readonly viewButton1: Locator;
+  private readonly requestButton: Locator;
+  private readonly requestButton1: Locator;
+  private readonly requestButton2: Locator;
   private readonly drawdownsHeaders: Array<string>;
-  private readonly nonFcDrawdownGuidance: string;
+  private readonly nonFcDrawdownGuidance: Locator;
   private readonly changeDrawdownLink: Locator;
   private readonly singleDrawdownTableHeaders: Array<string>;
   private readonly filesUploadedHeader: Locator;
   private readonly period1DrawdownRow: Array<string>;
+  private readonly fcDrawdownGuidance: Locator;
+  private readonly uploadDrawdownApprovalHeader: Locator;
+  private readonly drawdownApprovalGuidance: Locator;
+  private readonly additionalInfoHeading: Locator;
+  private readonly additionalInfoGuidance: Locator;
+  private readonly haveZeroCharacters: Locator;
+  private readonly nowSendHeading: Locator;
+  private readonly submissionGuidance: Locator;
+  private readonly uploadDocumentsButton: Locator;
+  private readonly noDocsUploadedText: Locator;
+  private readonly acceptAndSendButton: Locator;
+  private readonly drawdownTextbox: Locator;
+  private readonly requestedPeriod1DrawdownRow: Array<string>;
 
   constructor({ page, commands }: { page: Page; commands: Commands }) {
     this.page = page;
     this.commands = commands;
     this.drawdownsHeading = PageHeading.fromTitle(this.page, "Drawdowns");
     this.drawdownHeading = PageHeading.fromTitle(this.page, "Drawdown");
+    this.backToProject = this.commands.backLink("Back to Project");
+    this.backToLoansSummary = this.commands.backLink("Back to loans summary page");
     this.viewButton = this.page.getByRole("button").filter({ hasText: "View" });
     this.viewButton1 = this.page.locator(`//table//tbody//tr[1]//td[5]`).filter({ has: this.viewButton });
+    this.requestButton = this.page.getByRole("button").filter({ hasText: "Request" });
+    this.requestButton1 = this.page.locator(`//table//tbody//tr[1]//td[5]`).filter({ has: this.requestButton });
+    this.requestButton2 = this.page.locator(`//table//tbody//tr[2]//td[5]`).filter({ has: this.requestButton });
     this.drawdownsHeaders = ["Drawdown", "Due date", "Drawdown amount", "Status"];
-    this.nonFcDrawdownGuidance =
-      "Your Finance Contact can request your drawdown here. If you need to change the amount of your drawdown, you will need to submit a";
+    this.nonFcDrawdownGuidance = this.page.getByText(
+      "Your Finance Contact can request your drawdown here. If you need to change the amount of your drawdown, you will need to submit a",
+    );
     this.changeDrawdownLink = this.page.getByRole("link").filter({ hasText: "change drawdown" });
     this.singleDrawdownTableHeaders = [
       "Drawdown",
@@ -50,13 +75,36 @@ class LoanDrawdowns {
       "£110,000.00",
       "£78,076,000.00",
     ];
+    this.fcDrawdownGuidance = this.page.getByText(
+      "You can request your drawdown here. If the amount of your drawdown needs to be changed, your Project Manager will need to submit a change drawdown project change request.",
+    );
+    this.uploadDrawdownApprovalHeader = this.page
+      .getByRole("heading")
+      .filter({ hasText: "Upload drawdown approval request" });
+    this.drawdownApprovalGuidance = this.page.getByText("You must upload a signed drawdown approval request");
+    this.additionalInfoHeading = this.page.locator("legend").filter({ hasText: "Additional information (Optional)" });
+    this.additionalInfoGuidance = this.page
+      .locator("#hint-for-comments")
+      .filter({ hasText: "If you want to explain anything to Innovate UK, add it here." });
+    this.haveZeroCharacters = this.page.getByRole("paragraph").filter({ hasText: "You have 0 characters" });
+    this.nowSendHeading = this.page.locator("legend").filter({ hasText: "Now send your request" });
+    this.submissionGuidance = this.page.getByRole("paragraph").filter({
+      hasText:
+        "By submitting this drawdown request I confirm that the amount requested is in line with forecast eligible project costs and that this has been approved by an authorised signatory of the business. I understand and accept that if I knowingly submit a drawdown request without appropriate approvals or provide false or misleading information, this may result in my drawdown being rejected, termination of the loan, recovery of loan proceeds and outstanding interest, civil action and where there is evidence of fraud, criminal prosecution.",
+    });
+    this.uploadDocumentsButton = this.page.getByRole("button").filter({ hasText: "Upload documents" });
+    this.noDocsUploadedText = this.page.getByRole("paragraph").filter({ hasText: "No documents uploaded." });
+    this.acceptAndSendButton = this.page.getByRole("button").filter({ hasText: "Accept and send" });
+    this.drawdownTextbox = this.page.locator("#comments");
+    this.requestedPeriod1DrawdownRow = ["1", this.getDrawdownDate(0), "£110,000", "Requested"];
   }
 
-  @Then("the user will see the Drawdowns page")
-  async drawdownPage(table: DataTable) {
+  @Then("the user will see the {string} Drawdowns page")
+  async drawdownPage(user: string, table: DataTable) {
     const data = table.hashes();
     await this.drawdownsHeading.isVisible();
-    await this.assertDrawdownsTable(table);
+    await expect(this.backToProject).toBeVisible();
+    await this.assertDrawdownsTable(user, table);
   }
 
   @When("the user clicks the View button")
@@ -65,13 +113,24 @@ class LoanDrawdowns {
     await this.drawdownHeading.isVisible();
   }
 
-  @Then("the user will see the read-only drawdown")
-  async viewOnlyDrawdown() {
+  @When("the user clicks the Request button")
+  async clickRequestButton() {
+    await this.requestButton.click();
     await this.drawdownHeading.isVisible();
-    await this.commands.verifyTextOnPage(this.nonFcDrawdownGuidance);
-    await expect(this.changeDrawdownLink).toBeVisible();
+  }
+
+  @Then("the user will see the {string} drawdown")
+  async viewOnlyDrawdown(user: string) {
+    await this.drawdownHeading.isVisible();
+    await expect(this.backToLoansSummary).toBeVisible();
+    if (user === "PM") {
+      await this.pmOnlyDrawdown();
+    } else if (user === "FC") {
+      await this.fcOnlyDrawdown();
+    }
     await this.singleDrawdownTable();
     await expect(this.filesUploadedHeader).toBeVisible();
+    await expect(this.noDocsUploadedText).toBeVisible();
   }
 
   @When("this user clicks the change drawdown link")
@@ -84,17 +143,73 @@ class LoanDrawdowns {
     await expect(this.page.getByRole("heading").filter({ hasText: "Start a new request" })).toBeVisible();
   }
 
+  @When("the user attempts to submit the Drawdown without a document and too many comments")
+  async submitWithoutDoc() {
+    await this.drawdownTextbox.fill(getLorem(32_769));
+    await this.acceptAndSendButton.click();
+  }
+
+  @When("the enters 4 characters only and attempts to submit")
+  async submit4Characters() {
+    await this.drawdownTextbox.fill(getLorem(4));
+    await this.acceptAndSendButton.click();
+  }
+
+  @Then("the user will see Drawdown validation messaging")
+  async drawdownValidationMessages(table: DataTable) {
+    const data = table.hashes();
+    for (const msg of data) {
+      await this.commands.validationLink(msg["Message"]);
+      await expect(this.page.getByRole("paragraph").filter({ hasText: msg["Message"] })).toBeVisible();
+    }
+  }
+
+  @When("the user uploads a document and enters 5 characters")
+  async drawdownValidState() {
+    await this.commands.fileInput(["testfile.doc"]);
+    await this.drawdownTextbox.fill(getLorem(5));
+  }
+
+  @Then("the Drawdown validation messages will no longer appear")
+  async noDrawdownValMessages() {
+    await expect(this.page.getByTestId("validation-summary")).not.toBeVisible();
+  }
+
+  @When("the user submits the Drawdown request")
+  async submitDrawdown() {
+    await this.acceptAndSendButton.click();
+  }
+
+  @Then("the Drawdown status will be Requested")
+  async drawdownRequestedStatus() {
+    await this.drawdownsHeading.isVisible();
+    let i = 1;
+    for (const cell of this.requestedPeriod1DrawdownRow) {
+      await expect(this.page.getByTestId("drawdown-list").locator(`//table//tbody//tr[1]//td[${i}]`)).toHaveText(cell);
+      i++;
+    }
+  }
+
+  @Then("the Drawdown request button will be disabled")
+  async requestButtonDisabled() {
+    await expect(this.requestButton).toBeDisabled();
+  }
+
   /**
    * METHODS
    */
-  async assertDrawdownsTable(table: DataTable) {
+  async assertDrawdownsTable(user: string, table: DataTable) {
     const data = table.hashes();
     let th = 1;
     for (const header of this.drawdownsHeaders) {
       await expect(this.page.locator(`//table//thead/tr[1]//th[${th}]`).filter({ hasText: header })).toBeVisible();
       th++;
     }
-    await expect(this.viewButton1).toBeVisible();
+    if (user === "PM") {
+      await expect(this.viewButton1).toBeVisible();
+    } else if (user === "FC") {
+      await expect(this.requestButton1).toBeVisible();
+    }
     let i = 1;
     let dateCount = 0;
     for (const row of data) {
@@ -131,5 +246,24 @@ class LoanDrawdowns {
     let date = new Date();
     let fullDate = new Date(date.getFullYear(), date.getMonth() + increment, 1, 12);
     return `01/${fullDate.toLocaleDateString("en-GB", { month: "2-digit", year: "numeric" })}`;
+  }
+
+  async pmOnlyDrawdown() {
+    await expect(this.nonFcDrawdownGuidance).toBeVisible();
+    await expect(this.changeDrawdownLink).toBeVisible();
+  }
+
+  async fcOnlyDrawdown() {
+    await expect(this.fcDrawdownGuidance).toBeVisible();
+    await expect(this.uploadDrawdownApprovalHeader).toBeVisible();
+    await this.commands.learnFiles();
+    await expect(this.drawdownApprovalGuidance).toBeVisible();
+    await expect(this.uploadDocumentsButton).toBeVisible();
+    await expect(this.additionalInfoHeading).toBeVisible();
+    await expect(this.additionalInfoGuidance).toBeVisible();
+    await expect(this.haveZeroCharacters).toBeVisible();
+    await expect(this.nowSendHeading).toBeVisible();
+    await expect(this.submissionGuidance).toBeVisible();
+    await expect(this.acceptAndSendButton).toBeVisible();
   }
 }
