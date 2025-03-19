5d0aa01b7cdf5832461a6cb004f78807fd4c356e
Leondro Lio <leondro.lio@iuk.ukri.org>
2025-03-19 16:08:46 +0000

    ACC-11745: (zod) Simplify zod date validation

--- packages/app/src/shared/date-helpers.ts
+++ packages/app/src/shared/date-helpers.ts
@@ -18,3 +18,36 @@ export const totalCalendarMonths = calendarMonthNames.length;
 export const monthDifference = (dateFrom: Date, dateTo: Date): number => {
   return dateTo.getMonth() - dateFrom.getMonth() + 12 * (dateTo.getFullYear() - dateFrom.getFullYear());
 };
+
+export const monthNameToNumber = (input: string): string => {
+  // https://design-system.service.gov.uk/components/date-input/#research-on-this-component
+  // Convert named months to numbers to help mistaken users and those with discalculia
+  switch (input.toLowerCase().slice(0, 3)) {
+    case "jan":
+      return "1";
+    case "feb":
+      return "2";
+    case "mar":
+      return "3";
+    case "apr":
+      return "4";
+    case "may":
+      return "5";
+    case "jun":
+      return "6";
+    case "jul":
+      return "7";
+    case "aug":
+      return "8";
+    case "sep":
+      return "9";
+    case "oct":
+      return "10";
+    case "nov":
+      return "11";
+    case "dec":
+      return "12";
+    default:
+      return input;
+  }
+};
